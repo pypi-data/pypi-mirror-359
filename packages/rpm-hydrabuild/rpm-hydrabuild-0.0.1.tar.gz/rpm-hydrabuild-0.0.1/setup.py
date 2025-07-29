453f0b71976e6c6abbfdd9c47b5b7c67ddfbd281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package rpm-hydrabuild/0.0.1/rpm-hydrabuild-0.0.1.tar.gz

---
+++ packages/rpm-hydrabuild/rpm-hydrabuild-0.0.1.tar.gz/rpm-hydrabuild-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rpm-hydrabuild",
+    version="0.0.1",
+    packages=["rpm-hydrabuild"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
