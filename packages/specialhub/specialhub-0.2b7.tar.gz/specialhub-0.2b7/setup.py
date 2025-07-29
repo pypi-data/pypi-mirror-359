81ced01d6794612d54311bbf356fb7a2b9e3316a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package specialhub/0.2b7/specialhub-0.2b7.tar.gz

---
+++ packages/specialhub/specialhub-0.2b7.tar.gz/specialhub-0.2b7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="specialhub",
+    version="0.2b7",
+    packages=["specialhub"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
