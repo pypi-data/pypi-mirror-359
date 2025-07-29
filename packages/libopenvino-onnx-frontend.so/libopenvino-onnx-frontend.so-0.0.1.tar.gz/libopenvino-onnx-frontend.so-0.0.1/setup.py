5b08807bfb8da48f5076ada67e73b457f8c0f982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:52 +0000

    Add package libopenvino-onnx-frontend.so/0.0.1/libopenvino-onnx-frontend.so-0.0.1.tar.gz

---
+++ packages/libopenvino-onnx-frontend.so/libopenvino-onnx-frontend.so-0.0.1.tar.gz/libopenvino-onnx-frontend.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libopenvino-onnx-frontend.so",
+    version="0.0.1",
+    packages=["libopenvino-onnx-frontend.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
