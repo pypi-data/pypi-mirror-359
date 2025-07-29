77629ae4409729819183622fc58cad919ace1c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package libdfdata/0.0.1/libdfdata-0.0.1.tar.gz

---
+++ packages/libdfdata/libdfdata-0.0.1.tar.gz/libdfdata-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libdfdata",
+    version="0.0.1",
+    packages=["libdfdata"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
