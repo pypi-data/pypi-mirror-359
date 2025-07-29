ca5776c0e3a2a9a8cfb90937d212ba6a49826eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-connector-greenplum/0.0.1/dl-connector-greenplum-0.0.1.tar.gz

---
+++ packages/dl-connector-greenplum/dl-connector-greenplum-0.0.1.tar.gz/dl-connector-greenplum-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-greenplum",
+    version="0.0.1",
+    packages=["dl-connector-greenplum"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
