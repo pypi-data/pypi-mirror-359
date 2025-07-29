76b397e1152eb6af974f510bc4d616148b71f2d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package sklift/0.0.1/sklift-0.0.1.tar.gz

---
+++ packages/sklift/sklift-0.0.1.tar.gz/sklift-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sklift",
+    version="0.0.1",
+    packages=["sklift"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
