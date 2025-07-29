c5541a929e4828cf51cf8f82f512fbdb401bb3ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-annotations/2.17.2/jackson-annotations-2.17.2.tar.gz

---
+++ packages/jackson-annotations/jackson-annotations-2.17.2.tar.gz/jackson-annotations-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-annotations",
+    version="2.17.2",
+    packages=["jackson-annotations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
