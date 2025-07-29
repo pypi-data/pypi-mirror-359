cb316a3fb5f8c98f23b5ec94ac63d3ca1a62889b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-package-boilerplate/0.0.1/dl-package-boilerplate-0.0.1.tar.gz

---
+++ packages/dl-package-boilerplate/dl-package-boilerplate-0.0.1.tar.gz/dl-package-boilerplate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-package-boilerplate",
+    version="0.0.1",
+    packages=["dl-package-boilerplate"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
