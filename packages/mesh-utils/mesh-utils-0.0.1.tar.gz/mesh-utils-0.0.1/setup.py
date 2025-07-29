0d29ad7c8c8e664d4eebc738c508d83fd34feb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package mesh-utils/0.0.1/mesh-utils-0.0.1.tar.gz

---
+++ packages/mesh-utils/mesh-utils-0.0.1.tar.gz/mesh-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mesh-utils",
+    version="0.0.1",
+    packages=["mesh-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
