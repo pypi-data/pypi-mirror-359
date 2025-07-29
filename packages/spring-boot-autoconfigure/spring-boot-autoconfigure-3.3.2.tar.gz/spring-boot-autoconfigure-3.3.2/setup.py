5cab00c9971ddef632f81329ece70995d4c4d6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:10 +0000

    Add package spring-boot-autoconfigure/3.3.2/spring-boot-autoconfigure-3.3.2.tar.gz

---
+++ packages/spring-boot-autoconfigure/spring-boot-autoconfigure-3.3.2.tar.gz/spring-boot-autoconfigure-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-autoconfigure",
+    version="3.3.2",
+    packages=["spring-boot-autoconfigure"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
