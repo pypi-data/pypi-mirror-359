2daabfb01d9a10af8a836c8800c34510673c3121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:55 +0000

    Add package pymwm-diff/0.0.1/pymwm-diff-0.0.1.tar.gz

---
+++ packages/pymwm-diff/pymwm-diff-0.0.1.tar.gz/pymwm-diff-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pymwm-diff",
+    version="0.0.1",
+    packages=["pymwm-diff"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
