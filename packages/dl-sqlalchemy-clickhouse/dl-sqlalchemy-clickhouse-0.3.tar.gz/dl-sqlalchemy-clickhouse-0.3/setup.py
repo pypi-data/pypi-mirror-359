aa6b0a5bd69f65e8af56864f6d4728d22a26f0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-clickhouse/0.3/dl-sqlalchemy-clickhouse-0.3.tar.gz

---
+++ packages/dl-sqlalchemy-clickhouse/dl-sqlalchemy-clickhouse-0.3.tar.gz/dl-sqlalchemy-clickhouse-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-clickhouse",
+    version="0.3",
+    packages=["dl-sqlalchemy-clickhouse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
