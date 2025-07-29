60dfc9c210dadc8bef838aaf1db879f27970748b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-bigquery/0.0.1/dl-connector-bigquery-0.0.1.tar.gz

---
+++ packages/dl-connector-bigquery/dl-connector-bigquery-0.0.1.tar.gz/dl-connector-bigquery-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-bigquery",
+    version="0.0.1",
+    packages=["dl-connector-bigquery"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
