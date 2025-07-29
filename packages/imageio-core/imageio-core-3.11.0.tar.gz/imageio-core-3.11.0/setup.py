9895c6d8a1429ffb25de16658062bbf36e1d31c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package imageio-core/3.11.0/imageio-core-3.11.0.tar.gz

---
+++ packages/imageio-core/imageio-core-3.11.0.tar.gz/imageio-core-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-core",
+    version="3.11.0",
+    packages=["imageio-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
