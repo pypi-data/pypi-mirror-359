7abe27550f73f10d7321f66d54323be7974ffd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package rest-framework-jwt/0.0.1/rest-framework-jwt-0.0.1.tar.gz

---
+++ packages/rest-framework-jwt/rest-framework-jwt-0.0.1.tar.gz/rest-framework-jwt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-jwt",
+    version="0.0.1",
+    packages=["rest-framework-jwt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
