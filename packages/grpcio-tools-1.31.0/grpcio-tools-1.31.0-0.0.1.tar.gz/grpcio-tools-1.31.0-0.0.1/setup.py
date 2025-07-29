73700aaaa0792a54d709c9f6c254ec34de2c89c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:49 +0000

    Add package grpcio-tools-1.31.0/0.0.1/grpcio-tools-1.31.0-0.0.1.tar.gz

---
+++ packages/grpcio-tools-1.31.0/grpcio-tools-1.31.0-0.0.1.tar.gz/grpcio-tools-1.31.0-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="grpcio-tools-1.31.0",
+    version="0.0.1",
+    packages=["grpcio-tools-1.31.0"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
