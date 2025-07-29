5d9c72d78faf088b7390797a1ecfaa072554a8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package scrooge-plugin-vm-cpu-generation-20230516/1.0.5/scrooge-plugin-vm-cpu-generation-20230516-1.0.5.tar.gz

---
+++ packages/scrooge-plugin-vm-cpu-generation-20230516/scrooge-plugin-vm-cpu-generation-20230516-1.0.5.tar.gz/scrooge-plugin-vm-cpu-generation-20230516-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-plugin-vm-cpu-generation-20230516",
+    version="1.0.5",
+    packages=["scrooge-plugin-vm-cpu-generation-20230516"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
