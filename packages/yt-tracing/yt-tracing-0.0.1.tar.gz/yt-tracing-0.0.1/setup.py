61b2a4bd3e5e080eb90b4f440dc2691ec7417b8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:56 +0000

    Add package yt-tracing/0.0.1/yt-tracing-0.0.1.tar.gz

---
+++ packages/yt-tracing/yt-tracing-0.0.1.tar.gz/yt-tracing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yt-tracing",
+    version="0.0.1",
+    packages=["yt-tracing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
