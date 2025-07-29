fca0afed587180b29883b263d7eaa4eb674b6db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:58 +0000

    Add package jaxb-core/4.0.5/jaxb-core-4.0.5.tar.gz

---
+++ packages/jaxb-core/jaxb-core-4.0.5.tar.gz/jaxb-core-4.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jaxb-core",
+    version="4.0.5",
+    packages=["jaxb-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
