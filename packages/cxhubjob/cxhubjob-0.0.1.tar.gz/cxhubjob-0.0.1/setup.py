049ef300d9b0751f9b6779b251dc50bf5048f12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package cxhubjob/0.0.1/cxhubjob-0.0.1.tar.gz

---
+++ packages/cxhubjob/cxhubjob-0.0.1.tar.gz/cxhubjob-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cxhubjob",
+    version="0.0.1",
+    packages=["cxhubjob"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
