cb704a5572b439459e1196f6caac34f0bdcc56b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-datatype-jdk8/2.17.2/jackson-datatype-jdk8-2.17.2.tar.gz

---
+++ packages/jackson-datatype-jdk8/jackson-datatype-jdk8-2.17.2.tar.gz/jackson-datatype-jdk8-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-datatype-jdk8",
+    version="2.17.2",
+    packages=["jackson-datatype-jdk8"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
