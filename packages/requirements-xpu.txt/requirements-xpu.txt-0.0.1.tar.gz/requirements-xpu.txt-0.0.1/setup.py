eba6c5806075579149acc08200d30ec7d9620fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package requirements-xpu.txt/0.0.1/requirements-xpu.txt-0.0.1.tar.gz

---
+++ packages/requirements-xpu.txt/requirements-xpu.txt-0.0.1.tar.gz/requirements-xpu.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements-xpu.txt",
+    version="0.0.1",
+    packages=["requirements-xpu.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
