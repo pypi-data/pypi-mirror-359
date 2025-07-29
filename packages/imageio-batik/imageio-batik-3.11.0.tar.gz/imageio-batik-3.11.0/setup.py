186e9fb033e25f32eaed87c75b4eeade335178b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package imageio-batik/3.11.0/imageio-batik-3.11.0.tar.gz

---
+++ packages/imageio-batik/imageio-batik-3.11.0.tar.gz/imageio-batik-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-batik",
+    version="3.11.0",
+    packages=["imageio-batik"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
