48bd460eae9cf9fa3fc4caa1e11d7a7a064809dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package videollava/0.0.1/videollava-0.0.1.tar.gz

---
+++ packages/videollava/videollava-0.0.1.tar.gz/videollava-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="videollava",
+    version="0.0.1",
+    packages=["videollava"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
