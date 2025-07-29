bb191293299cc236505aff4bcb36fa235e7aa42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:30 +0000

    Add package mbnpy/0.1.7/mbnpy-0.1.7-py3-none-any.whl

---
+++ packages/mbnpy/mbnpy-0.1.7-py3-none-any.whl/demos/test_batch.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+from demos.pf_map import batch as pf_map
+from demos.rbd import batch as rbd
+from demos.power_house import batch as power_house
+from demos.routine import batch as routine
+from demos.road import batch as road
+from demos.SF import batch as sf
+
+
+def test_batch():
+
+    #rbd.main()
+    pf_map.debug()
+    #routine.main()
+    #road.main()
+    #sf.main(max_sf=10)
+    #power_house.batch()
