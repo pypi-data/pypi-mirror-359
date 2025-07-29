9dae6c200b605273ff16d3f9ce173238044a7c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package communities-coldstart-embeddings/0.0.1/communities-coldstart-embeddings-0.0.1.tar.gz

---
+++ packages/communities-coldstart-embeddings/communities-coldstart-embeddings-0.0.1.tar.gz/communities-coldstart-embeddings-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="communities-coldstart-embeddings",
+    version="0.0.1",
+    packages=["communities-coldstart-embeddings"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
