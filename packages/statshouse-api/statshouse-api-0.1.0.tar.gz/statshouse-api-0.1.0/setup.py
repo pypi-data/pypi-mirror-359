9bc1aeeecd5eefd784048095a0992db047c96d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:04 +0000

    Add package statshouse-api/0.1.0/statshouse-api-0.1.0.tar.gz

---
+++ packages/statshouse-api/statshouse-api-0.1.0.tar.gz/statshouse-api-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statshouse-api",
+    version="0.1.0",
+    packages=["statshouse-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
