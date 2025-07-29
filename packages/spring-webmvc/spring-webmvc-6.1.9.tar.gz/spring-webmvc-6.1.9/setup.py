a3ee27c3db67301bc6a7dd974a5b365cd9dc690e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:06 +0000

    Add package spring-webmvc/6.1.9/spring-webmvc-6.1.9.tar.gz

---
+++ packages/spring-webmvc/spring-webmvc-6.1.9.tar.gz/spring-webmvc-6.1.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-webmvc",
+    version="6.1.9",
+    packages=["spring-webmvc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
