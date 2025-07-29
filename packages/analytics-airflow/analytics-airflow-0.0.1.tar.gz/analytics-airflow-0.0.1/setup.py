0691147ce52ac23d271fb571dc9956b4ee05ac37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:45 +0000

    Add package analytics-airflow/0.0.1/analytics-airflow-0.0.1.tar.gz

---
+++ packages/analytics-airflow/analytics-airflow-0.0.1.tar.gz/analytics-airflow-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="analytics-airflow",
+    version="0.0.1",
+    packages=["analytics-airflow"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
