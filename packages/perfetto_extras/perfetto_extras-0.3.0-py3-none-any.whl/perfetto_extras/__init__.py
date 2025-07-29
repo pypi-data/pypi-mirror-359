6e9ae94eaa3c7e75234b465db136ad3174bccd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:14 +0000

    Add package perfetto_extras/0.3.0/perfetto_extras-0.3.0-py3-none-any.whl

---
+++ packages/perfetto_extras/perfetto_extras-0.3.0-py3-none-any.whl/perfetto_extras/__init__.py
@@ -0,0 +1,4 @@
+from .trace_events import Trace, ProcessTrack, ThreadTrack
+from .opentrace import open_trace_in_browser
+
+__all__ = ["Trace", "ProcessTrack", "ThreadTrack", "open_trace_in_browser"]
