768be171a6fd6b553aa48a422a34c2433f57cefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:23 +0000

    Add package text-model-core/0.0.1/text-model-core-0.0.1.tar.gz

---
+++ packages/text-model-core/text-model-core-0.0.1.tar.gz/text-model-core-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="text-model-core",
+    version="0.0.1",
+    packages=["text-model-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
