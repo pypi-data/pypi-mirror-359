a21e6871724e1bf78705c07b44c10e58012a949f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    Add package Points2D/0.4.0/points2d-0.4.0-py3-none-any.whl

---
+++ packages/Points2D/points2d-0.4.0-py3-none-any.whl/Points2D/__init__.py
@@ -0,0 +1,13 @@
+"""
+Author: Big Panda
+Created Time: 30.06.2025 10:42
+Modified Time: 30.06.2025 10:42
+Description:
+    
+"""
+from Points2D.Points import *
+from Points2D.ArrayPoints import *
+
+__all__ = ["Point2D",
+           "LinkPoint2D",
+           "ArrayPoint2D"]
