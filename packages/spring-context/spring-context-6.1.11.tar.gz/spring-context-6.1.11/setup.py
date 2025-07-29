29f183b018db17c9833bac379c0960d2c93c3f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package spring-context/6.1.11/spring-context-6.1.11.tar.gz

---
+++ packages/spring-context/spring-context-6.1.11.tar.gz/spring-context-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-context",
+    version="6.1.11",
+    packages=["spring-context"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
