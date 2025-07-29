c4ac49a44932825d221e10671d236cc6aa5264fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package webdav2/0.0.1/webdav2-0.0.1.tar.gz

---
+++ packages/webdav2/webdav2-0.0.1.tar.gz/webdav2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="webdav2",
+    version="0.0.1",
+    packages=["webdav2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
