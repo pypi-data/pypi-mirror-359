5d187eba5c3a035d6a1a6fbe9c6cd442447394be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package pyameli/0.0.1/pyameli-0.0.1.tar.gz

---
+++ packages/pyameli/pyameli-0.0.1.tar.gz/pyameli-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyameli",
+    version="0.0.1",
+    packages=["pyameli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
