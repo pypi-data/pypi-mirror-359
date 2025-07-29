3d4bdf678889cc33ffb454a83f626bec2a12dcda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-boot-starter-aop/3.3.2/spring-boot-starter-aop-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-aop/spring-boot-starter-aop-3.3.2.tar.gz/spring-boot-starter-aop-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-aop",
+    version="3.3.2",
+    packages=["spring-boot-starter-aop"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
