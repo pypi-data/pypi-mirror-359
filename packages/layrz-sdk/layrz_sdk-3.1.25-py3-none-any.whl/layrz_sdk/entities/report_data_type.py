cdc716bbb548b3a5ee40a420999021ee13c5b4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:06 +0000

    Add package layrz-sdk/3.1.25/layrz_sdk-3.1.25-py3-none-any.whl

---
+++ packages/layrz-sdk/layrz_sdk-3.1.25-py3-none-any.whl/layrz_sdk/entities/report_data_type.py
@@ -0,0 +1,30 @@
+import sys
+
+if sys.version_info >= (3, 11):
+  from enum import StrEnum
+  from typing import Self
+else:
+  from typing_extensions import Self
+
+  from layrz_sdk.backwards import StrEnum
+
+
+class ReportDataType(StrEnum):
+  """
+  Report date type
+  """
+
+  STR = 'str'
+  INT = 'int'
+  FLOAT = 'float'
+  DATETIME = 'datetime'
+  BOOL = 'bool'
+  CURRENCY = 'currency'
+
+  def __str__(self: Self) -> str:
+    """Readable property"""
+    return self.name
+
+  def __repr__(self: Self) -> str:
+    """Readable property"""
+    return f'ReportDataType.{self.value}'
