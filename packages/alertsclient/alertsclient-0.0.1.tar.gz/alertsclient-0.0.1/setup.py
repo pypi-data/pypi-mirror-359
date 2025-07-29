9e4569aa84600d26d7011be723f7bae2215ee885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package alertsclient/0.0.1/alertsclient-0.0.1.tar.gz

---
+++ packages/alertsclient/alertsclient-0.0.1.tar.gz/alertsclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="alertsclient",
+    version="0.0.1",
+    packages=["alertsclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
