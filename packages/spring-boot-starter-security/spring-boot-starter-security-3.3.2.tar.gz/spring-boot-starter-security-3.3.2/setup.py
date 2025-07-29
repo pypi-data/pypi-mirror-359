37bdccd287fc4daee22c59e674e130617edc2824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:49 +0000

    Add package spring-boot-starter-security/3.3.2/spring-boot-starter-security-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-security/spring-boot-starter-security-3.3.2.tar.gz/spring-boot-starter-security-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-security",
+    version="3.3.2",
+    packages=["spring-boot-starter-security"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
