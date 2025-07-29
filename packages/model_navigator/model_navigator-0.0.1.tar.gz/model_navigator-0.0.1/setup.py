ed3b1c73a4cf9928a6eba557cebf45fe2fc1b6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package model_navigator/0.0.1/model_navigator-0.0.1.tar.gz

---
+++ packages/model_navigator/model_navigator-0.0.1.tar.gz/model_navigator-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="model_navigator",
+    version="0.0.1",
+    packages=["model_navigator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
