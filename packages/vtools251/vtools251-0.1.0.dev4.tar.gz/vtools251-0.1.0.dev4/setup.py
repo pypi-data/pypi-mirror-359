debdb4b3ee62be3282e11576d032dad62798e36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package vtools251/0.1.0.dev4/vtools251-0.1.0.dev4.tar.gz

---
+++ packages/vtools251/vtools251-0.1.0.dev4.tar.gz/vtools251-0.1.0.dev4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vtools251",
+    version="0.1.0.dev4",
+    packages=["vtools251"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
