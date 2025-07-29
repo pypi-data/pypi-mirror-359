1b092b27c6eb649786778a56a46efdd639000f1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package yatest/0.0.1/yatest-0.0.1.tar.gz

---
+++ packages/yatest/yatest-0.0.1.tar.gz/yatest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yatest",
+    version="0.0.1",
+    packages=["yatest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
