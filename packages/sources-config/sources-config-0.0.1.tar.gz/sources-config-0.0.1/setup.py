cdb0a0dcc57931de1fbadb70ad9ffb12ae59081c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:49 +0000

    Add package sources-config/0.0.1/sources-config-0.0.1.tar.gz

---
+++ packages/sources-config/sources-config-0.0.1.tar.gz/sources-config-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sources-config",
+    version="0.0.1",
+    packages=["sources-config"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
