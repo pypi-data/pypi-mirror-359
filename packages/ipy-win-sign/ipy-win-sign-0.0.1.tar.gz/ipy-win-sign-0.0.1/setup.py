1d77ffaf787743987db4a963917b841fba081dce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-win-sign/0.0.1/ipy-win-sign-0.0.1.tar.gz

---
+++ packages/ipy-win-sign/ipy-win-sign-0.0.1.tar.gz/ipy-win-sign-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-win-sign",
+    version="0.0.1",
+    packages=["ipy-win-sign"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
