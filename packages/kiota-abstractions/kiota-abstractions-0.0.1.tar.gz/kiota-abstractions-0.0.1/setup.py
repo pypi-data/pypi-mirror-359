cf08bb86dfd822d0d5244ac8ecf3de929472119c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package kiota-abstractions/0.0.1/kiota-abstractions-0.0.1.tar.gz

---
+++ packages/kiota-abstractions/kiota-abstractions-0.0.1.tar.gz/kiota-abstractions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kiota-abstractions",
+    version="0.0.1",
+    packages=["kiota-abstractions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
