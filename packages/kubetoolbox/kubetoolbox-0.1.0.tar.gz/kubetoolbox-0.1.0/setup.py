7c215fd8a2f2d79b3ea20c0c610846b3463564e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package kubetoolbox/0.1.0/kubetoolbox-0.1.0.tar.gz

---
+++ packages/kubetoolbox/kubetoolbox-0.1.0.tar.gz/kubetoolbox-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kubetoolbox",
+    version="0.1.0",
+    packages=["kubetoolbox"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
