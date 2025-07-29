fbf4010b5bc211159d77d193c361b03290df04c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:04 +0000

    Add package vault-policy-sync/0.0.1/vault-policy-sync-0.0.1.tar.gz

---
+++ packages/vault-policy-sync/vault-policy-sync-0.0.1.tar.gz/vault-policy-sync-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vault-policy-sync",
+    version="0.0.1",
+    packages=["vault-policy-sync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
