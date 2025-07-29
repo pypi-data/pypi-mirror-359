752b8d25549fe3cc1b66e3668638487b391075bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package eoq-system/0.0.1/eoq-system-0.0.1.tar.gz

---
+++ packages/eoq-system/eoq-system-0.0.1.tar.gz/eoq-system-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="eoq-system",
+    version="0.0.1",
+    packages=["eoq-system"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
