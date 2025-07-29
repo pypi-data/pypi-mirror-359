483691f2720659e56d7a8b967adeac37327d0702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:47 +0000

    Add package yearn-treasury/0.0.20/yearn_treasury-0.0.20-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/yearn-treasury/yearn_treasury-0.0.20-cp311-cp311-macosx_11_0_arm64.whl/yearn_treasury/rules/ignore/swaps/pooltogether.py
@@ -0,0 +1,16 @@
+from dao_treasury import TreasuryTx
+from y import Network
+
+from yearn_treasury.constants import ZERO_ADDRESS
+from yearn_treasury.rules.ignore.swaps import swaps
+
+
+@swaps("PoolTogether:Deposit", Network.Mainnet)
+def is_pooltogether_deposit(tx: TreasuryTx) -> bool:
+    # TODO: figure out what was done with the withdrawals and put a note here
+    symbol = tx.symbol
+    return (
+        symbol == "POOL" and tx.to_address.address == "0x396b4489da692788e327E2e4b2B0459A5Ef26791"
+    ) or (  # type: ignore [union-attr]
+        symbol == "PPOOL" and tx.from_address.address == ZERO_ADDRESS
+    )  # type: ignore [union-attr]
