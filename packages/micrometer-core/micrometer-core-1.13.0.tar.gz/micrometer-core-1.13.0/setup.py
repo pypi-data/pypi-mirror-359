942c877fd7b9fa2885cab21c1d917e08c8c0b9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package micrometer-core/1.13.0/micrometer-core-1.13.0.tar.gz

---
+++ packages/micrometer-core/micrometer-core-1.13.0.tar.gz/micrometer-core-1.13.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micrometer-core",
+    version="1.13.0",
+    packages=["micrometer-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
