61afb8e2c0e8a3b46bc0bc8c5a8b3573bfd211b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:07 +0000

    Add package nexuscli/0.0.1/nexuscli-0.0.1.tar.gz

---
+++ packages/nexuscli/nexuscli-0.0.1.tar.gz/nexuscli-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nexuscli",
+    version="0.0.1",
+    packages=["nexuscli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
