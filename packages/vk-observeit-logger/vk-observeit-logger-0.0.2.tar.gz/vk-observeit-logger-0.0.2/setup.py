5294bc81dffacd53ac328831cc8811f0104ffaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package vk-observeit-logger/0.0.2/vk-observeit-logger-0.0.2.tar.gz

---
+++ packages/vk-observeit-logger/vk-observeit-logger-0.0.2.tar.gz/vk-observeit-logger-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-observeit-logger",
+    version="0.0.2",
+    packages=["vk-observeit-logger"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
