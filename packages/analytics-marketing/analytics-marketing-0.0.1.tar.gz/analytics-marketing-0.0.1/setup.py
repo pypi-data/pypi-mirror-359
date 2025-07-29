a741064712a26f4ee4c0f4558d42b8555afe87ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package analytics-marketing/0.0.1/analytics-marketing-0.0.1.tar.gz

---
+++ packages/analytics-marketing/analytics-marketing-0.0.1.tar.gz/analytics-marketing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="analytics-marketing",
+    version="0.0.1",
+    packages=["analytics-marketing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
