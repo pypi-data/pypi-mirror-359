2b4b5c24772b0f99c6a3f06e9f152164ca6323df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:28 +0000

    Add package asrthon/1.2/asrthon-1.2.tar.gz

---
+++ packages/asrthon/asrthon-1.2.tar.gz/asrthon-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="asrthon",
+    version="1.2",
+    description="The redest, loudest, most emotional Python interface in the world 🔥🟥",
+    author="Aser",
+    author_email="aserah411@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
