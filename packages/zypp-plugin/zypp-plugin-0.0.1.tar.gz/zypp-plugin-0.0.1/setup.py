fd820176b7031d6ee9eb1a1d468fd9bd6029fc1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package zypp-plugin/0.0.1/zypp-plugin-0.0.1.tar.gz

---
+++ packages/zypp-plugin/zypp-plugin-0.0.1.tar.gz/zypp-plugin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="zypp-plugin",
+    version="0.0.1",
+    packages=["zypp-plugin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
