912a14ec8335f37ffa4a9702299b4cd798082f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-configs/0.0.1/dl-configs-0.0.1.tar.gz

---
+++ packages/dl-configs/dl-configs-0.0.1.tar.gz/dl-configs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-configs",
+    version="0.0.1",
+    packages=["dl-configs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
