087e58e4448b981af57f2290bc3d39c0c3d588f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:31 +0000

    Add package ranged-fileresponse/0.0.1/ranged-fileresponse-0.0.1.tar.gz

---
+++ packages/ranged-fileresponse/ranged-fileresponse-0.0.1.tar.gz/ranged-fileresponse-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ranged-fileresponse",
+    version="0.0.1",
+    packages=["ranged-fileresponse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
