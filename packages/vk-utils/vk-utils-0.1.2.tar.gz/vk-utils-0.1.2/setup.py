41c6859e672ef57e00b9bbf48e6086c544b3ad5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package vk-utils/0.1.2/vk-utils-0.1.2.tar.gz

---
+++ packages/vk-utils/vk-utils-0.1.2.tar.gz/vk-utils-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-utils",
+    version="0.1.2",
+    packages=["vk-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
