a52d7430c7a2b28c196b62f05c26e17f3078fc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package asrbb-processor/0.0.1/asrbb-processor-0.0.1.tar.gz

---
+++ packages/asrbb-processor/asrbb-processor-0.0.1.tar.gz/asrbb-processor-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asrbb-processor",
+    version="0.0.1",
+    packages=["asrbb-processor"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
