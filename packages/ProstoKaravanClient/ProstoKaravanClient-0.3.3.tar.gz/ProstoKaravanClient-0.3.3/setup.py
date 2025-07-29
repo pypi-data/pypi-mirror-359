2cdeb3e862aca25e3fbae9ee2ed64c5bd4985d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package ProstoKaravanClient/0.3.3/ProstoKaravanClient-0.3.3.tar.gz

---
+++ packages/ProstoKaravanClient/ProstoKaravanClient-0.3.3.tar.gz/ProstoKaravanClient-0.3.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoKaravanClient",
+    version="0.3.3",
+    packages=["ProstoKaravanClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
