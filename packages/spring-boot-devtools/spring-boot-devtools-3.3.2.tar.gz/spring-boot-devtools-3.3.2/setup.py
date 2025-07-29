0e07042791e183dfb42fcc9b7b7627d6ca388672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package spring-boot-devtools/3.3.2/spring-boot-devtools-3.3.2.tar.gz

---
+++ packages/spring-boot-devtools/spring-boot-devtools-3.3.2.tar.gz/spring-boot-devtools-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-devtools",
+    version="3.3.2",
+    packages=["spring-boot-devtools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
