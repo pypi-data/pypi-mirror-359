b18594c043a64498c84d89054725506082a0e19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-file-uploader-api-lib/0.0.0/dl-file-uploader-api-lib-0.0.0.tar.gz

---
+++ packages/dl-file-uploader-api-lib/dl-file-uploader-api-lib-0.0.0.tar.gz/dl-file-uploader-api-lib-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-file-uploader-api-lib",
+    version="0.0.0",
+    packages=["dl-file-uploader-api-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
