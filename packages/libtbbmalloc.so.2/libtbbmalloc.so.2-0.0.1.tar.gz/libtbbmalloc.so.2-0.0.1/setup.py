d423836f809bdfe98fa146efa30a1c37a6f77fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package libtbbmalloc.so.2/0.0.1/libtbbmalloc.so.2-0.0.1.tar.gz

---
+++ packages/libtbbmalloc.so.2/libtbbmalloc.so.2-0.0.1.tar.gz/libtbbmalloc.so.2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libtbbmalloc.so.2",
+    version="0.0.1",
+    packages=["libtbbmalloc.so.2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
