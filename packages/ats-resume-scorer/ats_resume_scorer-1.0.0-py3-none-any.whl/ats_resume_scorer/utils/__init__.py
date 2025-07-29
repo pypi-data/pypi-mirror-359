0c258ca725f16f0b0e645c303d23bbaedeef46e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:27 +0000

    Add package ats-resume-scorer/1.0.0/ats_resume_scorer-1.0.0-py3-none-any.whl

---
+++ packages/ats-resume-scorer/ats_resume_scorer-1.0.0-py3-none-any.whl/ats_resume_scorer/utils/__init__.py
@@ -0,0 +1,8 @@
+# ats_resume_scorer/utils/__init__.py
+"""
+Utility Functions Package
+"""
+
+from .report_generator import ReportGenerator, RecommendationItem
+
+__all__ = ["ReportGenerator", "RecommendationItem"]
