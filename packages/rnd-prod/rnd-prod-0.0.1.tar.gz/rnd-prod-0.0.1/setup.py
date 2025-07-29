ad037519610b710635c5a2e63824ff3eab1bf529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package rnd-prod/0.0.1/rnd-prod-0.0.1.tar.gz

---
+++ packages/rnd-prod/rnd-prod-0.0.1.tar.gz/rnd-prod-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rnd-prod",
+    version="0.0.1",
+    packages=["rnd-prod"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
