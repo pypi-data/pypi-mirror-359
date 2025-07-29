4537c051ae5e657fc5142baf27a7012dbed09fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package junit-platform-engine/1.10.3/junit-platform-engine-1.10.3.tar.gz

---
+++ packages/junit-platform-engine/junit-platform-engine-1.10.3.tar.gz/junit-platform-engine-1.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-platform-engine",
+    version="1.10.3",
+    packages=["junit-platform-engine"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
