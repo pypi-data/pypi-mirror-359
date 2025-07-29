aace99b4595c2d2c09aea08f147628eb03455746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package catboost-spark/0.0.1/catboost-spark-0.0.1.tar.gz

---
+++ packages/catboost-spark/catboost-spark-0.0.1.tar.gz/catboost-spark-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="catboost-spark",
+    version="0.0.1",
+    packages=["catboost-spark"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
