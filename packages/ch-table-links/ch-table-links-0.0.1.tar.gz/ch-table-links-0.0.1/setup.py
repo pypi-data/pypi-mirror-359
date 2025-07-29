d83a4ebe76f181b7bdf8bd0f778ef4296eeb0c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package ch-table-links/0.0.1/ch-table-links-0.0.1.tar.gz

---
+++ packages/ch-table-links/ch-table-links-0.0.1.tar.gz/ch-table-links-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ch-table-links",
+    version="0.0.1",
+    packages=["ch-table-links"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
