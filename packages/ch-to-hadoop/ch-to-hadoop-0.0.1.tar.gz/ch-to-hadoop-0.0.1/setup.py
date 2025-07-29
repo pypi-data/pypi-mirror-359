0253ef227d518e12a7c9e5b014fd406a9af37c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package ch-to-hadoop/0.0.1/ch-to-hadoop-0.0.1.tar.gz

---
+++ packages/ch-to-hadoop/ch-to-hadoop-0.0.1.tar.gz/ch-to-hadoop-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ch-to-hadoop",
+    version="0.0.1",
+    packages=["ch-to-hadoop"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
