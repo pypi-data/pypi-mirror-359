e7890221f5c60cbf1dcee9d0ddb56f617c8316a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:59 +0000

    Add package prosto-dp-client/0.0.1/prosto-dp-client-0.0.1.tar.gz

---
+++ packages/prosto-dp-client/prosto-dp-client-0.0.1.tar.gz/prosto-dp-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prosto-dp-client",
+    version="0.0.1",
+    packages=["prosto-dp-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
