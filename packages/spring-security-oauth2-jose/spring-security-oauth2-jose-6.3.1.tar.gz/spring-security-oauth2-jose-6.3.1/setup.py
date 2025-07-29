01fd3acaf233924ee0c33588f254bd68ff3011de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package spring-security-oauth2-jose/6.3.1/spring-security-oauth2-jose-6.3.1.tar.gz

---
+++ packages/spring-security-oauth2-jose/spring-security-oauth2-jose-6.3.1.tar.gz/spring-security-oauth2-jose-6.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-security-oauth2-jose",
+    version="6.3.1",
+    packages=["spring-security-oauth2-jose"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
