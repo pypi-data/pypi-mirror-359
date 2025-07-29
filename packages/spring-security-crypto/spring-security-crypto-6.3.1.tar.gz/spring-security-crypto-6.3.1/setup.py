2f72f5fadf338835703bcfca80ff66510b6d0a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-security-crypto/6.3.1/spring-security-crypto-6.3.1.tar.gz

---
+++ packages/spring-security-crypto/spring-security-crypto-6.3.1.tar.gz/spring-security-crypto-6.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-security-crypto",
+    version="6.3.1",
+    packages=["spring-security-crypto"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
