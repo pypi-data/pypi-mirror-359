f3c0c25cab7678d6425722e22ddfe66484221399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package byte-buddy/1.14.18/byte-buddy-1.14.18.tar.gz

---
+++ packages/byte-buddy/byte-buddy-1.14.18.tar.gz/byte-buddy-1.14.18/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="byte-buddy",
+    version="1.14.18",
+    packages=["byte-buddy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
