ef4ed58c7e8d8f2cec7a5e1b521fbe69f04eaa90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:29 +0000

    Add package ads-model-workflow/0.0.1/ads-model-workflow-0.0.1.tar.gz

---
+++ packages/ads-model-workflow/ads-model-workflow-0.0.1.tar.gz/ads-model-workflow-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ads-model-workflow",
+    version="0.0.1",
+    packages=["ads-model-workflow"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
