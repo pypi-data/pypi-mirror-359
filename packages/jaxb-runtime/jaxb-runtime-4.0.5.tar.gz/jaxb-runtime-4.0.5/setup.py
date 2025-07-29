6151421a9e176ab3445107045b52bc2768636099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:58 +0000

    Add package jaxb-runtime/4.0.5/jaxb-runtime-4.0.5.tar.gz

---
+++ packages/jaxb-runtime/jaxb-runtime-4.0.5.tar.gz/jaxb-runtime-4.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jaxb-runtime",
+    version="4.0.5",
+    packages=["jaxb-runtime"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
