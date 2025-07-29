f25cdfda50691fc09dc2cd26428410b455e2f9c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package admin-confirm/0.0.1/admin-confirm-0.0.1.tar.gz

---
+++ packages/admin-confirm/admin-confirm-0.0.1.tar.gz/admin-confirm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="admin-confirm",
+    version="0.0.1",
+    packages=["admin-confirm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
