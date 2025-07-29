b90dbafa34c6ee2f66e9bd092c7246b79141ff70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package sage-auth/0.0.1/sage-auth-0.0.1.tar.gz

---
+++ packages/sage-auth/sage-auth-0.0.1.tar.gz/sage-auth-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sage-auth",
+    version="0.0.1",
+    packages=["sage-auth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
