09e2996f09cac1a85d8212672d6610ec103f11b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package jakarta.servlet-api/6.0.0/jakarta.servlet-api-6.0.0.tar.gz

---
+++ packages/jakarta.servlet-api/jakarta.servlet-api-6.0.0.tar.gz/jakarta.servlet-api-6.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.servlet-api",
+    version="6.0.0",
+    packages=["jakarta.servlet-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
