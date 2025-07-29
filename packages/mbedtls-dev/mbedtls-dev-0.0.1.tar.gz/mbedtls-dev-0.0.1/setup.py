63caaa021f3751583920e237c7dfbdd5e8b373bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:27 +0000

    Add package mbedtls-dev/0.0.1/mbedtls-dev-0.0.1.tar.gz

---
+++ packages/mbedtls-dev/mbedtls-dev-0.0.1.tar.gz/mbedtls-dev-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mbedtls-dev",
+    version="0.0.1",
+    packages=["mbedtls-dev"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
