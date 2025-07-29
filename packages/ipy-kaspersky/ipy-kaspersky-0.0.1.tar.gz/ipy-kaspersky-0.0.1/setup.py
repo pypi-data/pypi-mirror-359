81850dc48ebda4800e70bd8ed2c98bb57306d749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-kaspersky/0.0.1/ipy-kaspersky-0.0.1.tar.gz

---
+++ packages/ipy-kaspersky/ipy-kaspersky-0.0.1.tar.gz/ipy-kaspersky-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-kaspersky",
+    version="0.0.1",
+    packages=["ipy-kaspersky"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
