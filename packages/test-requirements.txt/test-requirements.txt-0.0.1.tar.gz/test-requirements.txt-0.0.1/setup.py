d32662fe7d842ce068539d862aa86ed99b7ee4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package test-requirements.txt/0.0.1/test-requirements.txt-0.0.1.tar.gz

---
+++ packages/test-requirements.txt/test-requirements.txt-0.0.1.tar.gz/test-requirements.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-requirements.txt",
+    version="0.0.1",
+    packages=["test-requirements.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
