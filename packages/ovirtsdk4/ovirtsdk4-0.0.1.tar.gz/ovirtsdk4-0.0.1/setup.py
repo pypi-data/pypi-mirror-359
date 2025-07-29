ec29a40ff2c94aa55b788f712a2af544722dc9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:07 +0000

    Add package ovirtsdk4/0.0.1/ovirtsdk4-0.0.1.tar.gz

---
+++ packages/ovirtsdk4/ovirtsdk4-0.0.1.tar.gz/ovirtsdk4-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ovirtsdk4",
+    version="0.0.1",
+    packages=["ovirtsdk4"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
