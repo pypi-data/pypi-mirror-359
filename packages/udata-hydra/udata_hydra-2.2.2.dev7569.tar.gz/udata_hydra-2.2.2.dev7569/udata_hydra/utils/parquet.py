88924849b7b018bf1dfb2e6f1acfa5e43b55ada5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:16 +0000

    Add package udata-hydra/2.2.2.dev7569/udata_hydra-2.2.2.dev7569.tar.gz

---
+++ packages/udata-hydra/udata_hydra-2.2.2.dev7569.tar.gz/udata_hydra-2.2.2.dev7569/udata_hydra/utils/parquet.py
@@ -0,0 +1,29 @@
+from typing import Iterator
+
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+PYTHON_TYPE_TO_PA = {
+    "string": pa.string(),
+    "float": pa.float64(),
+    "int": pa.int64(),
+    "bool": pa.bool_(),
+    "json": pa.string(),
+    "date": pa.date32(),
+    "datetime": pa.date64(),
+}
+
+
+def save_as_parquet(
+    records: Iterator[list],
+    columns: dict,
+    output_filename: str | None = None,
+) -> tuple[str, pa.Table]:
+    # the "output_name = None" case is only used in tests
+    table = pa.Table.from_pylist(
+        [{c: v for c, v in zip(columns, values)} for values in records],
+        schema=pa.schema([pa.field(c, PYTHON_TYPE_TO_PA[columns[c]]) for c in columns]),
+    )
+    if output_filename:
+        pq.write_table(table, f"{output_filename}.parquet")
+    return f"{output_filename}.parquet", table
