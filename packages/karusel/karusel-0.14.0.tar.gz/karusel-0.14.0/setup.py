b1b22578d08a0fe798174ebc439a7f37758106bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package karusel/0.14.0/karusel-0.14.0.tar.gz

---
+++ packages/karusel/karusel-0.14.0.tar.gz/karusel-0.14.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="karusel",
+    version="0.14.0",
+    packages=["karusel"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
