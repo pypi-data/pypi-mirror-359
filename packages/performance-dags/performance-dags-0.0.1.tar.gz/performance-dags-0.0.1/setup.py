6f001f6bad0ba2d3b336e0e262c56ddfce27371c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package performance-dags/0.0.1/performance-dags-0.0.1.tar.gz

---
+++ packages/performance-dags/performance-dags-0.0.1.tar.gz/performance-dags-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="performance-dags",
+    version="0.0.1",
+    packages=["performance-dags"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
