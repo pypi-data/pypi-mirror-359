abd2ce4b61b73f266fde860439f6114d6197204c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:23 +0000

    Add package invy/0.0.1/invy-0.0.1.tar.gz

---
+++ packages/invy/invy-0.0.1.tar.gz/invy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="invy",
+    version="0.0.1",
+    packages=["invy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
