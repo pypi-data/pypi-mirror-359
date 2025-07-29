9b8daea28dc24a07509a161df2c53034d11f13bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:12 +0000

    Add package x-mlps-pytorch/0.0.25/x_mlps_pytorch-0.0.25.tar.gz

---
+++ packages/x-mlps-pytorch/x_mlps_pytorch-0.0.25.tar.gz/x_mlps_pytorch-0.0.25/tests/test_mlp.py
@@ -0,0 +1,44 @@
+import pytest
+import torch
+
+def test_mlp():
+    from x_mlps_pytorch.mlp import MLP
+
+    mlp = MLP(256, 128, 64)
+
+    x = torch.randn(7, 3, 256)
+
+    assert mlp(x).shape == (7, 3, 64)
+
+# with depth
+
+def test_create_mlp():
+    from x_mlps_pytorch.mlp import create_mlp
+
+    mlp = create_mlp(
+        dim = 128,
+        dim_in = 256,
+        dim_out = 64,
+        depth = 4
+    )
+
+    # same as MLP(256, 128, 128, 128, 128, 64)
+
+    x = torch.randn(7, 3, 256)
+
+    assert mlp(x).shape == (7, 3, 64)
+
+@pytest.mark.parametrize('latent_mlp', (False, True))
+@pytest.mark.parametrize('condition_hadamard_hiddens', (False, True))
+def test_latent_conditioned_mlp(
+    latent_mlp,
+    condition_hadamard_hiddens
+):
+    from x_mlps_pytorch.mlp_with_latent import create_mlp
+
+    mlp = create_mlp(256, 4, dim_in = 128, dim_out = 128, dim_latent = 33, latent_mlp = latent_mlp, condition_hiddens = condition_hadamard_hiddens)
+
+    x = torch.randn(7, 3, 128)
+    latent = torch.randn(7, 33)
+
+    assert mlp(x, latent = latent).shape == x.shape
