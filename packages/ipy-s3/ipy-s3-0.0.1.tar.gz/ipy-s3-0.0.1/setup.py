9c66c5dbf57a82a225630d74a3453ff1aeed3347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-s3/0.0.1/ipy-s3-0.0.1.tar.gz

---
+++ packages/ipy-s3/ipy-s3-0.0.1.tar.gz/ipy-s3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-s3",
+    version="0.0.1",
+    packages=["ipy-s3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
