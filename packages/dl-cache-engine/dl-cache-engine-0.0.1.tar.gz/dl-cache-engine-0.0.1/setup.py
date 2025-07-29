b6bba29540c16a0003eebef106b9cf850fec1265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-cache-engine/0.0.1/dl-cache-engine-0.0.1.tar.gz

---
+++ packages/dl-cache-engine/dl-cache-engine-0.0.1.tar.gz/dl-cache-engine-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-cache-engine",
+    version="0.0.1",
+    packages=["dl-cache-engine"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
