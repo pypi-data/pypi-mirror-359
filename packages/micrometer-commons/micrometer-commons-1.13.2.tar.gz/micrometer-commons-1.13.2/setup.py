7830053c786a3ed855a966e02489402a5bb0c944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package micrometer-commons/1.13.2/micrometer-commons-1.13.2.tar.gz

---
+++ packages/micrometer-commons/micrometer-commons-1.13.2.tar.gz/micrometer-commons-1.13.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micrometer-commons",
+    version="1.13.2",
+    packages=["micrometer-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
