15b4d28326327342b4034751f61e0fa168470a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:48 +0000

    Add package thermoengine/1.5.3/thermoengine-1.5.3-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/thermoengine/thermoengine-1.5.3-cp310-cp310-macosx_10_13_x86_64.whl/thermoengine/phase_library/MELTS/__init__.py
@@ -0,0 +1,3 @@
+from . import Olivine_HSG, Liquid_v1_0, Spinel_SG91, Clinopyroxene_SG94, Orthopyroxene_SG94, aqueous, PurePhases_Berman
+
+__all__ = [Olivine_HSG, Liquid_v1_0, Spinel_SG91, Clinopyroxene_SG94, Orthopyroxene_SG94, aqueous, PurePhases_Berman]
