81af0b66aea653c0d526c0838d39c6ee1fa2756b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:11 +0000

    Add package vk-ml-models-exporter/0.3.22.43/vk-ml-models-exporter-0.3.22.43.tar.gz

---
+++ packages/vk-ml-models-exporter/vk-ml-models-exporter-0.3.22.43.tar.gz/vk-ml-models-exporter-0.3.22.43/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-ml-models-exporter",
+    version="0.3.22.43",
+    packages=["vk-ml-models-exporter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
