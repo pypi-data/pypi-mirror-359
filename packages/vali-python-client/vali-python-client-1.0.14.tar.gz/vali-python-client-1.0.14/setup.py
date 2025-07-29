c41a327650772b6788e6d83b5b42b99a34b4d553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package vali-python-client/1.0.14/vali-python-client-1.0.14.tar.gz

---
+++ packages/vali-python-client/vali-python-client-1.0.14.tar.gz/vali-python-client-1.0.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vali-python-client",
+    version="1.0.14",
+    packages=["vali-python-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
