1e13dfc672d20fbb6667fd408c9bcd6072f5f431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:32 +0000

    Add package yta-audio-narration/0.0.11/yta_audio_narration-0.0.11.tar.gz

---
+++ packages/yta-audio-narration/yta_audio_narration-0.0.11.tar.gz/yta_audio_narration-0.0.11/src/yta_audio_narration/voices/ttsmp3.py
@@ -0,0 +1,6 @@
+"""
+TODO: Do we make this optional or not? If it
+is optional we can allow installing the libs
+only if using this one.
+"""
+from yta_audio_narration_ttsmp3 import Ttsmp3VoiceName, LANGUAGE_OPTIONS, EMOTION_OPTIONS, SPEED_OPTIONS, PITCH_OPTIONS, Ttsmp3NarrationVoice, get_narrator_names_by_language, narrate_tts3
