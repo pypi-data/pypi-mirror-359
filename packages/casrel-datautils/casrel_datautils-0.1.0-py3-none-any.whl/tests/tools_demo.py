c72961c3515ef492e146425b70e66f0d634776ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:55 +0000

    Add package casrel-datautils/0.1.0/casrel_datautils-0.1.0-py3-none-any.whl

---
+++ packages/casrel-datautils/casrel_datautils-0.1.0-py3-none-any.whl/tests/tools_demo.py
@@ -0,0 +1,3 @@
+# from casrel_datautils import BaseConfig
+# baseconf = BaseConfig(bert_path="bert-base-chinese", train_data="train.json", test_data="test.json", rel_data="relation.json", max_length=128)
+# dataloaders = get_data(baseconf)
