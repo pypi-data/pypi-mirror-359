f144bd1474707432926c5b94603bbfadf15db3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package statshouse-client/0.0.1/statshouse-client-0.0.1.tar.gz

---
+++ packages/statshouse-client/statshouse-client-0.0.1.tar.gz/statshouse-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statshouse-client",
+    version="0.0.1",
+    packages=["statshouse-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
