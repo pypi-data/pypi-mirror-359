a1edb32ea1131d09f32efe4a95227249632f0e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:03 +0000

    Add package migrun/0.0.2/migrun-0.0.2.tar.gz

---
+++ packages/migrun/migrun-0.0.2.tar.gz/migrun-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="migrun",
+    version="0.0.2",
+    packages=["migrun"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
