9fbf75835cadfd2b688c2bf83bf7ce56d1a984a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-core-testing/0.0.1/dl-core-testing-0.0.1.tar.gz

---
+++ packages/dl-core-testing/dl-core-testing-0.0.1.tar.gz/dl-core-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-core-testing",
+    version="0.0.1",
+    packages=["dl-core-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
