04053a11913033a6c546c4a35db5eb4512fd94a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package ProstoKtoTamClient/0.2.0/ProstoKtoTamClient-0.2.0.tar.gz

---
+++ packages/ProstoKtoTamClient/ProstoKtoTamClient-0.2.0.tar.gz/ProstoKtoTamClient-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoKtoTamClient",
+    version="0.2.0",
+    packages=["ProstoKtoTamClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
