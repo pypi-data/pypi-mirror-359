302629a943a4c8f80e318c7c8d8484c29eac77b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package ipchains/0.0.1/ipchains-0.0.1.tar.gz

---
+++ packages/ipchains/ipchains-0.0.1.tar.gz/ipchains-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipchains",
+    version="0.0.1",
+    packages=["ipchains"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
