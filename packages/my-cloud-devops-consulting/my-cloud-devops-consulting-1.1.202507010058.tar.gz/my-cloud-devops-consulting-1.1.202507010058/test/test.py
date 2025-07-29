e505ecca635a2207a6261046203b573e9c3361fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    Add package my-cloud-devops-consulting/1.1.202507010058/my-cloud-devops-consulting-1.1.202507010058.tar.gz

---
+++ packages/my-cloud-devops-consulting/my-cloud-devops-consulting-1.1.202507010058.tar.gz/my-cloud-devops-consulting-1.1.202507010058/test/test.py
@@ -0,0 +1,10 @@
+def full_name(first_name,last_name):
+    return f"Hello {first_name}  {last_name}"
+message = full_name("Betrand ", "Mutagha ")
+print(message)
+if message == message:
+    print(" yes ")
+elif message != message:
+    print("No")
+else:
+    print("All wrong")
