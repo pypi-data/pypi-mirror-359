c0b80597c4e683e08b1d35edfbe434e5f7f828af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:01 +0000

    Add package volttron-listener/2.0.0rc3/volttron_listener-2.0.0rc3.tar.gz

---
+++ packages/volttron-listener/volttron_listener-2.0.0rc3.tar.gz/volttron_listener-2.0.0rc3/src/listener/agent.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*- {{{
+# ===----------------------------------------------------------------------===
+#
+#                 Installable Component of Eclipse VOLTTRON
+#
+# ===----------------------------------------------------------------------===
+#
+# Copyright 2022 Battelle Memorial Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# ===----------------------------------------------------------------------===
+# }}}
+import logging
+import sys
+from pprint import pformat
+import datetime
+
+from volttron.client.vip.agent.subsystems.configstore import VALID_ACTIONS
+from volttron.utils.commands import vip_main
+from volttron.client.messaging.health import STATUS_GOOD
+from volttron.client.vip.agent import Agent, Core, PubSub
+import volttron.utils as utils
+
+_log = logging.getLogger(__name__)
+#_log.setLevel(logging.DEBUG)
+__version__ = '4.0'
+DEFAULT_MESSAGE = 'Listener Message'
+DEFAULT_HEARTBEAT_PERIOD = 5
+
+
+class ListenerAgent(Agent):
+    """Listens to everything and publishes a heartbeat according to the
+    heartbeat period specified in the settings module.
+    """
+
+    def __init__(self, config_path, **kwargs):
+        super().__init__(**kwargs)
+        self.config = utils.load_config(config_path)
+        self._message = self.config.get('message', DEFAULT_MESSAGE)
+        self._heartbeat_period = self.config.get('heartbeat_period', DEFAULT_HEARTBEAT_PERIOD)
+        runtime_limit = int(self.config.get('runtime_limit', 0))
+        if runtime_limit and runtime_limit > 0:
+            stop_time = datetime.datetime.now() + datetime.timedelta(seconds=runtime_limit)
+            _log.info('Listener agent will stop at {}'.format(stop_time))
+            self.core.schedule(stop_time, self.core.stop)
+        else:
+            _log.info('No valid runtime_limit configured; listener agent will run until manually stopped')
+
+        try:
+            self._heartbeat_period = int(self._heartbeat_period)
+        except:
+            _log.warning('Invalid heartbeat period specified setting to default')
+            self._heartbeat_period = DEFAULT_HEARTBEAT_PERIOD
+        log_level = self.config.get('log-level', 'INFO')
+        if log_level == 'ERROR':
+            self._logfn = _log.error
+        elif log_level == 'WARN':
+            self._logfn = _log.warn
+        elif log_level == 'DEBUG':
+            self._logfn = _log.debug
+        else:
+            self._logfn = _log.info
+
+    @Core.receiver( 'onsetup')
+    def onsetup(self, sender, **kwargs):
+        # Demonstrate accessing a value from the config file
+        _log.info(self.config.get('message', DEFAULT_MESSAGE))
+
+    @Core.receiver('onstart')
+    def onstart(self, sender, **kwargs):
+        # TODO: Bring back version?
+        #_log.debug("VERSION IS: {}".format(self.core.version()))
+        if self._heartbeat_period != 0:
+            _log.debug(f"Heartbeat starting for {self.core.identity} published every {self._heartbeat_period}")
+            self.vip.heartbeat.start_with_period(self._heartbeat_period)
+            self.vip.health.set_status(STATUS_GOOD, self._message)
+
+        self.vip.pubsub.subscribe(peer='pubsub', prefix='', callback=self.on_match, all_platforms=True)
+
+    #@PubSub.subscribe('pubsub', '', all_platforms=True)
+    def on_match(self, peer, sender, bus, topic, headers, message):
+        """Use match_all to receive all messages and print them out."""
+        self._logfn(
+            "Peer: {0}, Sender: {1}:, Bus: {2}, Topic: {3}, Headers: {4}, "
+            "Message: \n{5}".format(peer, sender, bus, topic, headers, pformat(message)))
+
+
+def main():
+    """
+    Main method called during startup of agent.
+    :return:
+    """
+    try:
+        vip_main(ListenerAgent, version=__version__)
+    except Exception as e:
+        _log.exception('unhandled exception')
+        _log.exception(e)
+    finally:
+        _log.debug("Exiting")
+
+
+if __name__ == '__main__':
+    # Entry point for script
+    sys.exit(main())
