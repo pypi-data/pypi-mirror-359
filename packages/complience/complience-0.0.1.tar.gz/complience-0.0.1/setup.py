8fbadbcd5cb5136e0ae610d158ef13157c74fa04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package complience/0.0.1/complience-0.0.1.tar.gz

---
+++ packages/complience/complience-0.0.1.tar.gz/complience-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="complience",
+    version="0.0.1",
+    packages=["complience"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
