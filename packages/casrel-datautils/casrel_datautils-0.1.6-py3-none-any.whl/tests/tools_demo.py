79fafd4e8852a9e5158b1ed64e4e9c3aea8b53ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:49 +0000

    Add package casrel-datautils/0.1.6/casrel_datautils-0.1.6-py3-none-any.whl

---
+++ packages/casrel-datautils/casrel_datautils-0.1.6-py3-none-any.whl/tests/tools_demo.py
@@ -0,0 +1,15 @@
+
+from casrel_datautils.Base_Conf import BaseConfig
+from casrel_datautils.data_loader import get_data
+baseconf = BaseConfig(bert_path_or_bertModel_name="bert-base-chinese",
+                      train_data=r"C:\Users\lidat\PycharmProjects\Casrel_datautils\data\test.json",
+                      test_data=r"C:\Users\lidat\PycharmProjects\Casrel_datautils\data\one_Sample.json",
+                      rel_data=r"C:\Users\lidat\PycharmProjects\Casrel_datautils\data\relation.json",batch_size=4)
+dataloaders = get_data(baseconf)
+print(dataloaders)
+print(dataloaders.items())
+for batch_idx, (inputs, labels) in enumerate(dataloaders["test_dataloader"]):
+    print(f"Batch {batch_idx}:")
+    print(f"Inputs: {inputs}")
+    print(f"Labels: {labels}")
+    print("---")
