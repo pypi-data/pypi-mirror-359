3ff4bd31151d9105562356937096574befeeb9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:25 +0000

    Add package radiochart/0.3.0/radiochart-0.3.0.tar.gz

---
+++ packages/radiochart/radiochart-0.3.0.tar.gz/radiochart-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="radiochart",
+    version="0.3.0",
+    description="Generate hierarchical radio charts for Arma 3 MILSIM units.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="KingCharlesVII",
+    author_email="viikingcharles@gmail.com",
+    url="https://github.com/kingcharlesvi/radiochart",
+    packages=find_packages(),
+    install_requires=[
+        "graphviz",
+        "InquirerPy",
+        "rich"
+    ],
+    entry_points={
+        "console_scripts": [
+            "radiochart=radiochart.cli:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.7",
+    include_package_data=True,
+)
