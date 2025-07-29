9390efb1a727de74f69844275121a0945875a478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package vqe-processors/0.0.1/vqe-processors-0.0.1.tar.gz

---
+++ packages/vqe-processors/vqe-processors-0.0.1.tar.gz/vqe-processors-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vqe-processors",
+    version="0.0.1",
+    packages=["vqe-processors"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
