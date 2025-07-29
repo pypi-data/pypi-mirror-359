572a77db05c4ed9acf0fca2004e8aff4583a8328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package micrometer-jakarta9/1.13.2/micrometer-jakarta9-1.13.2.tar.gz

---
+++ packages/micrometer-jakarta9/micrometer-jakarta9-1.13.2.tar.gz/micrometer-jakarta9-1.13.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micrometer-jakarta9",
+    version="1.13.2",
+    packages=["micrometer-jakarta9"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
