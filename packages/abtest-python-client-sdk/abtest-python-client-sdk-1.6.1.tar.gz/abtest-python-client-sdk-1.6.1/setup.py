eb9923aa7e1f56d4918fcd9fdd0e677c3c9869da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:23 +0000

    Add package abtest-python-client-sdk/1.6.1/abtest-python-client-sdk-1.6.1.tar.gz

---
+++ packages/abtest-python-client-sdk/abtest-python-client-sdk-1.6.1.tar.gz/abtest-python-client-sdk-1.6.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="abtest-python-client-sdk",
+    version="1.6.1",
+    packages=["abtest-python-client-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
