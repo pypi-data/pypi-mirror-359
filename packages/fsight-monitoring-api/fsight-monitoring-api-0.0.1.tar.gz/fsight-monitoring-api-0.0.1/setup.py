2bdfab6210768727e580ceea8908c84f894d5351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package fsight-monitoring-api/0.0.1/fsight-monitoring-api-0.0.1.tar.gz

---
+++ packages/fsight-monitoring-api/fsight-monitoring-api-0.0.1.tar.gz/fsight-monitoring-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fsight-monitoring-api",
+    version="0.0.1",
+    packages=["fsight-monitoring-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
