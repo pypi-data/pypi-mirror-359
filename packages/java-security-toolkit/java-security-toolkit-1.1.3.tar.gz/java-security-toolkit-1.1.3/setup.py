fcddb7df5841ddaf29923d65b5535c8dd49b16fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:08 +0000

    Add package java-security-toolkit/1.1.3/java-security-toolkit-1.1.3.tar.gz

---
+++ packages/java-security-toolkit/java-security-toolkit-1.1.3.tar.gz/java-security-toolkit-1.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="java-security-toolkit",
+    version="1.1.3",
+    packages=["java-security-toolkit"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
