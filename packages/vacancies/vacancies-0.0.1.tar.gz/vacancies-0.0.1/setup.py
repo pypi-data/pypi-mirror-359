bfa035830359f8ac712457f704c9db1686f65835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package vacancies/0.0.1/vacancies-0.0.1.tar.gz

---
+++ packages/vacancies/vacancies-0.0.1.tar.gz/vacancies-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vacancies",
+    version="0.0.1",
+    packages=["vacancies"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
