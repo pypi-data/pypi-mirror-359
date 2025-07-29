c4d9d4aa10337bd626f5d353490f10d136ac97d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package spnego/0.0.1/spnego-0.0.1.tar.gz

---
+++ packages/spnego/spnego-0.0.1.tar.gz/spnego-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spnego",
+    version="0.0.1",
+    packages=["spnego"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
