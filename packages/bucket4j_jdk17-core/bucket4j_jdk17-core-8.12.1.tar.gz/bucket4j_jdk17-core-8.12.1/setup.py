40930c7d5b104654e7b7347810494fcf141ad0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package bucket4j_jdk17-core/8.12.1/bucket4j_jdk17-core-8.12.1.tar.gz

---
+++ packages/bucket4j_jdk17-core/bucket4j_jdk17-core-8.12.1.tar.gz/bucket4j_jdk17-core-8.12.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bucket4j_jdk17-core",
+    version="8.12.1",
+    packages=["bucket4j_jdk17-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
