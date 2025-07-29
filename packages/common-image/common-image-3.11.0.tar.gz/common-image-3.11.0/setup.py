23bcd1ff268f87c8b5d780143d13f808c67c3661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package common-image/3.11.0/common-image-3.11.0.tar.gz

---
+++ packages/common-image/common-image-3.11.0.tar.gz/common-image-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="common-image",
+    version="3.11.0",
+    packages=["common-image"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
