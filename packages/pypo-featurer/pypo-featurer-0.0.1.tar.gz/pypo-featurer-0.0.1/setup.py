68ebc34e17a58c0cd7b60fb332a8259da5b684a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package pypo-featurer/0.0.1/pypo-featurer-0.0.1.tar.gz

---
+++ packages/pypo-featurer/pypo-featurer-0.0.1.tar.gz/pypo-featurer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-featurer",
+    version="0.0.1",
+    packages=["pypo-featurer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
