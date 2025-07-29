8e7df8b585d7d4284c8809b47f9b6a4b64be97b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package global-defines/0.0.1/global-defines-0.0.1.tar.gz

---
+++ packages/global-defines/global-defines-0.0.1.tar.gz/global-defines-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="global-defines",
+    version="0.0.1",
+    packages=["global-defines"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
