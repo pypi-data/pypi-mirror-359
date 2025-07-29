793502d648fcb237083ad3121fb449b28a75ccdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:02 +0000

    Add package dl-connector-trino/0.0.1/dl-connector-trino-0.0.1.tar.gz

---
+++ packages/dl-connector-trino/dl-connector-trino-0.0.1.tar.gz/dl-connector-trino-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-trino",
+    version="0.0.1",
+    packages=["dl-connector-trino"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
