e7e1d848b85eb46a79c137bf09dcf1a3be0e9b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:01 +0000

    Add package yta-audio/0.1.4/yta_audio-0.1.4-py3-none-any.whl

---
+++ packages/yta-audio/yta_audio-0.1.4-py3-none-any.whl/yta_audio/__init__.py
@@ -0,0 +1,9 @@
+"""
+Welcome to Youtube Autonomous Audio Module.
+
+TODO: This module has to be renamed or
+refactored to include effects or something
+similar, because we have migrated the code
+to other projects and this has almost
+nothing now.
+"""
