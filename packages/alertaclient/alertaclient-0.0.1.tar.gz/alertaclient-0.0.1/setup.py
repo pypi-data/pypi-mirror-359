2218ad83e3c7a03d70537bed1a6f5d2fd0adc8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package alertaclient/0.0.1/alertaclient-0.0.1.tar.gz

---
+++ packages/alertaclient/alertaclient-0.0.1.tar.gz/alertaclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="alertaclient",
+    version="0.0.1",
+    packages=["alertaclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
