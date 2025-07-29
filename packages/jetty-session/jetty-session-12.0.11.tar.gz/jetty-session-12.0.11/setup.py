2a1e7eb9b25393502bdb71a98b540c888fac90f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:15 +0000

    Add package jetty-session/12.0.11/jetty-session-12.0.11.tar.gz

---
+++ packages/jetty-session/jetty-session-12.0.11.tar.gz/jetty-session-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-session",
+    version="12.0.11",
+    packages=["jetty-session"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
