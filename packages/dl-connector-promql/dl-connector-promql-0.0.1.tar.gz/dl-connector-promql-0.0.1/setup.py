3db809a612395c2ad455ed4e6c85d9c6c1e2ed52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-promql/0.0.1/dl-connector-promql-0.0.1.tar.gz

---
+++ packages/dl-connector-promql/dl-connector-promql-0.0.1.tar.gz/dl-connector-promql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-promql",
+    version="0.0.1",
+    packages=["dl-connector-promql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
