5c9268ca2b5ba38e78342bc53faebf55d67078b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package jetty-plus/12.0.11/jetty-plus-12.0.11.tar.gz

---
+++ packages/jetty-plus/jetty-plus-12.0.11.tar.gz/jetty-plus-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-plus",
+    version="12.0.11",
+    packages=["jetty-plus"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
