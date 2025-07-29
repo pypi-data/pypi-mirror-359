d200af59edf5c784f6407ee1949a08809c32d449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-boot-starter-jdbc/3.3.2/spring-boot-starter-jdbc-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-jdbc/spring-boot-starter-jdbc-3.3.2.tar.gz/spring-boot-starter-jdbc-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-jdbc",
+    version="3.3.2",
+    packages=["spring-boot-starter-jdbc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
