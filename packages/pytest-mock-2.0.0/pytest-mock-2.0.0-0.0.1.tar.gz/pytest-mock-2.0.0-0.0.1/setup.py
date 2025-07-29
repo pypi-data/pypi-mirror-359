fd0c0028d7d4342a82bd1aca91a74ba87bd32a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:06 +0000

    Add package pytest-mock-2.0.0/0.0.1/pytest-mock-2.0.0-0.0.1.tar.gz

---
+++ packages/pytest-mock-2.0.0/pytest-mock-2.0.0-0.0.1.tar.gz/pytest-mock-2.0.0-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pytest-mock-2.0.0",
+    version="0.0.1",
+    packages=["pytest-mock-2.0.0"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
