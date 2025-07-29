c4f0a8b905c7ab5f9e48e78112aaedc82a37d84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package batik-all/1.17/batik-all-1.17.tar.gz

---
+++ packages/batik-all/batik-all-1.17.tar.gz/batik-all-1.17/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="batik-all",
+    version="1.17",
+    packages=["batik-all"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
