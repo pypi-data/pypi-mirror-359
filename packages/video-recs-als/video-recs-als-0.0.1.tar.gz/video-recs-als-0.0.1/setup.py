d133eb40f1162cecb6659b83e0c11df15f0949be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package video-recs-als/0.0.1/video-recs-als-0.0.1.tar.gz

---
+++ packages/video-recs-als/video-recs-als-0.0.1.tar.gz/video-recs-als-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="video-recs-als",
+    version="0.0.1",
+    packages=["video-recs-als"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
