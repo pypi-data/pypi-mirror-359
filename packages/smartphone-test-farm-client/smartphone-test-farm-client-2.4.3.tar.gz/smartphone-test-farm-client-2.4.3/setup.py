a4d0f9c78d5f1cc89f20dc2b9ff660e7f7f564c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package smartphone-test-farm-client/2.4.3/smartphone-test-farm-client-2.4.3.tar.gz

---
+++ packages/smartphone-test-farm-client/smartphone-test-farm-client-2.4.3.tar.gz/smartphone-test-farm-client-2.4.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="smartphone-test-farm-client",
+    version="2.4.3",
+    packages=["smartphone-test-farm-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
