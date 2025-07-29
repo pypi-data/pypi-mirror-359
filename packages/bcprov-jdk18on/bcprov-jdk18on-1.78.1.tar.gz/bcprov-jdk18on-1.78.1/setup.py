8e8c0032bde30e6550f51094c4aa195f83f8f7de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package bcprov-jdk18on/1.78.1/bcprov-jdk18on-1.78.1.tar.gz

---
+++ packages/bcprov-jdk18on/bcprov-jdk18on-1.78.1.tar.gz/bcprov-jdk18on-1.78.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bcprov-jdk18on",
+    version="1.78.1",
+    packages=["bcprov-jdk18on"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
