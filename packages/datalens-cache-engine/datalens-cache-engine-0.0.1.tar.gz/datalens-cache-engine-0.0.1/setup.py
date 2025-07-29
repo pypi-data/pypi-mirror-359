44db440c0247870a8fc3483d9f1e033937b5a74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package datalens-cache-engine/0.0.1/datalens-cache-engine-0.0.1.tar.gz

---
+++ packages/datalens-cache-engine/datalens-cache-engine-0.0.1.tar.gz/datalens-cache-engine-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-cache-engine",
+    version="0.0.1",
+    packages=["datalens-cache-engine"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
