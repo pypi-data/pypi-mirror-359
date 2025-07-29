65f362f500b56911243b0a76328c69204085241e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:31 +0000

    Add package ob-metaflow-stubs/6.0.3.186/ob-metaflow-stubs-6.0.3.186.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.186.tar.gz/ob-metaflow-stubs-6.0.3.186/metaflow-stubs/plugins/aws/secrets_manager/__init__.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-01T08:40:33.028884                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import aws_secrets_manager_secrets_provider as aws_secrets_manager_secrets_provider
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.186.tar.gz/ob-metaflow-stubs-6.0.3.186/metaflow-stubs/system/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-01T08:40:32.987457                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import system_monitor as system_monitor
+from .system_monitor import SystemMonitor as SystemMonitor
+from . import system_logger as system_logger
+from .system_logger import SystemLogger as SystemLogger
+
