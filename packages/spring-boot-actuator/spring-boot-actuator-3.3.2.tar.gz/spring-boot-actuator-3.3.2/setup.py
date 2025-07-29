b93797a3b5951ced2c9463615b5ffabdcd150bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package spring-boot-actuator/3.3.2/spring-boot-actuator-3.3.2.tar.gz

---
+++ packages/spring-boot-actuator/spring-boot-actuator-3.3.2.tar.gz/spring-boot-actuator-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-actuator",
+    version="3.3.2",
+    packages=["spring-boot-actuator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
