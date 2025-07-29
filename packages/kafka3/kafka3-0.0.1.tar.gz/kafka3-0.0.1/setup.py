3e02644b218b644c4338efb3ba08d550c2d90095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package kafka3/0.0.1/kafka3-0.0.1.tar.gz

---
+++ packages/kafka3/kafka3-0.0.1.tar.gz/kafka3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kafka3",
+    version="0.0.1",
+    packages=["kafka3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
