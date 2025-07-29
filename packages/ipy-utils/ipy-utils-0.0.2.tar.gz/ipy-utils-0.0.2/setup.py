18decb8207935aaed3ced936f999462721b73b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-utils/0.0.2/ipy-utils-0.0.2.tar.gz

---
+++ packages/ipy-utils/ipy-utils-0.0.2.tar.gz/ipy-utils-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-utils",
+    version="0.0.2",
+    packages=["ipy-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
