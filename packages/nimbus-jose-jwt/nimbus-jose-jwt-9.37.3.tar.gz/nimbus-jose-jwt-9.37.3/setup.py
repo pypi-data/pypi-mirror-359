45ad748f0d7e2fcfef58780899da686e16db0ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package nimbus-jose-jwt/9.37.3/nimbus-jose-jwt-9.37.3.tar.gz

---
+++ packages/nimbus-jose-jwt/nimbus-jose-jwt-9.37.3.tar.gz/nimbus-jose-jwt-9.37.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nimbus-jose-jwt",
+    version="9.37.3",
+    packages=["nimbus-jose-jwt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
