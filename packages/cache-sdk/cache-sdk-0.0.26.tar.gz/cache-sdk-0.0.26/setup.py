1a78715a0de3740a6a9cb90a654d26c2e671b935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:33 +0000

    Add package cache-sdk/0.0.26/cache-sdk-0.0.26.tar.gz

---
+++ packages/cache-sdk/cache-sdk-0.0.26.tar.gz/cache-sdk-0.0.26/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cache-sdk",
+    version="0.0.26",
+    packages=["cache-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
