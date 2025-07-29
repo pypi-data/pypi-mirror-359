834c7718bc0b264b5fdd353485c1fffa9bc1f3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package vkcloud/0.2.17.1/vkcloud-0.2.17.1.tar.gz

---
+++ packages/vkcloud/vkcloud-0.2.17.1.tar.gz/vkcloud-0.2.17.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkcloud",
+    version="0.2.17.1",
+    packages=["vkcloud"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
