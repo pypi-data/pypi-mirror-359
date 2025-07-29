57f3638655199eaaed1f43fb7bf0606743332680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:33 +0000

    Add package mlserv/0.0.1/mlserv-0.0.1.tar.gz

---
+++ packages/mlserv/mlserv-0.0.1.tar.gz/mlserv-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mlserv",
+    version="0.0.1",
+    packages=["mlserv"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
