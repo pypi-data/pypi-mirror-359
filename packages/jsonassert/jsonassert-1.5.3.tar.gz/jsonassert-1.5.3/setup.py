9521556ff7653c4085d6ba7baa6a47961d8b5dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package jsonassert/1.5.3/jsonassert-1.5.3.tar.gz

---
+++ packages/jsonassert/jsonassert-1.5.3.tar.gz/jsonassert-1.5.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jsonassert",
+    version="1.5.3",
+    packages=["jsonassert"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
