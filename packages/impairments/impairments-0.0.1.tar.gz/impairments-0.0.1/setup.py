036a6ee880d27700b2238d4ffc54b1c4fb2fbf66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package impairments/0.0.1/impairments-0.0.1.tar.gz

---
+++ packages/impairments/impairments-0.0.1.tar.gz/impairments-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="impairments",
+    version="0.0.1",
+    packages=["impairments"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
