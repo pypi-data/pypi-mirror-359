cabe5921c743c47e82e7c010b4ed5ce44562b817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package test-data-specs/0.0.1/test-data-specs-0.0.1.tar.gz

---
+++ packages/test-data-specs/test-data-specs-0.0.1.tar.gz/test-data-specs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-data-specs",
+    version="0.0.1",
+    packages=["test-data-specs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
