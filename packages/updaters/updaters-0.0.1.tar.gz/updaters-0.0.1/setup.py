c3cfc91f65843c1f37f58ca4d1b0f3c292cf5af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package updaters/0.0.1/updaters-0.0.1.tar.gz

---
+++ packages/updaters/updaters-0.0.1.tar.gz/updaters-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="updaters",
+    version="0.0.1",
+    packages=["updaters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
