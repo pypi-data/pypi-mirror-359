a9d738015d93a7a38e7f2e78efb4fa47ef0048ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package reasoned-opinion/0.0.1/reasoned-opinion-0.0.1.tar.gz

---
+++ packages/reasoned-opinion/reasoned-opinion-0.0.1.tar.gz/reasoned-opinion-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="reasoned-opinion",
+    version="0.0.1",
+    packages=["reasoned-opinion"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
