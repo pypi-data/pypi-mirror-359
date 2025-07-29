1be56ac971ae3a25a10b6b40ac77cb33682923c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package json-smart/2.5.1/json-smart-2.5.1.tar.gz

---
+++ packages/json-smart/json-smart-2.5.1.tar.gz/json-smart-2.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="json-smart",
+    version="2.5.1",
+    packages=["json-smart"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
