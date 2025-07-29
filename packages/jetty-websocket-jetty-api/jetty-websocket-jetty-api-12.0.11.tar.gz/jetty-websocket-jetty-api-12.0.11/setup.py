d9a42b2e4608c778473bcabf354c6547eb0181f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:49 +0000

    Add package jetty-websocket-jetty-api/12.0.11/jetty-websocket-jetty-api-12.0.11.tar.gz

---
+++ packages/jetty-websocket-jetty-api/jetty-websocket-jetty-api-12.0.11.tar.gz/jetty-websocket-jetty-api-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-websocket-jetty-api",
+    version="12.0.11",
+    packages=["jetty-websocket-jetty-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
