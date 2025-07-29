cbcef28e82782d9723d20b784e14f50963689ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package tests-smoke/0.0.1/tests-smoke-0.0.1.tar.gz

---
+++ packages/tests-smoke/tests-smoke-0.0.1.tar.gz/tests-smoke-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tests-smoke",
+    version="0.0.1",
+    packages=["tests-smoke"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
