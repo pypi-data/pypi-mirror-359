beb553efe0d6628d5fcf09f3f03388fe4698ed62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package imageio-metadata/3.11.0/imageio-metadata-3.11.0.tar.gz

---
+++ packages/imageio-metadata/imageio-metadata-3.11.0.tar.gz/imageio-metadata-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-metadata",
+    version="3.11.0",
+    packages=["imageio-metadata"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
