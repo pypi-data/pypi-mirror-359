140f4a460eeee30c78ed05d2b09c40446209ef87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package karborclient/0.0.1/karborclient-0.0.1.tar.gz

---
+++ packages/karborclient/karborclient-0.0.1.tar.gz/karborclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="karborclient",
+    version="0.0.1",
+    packages=["karborclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
