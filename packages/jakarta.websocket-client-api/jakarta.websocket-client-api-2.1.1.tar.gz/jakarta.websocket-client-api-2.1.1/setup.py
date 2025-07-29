cd61919ce91fdbca2b891a32a92b8c97db8acf75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:12 +0000

    Add package jakarta.websocket-client-api/2.1.1/jakarta.websocket-client-api-2.1.1.tar.gz

---
+++ packages/jakarta.websocket-client-api/jakarta.websocket-client-api-2.1.1.tar.gz/jakarta.websocket-client-api-2.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jakarta.websocket-client-api",
+    version="2.1.1",
+    packages=["jakarta.websocket-client-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
