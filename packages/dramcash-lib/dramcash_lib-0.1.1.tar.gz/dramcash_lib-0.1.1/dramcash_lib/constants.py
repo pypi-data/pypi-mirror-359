1297a57e766368ed850840e4412ca49912ac4bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:32 +0000

    Add package dramcash-lib/0.1.1/dramcash_lib-0.1.1.tar.gz

---
+++ packages/dramcash-lib/dramcash_lib-0.1.1.tar.gz/dramcash_lib-0.1.1/dramcash_lib/constants.py
@@ -0,0 +1,6 @@
+# Define custom constant messages
+class Messages:
+    NOT_FOUND = "Resource not found"
+    INTERNAL_SERVER_ERROR = "Internal server error occurred"
+    BAD_REQUEST = "Invalid request data"
+    UNAUTHORIZED = "You are not authorized to access this resource"
