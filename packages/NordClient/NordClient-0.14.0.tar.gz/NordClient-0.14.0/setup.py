424304940fdff79af9f3acea458b92c0a47868ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package NordClient/0.14.0/NordClient-0.14.0.tar.gz

---
+++ packages/NordClient/NordClient-0.14.0.tar.gz/NordClient-0.14.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="NordClient",
+    version="0.14.0",
+    packages=["NordClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
