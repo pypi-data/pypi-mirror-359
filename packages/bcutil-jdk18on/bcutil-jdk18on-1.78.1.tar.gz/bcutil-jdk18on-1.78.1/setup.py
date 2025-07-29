e0d672ca5eaf152903a93dcbd353a59b294d66c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package bcutil-jdk18on/1.78.1/bcutil-jdk18on-1.78.1.tar.gz

---
+++ packages/bcutil-jdk18on/bcutil-jdk18on-1.78.1.tar.gz/bcutil-jdk18on-1.78.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bcutil-jdk18on",
+    version="1.78.1",
+    packages=["bcutil-jdk18on"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
