b8e04e1885c16d0f3949a4f5bae3e58c8d8956ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package spring-orm/6.1.11/spring-orm-6.1.11.tar.gz

---
+++ packages/spring-orm/spring-orm-6.1.11.tar.gz/spring-orm-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-orm",
+    version="6.1.11",
+    packages=["spring-orm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
