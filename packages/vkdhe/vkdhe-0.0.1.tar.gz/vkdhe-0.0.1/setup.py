a0bf9d4f522adafb08f9d6d25380b9a39565c521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package vkdhe/0.0.1/vkdhe-0.0.1.tar.gz

---
+++ packages/vkdhe/vkdhe-0.0.1.tar.gz/vkdhe-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkdhe",
+    version="0.0.1",
+    packages=["vkdhe"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
