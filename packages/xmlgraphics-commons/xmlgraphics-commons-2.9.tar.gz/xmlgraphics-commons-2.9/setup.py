0e2e2fc9fcb6d9614d4086e20bf47dbb18da7ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:52 +0000

    Add package xmlgraphics-commons/2.9/xmlgraphics-commons-2.9.tar.gz

---
+++ packages/xmlgraphics-commons/xmlgraphics-commons-2.9.tar.gz/xmlgraphics-commons-2.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xmlgraphics-commons",
+    version="2.9",
+    packages=["xmlgraphics-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
