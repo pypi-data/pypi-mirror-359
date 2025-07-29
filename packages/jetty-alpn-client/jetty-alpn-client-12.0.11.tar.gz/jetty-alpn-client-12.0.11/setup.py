3e03b9885dad662bb235ff1a5b35c64791daca77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package jetty-alpn-client/12.0.11/jetty-alpn-client-12.0.11.tar.gz

---
+++ packages/jetty-alpn-client/jetty-alpn-client-12.0.11.tar.gz/jetty-alpn-client-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-alpn-client",
+    version="12.0.11",
+    packages=["jetty-alpn-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
