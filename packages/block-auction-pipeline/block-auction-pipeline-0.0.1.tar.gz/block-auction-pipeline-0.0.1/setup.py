adcf06747f833802003319a5678868a36031bb03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package block-auction-pipeline/0.0.1/block-auction-pipeline-0.0.1.tar.gz

---
+++ packages/block-auction-pipeline/block-auction-pipeline-0.0.1.tar.gz/block-auction-pipeline-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="block-auction-pipeline",
+    version="0.0.1",
+    packages=["block-auction-pipeline"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
