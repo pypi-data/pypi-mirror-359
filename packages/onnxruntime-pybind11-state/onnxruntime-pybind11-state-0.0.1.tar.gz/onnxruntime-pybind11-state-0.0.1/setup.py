2b54c7bc1c69a3ff731a48b6c1ceccba11f677fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package onnxruntime-pybind11-state/0.0.1/onnxruntime-pybind11-state-0.0.1.tar.gz

---
+++ packages/onnxruntime-pybind11-state/onnxruntime-pybind11-state-0.0.1.tar.gz/onnxruntime-pybind11-state-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="onnxruntime-pybind11-state",
+    version="0.0.1",
+    packages=["onnxruntime-pybind11-state"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
