5fb111345997e1fed63677c0deebcf471f90e10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-connector-chyt/0.0.1/dl-connector-chyt-0.0.1.tar.gz

---
+++ packages/dl-connector-chyt/dl-connector-chyt-0.0.1.tar.gz/dl-connector-chyt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-chyt",
+    version="0.0.1",
+    packages=["dl-connector-chyt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
