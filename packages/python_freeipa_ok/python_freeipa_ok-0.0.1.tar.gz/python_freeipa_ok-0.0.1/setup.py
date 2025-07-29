3b865887eef2d791f6f05bd39d1a6cf778139fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package python_freeipa_ok/0.0.1/python_freeipa_ok-0.0.1.tar.gz

---
+++ packages/python_freeipa_ok/python_freeipa_ok-0.0.1.tar.gz/python_freeipa_ok-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python_freeipa_ok",
+    version="0.0.1",
+    packages=["python_freeipa_ok"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
