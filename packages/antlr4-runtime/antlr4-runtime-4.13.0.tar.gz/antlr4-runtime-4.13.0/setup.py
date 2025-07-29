272168319e1c38d6717c69157bd31565c8beefd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:47 +0000

    Add package antlr4-runtime/4.13.0/antlr4-runtime-4.13.0.tar.gz

---
+++ packages/antlr4-runtime/antlr4-runtime-4.13.0.tar.gz/antlr4-runtime-4.13.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="antlr4-runtime",
+    version="4.13.0",
+    packages=["antlr4-runtime"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
