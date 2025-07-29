4ac952abb712e6217a829c5262185407c147482d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package libtbb.so.2/0.0.1/libtbb.so.2-0.0.1.tar.gz

---
+++ packages/libtbb.so.2/libtbb.so.2-0.0.1.tar.gz/libtbb.so.2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libtbb.so.2",
+    version="0.0.1",
+    packages=["libtbb.so.2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
