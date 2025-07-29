444c0677fbddbec16f69c1190cade8fe5adff360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:18 +0000

    Add package asm-commons/9.7/asm-commons-9.7.tar.gz

---
+++ packages/asm-commons/asm-commons-9.7.tar.gz/asm-commons-9.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asm-commons",
+    version="9.7",
+    packages=["asm-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
