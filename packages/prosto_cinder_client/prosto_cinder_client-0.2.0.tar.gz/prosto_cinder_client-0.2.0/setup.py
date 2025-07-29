0e3af4650a8625643695450780892308ea69c658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:04 +0000

    Add package prosto_cinder_client/0.2.0/prosto_cinder_client-0.2.0.tar.gz

---
+++ packages/prosto_cinder_client/prosto_cinder_client-0.2.0.tar.gz/prosto_cinder_client-0.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prosto_cinder_client",
+    version="0.2.0",
+    packages=["prosto_cinder_client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
