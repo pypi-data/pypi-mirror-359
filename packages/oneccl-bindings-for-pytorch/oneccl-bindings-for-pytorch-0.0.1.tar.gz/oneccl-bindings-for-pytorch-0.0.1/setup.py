db925cb8cc8958edfdd17fd21c3413d2c2703d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:39 +0000

    Add package oneccl-bindings-for-pytorch/0.0.1/oneccl-bindings-for-pytorch-0.0.1.tar.gz

---
+++ packages/oneccl-bindings-for-pytorch/oneccl-bindings-for-pytorch-0.0.1.tar.gz/oneccl-bindings-for-pytorch-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oneccl-bindings-for-pytorch",
+    version="0.0.1",
+    packages=["oneccl-bindings-for-pytorch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
