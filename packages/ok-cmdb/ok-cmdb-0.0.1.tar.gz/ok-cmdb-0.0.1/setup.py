5391a00baf570d17d6567e955817b74f766b8061
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:07 +0000

    Add package ok-cmdb/0.0.1/ok-cmdb-0.0.1.tar.gz

---
+++ packages/ok-cmdb/ok-cmdb-0.0.1.tar.gz/ok-cmdb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok-cmdb",
+    version="0.0.1",
+    packages=["ok-cmdb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
