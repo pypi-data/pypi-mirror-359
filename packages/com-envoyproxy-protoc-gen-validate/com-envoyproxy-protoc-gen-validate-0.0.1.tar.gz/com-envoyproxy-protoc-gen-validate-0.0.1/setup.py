c0a95b58d54a5abb77ad64d80224f8171b6508ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package com-envoyproxy-protoc-gen-validate/0.0.1/com-envoyproxy-protoc-gen-validate-0.0.1.tar.gz

---
+++ packages/com-envoyproxy-protoc-gen-validate/com-envoyproxy-protoc-gen-validate-0.0.1.tar.gz/com-envoyproxy-protoc-gen-validate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="com-envoyproxy-protoc-gen-validate",
+    version="0.0.1",
+    packages=["com-envoyproxy-protoc-gen-validate"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
