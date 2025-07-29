775ba811e380cfbe07a308f0d199bd15036e59c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package bigdata-utils/0.0.1/bigdata-utils-0.0.1.tar.gz

---
+++ packages/bigdata-utils/bigdata-utils-0.0.1.tar.gz/bigdata-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bigdata-utils",
+    version="0.0.1",
+    packages=["bigdata-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
