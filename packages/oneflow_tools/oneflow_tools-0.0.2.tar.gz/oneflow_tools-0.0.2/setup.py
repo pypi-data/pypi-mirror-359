0b334bd71842a73ac2d37d483401327f48403a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package oneflow_tools/0.0.2/oneflow_tools-0.0.2.tar.gz

---
+++ packages/oneflow_tools/oneflow_tools-0.0.2.tar.gz/oneflow_tools-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oneflow_tools",
+    version="0.0.2",
+    packages=["oneflow_tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
