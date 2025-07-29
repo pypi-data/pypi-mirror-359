2f5f1fe04468d6b1d03274eee46b539c127d6e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package tarantool16-snapshot/0.0.1/tarantool16-snapshot-0.0.1.tar.gz

---
+++ packages/tarantool16-snapshot/tarantool16-snapshot-0.0.1.tar.gz/tarantool16-snapshot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tarantool16-snapshot",
+    version="0.0.1",
+    packages=["tarantool16-snapshot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
