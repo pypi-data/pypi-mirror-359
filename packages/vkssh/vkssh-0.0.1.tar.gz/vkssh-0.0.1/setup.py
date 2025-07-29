aadccbbc6b92c0e6774aaeea9639c0ddc49da7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package vkssh/0.0.1/vkssh-0.0.1.tar.gz

---
+++ packages/vkssh/vkssh-0.0.1.tar.gz/vkssh-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkssh",
+    version="0.0.1",
+    packages=["vkssh"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
