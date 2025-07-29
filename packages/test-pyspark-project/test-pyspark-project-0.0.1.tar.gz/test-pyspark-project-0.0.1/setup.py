ccf19c302537e47bd89c95306c1becf455c79199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package test-pyspark-project/0.0.1/test-pyspark-project-0.0.1.tar.gz

---
+++ packages/test-pyspark-project/test-pyspark-project-0.0.1.tar.gz/test-pyspark-project-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="test-pyspark-project",
+    version="0.0.1",
+    packages=["test-pyspark-project"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
