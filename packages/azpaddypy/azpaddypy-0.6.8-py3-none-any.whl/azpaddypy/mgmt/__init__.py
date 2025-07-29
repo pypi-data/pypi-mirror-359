0146bec68b2a82fb1e451485577b25b45c87d3f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:07:41 +0000

    Add package azpaddypy/0.6.8/azpaddypy-0.6.8-py3-none-any.whl

---
+++ packages/azpaddypy/azpaddypy-0.6.8-py3-none-any.whl/azpaddypy/mgmt/__init__.py
@@ -0,0 +1,16 @@
+"""
+AzPaddyPy - A standardized Python package for Azure cloud services integration.
+"""
+
+__version__ = "0.1.0"
+
+from azpaddypy.mgmt.logging import AzureLogger
+from azpaddypy.mgmt.identity import AzureIdentity
+from azpaddypy.mgmt.local_env_manager import LocalDevelopmentSettings, create_local_env_manager
+
+__all__ = [
+    "AzureLogger", 
+    "AzureIdentity",
+    "LocalDevelopmentSettings",
+    "create_local_env_manager",
+]
