ae210db599d6a115943046a79da6b2532c97a71a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package work-schedule/0.0.1/work-schedule-0.0.1.tar.gz

---
+++ packages/work-schedule/work-schedule-0.0.1.tar.gz/work-schedule-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="work-schedule",
+    version="0.0.1",
+    packages=["work-schedule"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
