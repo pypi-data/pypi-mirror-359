423083fe69a6cbb83237de477c4b1c5985ab1467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:48 +0000

    Add package jetty-websocket-core-server/12.0.11/jetty-websocket-core-server-12.0.11.tar.gz

---
+++ packages/jetty-websocket-core-server/jetty-websocket-core-server-12.0.11.tar.gz/jetty-websocket-core-server-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-websocket-core-server",
+    version="12.0.11",
+    packages=["jetty-websocket-core-server"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
