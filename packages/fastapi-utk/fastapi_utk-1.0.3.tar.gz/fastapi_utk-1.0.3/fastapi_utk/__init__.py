b264d12ff75d125b1d90a067a751c7e2a75678a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:39 +0000

    Add package fastapi-utk/1.0.3/fastapi_utk-1.0.3.tar.gz

---
+++ packages/fastapi-utk/fastapi_utk-1.0.3.tar.gz/fastapi_utk-1.0.3/fastapi_utk/__init__.py
@@ -0,0 +1,14 @@
+from .not_set import NotSet
+from .pagination import Paginated, Pagination, Paginator
+from .sorting import Sorting, SortingOption
+
+__all__ = [
+    "Pagination",
+    "Paginator",
+    "Paginated",
+    "Sorting",
+    "SortingOption",
+    "NotSet",
+]
+
+__version__ = "1.0.3"
---
+++ packages/fastapi-utk/fastapi_utk-1.0.3.tar.gz/fastapi_utk-1.0.3/fastapi_utk/pagination/response.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, HttpUrl
+from pydantic.alias_generators import to_camel
+
+__all__ = [
+    "PaginationInfo",
+    "Paginated",
+]
+
+
+class PaginationInfo(BaseModel):
+    page: int
+    page_size: int
+    total_pages: int | None = None
+    total: int | None = None
+    next_page: HttpUrl | None = None
+    prev_page: HttpUrl | None = None
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
+        use_enum_values = True
+        alias_generator = to_camel
+
+
+class Paginated[T: BaseModel](BaseModel):
+    data: list[T]
+    pagination: PaginationInfo
+
+    class Config:
+        from_attributes = True
+        populate_by_name = True
+        use_enum_values = True
+        alias_generator = to_camel
