b00b898fe09002a319865d0486ada1ae06056ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:23 +0000

    Add package core-python-app/0.0.1/core-python-app-0.0.1.tar.gz

---
+++ packages/core-python-app/core-python-app-0.0.1.tar.gz/core-python-app-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="core-python-app",
+    version="0.0.1",
+    packages=["core-python-app"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
