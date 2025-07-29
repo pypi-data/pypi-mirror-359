226249bd17c26183184af4d60e489c0eb61c6e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:10 +0000

    Add package spring-boot-starter-web/3.3.2/spring-boot-starter-web-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-web/spring-boot-starter-web-3.3.2.tar.gz/spring-boot-starter-web-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-web",
+    version="3.3.2",
+    packages=["spring-boot-starter-web"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
