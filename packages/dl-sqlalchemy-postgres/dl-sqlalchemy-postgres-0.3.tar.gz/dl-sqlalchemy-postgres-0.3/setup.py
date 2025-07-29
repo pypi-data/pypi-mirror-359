c574b23bc574a2475e8b5809f2d5686281b631a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-postgres/0.3/dl-sqlalchemy-postgres-0.3.tar.gz

---
+++ packages/dl-sqlalchemy-postgres/dl-sqlalchemy-postgres-0.3.tar.gz/dl-sqlalchemy-postgres-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-postgres",
+    version="0.3",
+    packages=["dl-sqlalchemy-postgres"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
