75b377f15914d2920219988c25e2912fd9abdb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-store-upload-gplay/0.0.1/ipy-store-upload-gplay-0.0.1.tar.gz

---
+++ packages/ipy-store-upload-gplay/ipy-store-upload-gplay-0.0.1.tar.gz/ipy-store-upload-gplay-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-store-upload-gplay",
+    version="0.0.1",
+    packages=["ipy-store-upload-gplay"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
