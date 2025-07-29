921dbd094e2357517259d3d5abd8eb990460478e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:55 +0000

    Add package resource-management/0.0.1/resource-management-0.0.1.tar.gz

---
+++ packages/resource-management/resource-management-0.0.1.tar.gz/resource-management-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="resource-management",
+    version="0.0.1",
+    packages=["resource-management"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
