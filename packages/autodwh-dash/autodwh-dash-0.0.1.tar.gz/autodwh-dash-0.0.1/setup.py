04dc3970d72b8f92ec995da01c63f4cb4fa13cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package autodwh-dash/0.0.1/autodwh-dash-0.0.1.tar.gz

---
+++ packages/autodwh-dash/autodwh-dash-0.0.1.tar.gz/autodwh-dash-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autodwh-dash",
+    version="0.0.1",
+    packages=["autodwh-dash"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
