6da355c343f7bc7224c03757ee2ed8cbdd368344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package cjkcodecs/0.0.1/cjkcodecs-0.0.1.tar.gz

---
+++ packages/cjkcodecs/cjkcodecs-0.0.1.tar.gz/cjkcodecs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cjkcodecs",
+    version="0.0.1",
+    packages=["cjkcodecs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
