47127a2e41b0136160048bc36e4fe888458cabbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-connector-clickhouse/0.0.1/dl-connector-clickhouse-0.0.1.tar.gz

---
+++ packages/dl-connector-clickhouse/dl-connector-clickhouse-0.0.1.tar.gz/dl-connector-clickhouse-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-clickhouse",
+    version="0.0.1",
+    packages=["dl-connector-clickhouse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
