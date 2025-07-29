5c7e07fa20c7bfad1dac38751358bfc9934b5b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package srv-spotter/0.0.1/srv-spotter-0.0.1.tar.gz

---
+++ packages/srv-spotter/srv-spotter-0.0.1.tar.gz/srv-spotter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="srv-spotter",
+    version="0.0.1",
+    packages=["srv-spotter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
