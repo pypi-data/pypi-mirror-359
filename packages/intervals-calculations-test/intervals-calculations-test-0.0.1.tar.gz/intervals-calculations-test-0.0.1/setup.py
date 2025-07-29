dfa9f43a17048de797ca95797098222466e4dc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package intervals-calculations-test/0.0.1/intervals-calculations-test-0.0.1.tar.gz

---
+++ packages/intervals-calculations-test/intervals-calculations-test-0.0.1.tar.gz/intervals-calculations-test-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="intervals-calculations-test",
+    version="0.0.1",
+    packages=["intervals-calculations-test"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
