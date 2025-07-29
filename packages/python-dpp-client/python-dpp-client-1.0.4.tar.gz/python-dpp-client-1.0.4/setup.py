9143efd4ef5dc021894f5b7a0e716a35d03d7d62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:02 +0000

    Add package python-dpp-client/1.0.4/python-dpp-client-1.0.4.tar.gz

---
+++ packages/python-dpp-client/python-dpp-client-1.0.4.tar.gz/python-dpp-client-1.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="python-dpp-client",
+    version="1.0.4",
+    packages=["python-dpp-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
