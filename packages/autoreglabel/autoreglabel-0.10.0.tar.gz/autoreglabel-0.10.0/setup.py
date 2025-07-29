8ceee7bdb91bcc48c4f42c65e6fba8de2473cd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package autoreglabel/0.10.0/autoreglabel-0.10.0.tar.gz

---
+++ packages/autoreglabel/autoreglabel-0.10.0.tar.gz/autoreglabel-0.10.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autoreglabel",
+    version="0.10.0",
+    packages=["autoreglabel"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
