805ff9e2ac2a9e4a598eaa9bdc219742b674d3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-core/2.17.2/jackson-core-2.17.2.tar.gz

---
+++ packages/jackson-core/jackson-core-2.17.2.tar.gz/jackson-core-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-core",
+    version="2.17.2",
+    packages=["jackson-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
