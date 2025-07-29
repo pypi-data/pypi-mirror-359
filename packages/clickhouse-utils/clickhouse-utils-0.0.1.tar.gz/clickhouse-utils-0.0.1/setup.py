8010ecc7b2fac39dedb11190fdb168930bd566f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:51 +0000

    Add package clickhouse-utils/0.0.1/clickhouse-utils-0.0.1.tar.gz

---
+++ packages/clickhouse-utils/clickhouse-utils-0.0.1.tar.gz/clickhouse-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="clickhouse-utils",
+    version="0.0.1",
+    packages=["clickhouse-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
