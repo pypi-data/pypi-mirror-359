7e0b2175dcdd3dbbc4ced2e2c8513465f09e16b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package kiota-http/0.0.1/kiota-http-0.0.1.tar.gz

---
+++ packages/kiota-http/kiota-http-0.0.1.tar.gz/kiota-http-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kiota-http",
+    version="0.0.1",
+    packages=["kiota-http"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
