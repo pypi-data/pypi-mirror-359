e31c06597c7b6282772a4986ed07ea04586b9b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-chyt/0.7/dl-sqlalchemy-chyt-0.7.tar.gz

---
+++ packages/dl-sqlalchemy-chyt/dl-sqlalchemy-chyt-0.7.tar.gz/dl-sqlalchemy-chyt-0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-chyt",
+    version="0.7",
+    packages=["dl-sqlalchemy-chyt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
