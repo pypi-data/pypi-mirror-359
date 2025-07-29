0ae578270e2a208c5b4bd01e67aee2ec38fd0e44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package rest-framework-yaml/0.0.1/rest-framework-yaml-0.0.1.tar.gz

---
+++ packages/rest-framework-yaml/rest-framework-yaml-0.0.1.tar.gz/rest-framework-yaml-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-yaml",
+    version="0.0.1",
+    packages=["rest-framework-yaml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
