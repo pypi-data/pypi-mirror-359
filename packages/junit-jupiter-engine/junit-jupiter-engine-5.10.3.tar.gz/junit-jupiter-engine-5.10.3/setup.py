ae95ff33735b1b167ba988e2436ea68197699e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package junit-jupiter-engine/5.10.3/junit-jupiter-engine-5.10.3.tar.gz

---
+++ packages/junit-jupiter-engine/junit-jupiter-engine-5.10.3.tar.gz/junit-jupiter-engine-5.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-jupiter-engine",
+    version="5.10.3",
+    packages=["junit-jupiter-engine"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
