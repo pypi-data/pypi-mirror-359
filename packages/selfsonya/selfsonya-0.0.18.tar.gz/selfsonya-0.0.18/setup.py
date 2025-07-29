2513149407538af857c6d74579c1581c581f8833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:48 +0000

    Add package selfsonya/0.0.18/selfsonya-0.0.18.tar.gz

---
+++ packages/selfsonya/selfsonya-0.0.18.tar.gz/selfsonya-0.0.18/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="selfsonya",
+    version="0.0.18",
+    packages=["selfsonya"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
