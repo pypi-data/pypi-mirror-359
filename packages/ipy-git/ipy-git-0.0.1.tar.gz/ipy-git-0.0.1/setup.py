4508359040245c4246fd2b9bbbc5eab13610a545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-git/0.0.1/ipy-git-0.0.1.tar.gz

---
+++ packages/ipy-git/ipy-git-0.0.1.tar.gz/ipy-git-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-git",
+    version="0.0.1",
+    packages=["ipy-git"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
