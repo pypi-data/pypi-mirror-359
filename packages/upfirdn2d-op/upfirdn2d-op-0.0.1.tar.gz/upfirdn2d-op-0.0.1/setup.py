3162cdf8286ef3231ebd7f267d64578df6065d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package upfirdn2d-op/0.0.1/upfirdn2d-op-0.0.1.tar.gz

---
+++ packages/upfirdn2d-op/upfirdn2d-op-0.0.1.tar.gz/upfirdn2d-op-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="upfirdn2d-op",
+    version="0.0.1",
+    packages=["upfirdn2d-op"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
