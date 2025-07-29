a6a83b8886b79edf0b3a2508e3dcc4a0bbe4a07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package mockito-inline/5.2.0/mockito-inline-5.2.0.tar.gz

---
+++ packages/mockito-inline/mockito-inline-5.2.0.tar.gz/mockito-inline-5.2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mockito-inline",
+    version="5.2.0",
+    packages=["mockito-inline"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
