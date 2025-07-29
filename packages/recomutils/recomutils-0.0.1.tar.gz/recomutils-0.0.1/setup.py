c6a47ad9dd6ee607316179b4ba65534cdb66da1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package recomutils/0.0.1/recomutils-0.0.1.tar.gz

---
+++ packages/recomutils/recomutils-0.0.1.tar.gz/recomutils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="recomutils",
+    version="0.0.1",
+    packages=["recomutils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
