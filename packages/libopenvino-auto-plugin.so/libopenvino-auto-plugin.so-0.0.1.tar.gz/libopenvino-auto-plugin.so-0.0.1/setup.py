8c0f5727caf8b225d7eb8cb9c247a32834cbf5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:52 +0000

    Add package libopenvino-auto-plugin.so/0.0.1/libopenvino-auto-plugin.so-0.0.1.tar.gz

---
+++ packages/libopenvino-auto-plugin.so/libopenvino-auto-plugin.so-0.0.1.tar.gz/libopenvino-auto-plugin.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libopenvino-auto-plugin.so",
+    version="0.0.1",
+    packages=["libopenvino-auto-plugin.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
