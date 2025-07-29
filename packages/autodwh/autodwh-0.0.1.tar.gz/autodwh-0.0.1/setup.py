38ba9c62bc5d8cb6f591b5309c7dc78159faa430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package autodwh/0.0.1/autodwh-0.0.1.tar.gz

---
+++ packages/autodwh/autodwh-0.0.1.tar.gz/autodwh-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="autodwh",
+    version="0.0.1",
+    packages=["autodwh"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
