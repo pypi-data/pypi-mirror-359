501a9d2e7311462c5f2cf84db8b2bc7356798b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package scheduler_failover_controller/1.0.8/scheduler_failover_controller-1.0.8.tar.gz

---
+++ packages/scheduler_failover_controller/scheduler_failover_controller-1.0.8.tar.gz/scheduler_failover_controller-1.0.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scheduler_failover_controller",
+    version="1.0.8",
+    packages=["scheduler_failover_controller"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
