9037f0b6e1f1fd86c75efe8fc20c49cf456ff3e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-auth-api-lib/0.0.1/dl-auth-api-lib-0.0.1.tar.gz

---
+++ packages/dl-auth-api-lib/dl-auth-api-lib-0.0.1.tar.gz/dl-auth-api-lib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-auth-api-lib",
+    version="0.0.1",
+    packages=["dl-auth-api-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
