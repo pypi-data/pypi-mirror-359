afe35c09a82cdb3675ed59da356ec0a67a7b4237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-tx/6.1.11/spring-tx-6.1.11.tar.gz

---
+++ packages/spring-tx/spring-tx-6.1.11.tar.gz/spring-tx-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-tx",
+    version="6.1.11",
+    packages=["spring-tx"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
