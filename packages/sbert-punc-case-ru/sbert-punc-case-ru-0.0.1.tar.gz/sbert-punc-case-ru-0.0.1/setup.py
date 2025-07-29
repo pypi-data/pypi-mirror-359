a747b73ec02b1c47ef2cf2f9040545479157cafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:51 +0000

    Add package sbert-punc-case-ru/0.0.1/sbert-punc-case-ru-0.0.1.tar.gz

---
+++ packages/sbert-punc-case-ru/sbert-punc-case-ru-0.0.1.tar.gz/sbert-punc-case-ru-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sbert-punc-case-ru",
+    version="0.0.1",
+    packages=["sbert-punc-case-ru"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
