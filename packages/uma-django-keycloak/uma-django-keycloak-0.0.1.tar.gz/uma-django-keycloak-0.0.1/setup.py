b9a84fbc12c7a3ce6e8c012e2ae2fbde2fb56d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package uma-django-keycloak/0.0.1/uma-django-keycloak-0.0.1.tar.gz

---
+++ packages/uma-django-keycloak/uma-django-keycloak-0.0.1.tar.gz/uma-django-keycloak-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="uma-django-keycloak",
+    version="0.0.1",
+    packages=["uma-django-keycloak"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
