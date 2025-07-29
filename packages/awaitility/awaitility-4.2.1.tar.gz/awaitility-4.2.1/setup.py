7c993ae7a25d6b47d6df7d890a14f8ff9c9c2799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package awaitility/4.2.1/awaitility-4.2.1.tar.gz

---
+++ packages/awaitility/awaitility-4.2.1.tar.gz/awaitility-4.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="awaitility",
+    version="4.2.1",
+    packages=["awaitility"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
