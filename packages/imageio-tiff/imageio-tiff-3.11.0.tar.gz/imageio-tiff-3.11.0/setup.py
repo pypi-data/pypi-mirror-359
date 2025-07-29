f7c587b74fae27883409192c3b9b6a4295c73fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package imageio-tiff/3.11.0/imageio-tiff-3.11.0.tar.gz

---
+++ packages/imageio-tiff/imageio-tiff-3.11.0.tar.gz/imageio-tiff-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-tiff",
+    version="3.11.0",
+    packages=["imageio-tiff"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
