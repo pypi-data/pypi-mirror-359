333881b99e205f2cae40b4e5bf1ae7fb6be08643
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package spring-expression/6.1.11/spring-expression-6.1.11.tar.gz

---
+++ packages/spring-expression/spring-expression-6.1.11.tar.gz/spring-expression-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-expression",
+    version="6.1.11",
+    packages=["spring-expression"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
