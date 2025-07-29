2e4e71e8574c0ea9e70d2eeb4af800c36cbe8c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package accessors-smart/2.5.1/accessors-smart-2.5.1.tar.gz

---
+++ packages/accessors-smart/accessors-smart-2.5.1.tar.gz/accessors-smart-2.5.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="accessors-smart",
+    version="2.5.1",
+    packages=["accessors-smart"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
