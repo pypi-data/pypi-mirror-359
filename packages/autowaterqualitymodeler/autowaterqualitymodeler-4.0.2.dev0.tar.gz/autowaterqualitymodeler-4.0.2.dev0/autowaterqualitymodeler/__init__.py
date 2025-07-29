bebe1cb79f444a30c8b8a6dfd1468d8a5f5d3efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:41 +0000

    Add package autowaterqualitymodeler/4.0.2.dev0/autowaterqualitymodeler-4.0.2.dev0.tar.gz

---
+++ packages/autowaterqualitymodeler/autowaterqualitymodeler-4.0.2.dev0.tar.gz/autowaterqualitymodeler-4.0.2.dev0/autowaterqualitymodeler/__init__.py
@@ -0,0 +1,21 @@
+"""
+AutoWaterQualityModeler - 自动水质光谱建模工具
+
+提供了一键式水质建模、预测和评估功能。
+"""
+
+from ._version import __version__
+
+# 导出主要类
+from .core.modeler import AutoWaterQualityModeler
+from .preprocessing.spectrum_processor import SpectrumProcessor
+from .features.calculator import FeatureCalculator
+from .models.builder import ModelBuilder
+
+__all__ = [
+    'AutoWaterQualityModeler',
+    'SpectrumProcessor',
+    'FeatureCalculator',
+    'ModelBuilder',
+    '__version__'
+] 
