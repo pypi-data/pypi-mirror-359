d49f655efbf67dbc69d83c8a1ebce0635a5d1a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package cloudcity-pb2-grpc/0.0.1/cloudcity-pb2-grpc-0.0.1.tar.gz

---
+++ packages/cloudcity-pb2-grpc/cloudcity-pb2-grpc-0.0.1.tar.gz/cloudcity-pb2-grpc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cloudcity-pb2-grpc",
+    version="0.0.1",
+    packages=["cloudcity-pb2-grpc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
