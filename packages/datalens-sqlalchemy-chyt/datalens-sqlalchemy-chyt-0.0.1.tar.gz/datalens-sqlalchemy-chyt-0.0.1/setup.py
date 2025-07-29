f4b1929401d66b5aad9feea693036e5e74572312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:45 +0000

    Add package datalens-sqlalchemy-chyt/0.0.1/datalens-sqlalchemy-chyt-0.0.1.tar.gz

---
+++ packages/datalens-sqlalchemy-chyt/datalens-sqlalchemy-chyt-0.0.1.tar.gz/datalens-sqlalchemy-chyt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-chyt",
+    version="0.0.1",
+    packages=["datalens-sqlalchemy-chyt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
