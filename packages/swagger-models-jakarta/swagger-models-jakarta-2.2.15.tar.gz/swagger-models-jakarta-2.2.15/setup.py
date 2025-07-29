42ed72a78befaa3a9d67dc0a7d3902ee18f79589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:55 +0000

    Add package swagger-models-jakarta/2.2.15/swagger-models-jakarta-2.2.15.tar.gz

---
+++ packages/swagger-models-jakarta/swagger-models-jakarta-2.2.15.tar.gz/swagger-models-jakarta-2.2.15/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="swagger-models-jakarta",
+    version="2.2.15",
+    packages=["swagger-models-jakarta"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
