ed341cffdb5ef00e2a3daea4752ab45b6b661725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package xmpbox/3.0.2/xmpbox-3.0.2.tar.gz

---
+++ packages/xmpbox/xmpbox-3.0.2.tar.gz/xmpbox-3.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="xmpbox",
+    version="3.0.2",
+    packages=["xmpbox"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
