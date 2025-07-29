9ace6e447c49113c5fc33588b3e8dc29394a83e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-module-parameter-names/2.17.2/jackson-module-parameter-names-2.17.2.tar.gz

---
+++ packages/jackson-module-parameter-names/jackson-module-parameter-names-2.17.2.tar.gz/jackson-module-parameter-names-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-module-parameter-names",
+    version="2.17.2",
+    packages=["jackson-module-parameter-names"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
