6638b5e43c0f9f58e7d68e381ef8c1ead82a601c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package asr-phoenix/0.0.1/asr-phoenix-0.0.1.tar.gz

---
+++ packages/asr-phoenix/asr-phoenix-0.0.1.tar.gz/asr-phoenix-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asr-phoenix",
+    version="0.0.1",
+    packages=["asr-phoenix"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
