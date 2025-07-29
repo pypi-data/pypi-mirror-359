39ea29540aba7b67595930c58c8202e3ec353270
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:24 +0000

    Add package scrooge-ra-orm/0.0.1/scrooge-ra-orm-0.0.1.tar.gz

---
+++ packages/scrooge-ra-orm/scrooge-ra-orm-0.0.1.tar.gz/scrooge-ra-orm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-ra-orm",
+    version="0.0.1",
+    packages=["scrooge-ra-orm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
