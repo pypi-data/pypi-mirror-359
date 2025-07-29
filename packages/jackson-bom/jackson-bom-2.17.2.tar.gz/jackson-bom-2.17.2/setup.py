ef52a43eb2e86a23913571afb7176dca363b48ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:11 +0000

    Add package jackson-bom/2.17.2/jackson-bom-2.17.2.tar.gz

---
+++ packages/jackson-bom/jackson-bom-2.17.2.tar.gz/jackson-bom-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-bom",
+    version="2.17.2",
+    packages=["jackson-bom"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
