000423314f8635a41eca55ddfcef484b62d1f346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package logback-classic/1.5.6/logback-classic-1.5.6.tar.gz

---
+++ packages/logback-classic/logback-classic-1.5.6.tar.gz/logback-classic-1.5.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="logback-classic",
+    version="1.5.6",
+    packages=["logback-classic"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
