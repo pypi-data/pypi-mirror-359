018e20e096a32ac86805cdb9bec94241ece4ca08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package process-whole-archive-option/0.0.1/process-whole-archive-option-0.0.1.tar.gz

---
+++ packages/process-whole-archive-option/process-whole-archive-option-0.0.1.tar.gz/process-whole-archive-option-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="process-whole-archive-option",
+    version="0.0.1",
+    packages=["process-whole-archive-option"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
