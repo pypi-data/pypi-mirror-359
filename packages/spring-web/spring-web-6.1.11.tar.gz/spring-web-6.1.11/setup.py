0c9c3ca168996ff864162d0095a5a12ca411cd76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:08 +0000

    Add package spring-web/6.1.11/spring-web-6.1.11.tar.gz

---
+++ packages/spring-web/spring-web-6.1.11.tar.gz/spring-web-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-web",
+    version="6.1.11",
+    packages=["spring-web"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
