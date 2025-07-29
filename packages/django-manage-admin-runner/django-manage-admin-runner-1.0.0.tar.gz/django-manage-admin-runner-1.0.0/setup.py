5d58ff7bcf2a4a6cfd975b5988b6a44cc5794c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:00 +0000

    Add package django-manage-admin-runner/1.0.0/django-manage-admin-runner-1.0.0.tar.gz

---
+++ packages/django-manage-admin-runner/django-manage-admin-runner-1.0.0.tar.gz/django-manage-admin-runner-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="django-manage-admin-runner",
+    version="1.0.0",
+    packages=["django-manage-admin-runner"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
