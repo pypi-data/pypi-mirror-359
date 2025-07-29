104735cf4ae5d5b33e28c42d2b454ac0975e7a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:56 +0000

    Add package tower-license/0.0.1/tower-license-0.0.1.tar.gz

---
+++ packages/tower-license/tower-license-0.0.1.tar.gz/tower-license-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tower-license",
+    version="0.0.1",
+    packages=["tower-license"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
