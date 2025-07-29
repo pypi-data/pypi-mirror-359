01b444375f531748887e8df871346fa8c71840b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:42 +0000

    Add package videosdk-plugins-turn-detector/0.0.18/videosdk_plugins_turn_detector-0.0.18.tar.gz

---
+++ packages/videosdk-plugins-turn-detector/videosdk_plugins_turn_detector-0.0.18.tar.gz/videosdk_plugins_turn_detector-0.0.18/videosdk/plugins/turn_detector/__init__.py
@@ -0,0 +1,4 @@
+from .turn_detector import TurnSenseEOU, pre_download_turn_sense
+from .turn_detector_v2 import TurnDetector, pre_download_model
+
+__all__ = ["TurnSenseEOU", "TurnDetector", "pre_download_turn_sense", "pre_download_model"]
