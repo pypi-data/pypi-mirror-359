a7ab70e54c9d0b2b60757a4b572072004b17c9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:34 +0000

    Add package neuroporn-backend/0.0.1/neuroporn-backend-0.0.1.tar.gz

---
+++ packages/neuroporn-backend/neuroporn-backend-0.0.1.tar.gz/neuroporn-backend-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="neuroporn-backend",
+    version="0.0.1",
+    packages=["neuroporn-backend"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
