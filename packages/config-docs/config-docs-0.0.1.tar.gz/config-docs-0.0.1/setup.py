0c54ff3f24c9919bb311a6ba43b70bfbc1fc02dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:39 +0000

    Add package config-docs/0.0.1/config-docs-0.0.1.tar.gz

---
+++ packages/config-docs/config-docs-0.0.1.tar.gz/config-docs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="config-docs",
+    version="0.0.1",
+    packages=["config-docs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
