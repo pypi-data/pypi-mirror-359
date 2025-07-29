d7c51c622aa65a4476e025d939c94d7395270436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:44 +0000

    Add package apache-airflow-providers-ekp/1.2.1/apache-airflow-providers-ekp-1.2.1.tar.gz

---
+++ packages/apache-airflow-providers-ekp/apache-airflow-providers-ekp-1.2.1.tar.gz/apache-airflow-providers-ekp-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="apache-airflow-providers-ekp",
+    version="1.2.1",
+    packages=["apache-airflow-providers-ekp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
