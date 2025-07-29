d9ddb4867ac5b5190c1b13bf7ac76a0b8b5d82e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package celery-app/0.0.1/celery-app-0.0.1.tar.gz

---
+++ packages/celery-app/celery-app-0.0.1.tar.gz/celery-app-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="celery-app",
+    version="0.0.1",
+    packages=["celery-app"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
