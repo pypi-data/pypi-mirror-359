174a03838685453bdebdec4cc381756ca64e2504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package mras-pys3customcli/1.0.2/mras-pys3customcli-1.0.2.tar.gz

---
+++ packages/mras-pys3customcli/mras-pys3customcli-1.0.2.tar.gz/mras-pys3customcli-1.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pys3customcli",
+    version="1.0.2",
+    packages=["mras-pys3customcli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
