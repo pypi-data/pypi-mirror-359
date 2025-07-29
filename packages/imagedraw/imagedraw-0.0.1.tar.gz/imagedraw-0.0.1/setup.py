fb76df0bfede720149885b02bdaaf9822726eff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:29 +0000

    Add package imagedraw/0.0.1/imagedraw-0.0.1.tar.gz

---
+++ packages/imagedraw/imagedraw-0.0.1.tar.gz/imagedraw-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imagedraw",
+    version="0.0.1",
+    packages=["imagedraw"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
