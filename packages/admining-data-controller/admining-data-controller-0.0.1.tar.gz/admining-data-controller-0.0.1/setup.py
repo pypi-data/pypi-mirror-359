d55913a462ec5f3c44c4ad3f193316e9b1f76c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package admining-data-controller/0.0.1/admining-data-controller-0.0.1.tar.gz

---
+++ packages/admining-data-controller/admining-data-controller-0.0.1.tar.gz/admining-data-controller-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="admining-data-controller",
+    version="0.0.1",
+    packages=["admining-data-controller"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
