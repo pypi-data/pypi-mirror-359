113477f8d7563659c473d3bbcd7a039ca8ca9cb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:24 +0000

    Add package drbdmanage/0.0.1/drbdmanage-0.0.1.tar.gz

---
+++ packages/drbdmanage/drbdmanage-0.0.1.tar.gz/drbdmanage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="drbdmanage",
+    version="0.0.1",
+    packages=["drbdmanage"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
