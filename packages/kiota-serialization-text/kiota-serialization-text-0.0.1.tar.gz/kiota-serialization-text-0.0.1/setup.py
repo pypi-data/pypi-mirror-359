756ae71265177284947a28c73ccc271270016f92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package kiota-serialization-text/0.0.1/kiota-serialization-text-0.0.1.tar.gz

---
+++ packages/kiota-serialization-text/kiota-serialization-text-0.0.1.tar.gz/kiota-serialization-text-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kiota-serialization-text",
+    version="0.0.1",
+    packages=["kiota-serialization-text"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
