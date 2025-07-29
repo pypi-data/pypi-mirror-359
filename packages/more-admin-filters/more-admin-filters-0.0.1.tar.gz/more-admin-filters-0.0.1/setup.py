bb1b8a6e3ec1c0272ea8f053056ab61d02fe93fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package more-admin-filters/0.0.1/more-admin-filters-0.0.1.tar.gz

---
+++ packages/more-admin-filters/more-admin-filters-0.0.1.tar.gz/more-admin-filters-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="more-admin-filters",
+    version="0.0.1",
+    packages=["more-admin-filters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
