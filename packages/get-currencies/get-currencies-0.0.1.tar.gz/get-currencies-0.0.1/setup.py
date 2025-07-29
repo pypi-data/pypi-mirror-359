8dec2fa07dec94069ef7e3779060efa46f7ea9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package get-currencies/0.0.1/get-currencies-0.0.1.tar.gz

---
+++ packages/get-currencies/get-currencies-0.0.1.tar.gz/get-currencies-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="get-currencies",
+    version="0.0.1",
+    packages=["get-currencies"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
