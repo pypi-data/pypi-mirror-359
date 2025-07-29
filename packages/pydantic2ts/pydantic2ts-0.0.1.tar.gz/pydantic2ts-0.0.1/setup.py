0308255b7fcdfc0c1d439672200bba492eee4318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package pydantic2ts/0.0.1/pydantic2ts-0.0.1.tar.gz

---
+++ packages/pydantic2ts/pydantic2ts-0.0.1.tar.gz/pydantic2ts-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pydantic2ts",
+    version="0.0.1",
+    packages=["pydantic2ts"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
