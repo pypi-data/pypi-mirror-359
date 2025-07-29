1c6772923fd4beda84bba3fac8b45b39a474fdf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-snowflake/0.0.1/dl-connector-snowflake-0.0.1.tar.gz

---
+++ packages/dl-connector-snowflake/dl-connector-snowflake-0.0.1.tar.gz/dl-connector-snowflake-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-snowflake",
+    version="0.0.1",
+    packages=["dl-connector-snowflake"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
