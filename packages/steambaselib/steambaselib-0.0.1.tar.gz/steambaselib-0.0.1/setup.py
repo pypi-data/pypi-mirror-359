26ea51c27d5098c98cf773e24cdfe0134aa0e840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:45 +0000

    Add package steambaselib/0.0.1/steambaselib-0.0.1.tar.gz

---
+++ packages/steambaselib/steambaselib-0.0.1.tar.gz/steambaselib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="steambaselib",
+    version="0.0.1",
+    packages=["steambaselib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
