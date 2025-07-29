2226e8778f367b3afbe93c1237b142860af6b5c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-testing/0.0.1/dl-testing-0.0.1.tar.gz

---
+++ packages/dl-testing/dl-testing-0.0.1.tar.gz/dl-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-testing",
+    version="0.0.1",
+    packages=["dl-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
