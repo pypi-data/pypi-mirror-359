5f1c891ba410bbea05c642f13ae2425c5a682657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-desktop-changelog/0.0.1/ipy-desktop-changelog-0.0.1.tar.gz

---
+++ packages/ipy-desktop-changelog/ipy-desktop-changelog-0.0.1.tar.gz/ipy-desktop-changelog-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-desktop-changelog",
+    version="0.0.1",
+    packages=["ipy-desktop-changelog"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
