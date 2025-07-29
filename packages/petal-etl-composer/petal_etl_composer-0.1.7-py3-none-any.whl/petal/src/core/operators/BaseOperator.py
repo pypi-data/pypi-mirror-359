c9e8c35157a2fadee163326943216b460686bc6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:53 +0000

    Add package petal-etl-composer/0.1.7/petal_etl_composer-0.1.7-py3-none-any.whl

---
+++ packages/petal-etl-composer/petal_etl_composer-0.1.7-py3-none-any.whl/petal/src/core/operators/BaseOperator.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from petal.src.core.context import get_current_pipeline
+
+
+class BaseOperator(ABC):
+    def __init__(self, operator_id: str):
+        self.operator_id = operator_id
+
+        # Register self to current DAG
+        pipeline = get_current_pipeline()
+        if pipeline:
+            pipeline.add_node(self)
+
+    def __rshift__(self, other):
+        raise NotImplementedError
+
+    def __lshift__(self, other):
+        raise NotImplementedError
+
+    @abstractmethod
+    def process(self, *inputs: Any) -> Any:
+        pass
+
