96d4785fc4bf4690abb3ca870e581fdd1cd3acc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:29 +0000

    Add package icostate/0.5/icostate-0.5.tar.gz

---
+++ packages/icostate/icostate-0.5.tar.gz/icostate-0.5/icostate/state.py
@@ -0,0 +1,38 @@
+"""Support code for determining the current state of the ICOtronic system"""
+
+# -- Imports ------------------------------------------------------------------
+
+from enum import auto, Enum
+
+# -- Classes ------------------------------------------------------------------
+
+
+class State(Enum):
+    """Contains the various states the ICOtronic system can be in"""
+
+    STU_CONNECTED = auto()
+    DISCONNECTED = auto()
+
+    def __repr__(self) -> str:
+        """Get string representation of state
+
+        Returns:
+
+            A human readable unique representation of the state
+
+        Examples:
+
+            Show the string representation of some states
+
+            >>> State.STU_CONNECTED
+            STU Connected
+
+            >>> State.DISCONNECTED
+            Disconnected
+
+        """
+
+        return " ".join([
+            word.upper() if word in {"STH", "STU"} else word.capitalize()
+            for word in self.name.split("_")
+        ])
