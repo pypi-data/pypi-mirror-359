fa9d0ede4d9116c4798b9b3535a2d2c83c2c514b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:10 +0000

    Add package spring-boot/3.3.2/spring-boot-3.3.2.tar.gz

---
+++ packages/spring-boot/spring-boot-3.3.2.tar.gz/spring-boot-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot",
+    version="3.3.2",
+    packages=["spring-boot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
