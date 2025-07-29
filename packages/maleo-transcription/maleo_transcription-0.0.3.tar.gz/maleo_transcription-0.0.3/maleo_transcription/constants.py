600daff76739580012e570c3dc6372e84d99bdf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:28 +0000

    Add package maleo-transcription/0.0.3/maleo_transcription-0.0.3.tar.gz

---
+++ packages/maleo-transcription/maleo_transcription-0.0.3.tar.gz/maleo_transcription-0.0.3/maleo_transcription/constants.py
@@ -0,0 +1,15 @@
+class MaleoTranscriptionConstants:
+    MIME_FORMAT_MAP:dict[str, str] = {
+        "audio/aac": "aac",
+        "audio/flac": "flac",
+        "audio/ogg": "ogg",
+        "audio/x-hx-aac-adts": "aac",
+        "audio/mpeg": "mp3",
+        "audio/mp4": "m4a",
+        "audio/x-m4a": "m4a",
+        "audio/wav": "wav",
+        "audio/x-wav": "wav",
+        "audio/webm": "webm",
+        "video/webm": "webm",
+        "video/mp4": "m4a",
+    }
