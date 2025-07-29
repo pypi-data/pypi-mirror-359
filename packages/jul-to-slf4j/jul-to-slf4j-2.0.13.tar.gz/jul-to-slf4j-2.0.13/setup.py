0fececa9e9e55f223600c17f5986697660b641e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package jul-to-slf4j/2.0.13/jul-to-slf4j-2.0.13.tar.gz

---
+++ packages/jul-to-slf4j/jul-to-slf4j-2.0.13.tar.gz/jul-to-slf4j-2.0.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jul-to-slf4j",
+    version="2.0.13",
+    packages=["jul-to-slf4j"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
