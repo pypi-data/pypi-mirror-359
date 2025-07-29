13ecf4cee5eecef05515e603fcca5153fd3f59de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package micrometer-observation/1.13.2/micrometer-observation-1.13.2.tar.gz

---
+++ packages/micrometer-observation/micrometer-observation-1.13.2.tar.gz/micrometer-observation-1.13.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="micrometer-observation",
+    version="1.13.2",
+    packages=["micrometer-observation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
