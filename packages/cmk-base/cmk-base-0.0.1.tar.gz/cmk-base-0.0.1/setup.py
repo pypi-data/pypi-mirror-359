541ca5ae248f1afea2a2f03b0202e660d5b0d62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:34 +0000

    Add package cmk-base/0.0.1/cmk-base-0.0.1.tar.gz

---
+++ packages/cmk-base/cmk-base-0.0.1.tar.gz/cmk-base-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cmk-base",
+    version="0.0.1",
+    packages=["cmk-base"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
