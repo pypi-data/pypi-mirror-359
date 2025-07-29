ee5283e3a34219960b237b2870ce323b181eac14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:49 +0000

    Add package helloworld-pb2-grpc/0.0.1/helloworld-pb2-grpc-0.0.1.tar.gz

---
+++ packages/helloworld-pb2-grpc/helloworld-pb2-grpc-0.0.1.tar.gz/helloworld-pb2-grpc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="helloworld-pb2-grpc",
+    version="0.0.1",
+    packages=["helloworld-pb2-grpc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
