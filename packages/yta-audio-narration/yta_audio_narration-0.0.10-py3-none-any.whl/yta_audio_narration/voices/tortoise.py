fda5685e55415eb1ed16af62319920521854ff50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:06:00 +0000

    Add package yta-audio-narration/0.0.10/yta_audio_narration-0.0.10-py3-none-any.whl

---
+++ packages/yta-audio-narration/yta_audio_narration-0.0.10-py3-none-any.whl/yta_audio_narration/voices/tortoise.py
@@ -0,0 +1,6 @@
+"""
+TODO: Do we make this optional or not? If it
+is optional we can allow installing the libs
+only if using this one.
+"""
+from yta_audio_narration_tortoise import TortoiseVoiceName, LANGUAGE_OPTIONS, EMOTION_OPTIONS, SPEED_OPTIONS, PITCH_OPTIONS, TortoiseNarrationVoice, get_narrator_names_by_language, narrate
