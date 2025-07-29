7231888ee22d40bc6bcb6dc77f3a6fc8640a6005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package candidates/0.0.1/candidates-0.0.1.tar.gz

---
+++ packages/candidates/candidates-0.0.1.tar.gz/candidates-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="candidates",
+    version="0.0.1",
+    packages=["candidates"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
