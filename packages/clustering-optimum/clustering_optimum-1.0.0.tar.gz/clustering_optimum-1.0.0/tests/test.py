267252278b0f9c5a6a92b43f3c3dc8b4afef2ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:44 +0000

    Add package clustering-optimum/1.0.0/clustering_optimum-1.0.0.tar.gz

---
+++ packages/clustering-optimum/clustering_optimum-1.0.0.tar.gz/clustering_optimum-1.0.0/tests/test.py
@@ -0,0 +1,18 @@
+import scanpy as sc
+import time
+
+from clustering_optimum import score_function as clo
+
+print("Loading data...")
+
+
+adata = sc.read_h5ad('datasets/neurons_2000/processed_data.h5ad')
+
+
+print("Data loaded")
+
+
+start_time = time.time()
+a = clo.clustering_score(adata, score_value='calinski', min_res=0.1, max_res=2, step=0.1, plot=True, dim_reduction='umap')
+end_time = time.time()
+print(f"OPTIMIZED, MULTIPROCESSING: Execution time: {end_time - start_time:.2f} seconds")
