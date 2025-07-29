6f20b6387e7482c80f1b2cf337ac8e0f88e6dae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:49 +0000

    Add package dl-version/1.0.0/dl-version-1.0.0.tar.gz

---
+++ packages/dl-version/dl-version-1.0.0.tar.gz/dl-version-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-version",
+    version="1.0.0",
+    packages=["dl-version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
