4f4063c3758be8c56f5031bc6b268bdbfbed90fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:32 +0000

    Add package useraction-pb2/0.0.1/useraction-pb2-0.0.1.tar.gz

---
+++ packages/useraction-pb2/useraction-pb2-0.0.1.tar.gz/useraction-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="useraction-pb2",
+    version="0.0.1",
+    packages=["useraction-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
