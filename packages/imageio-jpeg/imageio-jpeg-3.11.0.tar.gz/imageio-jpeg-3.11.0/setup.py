76dac3a839ea6614335f8ca2c7c8702405b8ab33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package imageio-jpeg/3.11.0/imageio-jpeg-3.11.0.tar.gz

---
+++ packages/imageio-jpeg/imageio-jpeg-3.11.0.tar.gz/imageio-jpeg-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-jpeg",
+    version="3.11.0",
+    packages=["imageio-jpeg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
