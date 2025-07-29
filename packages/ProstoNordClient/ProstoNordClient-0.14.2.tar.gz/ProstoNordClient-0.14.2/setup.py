4704330b378ef1f3a5ef9cad8945b5ab532f47d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package ProstoNordClient/0.14.2/ProstoNordClient-0.14.2.tar.gz

---
+++ packages/ProstoNordClient/ProstoNordClient-0.14.2.tar.gz/ProstoNordClient-0.14.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoNordClient",
+    version="0.14.2",
+    packages=["ProstoNordClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
