16c54e536c71487fa03b4fe9eb5812a8ecc4123c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package as-vk-auth/0.0.1/as-vk-auth-0.0.1.tar.gz

---
+++ packages/as-vk-auth/as-vk-auth-0.0.1.tar.gz/as-vk-auth-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="as-vk-auth",
+    version="0.0.1",
+    packages=["as-vk-auth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
