55685aca7dc1b252848efc33f234b47233bfc8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package datalens-rate-limiter/0.0.1/datalens-rate-limiter-0.0.1.tar.gz

---
+++ packages/datalens-rate-limiter/datalens-rate-limiter-0.0.1.tar.gz/datalens-rate-limiter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-rate-limiter",
+    version="0.0.1",
+    packages=["datalens-rate-limiter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
