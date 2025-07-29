abeb5f199d4e932eb010ad17332682c7a818d527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-model-tools/0.0.1/dl-model-tools-0.0.1.tar.gz

---
+++ packages/dl-model-tools/dl-model-tools-0.0.1.tar.gz/dl-model-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-model-tools",
+    version="0.0.1",
+    packages=["dl-model-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
