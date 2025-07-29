2308131caa4247a46fa4eccb215fa09600a9796e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-postgresql/0.0.1/dl-connector-postgresql-0.0.1.tar.gz

---
+++ packages/dl-connector-postgresql/dl-connector-postgresql-0.0.1.tar.gz/dl-connector-postgresql-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-postgresql",
+    version="0.0.1",
+    packages=["dl-connector-postgresql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
