88f0dc0713a8269a91a9e5f26fba6aa27c09e6f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package oauth2-oidc-sdk/9.43.4/oauth2-oidc-sdk-9.43.4.tar.gz

---
+++ packages/oauth2-oidc-sdk/oauth2-oidc-sdk-9.43.4.tar.gz/oauth2-oidc-sdk-9.43.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oauth2-oidc-sdk",
+    version="9.43.4",
+    packages=["oauth2-oidc-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
