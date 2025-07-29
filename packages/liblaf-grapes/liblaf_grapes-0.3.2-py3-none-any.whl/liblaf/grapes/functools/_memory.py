774ed952b9cf401b33461962a178bd1ee67b1356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:35 +0000

    Add package liblaf-grapes/0.3.2/liblaf_grapes-0.3.2-py3-none-any.whl

---
+++ packages/liblaf-grapes/liblaf_grapes-0.3.2-py3-none-any.whl/liblaf/grapes/functools/_memory.py
@@ -0,0 +1,65 @@
+import datetime
+import functools
+from collections.abc import Callable
+from typing import Any, Protocol, TypedDict, Unpack, overload
+
+import joblib
+
+
+class MemorizedFunc[**P, T](Protocol):
+    @property
+    def memory(self) -> joblib.Memory: ...
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T: ...
+
+
+class CacheKwargs(TypedDict, total=False):
+    pass
+
+
+class ReduceSizeKwargs(TypedDict, total=False):
+    bytes_limit: int | str | None
+    items_limit: int | None
+    age_limit: datetime.timedelta | None
+
+
+@overload
+def cache[**P, T](
+    *,
+    memory: joblib.Memory | None = None,
+    reduce_size: ReduceSizeKwargs | None = None,
+    **kwargs: Unpack[CacheKwargs],
+) -> Callable[[Callable[P, T]], MemorizedFunc[P, T]]: ...
+@overload
+def cache[**P, T](
+    func: Callable[P, T],
+    /,
+    *,
+    memory: joblib.Memory | None = None,
+    reduce_size: ReduceSizeKwargs | None = None,
+    **kwargs: Unpack[CacheKwargs],
+) -> MemorizedFunc[P, T]: ...
+def cache[**P, T](
+    func: Callable[P, T] | None = None,
+    /,
+    *,
+    memory: joblib.Memory | None = None,
+    reduce_size: ReduceSizeKwargs | None = None,
+    **kwargs: Unpack[CacheKwargs],
+) -> Any:
+    if func is None:
+        return functools.partial(
+            cache, memory=memory, reduce_size=reduce_size, **kwargs
+        )
+    if memory is None:
+        memory = joblib.Memory()
+    if reduce_size is None:
+        reduce_size = {"bytes_limit": "1G"}
+
+    @memory.cache(**kwargs)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+        ret: T = func(*args, **kwargs)
+        memory.reduce_size(**reduce_size)
+        return ret
+
+    wrapper.memory = memory  # pyright: ignore[reportFunctionMemberAccess]
+    return wrapper
