76b6754cc0dc446390c6a4449a8c5c4cc741bf59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:29 +0000

    Add package stage0-py-utils/0.2.11/stage0_py_utils-0.2.11-py3-none-any.whl

---
+++ packages/stage0-py-utils/stage0_py_utils-0.2.11-py3-none-any.whl/stage0_py_utils/echo_utils/token.py
@@ -0,0 +1,7 @@
+def create_echo_token():
+    """Create a access token from the Discord Environment."""
+    return {
+        # TODO: Get Values from Discord
+        "user_id": "aaaa00000000000000000001",
+        "roles": ["Staff", "admin"]
+    }
