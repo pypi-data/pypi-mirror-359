7f914f464cc7650b30452337510a07cc14b24ef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:22 +0000

    Add package thunderhorse/0.0.19/thunderhorse-0.0.19.tar.gz

---
+++ packages/thunderhorse/thunderhorse-0.0.19.tar.gz/thunderhorse-0.0.19/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="thunderhorse",
+    version="0.0.19",
+    packages=["thunderhorse"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
