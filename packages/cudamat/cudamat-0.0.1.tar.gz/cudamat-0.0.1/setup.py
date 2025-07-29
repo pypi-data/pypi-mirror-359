d00eb236045b982ffc0dc5ccd9427a1a8730d193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package cudamat/0.0.1/cudamat-0.0.1.tar.gz

---
+++ packages/cudamat/cudamat-0.0.1.tar.gz/cudamat-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cudamat",
+    version="0.0.1",
+    packages=["cudamat"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
