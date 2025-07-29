7718d0feb7a0ffa6e94a508e6a9832a689879e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package proxy-pagination/0.0.1/proxy-pagination-0.0.1.tar.gz

---
+++ packages/proxy-pagination/proxy-pagination-0.0.1.tar.gz/proxy-pagination-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="proxy-pagination",
+    version="0.0.1",
+    packages=["proxy-pagination"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
