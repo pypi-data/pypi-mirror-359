cd2a8224f231bba92f0ff060d67c76e18ae6968f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-control-api/0.0.1/dl-control-api-0.0.1.tar.gz

---
+++ packages/dl-control-api/dl-control-api-0.0.1.tar.gz/dl-control-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-control-api",
+    version="0.0.1",
+    packages=["dl-control-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
