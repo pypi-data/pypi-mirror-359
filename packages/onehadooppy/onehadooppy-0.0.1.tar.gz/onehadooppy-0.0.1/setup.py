603ce20e8b658f634b06b11ca09e4a1c353d94ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:04 +0000

    Add package onehadooppy/0.0.1/onehadooppy-0.0.1.tar.gz

---
+++ packages/onehadooppy/onehadooppy-0.0.1.tar.gz/onehadooppy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="onehadooppy",
+    version="0.0.1",
+    packages=["onehadooppy"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
