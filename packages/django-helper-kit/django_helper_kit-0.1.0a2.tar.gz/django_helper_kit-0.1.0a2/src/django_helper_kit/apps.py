dbf0d5c55e13fb6cc6c2d864e631d81b970b93d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:57 +0000

    Add package django-helper-kit/0.1.0a2/django_helper_kit-0.1.0a2.tar.gz

---
+++ packages/django-helper-kit/django_helper_kit-0.1.0a2.tar.gz/django_helper_kit-0.1.0a2/src/django_helper_kit/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoHelperKitConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_helper_kit'
---
+++ packages/django-helper-kit/django_helper_kit-0.1.0a2.tar.gz/django_helper_kit-0.1.0a2/src/django_helper_kit/utils/__init__.py
@@ -0,0 +1,13 @@
+from .funcs import (
+    dict_subset,
+    dict_to_str,
+    get_current_scheme_site,
+    get_form_fields,
+    get_module_names,
+    is_ajax,
+    kebab_case,
+    match_dict_keys,
+    redirect_next,
+    snake_case,
+    tokenize,
+)
