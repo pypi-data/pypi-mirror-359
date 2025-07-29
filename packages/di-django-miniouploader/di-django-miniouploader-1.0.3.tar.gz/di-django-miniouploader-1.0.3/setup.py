2a0bb96f2169a66bccf769fab17957f84d07545c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:07 +0000

    Add package di-django-miniouploader/1.0.3/di-django-miniouploader-1.0.3.tar.gz

---
+++ packages/di-django-miniouploader/di-django-miniouploader-1.0.3.tar.gz/di-django-miniouploader-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="di-django-miniouploader",
+    version="1.0.3",
+    packages=["di-django-miniouploader"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
