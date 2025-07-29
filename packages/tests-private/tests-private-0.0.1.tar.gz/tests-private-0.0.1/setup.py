88dcea15490303a94a7a6ec70ba7a02cb12d1466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:41 +0000

    Add package tests-private/0.0.1/tests-private-0.0.1.tar.gz

---
+++ packages/tests-private/tests-private-0.0.1.tar.gz/tests-private-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tests-private",
+    version="0.0.1",
+    packages=["tests-private"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
