f15fca934ad653e6b523d75f506159a579cb12ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package user-pools/0.0.1/user-pools-0.0.1.tar.gz

---
+++ packages/user-pools/user-pools-0.0.1.tar.gz/user-pools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="user-pools",
+    version="0.0.1",
+    packages=["user-pools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
