4207c6372276cac79cb704fe777b274e1b634f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:49 +0000

    Add package search-pyspark/0.0.69/search-pyspark-0.0.69.tar.gz

---
+++ packages/search-pyspark/search-pyspark-0.0.69.tar.gz/search-pyspark-0.0.69/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="search-pyspark",
+    version="0.0.69",
+    packages=["search-pyspark"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
