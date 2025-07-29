3e25f6c0c2b73d4b7aad82ee7c72d7ab65624c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package requirements-neuron.txt/0.0.1/requirements-neuron.txt-0.0.1.tar.gz

---
+++ packages/requirements-neuron.txt/requirements-neuron.txt-0.0.1.tar.gz/requirements-neuron.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements-neuron.txt",
+    version="0.0.1",
+    packages=["requirements-neuron.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
