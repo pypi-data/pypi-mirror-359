f1bf5eace8280340501412fc59777f5b73a9cc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:23 +0000

    Add package python-ics/920.14.1.post1.dev0/python_ics-920.14.1.post1.dev0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/python-ics/python_ics-920.14.1.post1.dev0-cp312-cp312-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/ics/structures/ics_spy_message_long.py
@@ -0,0 +1,56 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+
+
+class Nameless22278(ctypes.Structure):
+    _fields_ = [
+        ('StatusBitField3', ctypes.c_uint32),
+        ('StatusBitField4', ctypes.c_uint32),
+    ]
+
+
+
+class Nameless57618(ctypes.Union):
+    _anonymous_  = ('Nameless22278',)
+    _fields_ = [
+        ('Nameless22278', Nameless22278),
+        ('AckBytes', ctypes.c_uint8 * 8),
+    ]
+
+
+
+class ics_spy_message_long(ctypes.Structure):
+    _anonymous_  = ('Nameless57618',)
+    _fields_ = [
+        ('StatusBitField', ctypes.c_uint32),
+        ('StatusBitField2', ctypes.c_uint32),
+        ('TimeHardware', ctypes.c_uint32),
+        ('TimeHardware2', ctypes.c_uint32),
+        ('TimeSystem', ctypes.c_uint32),
+        ('TimeSystem2', ctypes.c_uint32),
+        ('TimeStampHardwareID', ctypes.c_uint8),
+        ('TimeStampSystemID', ctypes.c_uint8),
+        ('NetworkID', ctypes.c_uint8),
+        ('NodeID', ctypes.c_uint8),
+        ('Protocol', ctypes.c_uint8),
+        ('MessagePieceID', ctypes.c_uint8),
+        ('ExtraDataPtrEnabled', ctypes.c_uint8),
+        ('NumberBytesHeader', ctypes.c_uint8),
+        ('NumberBytesData', ctypes.c_uint8),
+        ('NetworkID2', ctypes.c_uint8),
+        ('DescriptionID', ctypes.c_uint16),
+        ('ArbIDOrHeader', ctypes.c_uint32),
+        ('DataMsb', ctypes.c_uint32),
+        ('DataLsb', ctypes.c_uint32),
+        ('Nameless57618', Nameless57618),
+        ('ExtraDataPtr', ctypes.c_void_p),
+        ('MiscData', ctypes.c_uint8),
+        ('Reserved', ctypes.c_uint8 * 3),
+    ]
+
+
+_icsSpyMessageLong = ics_spy_message_long
+icsSpyMessageLong = ics_spy_message_long
+
