63098d18e89a7af99f34717cba449e5604e3e6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:07 +0000

    Add package ok-s3/0.5.2/ok-s3-0.5.2.tar.gz

---
+++ packages/ok-s3/ok-s3-0.5.2.tar.gz/ok-s3-0.5.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok-s3",
+    version="0.5.2",
+    packages=["ok-s3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
