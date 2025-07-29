0ba79b48356762baed9128bea971b5aae874db4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package reservation/0.0.1/reservation-0.0.1.tar.gz

---
+++ packages/reservation/reservation-0.0.1.tar.gz/reservation-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="reservation",
+    version="0.0.1",
+    packages=["reservation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
