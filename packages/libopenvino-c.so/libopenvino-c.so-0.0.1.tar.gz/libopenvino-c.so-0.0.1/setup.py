0b0cb9589347a87358efc4cff1376bf334396c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:52 +0000

    Add package libopenvino-c.so/0.0.1/libopenvino-c.so-0.0.1.tar.gz

---
+++ packages/libopenvino-c.so/libopenvino-c.so-0.0.1.tar.gz/libopenvino-c.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libopenvino-c.so",
+    version="0.0.1",
+    packages=["libopenvino-c.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
