072ef1e20f3e966f4dbe2de3fcf6ec484e2b5056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package spring-boot-starter-thymeleaf/3.3.2/spring-boot-starter-thymeleaf-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-thymeleaf/spring-boot-starter-thymeleaf-3.3.2.tar.gz/spring-boot-starter-thymeleaf-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-thymeleaf",
+    version="3.3.2",
+    packages=["spring-boot-starter-thymeleaf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
