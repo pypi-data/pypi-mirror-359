fc0913bb8e0ea6bfb7089de37456880b970593d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package newautoreg/0.0.1/newautoreg-0.0.1.tar.gz

---
+++ packages/newautoreg/newautoreg-0.0.1.tar.gz/newautoreg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="newautoreg",
+    version="0.0.1",
+    packages=["newautoreg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
