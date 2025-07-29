1593266192d1e7a36c9071b1c035b8b30503710c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package imageio-bmp/3.11.0/imageio-bmp-3.11.0.tar.gz

---
+++ packages/imageio-bmp/imageio-bmp-3.11.0.tar.gz/imageio-bmp-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-bmp",
+    version="3.11.0",
+    packages=["imageio-bmp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
