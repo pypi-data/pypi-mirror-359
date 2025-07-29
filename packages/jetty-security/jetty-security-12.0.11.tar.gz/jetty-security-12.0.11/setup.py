88ca7f3dd34e0ac20cbf64c001a9ce30e7be84db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package jetty-security/12.0.11/jetty-security-12.0.11.tar.gz

---
+++ packages/jetty-security/jetty-security-12.0.11.tar.gz/jetty-security-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-security",
+    version="12.0.11",
+    packages=["jetty-security"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
