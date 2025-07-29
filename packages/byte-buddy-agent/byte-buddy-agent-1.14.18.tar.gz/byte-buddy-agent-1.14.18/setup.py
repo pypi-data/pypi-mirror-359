0f97428e8c663925047a2bb6ad52eb9243991e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package byte-buddy-agent/1.14.18/byte-buddy-agent-1.14.18.tar.gz

---
+++ packages/byte-buddy-agent/byte-buddy-agent-1.14.18.tar.gz/byte-buddy-agent-1.14.18/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="byte-buddy-agent",
+    version="1.14.18",
+    packages=["byte-buddy-agent"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
