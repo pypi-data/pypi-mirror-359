ae89bdcaca3a768a1a3e21d0da4c435422386639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package list-update/0.0.1/list-update-0.0.1.tar.gz

---
+++ packages/list-update/list-update-0.0.1.tar.gz/list-update-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="list-update",
+    version="0.0.1",
+    packages=["list-update"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
