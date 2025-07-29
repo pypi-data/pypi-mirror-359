07437744d7a74511a74fb3a72db7bc8888ecced9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-boot-starter-data-jpa/3.3.2/spring-boot-starter-data-jpa-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-data-jpa/spring-boot-starter-data-jpa-3.3.2.tar.gz/spring-boot-starter-data-jpa-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-data-jpa",
+    version="3.3.2",
+    packages=["spring-boot-starter-data-jpa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
