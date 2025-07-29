379331c7f42d45bdb9b31c6b166163486361c4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package tests-info-collector/0.0.1/tests-info-collector-0.0.1.tar.gz

---
+++ packages/tests-info-collector/tests-info-collector-0.0.1.tar.gz/tests-info-collector-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tests-info-collector",
+    version="0.0.1",
+    packages=["tests-info-collector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
