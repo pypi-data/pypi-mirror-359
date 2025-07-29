109d02b2a3dca59e0600d470bfbc4fadaf0409c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package theseustemplater/0.0.2/theseustemplater-0.0.2.tar.gz

---
+++ packages/theseustemplater/theseustemplater-0.0.2.tar.gz/theseustemplater-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="theseustemplater",
+    version="0.0.2",
+    packages=["theseustemplater"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
