497c3effb916e91e07967e3d3033ca2987f35c64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package jetty-ee10-annotations/12.0.11/jetty-ee10-annotations-12.0.11.tar.gz

---
+++ packages/jetty-ee10-annotations/jetty-ee10-annotations-12.0.11.tar.gz/jetty-ee10-annotations-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-ee10-annotations",
+    version="12.0.11",
+    packages=["jetty-ee10-annotations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
