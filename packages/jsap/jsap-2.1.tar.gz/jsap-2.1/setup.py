96c6a44fe269508840a1d394925aa46f19ad9beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:09 +0000

    Add package jsap/2.1/jsap-2.1.tar.gz

---
+++ packages/jsap/jsap-2.1.tar.gz/jsap-2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jsap",
+    version="2.1",
+    packages=["jsap"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
