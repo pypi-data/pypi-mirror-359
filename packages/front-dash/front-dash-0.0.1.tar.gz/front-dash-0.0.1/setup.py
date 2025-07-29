0969c798e91af37448bd07358114d670baa25c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package front-dash/0.0.1/front-dash-0.0.1.tar.gz

---
+++ packages/front-dash/front-dash-0.0.1.tar.gz/front-dash-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="front-dash",
+    version="0.0.1",
+    packages=["front-dash"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
