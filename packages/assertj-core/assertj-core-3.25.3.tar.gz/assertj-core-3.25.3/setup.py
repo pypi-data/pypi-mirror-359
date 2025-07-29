5791a50f67dceddc65ed09b1f4df3c2a00b03dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package assertj-core/3.25.3/assertj-core-3.25.3.tar.gz

---
+++ packages/assertj-core/assertj-core-3.25.3.tar.gz/assertj-core-3.25.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="assertj-core",
+    version="3.25.3",
+    packages=["assertj-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
