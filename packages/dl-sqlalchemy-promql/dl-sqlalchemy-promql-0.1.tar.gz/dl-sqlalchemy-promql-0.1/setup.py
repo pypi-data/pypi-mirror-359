70be19d5d63cd5fbb69ee1380b616c11d9ca7dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-promql/0.1/dl-sqlalchemy-promql-0.1.tar.gz

---
+++ packages/dl-sqlalchemy-promql/dl-sqlalchemy-promql-0.1.tar.gz/dl-sqlalchemy-promql-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-promql",
+    version="0.1",
+    packages=["dl-sqlalchemy-promql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
