31856d220484ff45a3e69f3ea764f1d133d50153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package slf4j-api/2.0.13/slf4j-api-2.0.13.tar.gz

---
+++ packages/slf4j-api/slf4j-api-2.0.13.tar.gz/slf4j-api-2.0.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="slf4j-api",
+    version="2.0.13",
+    packages=["slf4j-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
