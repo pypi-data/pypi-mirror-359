91e213a5de1af86289da649c0900fe9e2f9ba314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package ceph-module/0.0.1/ceph-module-0.0.1.tar.gz

---
+++ packages/ceph-module/ceph-module-0.0.1.tar.gz/ceph-module-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ceph-module",
+    version="0.0.1",
+    packages=["ceph-module"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
