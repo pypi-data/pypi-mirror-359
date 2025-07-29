23123df22c8ee9580ab81a18bd628b0b24b2e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-pivot-pandas/0.0.1/dl-pivot-pandas-0.0.1.tar.gz

---
+++ packages/dl-pivot-pandas/dl-pivot-pandas-0.0.1.tar.gz/dl-pivot-pandas-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-pivot-pandas",
+    version="0.0.1",
+    packages=["dl-pivot-pandas"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
