860d9669154006f21960645a46827f9330fbd900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:11 +0000

    Add package mmm-tools/0.3.20/mmm-tools-0.3.20.tar.gz

---
+++ packages/mmm-tools/mmm-tools-0.3.20.tar.gz/mmm-tools-0.3.20/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mmm-tools",
+    version="0.3.20",
+    packages=["mmm-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
