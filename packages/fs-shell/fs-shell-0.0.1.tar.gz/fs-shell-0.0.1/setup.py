5fa93ab2c736cbd4e1655794c7be4ceaa210fd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:48 +0000

    Add package fs-shell/0.0.1/fs-shell-0.0.1.tar.gz

---
+++ packages/fs-shell/fs-shell-0.0.1.tar.gz/fs-shell-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fs-shell",
+    version="0.0.1",
+    packages=["fs-shell"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
