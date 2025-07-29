e7d946a4ced13dbea013dc01a5f5c500be830f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:41 +0000

    Add package openfeature/0.0.1/openfeature-0.0.1.tar.gz

---
+++ packages/openfeature/openfeature-0.0.1.tar.gz/openfeature-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="openfeature",
+    version="0.0.1",
+    packages=["openfeature"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
