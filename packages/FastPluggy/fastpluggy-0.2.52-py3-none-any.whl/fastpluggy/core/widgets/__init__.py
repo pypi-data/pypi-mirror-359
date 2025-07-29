78e716816c15944c6fd89ce26d3946fdfb9ae910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:17 +0000

    Add package FastPluggy/0.2.52/fastpluggy-0.2.52-py3-none-any.whl

---
+++ packages/FastPluggy/fastpluggy-0.2.52-py3-none-any.whl/fastpluggy/core/widgets/__init__.py
@@ -0,0 +1,38 @@
+"""
+FastPluggy Widget System
+
+Clean, extensible widget architecture with auto-registration.
+"""
+
+from .base import AbstractWidget
+from .fastpluggy_integration import FastPluggyWidgets
+
+# Import all core widgets to trigger auto-registration
+
+# Display widgets
+from .categories.display.custom import *
+
+# Data widgets
+from .categories.data.table import *
+
+# Input widgets
+from .categories.input.button import *
+from .categories.input.button_list import *
+from .categories.input.form import *
+
+# Debug widgets
+from .categories.debug.debug_panel import *
+
+__all__ = [
+    'AbstractWidget',
+    'FastPluggyWidgets',
+    # Data widgets
+    'TableWidget',
+    # Display widgets
+    'CustomTemplateWidget',
+    # Input widgets
+    'FormWidget',
+    'ButtonWidget',
+    # Debug widgets
+    'DebugPanelWidget',
+]
