4806f28d9163cb72b39c1b0a3f8ac3d6e55456ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package audience/0.0.1/audience-0.0.1.tar.gz

---
+++ packages/audience/audience-0.0.1.tar.gz/audience-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="audience",
+    version="0.0.1",
+    packages=["audience"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
