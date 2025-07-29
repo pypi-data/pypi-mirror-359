b0d3aeed068fd0b7dc4d195048ed57993cd72277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package inferhad/0.0.1/inferhad-0.0.1.tar.gz

---
+++ packages/inferhad/inferhad-0.0.1.tar.gz/inferhad-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="inferhad",
+    version="0.0.1",
+    packages=["inferhad"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
