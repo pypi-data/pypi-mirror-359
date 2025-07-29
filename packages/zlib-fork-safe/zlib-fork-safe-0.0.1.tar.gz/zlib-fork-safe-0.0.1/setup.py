1e46674e9168e21391d2008c6efc134cb85757b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package zlib-fork-safe/0.0.1/zlib-fork-safe-0.0.1.tar.gz

---
+++ packages/zlib-fork-safe/zlib-fork-safe-0.0.1.tar.gz/zlib-fork-safe-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="zlib-fork-safe",
+    version="0.0.1",
+    packages=["zlib-fork-safe"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
