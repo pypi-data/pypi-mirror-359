816046dbfd5640e455234c64cc1a287a7ce83ea8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:31 +0000

    Add package concrete-recommender/0.0.1/concrete-recommender-0.0.1.tar.gz

---
+++ packages/concrete-recommender/concrete-recommender-0.0.1.tar.gz/concrete-recommender-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="concrete-recommender",
+    version="0.0.1",
+    packages=["concrete-recommender"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
