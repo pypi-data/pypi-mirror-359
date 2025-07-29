602ec8ccde591f2759e628e3d5f5b0477094ac95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package spring-beans/6.1.11/spring-beans-6.1.11.tar.gz

---
+++ packages/spring-beans/spring-beans-6.1.11.tar.gz/spring-beans-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-beans",
+    version="6.1.11",
+    packages=["spring-beans"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
