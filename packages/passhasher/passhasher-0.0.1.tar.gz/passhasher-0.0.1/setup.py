26020aa7f2a0e0e845252b239b52c62265416d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package passhasher/0.0.1/passhasher-0.0.1.tar.gz

---
+++ packages/passhasher/passhasher-0.0.1.tar.gz/passhasher-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="passhasher",
+    version="0.0.1",
+    packages=["passhasher"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
