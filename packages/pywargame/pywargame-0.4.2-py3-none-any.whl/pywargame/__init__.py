92682a6d2099b0c6c24ea8a8eb7cd05103aee599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:28 +0000

    Add package pywargame/0.4.2/pywargame-0.4.2-py3-none-any.whl

---
+++ packages/pywargame/pywargame-0.4.2-py3-none-any.whl/pywargame/__init__.py
@@ -0,0 +1,8 @@
+'''Top-level of module'''
+version = '0.4.2'
+
+import pywargame.common     
+import pywargame.vassal     
+import pywargame.cyberboard 
+import pywargame.zuntzu     
+import pywargame.latex      
