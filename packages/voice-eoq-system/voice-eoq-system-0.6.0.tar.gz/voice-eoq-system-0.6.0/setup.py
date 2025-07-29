9782062b51739a0b76a98df6063078c4f012de77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package voice-eoq-system/0.6.0/voice-eoq-system-0.6.0.tar.gz

---
+++ packages/voice-eoq-system/voice-eoq-system-0.6.0.tar.gz/voice-eoq-system-0.6.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-eoq-system",
+    version="0.6.0",
+    packages=["voice-eoq-system"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
