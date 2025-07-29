5c72bc6438c4f61fec28dcd719492c509438b61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:56 +0000

    Add package win32crypt/0.0.1/win32crypt-0.0.1.tar.gz

---
+++ packages/win32crypt/win32crypt-0.0.1.tar.gz/win32crypt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="win32crypt",
+    version="0.0.1",
+    packages=["win32crypt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
