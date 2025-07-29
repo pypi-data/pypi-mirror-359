230bfb57a637ca1ed017e59364f040ef687fc85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package testit-components-provider/0.0.1/testit-components-provider-0.0.1.tar.gz

---
+++ packages/testit-components-provider/testit-components-provider-0.0.1.tar.gz/testit-components-provider-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="testit-components-provider",
+    version="0.0.1",
+    packages=["testit-components-provider"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
