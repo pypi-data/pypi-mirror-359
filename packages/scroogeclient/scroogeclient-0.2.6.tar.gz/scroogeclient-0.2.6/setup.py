ceff2216cfd23341b637c10d91aacf8175eaa699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package scroogeclient/0.2.6/scroogeclient-0.2.6.tar.gz

---
+++ packages/scroogeclient/scroogeclient-0.2.6.tar.gz/scroogeclient-0.2.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scroogeclient",
+    version="0.2.6",
+    packages=["scroogeclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
