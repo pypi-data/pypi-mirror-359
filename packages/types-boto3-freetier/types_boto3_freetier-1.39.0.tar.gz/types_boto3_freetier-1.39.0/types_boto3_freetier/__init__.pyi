16192aadf3557f88a67895d34a92b9a4c691c8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:57 +0000

    Add package types-boto3-freetier/1.39.0/types_boto3_freetier-1.39.0.tar.gz

---
+++ packages/types-boto3-freetier/types_boto3_freetier-1.39.0.tar.gz/types_boto3_freetier-1.39.0/types_boto3_freetier/__init__.pyi
@@ -0,0 +1,30 @@
+"""
+Main interface for freetier service.
+
+[Documentation](https://youtype.github.io/types_boto3_docs/types_boto3_freetier/)
+
+Copyright 2025 Vlad Emelianov
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from types_boto3_freetier import (
+        Client,
+        FreeTierClient,
+        GetFreeTierUsagePaginator,
+    )
+
+    session = Session()
+    client: FreeTierClient = session.client("freetier")
+
+    get_free_tier_usage_paginator: GetFreeTierUsagePaginator = client.get_paginator("get_free_tier_usage")
+    ```
+"""
+
+from .client import FreeTierClient
+from .paginator import GetFreeTierUsagePaginator
+
+Client = FreeTierClient
+
+__all__ = ("Client", "FreeTierClient", "GetFreeTierUsagePaginator")
