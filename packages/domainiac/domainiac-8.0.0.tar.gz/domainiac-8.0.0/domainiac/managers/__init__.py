16acac130484ef935e0f7f00ae3dbc1d83c55d37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:05 +0000

    Add package domainiac/8.0.0/domainiac-8.0.0.tar.gz

---
+++ packages/domainiac/domainiac-8.0.0.tar.gz/domainiac-8.0.0/domainiac/managers/__init__.py
@@ -0,0 +1,6 @@
+from .masterdata_manager import MasterdataManager
+from .metering_manager import MeteringManager
+from .nwp_manager import NWPManager
+from .plant_manager import PlantManager
+from .resource_manager import ResourceManager
+from .unit_manager import UnitManager
