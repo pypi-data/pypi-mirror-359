02a246ccf7f4e6966f5cfe9cb1f53ea2e8429298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:44 +0000

    Add package ynabkit/0.2.1/ynabkit-0.2.1.tar.gz

---
+++ packages/ynabkit/ynabkit-0.2.1.tar.gz/ynabkit-0.2.1/ynabkit/satispay/models.py
@@ -0,0 +1,16 @@
+import dataclasses
+import datetime
+
+
+@dataclasses.dataclass
+class Transaction:
+    id: str
+    name: str
+    state: str
+    kind: str
+    date: datetime.datetime
+    amount: float
+    currency: str
+    extra_info: str
+    payee: str
+    
