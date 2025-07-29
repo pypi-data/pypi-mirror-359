59ac41dff5e9633be52a0230288965595af26057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:04 +0000

    Add package prosto_dpp_client/0.0.4/prosto_dpp_client-0.0.4.tar.gz

---
+++ packages/prosto_dpp_client/prosto_dpp_client-0.0.4.tar.gz/prosto_dpp_client-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prosto_dpp_client",
+    version="0.0.4",
+    packages=["prosto_dpp_client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
