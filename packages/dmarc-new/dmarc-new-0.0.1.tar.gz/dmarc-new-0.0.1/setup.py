c25e968ce3d08ca86a3e9813b703a7ac4c3b4d61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:52 +0000

    Add package dmarc-new/0.0.1/dmarc-new-0.0.1.tar.gz

---
+++ packages/dmarc-new/dmarc-new-0.0.1.tar.gz/dmarc-new-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dmarc-new",
+    version="0.0.1",
+    packages=["dmarc-new"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
