689d7668bedc22bff7e7cdb7a76bdd6b7df54930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:49 +0000

    Add package voice-skills-utils/0.0.1/voice-skills-utils-0.0.1.tar.gz

---
+++ packages/voice-skills-utils/voice-skills-utils-0.0.1.tar.gz/voice-skills-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-skills-utils",
+    version="0.0.1",
+    packages=["voice-skills-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
