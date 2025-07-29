ffed65454dc8a59343af770b41fcca2729e5cc2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:53 +0000

    Add package Shadowcord/1.7.5/shadowcord-1.7.5-py3-none-any.whl

---
+++ packages/Shadowcord/shadowcord-1.7.5-py3-none-any.whl/discord/ext/__init__.py
@@ -0,0 +1,15 @@
+"""
+Discord.py Extensions
+~~~~~~~~~~~~~~~~~~~~~
+
+A collection of extensions for discord.py.
+
+:copyright: (c) 2015-present Rapptz
+:license: MIT, see LICENSE for more details.
+
+"""
+
+from . import commands
+from . import tasks
+
+__all__ = ['commands', 'tasks'] 
