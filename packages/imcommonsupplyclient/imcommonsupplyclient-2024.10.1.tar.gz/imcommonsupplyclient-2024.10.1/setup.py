d6f25dff38eb06b677177afca4a19fbfcc213ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package imcommonsupplyclient/2024.10.1/imcommonsupplyclient-2024.10.1.tar.gz

---
+++ packages/imcommonsupplyclient/imcommonsupplyclient-2024.10.1.tar.gz/imcommonsupplyclient-2024.10.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imcommonsupplyclient",
+    version="2024.10.1",
+    packages=["imcommonsupplyclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
