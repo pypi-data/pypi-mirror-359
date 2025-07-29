efb1e14720cf9df40a3c8e6a2cbf6b59f0722ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:21 +0000

    Add package TP-RequestFlows/2025.7.1/tp_requestflows-2025.7.1.tar.gz

---
+++ packages/TP-RequestFlows/tp_requestflows-2025.7.1.tar.gz/tp_requestflows-2025.7.1/setup.py
@@ -0,0 +1,20 @@
+from TP_RequestFlows import TP_RequestFlows_VERSION
+import setuptools
+
+setuptools.setup(
+	name="TP-RequestFlows",
+	version=TP_RequestFlows_VERSION,
+	author="TP Cyber Security",
+	license="MIT",
+	author_email="tpcybersec2023@gmail.com",
+	description="A Python library for sending raw HTTP requests in a predefined sequence configured via a rules.json file. Supports automatic re-login when the session token expires",
+	long_description=open("README.md").read(),
+	long_description_content_type="text/markdown",
+	install_requires=open("requirements.txt").read().split(),
+	url="https://github.com/tpcybersec/TP-RequestFlows",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+	],
+	keywords=["TPCyberSec", "TP-RequestFlows", "Request Flows"],
+	packages=setuptools.find_packages(),
+)
