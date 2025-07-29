40b138a6f8bcbc4f82b37bc59981df1e71688be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package post-build/0.0.1/post-build-0.0.1.tar.gz

---
+++ packages/post-build/post-build-0.0.1.tar.gz/post-build-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="post-build",
+    version="0.0.1",
+    packages=["post-build"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
