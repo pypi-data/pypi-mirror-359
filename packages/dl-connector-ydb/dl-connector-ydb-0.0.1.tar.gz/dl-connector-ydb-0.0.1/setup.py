836e7188f8c4fdcc3a2bd5f8ea6bf19a56412931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-ydb/0.0.1/dl-connector-ydb-0.0.1.tar.gz

---
+++ packages/dl-connector-ydb/dl-connector-ydb-0.0.1.tar.gz/dl-connector-ydb-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-ydb",
+    version="0.0.1",
+    packages=["dl-connector-ydb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
