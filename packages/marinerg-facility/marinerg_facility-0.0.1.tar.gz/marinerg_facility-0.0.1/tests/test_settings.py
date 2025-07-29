e71fe357bd2bec32820311ac130fc60df1e2c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:19 +0000

    Add package marinerg-facility/0.0.1/marinerg_facility-0.0.1.tar.gz

---
+++ packages/marinerg-facility/marinerg_facility-0.0.1.tar.gz/marinerg_facility-0.0.1/tests/test_settings.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import os
+
+from ichec_django_core import settings
+
+SECRET_KEY = "fake-key"
+
+MEDIA_ROOT = Path(__file__).parent / "test_output/mediafiles/"
+os.makedirs(MEDIA_ROOT, exist_ok=True)
+
+INSTALLED_APPS = ["marinerg_facility.apps.MarinergFacilityConfig"]
+INSTALLED_APPS.extend(settings.INSTALLED_APPS)
+
+MIDDLEWARE = settings.MIDDLEWARE
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+        "USER": "user",
+        "PASSWORD": "password",
+    }
+}
+
+ROOT_URLCONF = "app.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(MEDIA_ROOT, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+}
+
+API_AUTH_URL = settings.API_AUTH_URL
+ADMIN_URL = settings.ADMIN_URL
