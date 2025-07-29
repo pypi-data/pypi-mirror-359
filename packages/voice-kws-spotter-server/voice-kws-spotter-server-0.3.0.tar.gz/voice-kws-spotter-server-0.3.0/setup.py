68c0ba108d6407685540f2eca3732988110b5f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package voice-kws-spotter-server/0.3.0/voice-kws-spotter-server-0.3.0.tar.gz

---
+++ packages/voice-kws-spotter-server/voice-kws-spotter-server-0.3.0.tar.gz/voice-kws-spotter-server-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-kws-spotter-server",
+    version="0.3.0",
+    packages=["voice-kws-spotter-server"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
