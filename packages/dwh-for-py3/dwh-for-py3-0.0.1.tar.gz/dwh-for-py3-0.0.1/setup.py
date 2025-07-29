9b87e4472a81d2ebadad3ac01bf8db155cb82fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package dwh-for-py3/0.0.1/dwh-for-py3-0.0.1.tar.gz

---
+++ packages/dwh-for-py3/dwh-for-py3-0.0.1.tar.gz/dwh-for-py3-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dwh-for-py3",
+    version="0.0.1",
+    packages=["dwh-for-py3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
