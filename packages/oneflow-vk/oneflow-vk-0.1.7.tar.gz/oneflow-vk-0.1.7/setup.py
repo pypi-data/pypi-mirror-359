99aa7a72bddb33142bbf1d31c23ae33187eac38e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:39 +0000

    Add package oneflow-vk/0.1.7/oneflow-vk-0.1.7.tar.gz

---
+++ packages/oneflow-vk/oneflow-vk-0.1.7.tar.gz/oneflow-vk-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oneflow-vk",
+    version="0.1.7",
+    packages=["oneflow-vk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
