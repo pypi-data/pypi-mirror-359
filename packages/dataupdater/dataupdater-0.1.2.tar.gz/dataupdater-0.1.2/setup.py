e2556ed345b464d8bd39fdc27d3020c766295418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:32 +0000

    Add package dataupdater/0.1.2/dataupdater-0.1.2.tar.gz

---
+++ packages/dataupdater/dataupdater-0.1.2.tar.gz/dataupdater-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataupdater',
+    version='0.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['flask'],
+    author='Your Name',
+    description='Live Python variable monitor and editor via web browser.',
+    license='MIT',
+)
