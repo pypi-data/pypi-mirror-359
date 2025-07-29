6e565779e7cbaf2a78d06ca8839f0cefb0819193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-compeng-pg/0.0.1/dl-compeng-pg-0.0.1.tar.gz

---
+++ packages/dl-compeng-pg/dl-compeng-pg-0.0.1.tar.gz/dl-compeng-pg-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-compeng-pg",
+    version="0.0.1",
+    packages=["dl-compeng-pg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
