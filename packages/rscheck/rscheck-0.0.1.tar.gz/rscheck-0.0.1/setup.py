f1d5c6a6d1069f4083d698854b688313fa705993
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package rscheck/0.0.1/rscheck-0.0.1.tar.gz

---
+++ packages/rscheck/rscheck-0.0.1.tar.gz/rscheck-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rscheck",
+    version="0.0.1",
+    packages=["rscheck"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
