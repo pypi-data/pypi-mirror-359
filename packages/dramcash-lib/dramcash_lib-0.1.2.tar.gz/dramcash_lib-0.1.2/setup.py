d2773974ff96716385f4dd78fd52768df0183dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:54 +0000

    Add package dramcash-lib/0.1.2/dramcash_lib-0.1.2.tar.gz

---
+++ packages/dramcash-lib/dramcash_lib-0.1.2.tar.gz/dramcash_lib-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dramcash_lib",  # This will be the pip package name
+    version="0.1.2",
+    description="Shared utility library for all microservices",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sudhin M",
+    author_email="sudhin.m@abilytics.com",
+    url="https://github.com/sudhinm/dramcash_lib",  # Optional
+    packages=find_packages(),  # 👈 Important: look inside src/
+    include_package_data=True,
+    install_requires=[
+        "boto3",
+        "pymongo",
+        "pydantic-settings"
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
