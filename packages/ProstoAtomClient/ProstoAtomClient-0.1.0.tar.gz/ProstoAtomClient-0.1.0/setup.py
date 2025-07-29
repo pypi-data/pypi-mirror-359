a45aea0e016a3e24690f3a0c3abdfdb4ab11debd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:30 +0000

    Add package ProstoAtomClient/0.1.0/ProstoAtomClient-0.1.0.tar.gz

---
+++ packages/ProstoAtomClient/ProstoAtomClient-0.1.0.tar.gz/ProstoAtomClient-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoAtomClient",
+    version="0.1.0",
+    packages=["ProstoAtomClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
