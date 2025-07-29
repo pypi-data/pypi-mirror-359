a435691e4a4bcc4b197e8e0e5f63a9ad23f693f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:42 +0000

    Add package gpof/0.1.0/gpof-0.1.0.tar.gz

---
+++ packages/gpof/gpof-0.1.0.tar.gz/gpof-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gpof",
+    version="0.1.0",
+    packages=["gpof"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
