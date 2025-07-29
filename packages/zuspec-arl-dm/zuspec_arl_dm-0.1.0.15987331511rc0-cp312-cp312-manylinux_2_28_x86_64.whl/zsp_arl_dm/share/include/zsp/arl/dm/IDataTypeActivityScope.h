92dc8f59d7d061dae033a4048501d7d031d299af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:32 +0000

    Add package zuspec-arl-dm/0.1.0.15987331511rc0/zuspec_arl_dm-0.1.0.15987331511rc0-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/zuspec-arl-dm/zuspec_arl_dm-0.1.0.15987331511rc0-cp312-cp312-manylinux_2_28_x86_64.whl/zsp_arl_dm/share/include/zsp/arl/dm/IDataTypeActivityScope.h
@@ -0,0 +1,46 @@
+/*
+ * IDataTypeActivityScope.h
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: mballance
+ */
+
+#pragma once
+#include <vector>
+#include "vsc/dm/ITypeExprFieldRef.h"
+#include "zsp/arl/dm/IDataTypeActivity.h"
+#include "zsp/arl/dm/IDataTypeActivityBind.h"
+#include "zsp/arl/dm/ITypeFieldActivity.h"
+#include "vsc/dm/IDataTypeStruct.h"
+
+namespace zsp {
+namespace arl {
+namespace dm {
+
+
+class IDataTypeActivityScope : 
+	public virtual IDataTypeActivity,
+	public virtual vsc::dm::IDataTypeStruct {
+public:
+
+	virtual ~IDataTypeActivityScope() { }
+
+	virtual const std::vector<ITypeFieldActivityUP> &getActivities() const = 0;
+
+	virtual void addActivity(
+        ITypeFieldActivity      *a,
+        bool                    owned=true) = 0;
+
+    virtual int32_t getActivityOffset() const = 0;
+
+	virtual void addActivityField(ITypeFieldActivity *a) = 0;
+
+    virtual const std::vector<IDataTypeActivityBindUP> &getBinds() const = 0;
+
+    virtual void addBind(IDataTypeActivityBind *b) = 0;
+
+};
+
+}
+}
+}
