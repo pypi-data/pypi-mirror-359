7591978444ec2830ef389a53f331a3b4a3d6da2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package requirements-tpu.txt/0.0.1/requirements-tpu.txt-0.0.1.tar.gz

---
+++ packages/requirements-tpu.txt/requirements-tpu.txt-0.0.1.tar.gz/requirements-tpu.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements-tpu.txt",
+    version="0.0.1",
+    packages=["requirements-tpu.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
