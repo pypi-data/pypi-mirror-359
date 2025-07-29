36e65d87cab8b2955caecd995bc44298102aa522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:51 +0000

    Add package screens/0.0.1/screens-0.0.1.tar.gz

---
+++ packages/screens/screens-0.0.1.tar.gz/screens-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="screens",
+    version="0.0.1",
+    packages=["screens"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
