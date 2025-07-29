e94f17aef3d18ac695793647532b3a59018b92ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:15 +0000

    Add package jetty-xml/12.0.11/jetty-xml-12.0.11.tar.gz

---
+++ packages/jetty-xml/jetty-xml-12.0.11.tar.gz/jetty-xml-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-xml",
+    version="12.0.11",
+    packages=["jetty-xml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
