5170645aa7ec4cb4614a7b126453e5f85415b589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:24 +0000

    Add package clickhouse_gate/0.0.1/clickhouse_gate-0.0.1.tar.gz

---
+++ packages/clickhouse_gate/clickhouse_gate-0.0.1.tar.gz/clickhouse_gate-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="clickhouse_gate",
+    version="0.0.1",
+    packages=["clickhouse_gate"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
