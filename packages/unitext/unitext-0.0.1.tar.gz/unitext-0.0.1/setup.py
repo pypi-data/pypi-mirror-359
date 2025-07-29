686e7aa565774e199f58733671073d69b3c8b2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package unitext/0.0.1/unitext-0.0.1.tar.gz

---
+++ packages/unitext/unitext-0.0.1.tar.gz/unitext-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="unitext",
+    version="0.0.1",
+    packages=["unitext"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
