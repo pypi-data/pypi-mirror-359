03406466313c74f823d3f220cd624e2d292433eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:23 +0000

    Add package wp21-train/0.2.0/wp21_train-0.2.0-py3-none-any.whl

---
+++ packages/wp21-train/wp21_train-0.2.0-py3-none-any.whl/wp21_train/savers/pickle_adapter.py
@@ -0,0 +1,24 @@
+from wp21_train.savers.adapter import *
+import pickle
+
+class pickle_adapter(adapter):
+
+    def __init__(self, file_name, dump_data = {}, dump_meta = {}):
+        self._file = file_name + '.pkl'
+        self._data = dump_data
+        self._meta = dump_meta
+        self._data['Title'] = 'data'
+        self._meta['Title'] = 'meta-data'
+
+    def write_data(self):
+        with open(self._file, 'wb') as pickle_file:
+            pickle.dump(self._meta, pickle_file)
+            pickle.dump(self._data, pickle_file)
+        print(f"Successful dump to {self._file}")
+
+    def read_data(self):
+        with open(self._file, 'rb') as pickle_file:
+            temp_data = pickle.load(pickle_file)
+        self._meta, self._data = temp_data
+        print(f"Successful read from {self._file}")
+        return [self._meta, self._data]
