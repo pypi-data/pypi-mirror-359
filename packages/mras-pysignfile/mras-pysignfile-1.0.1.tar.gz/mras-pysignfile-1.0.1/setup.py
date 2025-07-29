5a1936cbe3cbbf0b709f3e3d4e9923b61a1da403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package mras-pysignfile/1.0.1/mras-pysignfile-1.0.1.tar.gz

---
+++ packages/mras-pysignfile/mras-pysignfile-1.0.1.tar.gz/mras-pysignfile-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pysignfile",
+    version="1.0.1",
+    packages=["mras-pysignfile"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
