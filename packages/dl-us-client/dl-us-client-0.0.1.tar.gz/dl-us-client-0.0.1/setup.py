5df7aefbb42d2145bf33ec50f1fe7959a9f1ee13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-us-client/0.0.1/dl-us-client-0.0.1.tar.gz

---
+++ packages/dl-us-client/dl-us-client-0.0.1.tar.gz/dl-us-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-us-client",
+    version="0.0.1",
+    packages=["dl-us-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
