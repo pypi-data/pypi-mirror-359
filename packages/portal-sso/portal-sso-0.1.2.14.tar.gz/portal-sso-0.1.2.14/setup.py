1becdb94ae15d478cea3c2621355d93961e91d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package portal-sso/0.1.2.14/portal-sso-0.1.2.14.tar.gz

---
+++ packages/portal-sso/portal-sso-0.1.2.14.tar.gz/portal-sso-0.1.2.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="portal-sso",
+    version="0.1.2.14",
+    packages=["portal-sso"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
