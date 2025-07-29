f7f71e2294407afc0263cb842ec6f554dc9f66a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package tests-common/0.0.1/tests-common-0.0.1.tar.gz

---
+++ packages/tests-common/tests-common-0.0.1.tar.gz/tests-common-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tests-common",
+    version="0.0.1",
+    packages=["tests-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
