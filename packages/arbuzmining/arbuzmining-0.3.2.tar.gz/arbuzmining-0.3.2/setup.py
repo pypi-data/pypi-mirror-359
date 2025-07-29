e69aa7a3e9f4e5bcbe4ac0b6e9741995c1948d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package arbuzmining/0.3.2/arbuzmining-0.3.2.tar.gz

---
+++ packages/arbuzmining/arbuzmining-0.3.2.tar.gz/arbuzmining-0.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="arbuzmining",
+    version="0.3.2",
+    packages=["arbuzmining"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
