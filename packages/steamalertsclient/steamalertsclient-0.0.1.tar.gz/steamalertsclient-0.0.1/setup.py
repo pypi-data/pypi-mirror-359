bce1708f876687fd87fb70eebf840d1afce8d590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:45 +0000

    Add package steamalertsclient/0.0.1/steamalertsclient-0.0.1.tar.gz

---
+++ packages/steamalertsclient/steamalertsclient-0.0.1.tar.gz/steamalertsclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="steamalertsclient",
+    version="0.0.1",
+    packages=["steamalertsclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
