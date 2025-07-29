0b3f257e86cdd1bc848cdb244fb67089961d34ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:16 +0000

    Add package airflow-supervisor/1.8.0/airflow_supervisor-1.8.0-py3-none-any.whl

---
+++ packages/airflow-supervisor/airflow_supervisor-1.8.0-py3-none-any.whl/airflow_supervisor/tests/test_ssh.py
@@ -0,0 +1,27 @@
+import pytest
+from airflow.models.dag import DAG
+
+
+class TestSSH:
+    def test_ssh_setup(self, supervisor_airflow_ssh_configuration):
+        from airflow_supervisor import SupervisorSSHAirflowConfiguration
+
+        assert supervisor_airflow_ssh_configuration is not None
+        assert isinstance(supervisor_airflow_ssh_configuration, SupervisorSSHAirflowConfiguration)
+        assert supervisor_airflow_ssh_configuration.ssh_operator_args.ssh_hook.remote_host == "localhost"
+
+    def test_ssh_overrides(self, supervisor_airflow_ssh_configuration):
+        try:
+            from airflow_balancer import Host
+        except ImportError:
+            pytest.skip("airflow-balancer not available")
+
+        from airflow_supervisor import SupervisorSSH
+
+        dag = DAG(dag_id="test_dag", default_args={}, schedule=None, params={})
+        inst = SupervisorSSH(
+            dag=dag,
+            cfg=supervisor_airflow_ssh_configuration,
+            host=Host(name="test-host"),
+        )
+        assert inst._cfg.ssh_operator_args.ssh_hook.remote_host == "test-host"
