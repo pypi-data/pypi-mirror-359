cbc207f58d198f2b628d38f72caccd273885a0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-bitrix-gds/0.0.1/dl-connector-bitrix-gds-0.0.1.tar.gz

---
+++ packages/dl-connector-bitrix-gds/dl-connector-bitrix-gds-0.0.1.tar.gz/dl-connector-bitrix-gds-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-bitrix-gds",
+    version="0.0.1",
+    packages=["dl-connector-bitrix-gds"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
