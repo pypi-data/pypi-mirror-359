14441d9c75b17f4eb8044c5c39ec9044d732a5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:26 +0000

    Add package delinea/0.0.1/delinea-0.0.1.tar.gz

---
+++ packages/delinea/delinea-0.0.1.tar.gz/delinea-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="delinea",
+    version="0.0.1",
+    packages=["delinea"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
