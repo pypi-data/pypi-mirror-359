bb1e9c506cc200dc56ace7b35043bc6441f045f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:40 +0000

    Add package RPGshare/0.1.1/rpgshare-0.1.1.tar.gz

---
+++ packages/RPGshare/rpgshare-0.1.1.tar.gz/rpgshare-0.1.1/RPGshare/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RPGshare',
+    version='0.1.1',
+    description='中信证券榜单数据获取与处理库',
+    author='tedelon',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+    url='',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+) 
