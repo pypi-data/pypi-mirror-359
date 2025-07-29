8fcd8c1ce4941d41221fe68ab630253805588214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:54 +0000

    Add package sampledb/0.31.0/sampledb-0.31.0.tar.gz

---
+++ packages/sampledb/sampledb-0.31.0.tar.gz/sampledb-0.31.0/sampledb/frontend/users_forms.py
@@ -0,0 +1,76 @@
+# coding: utf-8
+"""
+
+"""
+
+import flask
+from flask_babel import _
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.validators import InputRequired, Email, Length, EqualTo, DataRequired, ValidationError
+
+
+class SigninForm(FlaskForm):
+    username = StringField(validators=[InputRequired()])
+    password = PasswordField(validators=[InputRequired()])
+    remember_me = BooleanField()
+    shared_device = BooleanField(default=False)
+
+
+class SignoutForm(FlaskForm):
+    pass
+
+
+class InvitationForm(FlaskForm):
+    email = StringField(validators=[InputRequired()])
+
+
+class RequestPasswordResetForm(FlaskForm):
+    email = StringField(validators=[InputRequired()])
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Contact Email', validators=[
+        InputRequired(message=_("Please enter your contact email.")),
+        Email(message=_("Please enter your contact email."))
+    ])
+    name = StringField('Name', validators=[InputRequired()])
+    password = PasswordField('New Password', validators=[
+        InputRequired(),
+        Length(min=3),
+        EqualTo('password2', message=_('Passwords must match'))
+    ])
+    password2 = PasswordField('Confirm password', validators=[InputRequired()])
+    submit = SubmitField('Register')
+
+    def validate_name(self, field: StringField) -> None:
+        if flask.current_app.config['ENFORCE_SPLIT_NAMES']:
+            name = field.data
+            if ', ' not in name[1:-1]:
+                raise ValidationError(_("Please enter your name as: surname, given names."))
+
+
+class PasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[
+        InputRequired(),
+        Length(min=3)
+    ])
+    submit = SubmitField('Change Password')
+
+
+class AuthenticationPasswordForm(FlaskForm):
+    id = IntegerField('Authentication_method_id', validators=[DataRequired()])
+    password = PasswordField('New Password', validators=[
+        InputRequired(),
+        Length(min=3)
+    ])
+    password_confirmation = PasswordField(validators=[
+        InputRequired(),
+        Length(min=3),
+        EqualTo('password')
+    ])
+    submit = SubmitField('Change Password')
+
+
+class RevokeInvitationForm(FlaskForm):
+    invitation_id = IntegerField(validators=[DataRequired()])
