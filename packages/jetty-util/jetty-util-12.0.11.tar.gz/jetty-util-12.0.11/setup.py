b134faedc2e723aa6cd4ae6cbdc8c3787233435a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:14 +0000

    Add package jetty-util/12.0.11/jetty-util-12.0.11.tar.gz

---
+++ packages/jetty-util/jetty-util-12.0.11.tar.gz/jetty-util-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-util",
+    version="12.0.11",
+    packages=["jetty-util"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
