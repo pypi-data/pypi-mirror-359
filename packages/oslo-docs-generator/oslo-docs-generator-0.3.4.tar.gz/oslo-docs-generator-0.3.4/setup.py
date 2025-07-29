b2d952bc4c60d4673046b8a381d5016d9b4f07e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:04 +0000

    Add package oslo-docs-generator/0.3.4/oslo-docs-generator-0.3.4.tar.gz

---
+++ packages/oslo-docs-generator/oslo-docs-generator-0.3.4.tar.gz/oslo-docs-generator-0.3.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oslo-docs-generator",
+    version="0.3.4",
+    packages=["oslo-docs-generator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
