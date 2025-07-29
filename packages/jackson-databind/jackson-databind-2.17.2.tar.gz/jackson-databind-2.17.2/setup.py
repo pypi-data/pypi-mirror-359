c4bd26647134cd795f558effc78d86601dee654e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-databind/2.17.2/jackson-databind-2.17.2.tar.gz

---
+++ packages/jackson-databind/jackson-databind-2.17.2.tar.gz/jackson-databind-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-databind",
+    version="2.17.2",
+    packages=["jackson-databind"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
