fa99a4cda9067720520f109349305f6aa069950d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:55 +0000

    Add package smart-selects/0.0.1/smart-selects-0.0.1.tar.gz

---
+++ packages/smart-selects/smart-selects-0.0.1.tar.gz/smart-selects-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="smart-selects",
+    version="0.0.1",
+    packages=["smart-selects"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
