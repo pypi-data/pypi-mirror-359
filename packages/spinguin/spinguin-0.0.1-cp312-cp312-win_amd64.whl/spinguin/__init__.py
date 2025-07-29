bd3164885226b1b81c93dee58e164fe75c7d5167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:38 +0000

    Add package spinguin/0.0.1/spinguin-0.0.1-cp312-cp312-win_amd64.whl

---
+++ packages/spinguin/spinguin-0.0.1-cp312-cp312-win_amd64.whl/spinguin/__init__.py
@@ -0,0 +1,10 @@
+"""
+Spinguin package is desined to be imported as `import spinguin as sg`, which
+reveals the user-friendly functionality to the user. This is documented under
+the Spinguin (Basic). For more in-depth documentation of the package, including
+the documentation of the re-usable, core functionality, see Spinguin (Advanced).
+"""
+
+# Make functionality from the API accessible directly under the spinguin
+# namespace
+from spinguin.api import *
---
+++ packages/spinguin/spinguin-0.0.1-cp312-cp312-win_amd64.whl/spinguin/core/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module provides the core functionality of Spinguin which is not dependent
+on any inbuilt class. Therefore, these functions can be reused in any context
+elsewhere. These can be accessed using::
+
+    # Replace module and function with something
+    from spinguin.core.module.function
+
+The preferred way to use Spinguin is to use the functionality under `spinguin`
+namespace, described in Spinguin (Basic), using::
+
+    import spinguin as sg
+"""
