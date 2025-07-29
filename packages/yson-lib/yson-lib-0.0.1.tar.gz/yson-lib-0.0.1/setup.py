7d31f2b3161680a085fd22b30916584a8dc1aa95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package yson-lib/0.0.1/yson-lib-0.0.1.tar.gz

---
+++ packages/yson-lib/yson-lib-0.0.1.tar.gz/yson-lib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yson-lib",
+    version="0.0.1",
+    packages=["yson-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
