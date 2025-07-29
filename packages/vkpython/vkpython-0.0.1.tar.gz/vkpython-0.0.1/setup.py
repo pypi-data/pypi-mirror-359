6f919f1810998bd747275b31f6e3d8bf2068099e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package vkpython/0.0.1/vkpython-0.0.1.tar.gz

---
+++ packages/vkpython/vkpython-0.0.1.tar.gz/vkpython-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpython",
+    version="0.0.1",
+    packages=["vkpython"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
