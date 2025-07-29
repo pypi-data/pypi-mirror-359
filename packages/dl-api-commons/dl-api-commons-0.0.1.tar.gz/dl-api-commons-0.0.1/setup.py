dcd9b78bf8f14586171e66e118c8e52e62fb02b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-api-commons/0.0.1/dl-api-commons-0.0.1.tar.gz

---
+++ packages/dl-api-commons/dl-api-commons-0.0.1.tar.gz/dl-api-commons-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-api-commons",
+    version="0.0.1",
+    packages=["dl-api-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
