5a7c072107944ca60f005e0c347950488088d249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package prosto-aio-s3-client/0.0.1/prosto-aio-s3-client-0.0.1.tar.gz

---
+++ packages/prosto-aio-s3-client/prosto-aio-s3-client-0.0.1.tar.gz/prosto-aio-s3-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prosto-aio-s3-client",
+    version="0.0.1",
+    packages=["prosto-aio-s3-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
