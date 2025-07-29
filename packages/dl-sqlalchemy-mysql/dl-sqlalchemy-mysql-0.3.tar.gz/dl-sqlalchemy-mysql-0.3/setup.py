e6262974018ae5074d8aacbbeb07dd460742f247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:02 +0000

    Add package dl-sqlalchemy-mysql/0.3/dl-sqlalchemy-mysql-0.3.tar.gz

---
+++ packages/dl-sqlalchemy-mysql/dl-sqlalchemy-mysql-0.3.tar.gz/dl-sqlalchemy-mysql-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-mysql",
+    version="0.3",
+    packages=["dl-sqlalchemy-mysql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
