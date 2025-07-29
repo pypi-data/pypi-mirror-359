f837d17a5a4e4d5ebd2dfdab319180ffbb67f9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:45 +0000

    Add package vkclips/0.0.1/vkclips-0.0.1.tar.gz

---
+++ packages/vkclips/vkclips-0.0.1.tar.gz/vkclips-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkclips",
+    version="0.0.1",
+    packages=["vkclips"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
