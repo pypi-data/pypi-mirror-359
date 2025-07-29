a203800869e5c995b680be1541599ca445acd1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:30 +0000

    Add package premai/0.5.0/premai-0.5.0-py3-none-any.whl

---
+++ packages/premai/premai-0.5.0-py3-none-any.whl/premai/types/__init__.py
@@ -0,0 +1,7 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .chat_completions_params import ChatCompletionsParams as ChatCompletionsParams
+from .chat_completions_response import ChatCompletionsResponse as ChatCompletionsResponse
+from .chat_list_models_response import ChatListModelsResponse as ChatListModelsResponse
