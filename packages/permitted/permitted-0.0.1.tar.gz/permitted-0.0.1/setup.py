5c0fbe50320de1d9c05a6ce02d1ce99704351a14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:10 +0000

    Add package permitted/0.0.1/permitted-0.0.1.tar.gz

---
+++ packages/permitted/permitted-0.0.1.tar.gz/permitted-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="permitted",
+    version="0.0.1",
+    packages=["permitted"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
