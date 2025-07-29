62d1b175c4aa32b99413455fadbf15af1ccce2b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package scrooge-service-runner/0.0.7/scrooge-service-runner-0.0.7.tar.gz

---
+++ packages/scrooge-service-runner/scrooge-service-runner-0.0.7.tar.gz/scrooge-service-runner-0.0.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-service-runner",
+    version="0.0.7",
+    packages=["scrooge-service-runner"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
