984936f559fc446ba3416c848fceeda6bf5cd5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package starwrap/0.0.1/starwrap-0.0.1.tar.gz

---
+++ packages/starwrap/starwrap-0.0.1.tar.gz/starwrap-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="starwrap",
+    version="0.0.1",
+    packages=["starwrap"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
