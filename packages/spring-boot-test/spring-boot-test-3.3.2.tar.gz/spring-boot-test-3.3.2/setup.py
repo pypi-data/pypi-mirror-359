f74f343bd365103cd5428290e8846f0a0697942a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package spring-boot-test/3.3.2/spring-boot-test-3.3.2.tar.gz

---
+++ packages/spring-boot-test/spring-boot-test-3.3.2.tar.gz/spring-boot-test-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-test",
+    version="3.3.2",
+    packages=["spring-boot-test"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
