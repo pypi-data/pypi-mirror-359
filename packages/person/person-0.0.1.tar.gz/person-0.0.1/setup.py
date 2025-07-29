b8b8f35d808bbaddf2abd470dcc8abb02fff2a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:10 +0000

    Add package person/0.0.1/person-0.0.1.tar.gz

---
+++ packages/person/person-0.0.1.tar.gz/person-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="person",
+    version="0.0.1",
+    packages=["person"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
