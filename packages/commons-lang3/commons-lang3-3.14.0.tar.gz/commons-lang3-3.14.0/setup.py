6f08cfd687e9a16f15174964a6e019cd6c729af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:55 +0000

    Add package commons-lang3/3.14.0/commons-lang3-3.14.0.tar.gz

---
+++ packages/commons-lang3/commons-lang3-3.14.0.tar.gz/commons-lang3-3.14.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="commons-lang3",
+    version="3.14.0",
+    packages=["commons-lang3"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
