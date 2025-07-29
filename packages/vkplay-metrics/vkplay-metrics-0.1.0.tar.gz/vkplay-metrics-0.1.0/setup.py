f3a6021c09c0f46ccdca2819d4667e5e3c7ed8ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package vkplay-metrics/0.1.0/vkplay-metrics-0.1.0.tar.gz

---
+++ packages/vkplay-metrics/vkplay-metrics-0.1.0.tar.gz/vkplay-metrics-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkplay-metrics",
+    version="0.1.0",
+    packages=["vkplay-metrics"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
