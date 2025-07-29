2ace2aa88a745f199830a186552b8c05ed6f6f64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:20 +0000

    Add package proxar/0.4.0/proxar-0.4.0-py3-none-any.whl

---
+++ packages/proxar/proxar-0.4.0-py3-none-any.whl/proxar/handlers/fetch.py
@@ -0,0 +1,42 @@
+import asyncio
+import logging
+from collections.abc import Awaitable
+
+import aiohttp
+
+from ..config import HEADERS
+from .storage import StorageHandler
+
+logger = logging.getLogger(__name__)
+
+
+class FetchHandler:
+    """Manages the operations of fetching proxies from various sources."""
+
+    def __init__(self, storage_handler: StorageHandler) -> None:
+        """Initialize the FetchHandler instance.
+
+        Args:
+            storage_handler: A handler for storage operations.
+        """
+        self.storage_handler = storage_handler
+
+        logger.debug("FetchHandler has been initialized.")
+
+    async def get_proxies(self) -> None:
+        """Fetch and store proxies from various sources."""
+        logger.debug("Attempting to fetch proxies from various sources.")
+
+        async with aiohttp.ClientSession(headers=HEADERS):
+            tasks: dict[str, Awaitable[None]] = {}
+
+            results = await asyncio.gather(*tasks.values(), return_exceptions=True)
+            task_names = list(tasks.keys())
+
+            for i, result in enumerate(results):
+                if isinstance(result, Exception):
+                    logger.exception(
+                        "Task '%s' raised an exception: %s", task_names[i], result
+                    )
+
+        logger.debug("Fetching proxy process completed.")
