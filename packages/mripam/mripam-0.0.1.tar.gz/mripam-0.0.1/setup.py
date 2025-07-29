cc3a7e03ad2abca5cf479ab1d79c68d5b9d64086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package mripam/0.0.1/mripam-0.0.1.tar.gz

---
+++ packages/mripam/mripam-0.0.1.tar.gz/mripam-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mripam",
+    version="0.0.1",
+    packages=["mripam"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
