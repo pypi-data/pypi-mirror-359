681d8bdd792ccc8261ca5b6e3003cb4082b0538e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package datalens-sqlalchemy-promql/0.1/datalens-sqlalchemy-promql-0.1.tar.gz

---
+++ packages/datalens-sqlalchemy-promql/datalens-sqlalchemy-promql-0.1.tar.gz/datalens-sqlalchemy-promql-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-promql",
+    version="0.1",
+    packages=["datalens-sqlalchemy-promql"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
