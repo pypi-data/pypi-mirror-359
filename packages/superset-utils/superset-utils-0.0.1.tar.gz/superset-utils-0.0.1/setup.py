61d7dc8461f930b72848899cea3fbd05260bea2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package superset-utils/0.0.1/superset-utils-0.0.1.tar.gz

---
+++ packages/superset-utils/superset-utils-0.0.1.tar.gz/superset-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="superset-utils",
+    version="0.0.1",
+    packages=["superset-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
