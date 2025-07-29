d447a6483bc775b9226264f0614db85e4dd8868d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:34 +0000

    Add package neuroporn-frontend/0.0.1/neuroporn-frontend-0.0.1.tar.gz

---
+++ packages/neuroporn-frontend/neuroporn-frontend-0.0.1.tar.gz/neuroporn-frontend-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="neuroporn-frontend",
+    version="0.0.1",
+    packages=["neuroporn-frontend"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
