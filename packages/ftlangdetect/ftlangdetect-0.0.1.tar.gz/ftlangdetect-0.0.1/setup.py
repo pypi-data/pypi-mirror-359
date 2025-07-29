834913f885b8a2e2af4c14fc084e2a597100d80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package ftlangdetect/0.0.1/ftlangdetect-0.0.1.tar.gz

---
+++ packages/ftlangdetect/ftlangdetect-0.0.1.tar.gz/ftlangdetect-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ftlangdetect",
+    version="0.0.1",
+    packages=["ftlangdetect"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
