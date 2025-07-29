db6216f396f431932dbe0eae7cd5cf015a738234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:02 +0000

    Add package spark-transformations/0.0.1/spark-transformations-0.0.1.tar.gz

---
+++ packages/spark-transformations/spark-transformations-0.0.1.tar.gz/spark-transformations-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spark-transformations",
+    version="0.0.1",
+    packages=["spark-transformations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
