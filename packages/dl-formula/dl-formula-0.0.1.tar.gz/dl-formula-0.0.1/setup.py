061a5f884db6bafbae3764540d4bb880bf43950e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-formula/0.0.1/dl-formula-0.0.1.tar.gz

---
+++ packages/dl-formula/dl-formula-0.0.1.tar.gz/dl-formula-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-formula",
+    version="0.0.1",
+    packages=["dl-formula"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
