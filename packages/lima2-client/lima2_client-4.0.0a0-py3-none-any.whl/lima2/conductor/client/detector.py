f6a4964e7a7ebf8089e5884c9c03e5762bf57ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:02 +0000

    Add package lima2-client/4.0.0a0/lima2_client-4.0.0a0-py3-none-any.whl

---
+++ packages/lima2-client/lima2_client-4.0.0a0-py3-none-any.whl/lima2/conductor/client/detector.py
@@ -0,0 +1,25 @@
+# This file is part of the Lima2 project
+#
+# Copyright (c) 2020-2024 Beamline Control Unit, ESRF
+# Distributed under the MIT licence. See LICENSE for more info.
+
+"""Conductor client /detector requests."""
+
+from typing import Any, cast
+
+from lima2.conductor.client.session import ConductorSession
+
+
+def info(session: ConductorSession) -> dict[str, Any]:
+    """Retrieve the detector info dictionary."""
+    return cast(dict[str, Any], session.get("/detector/info").json())
+
+
+def status(session: ConductorSession) -> dict[str, Any]:
+    """Retrieve the detector status dictionary."""
+    return cast(dict[str, Any], session.get("/detector/status").json())
+
+
+def capabilities(session: ConductorSession) -> dict[str, Any]:
+    """Retrieve the detector capabilities dictionary."""
+    return cast(dict[str, Any], session.get("/detector/capabilities").json())
