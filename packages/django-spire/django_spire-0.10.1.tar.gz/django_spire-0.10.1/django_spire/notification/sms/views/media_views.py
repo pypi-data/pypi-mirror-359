b6e5082c9d0a89a90a40f913f27c3260be98129a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:19 +0000

    Add package django-spire/0.10.1/django_spire-0.10.1.tar.gz

---
+++ packages/django-spire/django_spire-0.10.1.tar.gz/django_spire-0.10.1/django_spire/notification/sms/views/media_views.py
@@ -0,0 +1,28 @@
+import base64
+import uuid
+from io import BytesIO
+
+from PIL import Image
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from django_spire.notification.sms.models import SmsTemporaryMedia
+
+
+@csrf_exempt
+def external_temporary_media_view(request, external_access_key: uuid.UUID) -> HttpResponse:
+    temporary_media = SmsTemporaryMedia.objects.get(external_access_key=external_access_key)
+
+    image = Image.open(
+        BytesIO(base64.b64decode(temporary_media.content))
+    )
+    image = image.convert('P', palette=Image.ADAPTIVE, colors=32)
+
+    buffer = BytesIO()
+    image.save(buffer, 'PNG')
+
+    return HttpResponse(
+        content=buffer.getvalue(),
+        content_type=temporary_media.content_type
+    )
