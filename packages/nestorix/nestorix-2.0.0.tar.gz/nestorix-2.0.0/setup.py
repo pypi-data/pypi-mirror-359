78385dcdf882fcf4990c554e1e2321040d4b91ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package nestorix/2.0.0/nestorix-2.0.0.tar.gz

---
+++ packages/nestorix/nestorix-2.0.0.tar.gz/nestorix-2.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nestorix',  # Replace with your final chosen name
+    version='2.0.0',
+    description='Nestorix: A proprietary Python toolkit for deeply nested data manipulation and analysis.',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Ayush Agrawal',
+    author_email='aagrawal963@gmail.com',  # Replace with your email
+    url='https://github.com/yourusername/nestorix',  # Replace with your repo
+    license='Proprietary',
+    packages=find_packages(),
+    py_modules=['ayushlist', 'glaze'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: Other/Proprietary License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords=[
+        'nested-data', 'tree-structure', 'linked-list', 'data-structure',
+        'educational-data', 'data-manipulation', 'python-tools', 'dstruct',
+        'data-analysis', 'curriculum-parser', 'matrix-conversion', 'glaze',
+        'ayushlist', 'nestorix'
+    ],
+    python_requires='>=3.7',
+    include_package_data=True,
+    install_requires=[
+        'numpy'
+    ],
+)
