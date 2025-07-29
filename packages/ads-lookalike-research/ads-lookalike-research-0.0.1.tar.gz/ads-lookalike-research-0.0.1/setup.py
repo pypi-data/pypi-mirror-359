28c4b28fde7c6d593d6635912ef2dd1ca8f4046a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:29 +0000

    Add package ads-lookalike-research/0.0.1/ads-lookalike-research-0.0.1.tar.gz

---
+++ packages/ads-lookalike-research/ads-lookalike-research-0.0.1.tar.gz/ads-lookalike-research-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ads-lookalike-research",
+    version="0.0.1",
+    packages=["ads-lookalike-research"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
