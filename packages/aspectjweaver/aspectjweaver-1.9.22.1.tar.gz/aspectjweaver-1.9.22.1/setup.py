da14f8f9a1307da06cd66c7d22f5a439f22dfb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package aspectjweaver/1.9.22.1/aspectjweaver-1.9.22.1.tar.gz

---
+++ packages/aspectjweaver/aspectjweaver-1.9.22.1.tar.gz/aspectjweaver-1.9.22.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="aspectjweaver",
+    version="1.9.22.1",
+    packages=["aspectjweaver"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
