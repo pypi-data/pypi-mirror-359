49bcedeb8c37f9a8b5eb676687e13c3c598dc7d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:18 +0000

    Add package Qubx/0.6.61/qubx-0.6.61-cp312-cp312-manylinux_2_39_x86_64.whl

---
+++ packages/Qubx/qubx-0.6.61-cp312-cp312-manylinux_2_39_x86_64.whl/qubx/emitters/__init__.py
@@ -0,0 +1,26 @@
+"""
+Metric emitters for exporting metrics to external monitoring systems.
+"""
+
+from qubx.emitters.base import BaseMetricEmitter
+from qubx.emitters.composite import CompositeMetricEmitter
+from qubx.emitters.csv import CSVMetricEmitter
+from qubx.emitters.indicator import IndicatorEmitter, indicator_emitter
+from qubx.emitters.prometheus import PrometheusMetricEmitter
+
+__all__ = [
+    "BaseMetricEmitter",
+    "CompositeMetricEmitter",
+    "PrometheusMetricEmitter",
+    "CSVMetricEmitter",
+    "IndicatorEmitter",
+    "indicator_emitter",
+]
+
+try:
+    from qubx.emitters.questdb import QuestDBMetricEmitter
+
+    __all__.append("QuestDBMetricEmitter")
+except ImportError:
+    # QuestDB is optional
+    pass
