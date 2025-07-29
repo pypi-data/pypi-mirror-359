453d83e2fe949d030edee71c9c36a58d8220019d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-vktesters/0.0.1/ipy-vktesters-0.0.1.tar.gz

---
+++ packages/ipy-vktesters/ipy-vktesters-0.0.1.tar.gz/ipy-vktesters-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-vktesters",
+    version="0.0.1",
+    packages=["ipy-vktesters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
