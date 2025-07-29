6c55455c7b959872bbd37f49c1807260da766a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:42 +0000

    Add package toggles/0.0.1/toggles-0.0.1.tar.gz

---
+++ packages/toggles/toggles-0.0.1.tar.gz/toggles-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="toggles",
+    version="0.0.1",
+    packages=["toggles"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
