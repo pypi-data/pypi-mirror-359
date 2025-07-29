81e6aeb980b2ac195d8ac7eddb78dfff72b971da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package scrooge-common-api/0.0.6/scrooge-common-api-0.0.6.tar.gz

---
+++ packages/scrooge-common-api/scrooge-common-api-0.0.6.tar.gz/scrooge-common-api-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scrooge-common-api",
+    version="0.0.6",
+    packages=["scrooge-common-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
