33f2fe1888bcc45fdf6dce699498f86d8371eae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package arpakitlib/1.8.140/arpakitlib-1.8.140-py3-none-any.whl

---
+++ packages/arpakitlib/arpakitlib-1.8.140-py3-none-any.whl/arpakitlib/ar_cryptomus_api_client_util.py
@@ -0,0 +1,21 @@
+# arpakit
+import asyncio
+
+_ARPAKIT_LIB_MODULE_VERSION = "3.0"
+
+
+class CryptomusAPIClient:
+    pass
+
+
+def __example():
+    pass
+
+
+async def __async_example():
+    pass
+
+
+if __name__ == '__main__':
+    __example()
+    asyncio.run(__async_example())
