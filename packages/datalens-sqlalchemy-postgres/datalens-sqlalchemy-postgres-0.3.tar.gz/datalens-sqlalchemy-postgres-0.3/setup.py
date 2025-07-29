c29752a7d2fefaef766fb315f35e071f71454f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package datalens-sqlalchemy-postgres/0.3/datalens-sqlalchemy-postgres-0.3.tar.gz

---
+++ packages/datalens-sqlalchemy-postgres/datalens-sqlalchemy-postgres-0.3.tar.gz/datalens-sqlalchemy-postgres-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-postgres",
+    version="0.3",
+    packages=["datalens-sqlalchemy-postgres"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
