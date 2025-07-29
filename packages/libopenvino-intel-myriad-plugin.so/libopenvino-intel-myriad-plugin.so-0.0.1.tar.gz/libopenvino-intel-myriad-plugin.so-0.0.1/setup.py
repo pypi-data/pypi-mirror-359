1298d24aeb6295cc7929bde48709842724935cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:52 +0000

    Add package libopenvino-intel-myriad-plugin.so/0.0.1/libopenvino-intel-myriad-plugin.so-0.0.1.tar.gz

---
+++ packages/libopenvino-intel-myriad-plugin.so/libopenvino-intel-myriad-plugin.so-0.0.1.tar.gz/libopenvino-intel-myriad-plugin.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libopenvino-intel-myriad-plugin.so",
+    version="0.0.1",
+    packages=["libopenvino-intel-myriad-plugin.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
