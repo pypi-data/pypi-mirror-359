bc9630eeaf99053241d310ce44ebb46d91962056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-static-manifest/0.0.1/ipy-static-manifest-0.0.1.tar.gz

---
+++ packages/ipy-static-manifest/ipy-static-manifest-0.0.1.tar.gz/ipy-static-manifest-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-static-manifest",
+    version="0.0.1",
+    packages=["ipy-static-manifest"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
