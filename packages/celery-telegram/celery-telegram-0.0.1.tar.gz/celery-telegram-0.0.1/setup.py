c0732483fd0f3bdb7f33c98431d8bfe3ded52b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:53 +0000

    Add package celery-telegram/0.0.1/celery-telegram-0.0.1.tar.gz

---
+++ packages/celery-telegram/celery-telegram-0.0.1.tar.gz/celery-telegram-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="celery-telegram",
+    version="0.0.1",
+    packages=["celery-telegram"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
