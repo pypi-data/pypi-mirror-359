0ba574161160d6e85050ec98b9b4abbaecc2437a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package spring-aspects/6.1.11/spring-aspects-6.1.11.tar.gz

---
+++ packages/spring-aspects/spring-aspects-6.1.11.tar.gz/spring-aspects-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-aspects",
+    version="6.1.11",
+    packages=["spring-aspects"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
