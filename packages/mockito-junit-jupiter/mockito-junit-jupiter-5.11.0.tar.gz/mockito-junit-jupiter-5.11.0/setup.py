f65a03a4eea1f7748acbe686f04fcf289ea3cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package mockito-junit-jupiter/5.11.0/mockito-junit-jupiter-5.11.0.tar.gz

---
+++ packages/mockito-junit-jupiter/mockito-junit-jupiter-5.11.0.tar.gz/mockito-junit-jupiter-5.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mockito-junit-jupiter",
+    version="5.11.0",
+    packages=["mockito-junit-jupiter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
