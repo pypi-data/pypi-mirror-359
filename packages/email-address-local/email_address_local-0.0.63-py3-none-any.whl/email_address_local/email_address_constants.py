4e3f17cdb3a609df4496203a2594abb9ff940c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:15 +0000

    Add package email-address-local/0.0.63/email_address_local-0.0.63-py3-none-any.whl

---
+++ packages/email-address-local/email_address_local-0.0.63-py3-none-any.whl/email_address_local/email_address_constants.py
@@ -0,0 +1,15 @@
+class EmailAddressConstants:
+    EMAIL_ADDRESS_LOCAL_PYTHON_COMPONENT_ID = 174
+    EMAIL_ADDRESS_LOCAL_PYTHON_COMPONENT_NAME = 'email address local'
+    DEVELOPER_EMAIL = "idan.a@circ.zone"
+    EMAIL_ADDRESS_SCHEMA_NAME = "email_address"
+    EMAIL_ADDRESS_TABLE_NAME = "email_address_table"
+    EMAIL_ADDRESS_VIEW_TABLE_NAME = "email_address_view"
+    EMAIL_ADDRESS_ID_COLLUMN_NAME = "email_address_id"
+    EMAIL_ADDRESS_ML_TABLE_NAME = "email_address_ml_table"
+    EMAIL_ADDRESS_ML_VIEW_TABLE_NAME = "email_address_ml_view"
+    CONTACT_EMAIL_ADDRESS_TABLE_NAME = "contact_email_address_table"
+    CONTACT_EMAIL_ADDRESS_SCHEMA_NAME = "contact_email_address"
+    EMAIL_ADDRESS_ENTITY_NAME1 = "contact"
+    EMAIL_ADDRESS_ENTITY_NAME2 = "email_address"
+    EMAIL_ADDRESS_COLUMN_NAME = "email_address"
