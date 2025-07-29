07c8f9c8a93ba099046c5b37de0e8fc788c1f5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package nps/0.0.1/nps-0.0.1.tar.gz

---
+++ packages/nps/nps-0.0.1.tar.gz/nps-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="nps",
+    version="0.0.1",
+    packages=["nps"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
