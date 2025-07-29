16ba4d2aa32fa600a5ffc898440f36904dbcf629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package datalens-testing/0.0.1/datalens-testing-0.0.1.tar.gz

---
+++ packages/datalens-testing/datalens-testing-0.0.1.tar.gz/datalens-testing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-testing",
+    version="0.0.1",
+    packages=["datalens-testing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
