ae6e81f81cce1d18c9296859b3a57b9c9647e322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:03 +0000

    Add package player-client/1.0.1/player-client-1.0.1.tar.gz

---
+++ packages/player-client/player-client-1.0.1.tar.gz/player-client-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="player-client",
+    version="1.0.1",
+    packages=["player-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
