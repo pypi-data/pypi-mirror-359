e76eab8276243b111c4979bc1bf750a8b50512bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-ios-beta-announcement/0.0.1/ipy-ios-beta-announcement-0.0.1.tar.gz

---
+++ packages/ipy-ios-beta-announcement/ipy-ios-beta-announcement-0.0.1.tar.gz/ipy-ios-beta-announcement-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-ios-beta-announcement",
+    version="0.0.1",
+    packages=["ipy-ios-beta-announcement"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
