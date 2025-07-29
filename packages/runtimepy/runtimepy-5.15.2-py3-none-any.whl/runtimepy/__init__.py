bdacc96180ad8aa1709907205fa7071307f1a0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:02 +0000

    Add package runtimepy/5.15.2/runtimepy-5.15.2-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-5.15.2-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.2.3
+# hash=839ec3c3fa52de16f775789c0d38bf3e
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "5.15.2"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
---
+++ packages/runtimepy/runtimepy-5.15.2-py3-none-any.whl/runtimepy/data/js/sample.js
@@ -0,0 +1,2 @@
+console.log(`sample.js included (${tab.name})`);
+// tab.worker.bus({a: 1, b: 2, c: 3});
