8c5e8bfc8339e8e5de6c548675211cc99c3131fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package log-aggregator-client/0.0.dev64/log-aggregator-client-0.0.dev64.tar.gz

---
+++ packages/log-aggregator-client/log-aggregator-client-0.0.dev64.tar.gz/log-aggregator-client-0.0.dev64/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="log-aggregator-client",
+    version="0.0.dev64",
+    packages=["log-aggregator-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
