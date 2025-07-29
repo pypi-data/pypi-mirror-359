6dc878adffd8bcd59c54cc00c18c7c2da087f5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:22 +0000

    Add package rtmon-libs/0.0.1/rtmon-libs-0.0.1.tar.gz

---
+++ packages/rtmon-libs/rtmon-libs-0.0.1.tar.gz/rtmon-libs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rtmon-libs",
+    version="0.0.1",
+    packages=["rtmon-libs"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
