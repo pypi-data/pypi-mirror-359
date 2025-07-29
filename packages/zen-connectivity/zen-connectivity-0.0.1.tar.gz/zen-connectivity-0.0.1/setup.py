7b828436b231456c2d95155fcc8ce9f173412f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package zen-connectivity/0.0.1/zen-connectivity-0.0.1.tar.gz

---
+++ packages/zen-connectivity/zen-connectivity-0.0.1.tar.gz/zen-connectivity-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="zen-connectivity",
+    version="0.0.1",
+    packages=["zen-connectivity"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
