004ca360d16f4de11f79c4ebb3dc0b00a50b1730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package loadtests/0.0.1/loadtests-0.0.1.tar.gz

---
+++ packages/loadtests/loadtests-0.0.1.tar.gz/loadtests-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="loadtests",
+    version="0.0.1",
+    packages=["loadtests"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
