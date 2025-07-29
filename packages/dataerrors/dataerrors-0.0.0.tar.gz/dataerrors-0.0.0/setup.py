5e69200a642e32148420bcc2264df4b2eb3a62a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:28 +0000

    Add package dataerrors/0.0.0/dataerrors-0.0.0.tar.gz

---
+++ packages/dataerrors/dataerrors-0.0.0.tar.gz/dataerrors-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dataerrors",
+    version="0.0.0",
+    packages=["dataerrors"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
