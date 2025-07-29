d758dc29747680010accc5a1860f56ac2309948d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package feature-store-pb2/0.0.1/feature-store-pb2-0.0.1.tar.gz

---
+++ packages/feature-store-pb2/feature-store-pb2-0.0.1.tar.gz/feature-store-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="feature-store-pb2",
+    version="0.0.1",
+    packages=["feature-store-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
