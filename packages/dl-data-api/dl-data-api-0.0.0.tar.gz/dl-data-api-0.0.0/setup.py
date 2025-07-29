102fb92d3534d25def077e93a514de479ed907ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-data-api/0.0.0/dl-data-api-0.0.0.tar.gz

---
+++ packages/dl-data-api/dl-data-api-0.0.0.tar.gz/dl-data-api-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-data-api",
+    version="0.0.0",
+    packages=["dl-data-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
