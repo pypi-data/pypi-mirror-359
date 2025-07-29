dc854b4c20039f7c88327650568de94c235982db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:05 +0000

    Add package ProstoVKSecretsClient/0.0.3/ProstoVKSecretsClient-0.0.3.tar.gz

---
+++ packages/ProstoVKSecretsClient/ProstoVKSecretsClient-0.0.3.tar.gz/ProstoVKSecretsClient-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoVKSecretsClient",
+    version="0.0.3",
+    packages=["ProstoVKSecretsClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
