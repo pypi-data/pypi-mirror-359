b38e07f75548ccb8de1891e205c4554d2f65066f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:06 +0000

    Add package onecmdb/1.0.1/onecmdb-1.0.1.tar.gz

---
+++ packages/onecmdb/onecmdb-1.0.1.tar.gz/onecmdb-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="onecmdb",
+    version="1.0.1",
+    packages=["onecmdb"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
