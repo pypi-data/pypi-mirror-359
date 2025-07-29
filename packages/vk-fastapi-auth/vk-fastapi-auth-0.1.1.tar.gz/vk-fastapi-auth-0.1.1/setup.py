8a068b91adb45fb670222239bc4a9d805b514a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:48 +0000

    Add package vk-fastapi-auth/0.1.1/vk-fastapi-auth-0.1.1.tar.gz

---
+++ packages/vk-fastapi-auth/vk-fastapi-auth-0.1.1.tar.gz/vk-fastapi-auth-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-fastapi-auth",
+    version="0.1.1",
+    packages=["vk-fastapi-auth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
