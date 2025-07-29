c22add9ba9a8da89f7e45a302d1649ea08001fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:43 +0000

    Add package json-loader-with-include/0.3.3/json_loader_with_include-0.3.3-py3-none-any.whl

---
+++ packages/json-loader-with-include/json_loader_with_include-0.3.3-py3-none-any.whl/json_loader_with_include/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.warn(
+    "DEPRECATED: 'json-loader-with-include' has been renamed to 'pypaya-json'. "
+    "Please switch to 'pip install pypaya-json' for future updates. "
+    "This package will no longer receive updates.",
+    DeprecationWarning,
+    stacklevel=2
+)
