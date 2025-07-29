ab23b324a754e6b319bdfb0c30510b7801b3a77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:06 +0000

    Add package darray/0.3.2/darray-0.3.2.tar.gz

---
+++ packages/darray/darray-0.3.2.tar.gz/darray-0.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="darray",
+    version="0.3.2",
+    packages=["darray"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
