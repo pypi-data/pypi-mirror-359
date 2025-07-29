c005f1285826329e55b9c786aacf152899e2382d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package query-parser-py/0.0.1/query-parser-py-0.0.1.tar.gz

---
+++ packages/query-parser-py/query-parser-py-0.0.1.tar.gz/query-parser-py-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="query-parser-py",
+    version="0.0.1",
+    packages=["query-parser-py"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
