4a5c80a4d445aa135bd40d32ba4166f9bb2fa05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:33 +0000

    Add package calendar-access/1.0.0/calendar-access-1.0.0.tar.gz

---
+++ packages/calendar-access/calendar-access-1.0.0.tar.gz/calendar-access-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="calendar-access",
+    version="1.0.0",
+    packages=["calendar-access"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
