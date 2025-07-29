413203a40dfe175b473756ce9f05c835e5e16946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package javaluator/3.0.4/javaluator-3.0.4.tar.gz

---
+++ packages/javaluator/javaluator-3.0.4.tar.gz/javaluator-3.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="javaluator",
+    version="3.0.4",
+    packages=["javaluator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
