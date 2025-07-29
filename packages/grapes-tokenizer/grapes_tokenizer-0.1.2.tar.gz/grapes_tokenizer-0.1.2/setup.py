511cc349889676e99250b52eb9be9d9d5e7db507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:00 +0000

    Add package grapes-tokenizer/0.1.2/grapes_tokenizer-0.1.2.tar.gz

---
+++ packages/grapes-tokenizer/grapes_tokenizer-0.1.2.tar.gz/grapes_tokenizer-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="grapes-tokenizer",
+    version="0.1.2",
+    author="Akash Pittalwar",
+    author_email="akashpittalwar107@gmail.com",
+    description="A simple sum-based tokenizer for NLP/LLM research",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/akashpittalwar/Grapes-tokenizer",
+    project_urls={
+        "Source": "https://github.com/akashpittalwar/Grapes-tokenizer",
+        "Tracker": "https://github.com/akashpittalwar/Grapes-tokenizer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(exclude=["tests*", "examples*"]),
+    python_requires=">=3.6",
+)
