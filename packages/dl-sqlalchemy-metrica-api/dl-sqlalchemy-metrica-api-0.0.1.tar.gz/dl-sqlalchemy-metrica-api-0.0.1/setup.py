d9c217e0f1bc597d4557a2df6e41a67f3a525fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-metrica-api/0.0.1/dl-sqlalchemy-metrica-api-0.0.1.tar.gz

---
+++ packages/dl-sqlalchemy-metrica-api/dl-sqlalchemy-metrica-api-0.0.1.tar.gz/dl-sqlalchemy-metrica-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-metrica-api",
+    version="0.0.1",
+    packages=["dl-sqlalchemy-metrica-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
