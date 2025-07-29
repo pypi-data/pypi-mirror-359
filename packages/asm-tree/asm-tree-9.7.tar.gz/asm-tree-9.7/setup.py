6624b2debcb9377cf93a071427d62a1ecc6b57b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:00 +0000

    Add package asm-tree/9.7/asm-tree-9.7.tar.gz

---
+++ packages/asm-tree/asm-tree-9.7.tar.gz/asm-tree-9.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="asm-tree",
+    version="9.7",
+    packages=["asm-tree"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
