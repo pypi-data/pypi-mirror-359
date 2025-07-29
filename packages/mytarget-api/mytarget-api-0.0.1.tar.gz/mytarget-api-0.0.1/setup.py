1264ebd381b7e55cace485c9cb951c7c133c3163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:23 +0000

    Add package mytarget-api/0.0.1/mytarget-api-0.0.1.tar.gz

---
+++ packages/mytarget-api/mytarget-api-0.0.1.tar.gz/mytarget-api-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mytarget-api",
+    version="0.0.1",
+    packages=["mytarget-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
