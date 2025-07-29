dcb5bfca7f908782138e383880efb228958091ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package modelcluster/0.0.1/modelcluster-0.0.1.tar.gz

---
+++ packages/modelcluster/modelcluster-0.0.1.tar.gz/modelcluster-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="modelcluster",
+    version="0.0.1",
+    packages=["modelcluster"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
