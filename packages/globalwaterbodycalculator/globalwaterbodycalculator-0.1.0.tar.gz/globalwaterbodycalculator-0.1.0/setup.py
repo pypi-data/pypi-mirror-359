6a4da7389d6200cb3c44133aa982bff9736eadee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:51 +0000

    Add package globalwaterbodycalculator/0.1.0/globalwaterbodycalculator-0.1.0.tar.gz

---
+++ packages/globalwaterbodycalculator/globalwaterbodycalculator-0.1.0.tar.gz/globalwaterbodycalculator-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='globalwaterbodycalculator',
+    version='0.1.0',
+    description='A package for calculating water storage based on global water body depth-area-volume equation and visualizing waterbody geometries.',
+    author='Shengde Yu',
+    author_email='s228yu@uwaterloo.ca',
+    url='https://github.com/SYubaby/Globalwaterbodycalculator',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'globalwaterbodycalculator': ['data/all_equations.csv', 'data/HydroLAKES_polys_v10_shp/*'],
+    },
+    install_requires=[
+        'pandas',
+        'numpy',
+        'sympy',
+        'geopy',
+        'matplotlib',
+        'scipy',
+        'geopy',
+        'rasterio',
+        'sympy',
+        'scikit-learn',
+        'gdal',
+        'osgeo',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
