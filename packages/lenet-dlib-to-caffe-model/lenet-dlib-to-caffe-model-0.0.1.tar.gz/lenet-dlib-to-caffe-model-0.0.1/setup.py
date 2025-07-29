7168d002e7a585a7acedf9d862cbb8c47ea1de9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package lenet-dlib-to-caffe-model/0.0.1/lenet-dlib-to-caffe-model-0.0.1.tar.gz

---
+++ packages/lenet-dlib-to-caffe-model/lenet-dlib-to-caffe-model-0.0.1.tar.gz/lenet-dlib-to-caffe-model-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="lenet-dlib-to-caffe-model",
+    version="0.0.1",
+    packages=["lenet-dlib-to-caffe-model"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
