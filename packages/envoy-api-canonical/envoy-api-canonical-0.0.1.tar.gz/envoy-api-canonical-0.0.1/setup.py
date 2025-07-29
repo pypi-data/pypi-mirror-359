cf80d588869b88dadc0795ac35503725017f6d0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package envoy-api-canonical/0.0.1/envoy-api-canonical-0.0.1.tar.gz

---
+++ packages/envoy-api-canonical/envoy-api-canonical-0.0.1.tar.gz/envoy-api-canonical-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="envoy-api-canonical",
+    version="0.0.1",
+    packages=["envoy-api-canonical"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
