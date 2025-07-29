77c9fa174165200b7d563e56d7d479389d14de87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-pms/0.0.1/ipy-pms-0.0.1.tar.gz

---
+++ packages/ipy-pms/ipy-pms-0.0.1.tar.gz/ipy-pms-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-pms",
+    version="0.0.1",
+    packages=["ipy-pms"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
