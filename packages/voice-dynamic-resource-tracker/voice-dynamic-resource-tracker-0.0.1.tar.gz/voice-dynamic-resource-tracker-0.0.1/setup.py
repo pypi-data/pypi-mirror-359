4497ef0a06635a8a19e03d43355f049a96f5c9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package voice-dynamic-resource-tracker/0.0.1/voice-dynamic-resource-tracker-0.0.1.tar.gz

---
+++ packages/voice-dynamic-resource-tracker/voice-dynamic-resource-tracker-0.0.1.tar.gz/voice-dynamic-resource-tracker-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice-dynamic-resource-tracker",
+    version="0.0.1",
+    packages=["voice-dynamic-resource-tracker"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
