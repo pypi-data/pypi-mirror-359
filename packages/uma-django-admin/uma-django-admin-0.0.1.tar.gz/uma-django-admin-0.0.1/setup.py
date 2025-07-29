b12465a4339995fab1a609f2708a27b1b06f1fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package uma-django-admin/0.0.1/uma-django-admin-0.0.1.tar.gz

---
+++ packages/uma-django-admin/uma-django-admin-0.0.1.tar.gz/uma-django-admin-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="uma-django-admin",
+    version="0.0.1",
+    packages=["uma-django-admin"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
