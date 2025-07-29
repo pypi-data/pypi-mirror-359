5b2baacfa1b97b3f1e841085186822b59410dac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package autobf/1.7.8/autobf-1.7.8.tar.gz

---
+++ packages/autobf/autobf-1.7.8.tar.gz/autobf-1.7.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+# 若Discription.md中有中文 須加上 encoding="utf-8"
+with open("README.md", "r",encoding="utf-8") as f:
+    long_description = f.read()
+install_requires=["opencv-contrib-python","pyautogui","pydirectinput"]
+setuptools.setup(
+    name = "autobf",
+    version = " 1.7.8",
+    author = "KuoYuanLi",
+    author_email="funny4875@gmail.com",
+    description="roblox blox fruit tool",
+    install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/autobf/",                                         packages=setuptools.find_packages(),     
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+    )
