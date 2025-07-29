43fd0d662d8d228152750341f2fbdc660de54042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package models-fixed/0.0.1/models-fixed-0.0.1.tar.gz

---
+++ packages/models-fixed/models-fixed-0.0.1.tar.gz/models-fixed-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="models-fixed",
+    version="0.0.1",
+    packages=["models-fixed"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
