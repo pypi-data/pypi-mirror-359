4d3afd02e12ba434554e677538f9599102f4302b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:35 +0000

    Add package mockito-core/5.11.0/mockito-core-5.11.0.tar.gz

---
+++ packages/mockito-core/mockito-core-5.11.0.tar.gz/mockito-core-5.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mockito-core",
+    version="5.11.0",
+    packages=["mockito-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
