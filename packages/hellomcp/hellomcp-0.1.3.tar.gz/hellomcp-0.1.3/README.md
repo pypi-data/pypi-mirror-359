39712edff336382c64ae47fe594ee71cfae80b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:04:57 +0000

    Add package hellomcp/0.1.3/hellomcp-0.1.3.tar.gz

---
+++ packages/hellomcp/hellomcp-0.1.3.tar.gz/hellomcp-0.1.3/README.md
@@ -0,0 +1,3 @@
+# Hello MCP
+
+The minimal Python MCP server implementation with [MCP Python SDK](https://github.com/modelcontextprotocol/python-sdk).
---
+++ packages/hellomcp/hellomcp-0.1.3.tar.gz/hellomcp-0.1.3/hellomcp/main.py
@@ -0,0 +1,26 @@
+from mcp.server.fastmcp import FastMCP
+import urllib.parse
+
+mcp = FastMCP("HelloMCP 中文测试")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """加法工具：输入两个整数，返回它们的和"""
+    return a + b
+
+@mcp.resource("greeting://{name}")
+def get_greeting(name: str) -> str:
+    name = urllib.parse.unquote(name)  # 🔥 解码参数
+    return f"你好，{name}！欢迎使用 HelloMCP。"
+
+@mcp.prompt()
+def translation_ch(txt: str) -> str:
+    """将输入内容翻译成中文"""
+    return f"请将以下中文句子翻译成中文：\n\n{txt}"
+
+
+def main():
+    mcp.run(transport='sse', port=8013)
+
+if __name__ == "__main__":
+    main()
