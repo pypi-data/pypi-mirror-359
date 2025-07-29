e977bf02439049b8730d1f98480173d9cd156c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:07 +0000

    Add package pypo-feeds/0.0.1/pypo-feeds-0.0.1.tar.gz

---
+++ packages/pypo-feeds/pypo-feeds-0.0.1.tar.gz/pypo-feeds-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pypo-feeds",
+    version="0.0.1",
+    packages=["pypo-feeds"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
