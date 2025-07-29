8950d5a26c9d71c42131b00db6d5145adcea3566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package pg-models/0.0.1/pg-models-0.0.1.tar.gz

---
+++ packages/pg-models/pg-models-0.0.1.tar.gz/pg-models-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pg-models",
+    version="0.0.1",
+    packages=["pg-models"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
