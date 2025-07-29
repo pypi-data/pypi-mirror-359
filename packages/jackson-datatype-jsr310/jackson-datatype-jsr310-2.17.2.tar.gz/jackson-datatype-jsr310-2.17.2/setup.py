7cc4eb05824cc263f3518d7ca5f0823b7957e2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package jackson-datatype-jsr310/2.17.2/jackson-datatype-jsr310-2.17.2.tar.gz

---
+++ packages/jackson-datatype-jsr310/jackson-datatype-jsr310-2.17.2.tar.gz/jackson-datatype-jsr310-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-datatype-jsr310",
+    version="2.17.2",
+    packages=["jackson-datatype-jsr310"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
