0c7d4401410e11ea78b25d715fe4f02bfc53b793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-rate-limiter/0.0.1/dl-rate-limiter-0.0.1.tar.gz

---
+++ packages/dl-rate-limiter/dl-rate-limiter-0.0.1.tar.gz/dl-rate-limiter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-rate-limiter",
+    version="0.0.1",
+    packages=["dl-rate-limiter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
