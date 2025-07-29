0383ebf24d387b0e8182c3b3e82dc5b0361ea9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:06 +0000

    Add package spring-core/6.1.11/spring-core-6.1.11.tar.gz

---
+++ packages/spring-core/spring-core-6.1.11.tar.gz/spring-core-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-core",
+    version="6.1.11",
+    packages=["spring-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
