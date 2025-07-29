87c61cdc8cf9daade4ef7e685ec8f6434a4eaf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:38 +0000

    Add package artm/0.0.1/artm-0.0.1.tar.gz

---
+++ packages/artm/artm-0.0.1.tar.gz/artm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="artm",
+    version="0.0.1",
+    packages=["artm"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
