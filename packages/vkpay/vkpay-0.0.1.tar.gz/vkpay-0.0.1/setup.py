4926b0be0e41674dbadf328d7cad5e0dcb7f4caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package vkpay/0.0.1/vkpay-0.0.1.tar.gz

---
+++ packages/vkpay/vkpay-0.0.1.tar.gz/vkpay-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpay",
+    version="0.0.1",
+    packages=["vkpay"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
