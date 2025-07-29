02ee027801396fe9729fc03ecfe36e12e047701e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-android-beta-announcement/0.0.1/ipy-android-beta-announcement-0.0.1.tar.gz

---
+++ packages/ipy-android-beta-announcement/ipy-android-beta-announcement-0.0.1.tar.gz/ipy-android-beta-announcement-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-android-beta-announcement",
+    version="0.0.1",
+    packages=["ipy-android-beta-announcement"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
