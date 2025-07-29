d1e0904d00d1bea3873532aa2680a93c01811a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:45 +0000

    Add package ktsutils/1.0.1/ktsutils-1.0.1.tar.gz

---
+++ packages/ktsutils/ktsutils-1.0.1.tar.gz/ktsutils-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ktsutils",
+    version="1.0.1",
+    packages=["ktsutils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
