7c77693c748b48da6705f3d493e49d8c8b073f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:01 +0000

    Add package dl-file-uploader-worker-lib/0.0.0/dl-file-uploader-worker-lib-0.0.0.tar.gz

---
+++ packages/dl-file-uploader-worker-lib/dl-file-uploader-worker-lib-0.0.0.tar.gz/dl-file-uploader-worker-lib-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-file-uploader-worker-lib",
+    version="0.0.0",
+    packages=["dl-file-uploader-worker-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
