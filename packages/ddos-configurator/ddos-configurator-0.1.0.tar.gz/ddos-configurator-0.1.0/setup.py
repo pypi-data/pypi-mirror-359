d83a93231a099a796e865f3317136a6dd8e144c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package ddos-configurator/0.1.0/ddos-configurator-0.1.0.tar.gz

---
+++ packages/ddos-configurator/ddos-configurator-0.1.0.tar.gz/ddos-configurator-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ddos-configurator",
+    version="0.1.0",
+    packages=["ddos-configurator"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
