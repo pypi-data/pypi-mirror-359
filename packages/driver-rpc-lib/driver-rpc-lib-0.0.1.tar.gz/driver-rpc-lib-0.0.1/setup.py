67f0b30abc21944911c08c004fc1e156e3c14f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package driver-rpc-lib/0.0.1/driver-rpc-lib-0.0.1.tar.gz

---
+++ packages/driver-rpc-lib/driver-rpc-lib-0.0.1.tar.gz/driver-rpc-lib-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="driver-rpc-lib",
+    version="0.0.1",
+    packages=["driver-rpc-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
