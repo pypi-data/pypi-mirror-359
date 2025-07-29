aba619115b3df72afb7e4e613e3272b3282244a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:51 +0000

    Add package pygitclient/1.0.0/pygitclient-1.0.0.tar.gz

---
+++ packages/pygitclient/pygitclient-1.0.0.tar.gz/pygitclient-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pygitclient",
+    version="1.0.0",
+    packages=["pygitclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
