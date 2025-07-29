3397053d5346f3c17000f875311d62fc6faae03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:56 +0000

    Add package datalens-sqlalchemy-oracle/0.3/datalens-sqlalchemy-oracle-0.3.tar.gz

---
+++ packages/datalens-sqlalchemy-oracle/datalens-sqlalchemy-oracle-0.3.tar.gz/datalens-sqlalchemy-oracle-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-sqlalchemy-oracle",
+    version="0.3",
+    packages=["datalens-sqlalchemy-oracle"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
