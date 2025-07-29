d6f1f97d965f8a952c5b3ad5d434ee71483c358d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-archive/0.0.1/ipy-archive-0.0.1.tar.gz

---
+++ packages/ipy-archive/ipy-archive-0.0.1.tar.gz/ipy-archive-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-archive",
+    version="0.0.1",
+    packages=["ipy-archive"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
