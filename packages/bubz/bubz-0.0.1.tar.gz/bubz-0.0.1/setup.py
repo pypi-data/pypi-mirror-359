0943b3849f2224e29cdbb56edc0c938381aa4a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:33 +0000

    Add package bubz/0.0.1/bubz-0.0.1.tar.gz

---
+++ packages/bubz/bubz-0.0.1.tar.gz/bubz-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="bubz",
+    version="0.0.1",
+    packages=["bubz"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
