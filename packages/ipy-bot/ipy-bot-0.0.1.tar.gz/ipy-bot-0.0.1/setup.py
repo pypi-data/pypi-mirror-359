18cb18a569b36e3dc857f0993a68ccb54141ff78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-bot/0.0.1/ipy-bot-0.0.1.tar.gz

---
+++ packages/ipy-bot/ipy-bot-0.0.1.tar.gz/ipy-bot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-bot",
+    version="0.0.1",
+    packages=["ipy-bot"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
