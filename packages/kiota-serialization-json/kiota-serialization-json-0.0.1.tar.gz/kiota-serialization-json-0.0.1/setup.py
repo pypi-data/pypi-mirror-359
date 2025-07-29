c543b655b994d8a8d720dd918f3249c0f33b2844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package kiota-serialization-json/0.0.1/kiota-serialization-json-0.0.1.tar.gz

---
+++ packages/kiota-serialization-json/kiota-serialization-json-0.0.1.tar.gz/kiota-serialization-json-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kiota-serialization-json",
+    version="0.0.1",
+    packages=["kiota-serialization-json"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
