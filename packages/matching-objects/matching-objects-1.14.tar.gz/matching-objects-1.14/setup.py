d7c9354cb918577cecd0b2727cc9df542c5f8a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:00 +0000

    Add package matching-objects/1.14/matching-objects-1.14.tar.gz

---
+++ packages/matching-objects/matching-objects-1.14.tar.gz/matching-objects-1.14/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="matching-objects",
+    version="1.14",
+    packages=["matching-objects"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
