f1c6e066842b6346f720bce56f0b96c20b0737e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-store-upload-huawei/0.0.1/ipy-store-upload-huawei-0.0.1.tar.gz

---
+++ packages/ipy-store-upload-huawei/ipy-store-upload-huawei-0.0.1.tar.gz/ipy-store-upload-huawei-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-store-upload-huawei",
+    version="0.0.1",
+    packages=["ipy-store-upload-huawei"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
