03f4e5c44b200fc72ce559615baa7817fa016544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:56 +0000

    Add package textbuddy/0.3.3/textbuddy-0.3.3.tar.gz

---
+++ packages/textbuddy/textbuddy-0.3.3.tar.gz/textbuddy-0.3.3/textbuddy/__init__.py
@@ -0,0 +1,26 @@
+# textbuddy/__init__.py
+
+from .utils import (
+    format_rupee,
+    standardize_indian_phone,
+    parse_indian_date,
+    clean_text_basic,
+    remove_emoji,
+    about # <--- This ensures the about() function is accessible
+)
+
+__all__ = [
+    'format_rupee',
+    'standardize_indian_phone',
+    'parse_indian_date',
+    'clean_text_basic',
+    'remove_emoji',
+    'about' # <--- This makes 'about' part of what's exposed by 'import *'
+]
+
+__version__ = "0.3.3" # <--- MAKE SURE THIS IS 0.1.3
+
+# This message will print to the console when the package is first imported in a session
+print(f"\n✨ Welcome to 'textbuddy' v{__version__}! ✨")
+print("Developed by [Your Full Name Here].") # <--- Your name will show here
+print("Simplifying text tasks for an Indian context. Happy coding!\n")
