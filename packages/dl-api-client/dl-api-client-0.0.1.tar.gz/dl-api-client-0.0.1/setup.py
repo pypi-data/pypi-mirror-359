743c81a37b6cda3d0a9f3f74e2eb0adac846e63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-api-client/0.0.1/dl-api-client-0.0.1.tar.gz

---
+++ packages/dl-api-client/dl-api-client-0.0.1.tar.gz/dl-api-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-api-client",
+    version="0.0.1",
+    packages=["dl-api-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
