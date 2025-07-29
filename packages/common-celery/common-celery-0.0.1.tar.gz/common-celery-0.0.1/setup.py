00aa1b629d5a7d86ff5e93d61c17071d7a7e58e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package common-celery/0.0.1/common-celery-0.0.1.tar.gz

---
+++ packages/common-celery/common-celery-0.0.1.tar.gz/common-celery-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="common-celery",
+    version="0.0.1",
+    packages=["common-celery"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
