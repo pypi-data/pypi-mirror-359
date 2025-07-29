9a1e3a0f8c19164ba7c1f03fabdcc5cf1b4e9bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:39 +0000

    Add package cutlass-library/0.0.1/cutlass-library-0.0.1.tar.gz

---
+++ packages/cutlass-library/cutlass-library-0.0.1.tar.gz/cutlass-library-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cutlass-library",
+    version="0.0.1",
+    packages=["cutlass-library"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
