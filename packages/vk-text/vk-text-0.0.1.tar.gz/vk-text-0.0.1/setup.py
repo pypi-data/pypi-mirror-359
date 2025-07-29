cbc24887791291d6a53e216f67bc6e5e90b360d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package vk-text/0.0.1/vk-text-0.0.1.tar.gz

---
+++ packages/vk-text/vk-text-0.0.1.tar.gz/vk-text-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-text",
+    version="0.0.1",
+    packages=["vk-text"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
