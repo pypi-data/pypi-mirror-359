9d88b465c6ff344a132b87a801c1211be641abbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package tomcat-embed-el/10.1.26/tomcat-embed-el-10.1.26.tar.gz

---
+++ packages/tomcat-embed-el/tomcat-embed-el-10.1.26.tar.gz/tomcat-embed-el-10.1.26/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tomcat-embed-el",
+    version="10.1.26",
+    packages=["tomcat-embed-el"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
