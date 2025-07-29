9704ad3859901d307dabdb9952c02f9a3bfd0f8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-query-processing/0.0.1/dl-query-processing-0.0.1.tar.gz

---
+++ packages/dl-query-processing/dl-query-processing-0.0.1.tar.gz/dl-query-processing-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-query-processing",
+    version="0.0.1",
+    packages=["dl-query-processing"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
