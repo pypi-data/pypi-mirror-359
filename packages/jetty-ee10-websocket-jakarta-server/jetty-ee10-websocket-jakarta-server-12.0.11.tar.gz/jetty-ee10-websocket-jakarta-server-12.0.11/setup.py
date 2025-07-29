b70f492d1117b7eedabc2378eb478f8483ab4d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package jetty-ee10-websocket-jakarta-server/12.0.11/jetty-ee10-websocket-jakarta-server-12.0.11.tar.gz

---
+++ packages/jetty-ee10-websocket-jakarta-server/jetty-ee10-websocket-jakarta-server-12.0.11.tar.gz/jetty-ee10-websocket-jakarta-server-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-ee10-websocket-jakarta-server",
+    version="12.0.11",
+    packages=["jetty-ee10-websocket-jakarta-server"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
