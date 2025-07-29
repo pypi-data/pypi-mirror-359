b277b1ec482891976f01f63ed369ed9ab8acc7a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:34 +0000

    Add package anytls-py/0.1.14/anytls_py-0.1.14-py3-none-any.whl

---
+++ packages/anytls-py/anytls_py-0.1.14-py3-none-any.whl/anytls/core/constants.py
@@ -0,0 +1,38 @@
+"""全局常量配置"""
+
+from pathlib import Path
+
+BASE_DIR = Path("/home/anytls")
+DOCKER_COMPOSE_PATH = BASE_DIR / "docker-compose.yaml"
+CONFIG_PATH = BASE_DIR / "config.yaml"
+
+LISTEN_PORT = 8443
+SERVICE_IMAGE = "metacubex/mihomo:latest"
+
+MIHOMO_PROXIES_DOCS = "https://wiki.metacubex.one/config/proxies/anytls/#anytls"
+
+TOOL_NAME = "AnyTLS"
+COMPOSE_SERVICE_NAME = "anytls-inbound"
+COMPOSE_CONTAINER_PREFIX = f"{COMPOSE_SERVICE_NAME}-"
+
+MIHOMO_LISTEN_TYPE = "anytls"
+MIHOMO_LISTENER_NAME_PREFIX = f"{MIHOMO_LISTEN_TYPE}-in-"
+
+SHARE_LINK_TPL = "anytls://{pwd}@{server}:{port}?sni={sni}#{alias}"
+
+# 客户端配置模板
+DEFAULT_CLIENT_CONFIG = {
+    "name": "{{DOMAIN}}",
+    "type": MIHOMO_LISTEN_TYPE,
+    "server": "{{PUBLIC_IP}}",
+    "port": "{{PORT}}",
+    "password": "{{PASSWORD}}",
+    "client_fingerprint": "chrome",
+    "udp": True,
+    "idle_session_check_interval": 30,
+    "idle_session_timeout": 30,
+    "min_idle_session": 0,
+    "sni": "{{SNI}}",
+    "alpn": ["h2", "http/1.1"],
+    "skip_cert_verify": False,
+}
