d1d2084358c9d47e86fbc68ca7c5079853197cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package kiota-authentication-azure/0.0.1/kiota-authentication-azure-0.0.1.tar.gz

---
+++ packages/kiota-authentication-azure/kiota-authentication-azure-0.0.1.tar.gz/kiota-authentication-azure-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="kiota-authentication-azure",
+    version="0.0.1",
+    packages=["kiota-authentication-azure"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
