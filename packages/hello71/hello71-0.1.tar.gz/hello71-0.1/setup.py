34e5a3123c58560c414a519173a093298d121332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:56 +0000

    Add package hello71/0.1/hello71-0.1.tar.gz

---
+++ packages/hello71/hello71-0.1.tar.gz/hello71-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hello71',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # add dependencies here
+        # will install on its own
+    ],
+)
