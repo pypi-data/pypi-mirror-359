5157d5a824248b5aab8b850514059b8b70ad58f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:36 +0000

    Add package abprovider/0.0.1/abprovider-0.0.1.tar.gz

---
+++ packages/abprovider/abprovider-0.0.1.tar.gz/abprovider-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="abprovider",
+    version="0.0.1",
+    packages=["abprovider"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
