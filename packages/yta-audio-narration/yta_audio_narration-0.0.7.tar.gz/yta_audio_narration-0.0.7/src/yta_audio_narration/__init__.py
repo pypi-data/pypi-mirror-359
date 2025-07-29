dc794a22048147fa68de8ff9f1d638ed9c324905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:29 +0000

    Add package yta-audio-narration/0.0.7/yta_audio_narration-0.0.7.tar.gz

---
+++ packages/yta-audio-narration/yta_audio_narration-0.0.7.tar.gz/yta_audio_narration-0.0.7/src/yta_audio_narration/__init__.py
@@ -0,0 +1,10 @@
+"""
+Welcome to Youtube Autonomous Audio Narration
+Module.
+
+This module is based on the voice narration
+modules that are specific for each voice
+available for our system. Check libs like
+'yta_audio_narration_coqui' for specific
+voices.
+"""
