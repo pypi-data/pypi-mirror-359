dda07162c891d825f9989800252469d64d85aeae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package brandquad-sdk/0.0.1/brandquad-sdk-0.0.1.tar.gz

---
+++ packages/brandquad-sdk/brandquad-sdk-0.0.1.tar.gz/brandquad-sdk-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="brandquad-sdk",
+    version="0.0.1",
+    packages=["brandquad-sdk"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
