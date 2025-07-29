ccbeb1c2f65d596c1b17825b8667e4b68f9b0016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package statscollector/0.3.0/statscollector-0.3.0.tar.gz

---
+++ packages/statscollector/statscollector-0.3.0.tar.gz/statscollector-0.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statscollector",
+    version="0.3.0",
+    packages=["statscollector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
