5a8a277366f5a5954d1e2160ab8d5be4cabca9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package fontbox/3.0.2/fontbox-3.0.2.tar.gz

---
+++ packages/fontbox/fontbox-3.0.2.tar.gz/fontbox-3.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="fontbox",
+    version="3.0.2",
+    packages=["fontbox"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
