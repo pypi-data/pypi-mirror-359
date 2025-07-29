f612e999db06f91b764408d55385b3285ade65ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package logback-core/1.5.6/logback-core-1.5.6.tar.gz

---
+++ packages/logback-core/logback-core-1.5.6.tar.gz/logback-core-1.5.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="logback-core",
+    version="1.5.6",
+    packages=["logback-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
