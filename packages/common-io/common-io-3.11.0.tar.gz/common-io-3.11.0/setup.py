38b0d03a79bcf138c3b0c5b43099c9ea2b1f1172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:03 +0000

    Add package common-io/3.11.0/common-io-3.11.0.tar.gz

---
+++ packages/common-io/common-io-3.11.0.tar.gz/common-io-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="common-io",
+    version="3.11.0",
+    packages=["common-io"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
