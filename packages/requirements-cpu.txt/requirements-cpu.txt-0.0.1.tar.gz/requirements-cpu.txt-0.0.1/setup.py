dd832287e44e6b65baa4fe281c02c6f138fb4f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package requirements-cpu.txt/0.0.1/requirements-cpu.txt-0.0.1.tar.gz

---
+++ packages/requirements-cpu.txt/requirements-cpu.txt-0.0.1.tar.gz/requirements-cpu.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements-cpu.txt",
+    version="0.0.1",
+    packages=["requirements-cpu.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
