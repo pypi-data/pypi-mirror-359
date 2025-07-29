356ba820b7ffe0ca20b7623d14c4718ebf1adebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package client-creation-menu/0.0.1/client-creation-menu-0.0.1.tar.gz

---
+++ packages/client-creation-menu/client-creation-menu-0.0.1.tar.gz/client-creation-menu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="client-creation-menu",
+    version="0.0.1",
+    packages=["client-creation-menu"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
