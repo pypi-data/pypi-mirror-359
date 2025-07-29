88e54698a1d79580644e67f007e62bc77347e0e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package adafruit-am2320/0.0.1/adafruit-am2320-0.0.1.tar.gz

---
+++ packages/adafruit-am2320/adafruit-am2320-0.0.1.tar.gz/adafruit-am2320-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="adafruit-am2320",
+    version="0.0.1",
+    packages=["adafruit-am2320"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
