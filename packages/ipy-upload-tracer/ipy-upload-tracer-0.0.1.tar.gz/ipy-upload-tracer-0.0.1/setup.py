6e75e920f118205db55f25a31b474b07da7225ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-upload-tracer/0.0.1/ipy-upload-tracer-0.0.1.tar.gz

---
+++ packages/ipy-upload-tracer/ipy-upload-tracer-0.0.1.tar.gz/ipy-upload-tracer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-upload-tracer",
+    version="0.0.1",
+    packages=["ipy-upload-tracer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
