e58dced1257fdf820a3c44281e40390ae4e100e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package imageio-webp/3.11.0/imageio-webp-3.11.0.tar.gz

---
+++ packages/imageio-webp/imageio-webp-3.11.0.tar.gz/imageio-webp-3.11.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="imageio-webp",
+    version="3.11.0",
+    packages=["imageio-webp"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
