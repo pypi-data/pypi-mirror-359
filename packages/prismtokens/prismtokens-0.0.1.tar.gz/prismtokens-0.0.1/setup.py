22c2175fdfcc2eda4e1c424798e2e478b79128e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:34 +0000

    Add package prismtokens/0.0.1/prismtokens-0.0.1.tar.gz

---
+++ packages/prismtokens/prismtokens-0.0.1.tar.gz/prismtokens-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prismtokens",
+    version="0.0.1",
+    packages=["prismtokens"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
