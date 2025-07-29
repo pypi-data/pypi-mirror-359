bdff0e20ad7e66f07b84b2afab667741ac3a3490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:31 +0000

    Add package jetty-client/12.0.11/jetty-client-12.0.11.tar.gz

---
+++ packages/jetty-client/jetty-client-12.0.11.tar.gz/jetty-client-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-client",
+    version="12.0.11",
+    packages=["jetty-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
