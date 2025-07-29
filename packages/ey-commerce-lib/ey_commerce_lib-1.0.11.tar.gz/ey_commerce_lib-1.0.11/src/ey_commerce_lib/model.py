00f0e52412555193dd0c242a6e7d50211325cc14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:34 +0000

    Add package ey-commerce-lib/1.0.11/ey_commerce_lib-1.0.11.tar.gz

---
+++ packages/ey-commerce-lib/ey_commerce_lib-1.0.11.tar.gz/ey_commerce_lib-1.0.11/src/ey_commerce_lib/model.py
@@ -0,0 +1,12 @@
+from typing import Generic, TypeVar
+from pydantic import BaseModel
+
+T = TypeVar('T')
+
+
+class Page(Generic[T], BaseModel):
+    records: list[T]
+    page_number: int
+    page_size: int
+    total: int
+    total_page: int
