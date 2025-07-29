59e709b514fe07cfdb2eb1bbd982b1622b5df3d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:31 +0000

    Add package current-locations-pb2/0.0.1/current-locations-pb2-0.0.1.tar.gz

---
+++ packages/current-locations-pb2/current-locations-pb2-0.0.1.tar.gz/current-locations-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="current-locations-pb2",
+    version="0.0.1",
+    packages=["current-locations-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
