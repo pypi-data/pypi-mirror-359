38b7f24455a02be6b856dcfb88f7dbf586be1d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:31 +0000

    Add package data-platform-tools/0.0.1/data-platform-tools-0.0.1.tar.gz

---
+++ packages/data-platform-tools/data-platform-tools-0.0.1.tar.gz/data-platform-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="data-platform-tools",
+    version="0.0.1",
+    packages=["data-platform-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
