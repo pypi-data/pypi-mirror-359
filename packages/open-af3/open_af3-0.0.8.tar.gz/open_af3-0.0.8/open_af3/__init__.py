e340970ae9126819b36fbd2780304bb51a4634a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:58 +0000

    Add package open-af3/0.0.8/open_af3-0.0.8.tar.gz

---
+++ packages/open-af3/open_af3-0.0.8.tar.gz/open_af3-0.0.8/open_af3/__init__.py
@@ -0,0 +1,12 @@
+from open_af3.diffusion import (
+    GeneticDiffusionModuleBlock,
+    GeneticDiffusion,
+)
+from open_af3.model import AlphaFold3
+
+
+__all__ = [
+    "GeneticDiffusionModuleBlock",
+    "AlphaFold3",
+    "GeneticDiffusion",
+]
