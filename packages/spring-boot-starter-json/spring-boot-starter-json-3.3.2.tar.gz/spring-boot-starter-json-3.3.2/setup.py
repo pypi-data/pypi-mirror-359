a34bac97c054d04006fa23781fd2cf5c3215246a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:11 +0000

    Add package spring-boot-starter-json/3.3.2/spring-boot-starter-json-3.3.2.tar.gz

---
+++ packages/spring-boot-starter-json/spring-boot-starter-json-3.3.2.tar.gz/spring-boot-starter-json-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-starter-json",
+    version="3.3.2",
+    packages=["spring-boot-starter-json"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
