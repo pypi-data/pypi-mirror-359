433fd1ef36e74d7eff859eb7e7ddde04c455b1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package analytics-connectors/1.0.9/analytics-connectors-1.0.9.tar.gz

---
+++ packages/analytics-connectors/analytics-connectors-1.0.9.tar.gz/analytics-connectors-1.0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="analytics-connectors",
+    version="1.0.9",
+    packages=["analytics-connectors"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
