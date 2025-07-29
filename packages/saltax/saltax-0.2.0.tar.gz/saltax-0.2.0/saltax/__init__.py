8af888549c1f02d583b5844877f8c311cc0abcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package saltax/0.2.0/saltax-0.2.0.tar.gz

---
+++ packages/saltax/saltax-0.2.0.tar.gz/saltax-0.2.0/saltax/__init__.py
@@ -0,0 +1,16 @@
+__version__ = "0.2.0"
+
+from . import patches
+
+from . import utils
+from .utils import *
+
+from . import instructions
+from .instructions import *
+
+from . import plugins
+from .plugins import *
+
+from . import contexts
+
+from . import match
