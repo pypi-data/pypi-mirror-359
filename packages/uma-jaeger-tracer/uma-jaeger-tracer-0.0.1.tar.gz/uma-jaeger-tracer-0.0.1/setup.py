78a290fa71cbf4e1b882ed9c2080d5bf5c948609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package uma-jaeger-tracer/0.0.1/uma-jaeger-tracer-0.0.1.tar.gz

---
+++ packages/uma-jaeger-tracer/uma-jaeger-tracer-0.0.1.tar.gz/uma-jaeger-tracer-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="uma-jaeger-tracer",
+    version="0.0.1",
+    packages=["uma-jaeger-tracer"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
