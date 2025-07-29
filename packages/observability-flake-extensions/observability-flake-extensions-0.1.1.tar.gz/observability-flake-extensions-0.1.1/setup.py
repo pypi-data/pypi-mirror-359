2c8c6805b8933f46eed7cb31b615f6a6efb13279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package observability-flake-extensions/0.1.1/observability-flake-extensions-0.1.1.tar.gz

---
+++ packages/observability-flake-extensions/observability-flake-extensions-0.1.1.tar.gz/observability-flake-extensions-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="observability-flake-extensions",
+    version="0.1.1",
+    packages=["observability-flake-extensions"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
