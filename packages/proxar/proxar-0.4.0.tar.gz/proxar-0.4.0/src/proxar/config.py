a000230263a16b618ef53ad16bdd5a04eb8539f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:20 +0000

    Add package proxar/0.4.0/proxar-0.4.0.tar.gz

---
+++ packages/proxar/proxar-0.4.0.tar.gz/proxar-0.4.0/src/proxar/config.py
@@ -0,0 +1,26 @@
+# Network variables
+HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/113.0.0.0 Safari/537.36"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;q=0.9,"
+        "image/avif,image/webp,image/apng,*/*;q=0.8"
+    ),
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Priority": "u=0, i",
+    "Sec-Ch-UA": '"Chromium";v="136", "Brave";v="136", "Not.A/Brand";v="99"',
+    "Sec-Ch-UA-Mobile": "?0",
+    "Sec-Ch-UA-Platform": '"macOS"',
+    "Sec-Gpc": "1",
+}
