6bca5bdea7670bdf0bd98b3d023b879be0f7876d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package spring-boot-test-autoconfigure/3.3.2/spring-boot-test-autoconfigure-3.3.2.tar.gz

---
+++ packages/spring-boot-test-autoconfigure/spring-boot-test-autoconfigure-3.3.2.tar.gz/spring-boot-test-autoconfigure-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-boot-test-autoconfigure",
+    version="3.3.2",
+    packages=["spring-boot-test-autoconfigure"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
