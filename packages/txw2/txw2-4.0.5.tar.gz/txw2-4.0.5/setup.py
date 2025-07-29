a9c5c3e07e90b5a8af39e1e44aa0ac311f3c786c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package txw2/4.0.5/txw2-4.0.5.tar.gz

---
+++ packages/txw2/txw2-4.0.5.tar.gz/txw2-4.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="txw2",
+    version="4.0.5",
+    packages=["txw2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
