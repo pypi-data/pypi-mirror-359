38aeac5107ce8571afd8c2472af4bbfce913010e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package sphinxarg/0.2.5/sphinxarg-0.2.5.tar.gz

---
+++ packages/sphinxarg/sphinxarg-0.2.5.tar.gz/sphinxarg-0.2.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sphinxarg",
+    version="0.2.5",
+    packages=["sphinxarg"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
