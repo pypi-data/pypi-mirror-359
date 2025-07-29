b7b98539a1fbcc1e129691e3abfc8ee30b06de21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package spring-data-commons/3.3.2/spring-data-commons-3.3.2.tar.gz

---
+++ packages/spring-data-commons/spring-data-commons-3.3.2.tar.gz/spring-data-commons-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-data-commons",
+    version="3.3.2",
+    packages=["spring-data-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
