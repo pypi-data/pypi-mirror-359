2c472d64e264c29493cf79d7e79e24b8a17929f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:31 +0000

    Add package htas-common/0.1/htas-common-0.1.tar.gz

---
+++ packages/htas-common/htas-common-0.1.tar.gz/htas-common-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="htas-common",
+    version="0.1",
+    packages=["htas-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
