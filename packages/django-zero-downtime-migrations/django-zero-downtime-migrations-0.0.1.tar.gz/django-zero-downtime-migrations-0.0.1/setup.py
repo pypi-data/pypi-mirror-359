f503c8902a9657cdf4c12deb29b53730c9e6aeee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package django-zero-downtime-migrations/0.0.1/django-zero-downtime-migrations-0.0.1.tar.gz

---
+++ packages/django-zero-downtime-migrations/django-zero-downtime-migrations-0.0.1.tar.gz/django-zero-downtime-migrations-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="django-zero-downtime-migrations",
+    version="0.0.1",
+    packages=["django-zero-downtime-migrations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
