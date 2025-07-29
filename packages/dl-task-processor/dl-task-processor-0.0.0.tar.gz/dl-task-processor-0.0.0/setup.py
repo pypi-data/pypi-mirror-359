a9160c69fa739834e86a8610e0b45ac04074c20e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-task-processor/0.0.0/dl-task-processor-0.0.0.tar.gz

---
+++ packages/dl-task-processor/dl-task-processor-0.0.0.tar.gz/dl-task-processor-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-task-processor",
+    version="0.0.0",
+    packages=["dl-task-processor"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
