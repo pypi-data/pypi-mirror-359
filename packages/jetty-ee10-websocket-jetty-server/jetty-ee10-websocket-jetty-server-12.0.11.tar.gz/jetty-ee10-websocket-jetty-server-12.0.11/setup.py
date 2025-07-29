005078fd910a5bd5bbd8ac7b467be91b74ae79d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:49 +0000

    Add package jetty-ee10-websocket-jetty-server/12.0.11/jetty-ee10-websocket-jetty-server-12.0.11.tar.gz

---
+++ packages/jetty-ee10-websocket-jetty-server/jetty-ee10-websocket-jetty-server-12.0.11.tar.gz/jetty-ee10-websocket-jetty-server-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-ee10-websocket-jetty-server",
+    version="12.0.11",
+    packages=["jetty-ee10-websocket-jetty-server"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
