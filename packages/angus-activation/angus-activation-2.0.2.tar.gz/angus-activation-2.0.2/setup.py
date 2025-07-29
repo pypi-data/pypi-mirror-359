fd3febc00fc916888a05b05b082111e887b87264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package angus-activation/2.0.2/angus-activation-2.0.2.tar.gz

---
+++ packages/angus-activation/angus-activation-2.0.2.tar.gz/angus-activation-2.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="angus-activation",
+    version="2.0.2",
+    packages=["angus-activation"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
