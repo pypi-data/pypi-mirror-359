645fac40406922b175d0627840d38fc8a99fd2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:40 +0000

    Add package requirements-openvino.txt/0.0.1/requirements-openvino.txt-0.0.1.tar.gz

---
+++ packages/requirements-openvino.txt/requirements-openvino.txt-0.0.1.tar.gz/requirements-openvino.txt-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="requirements-openvino.txt",
+    version="0.0.1",
+    packages=["requirements-openvino.txt"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
