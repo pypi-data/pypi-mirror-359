903f9672a51e38b75e07322328adc1a2e5301c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package iconv-codec/0.0.1/iconv-codec-0.0.1.tar.gz

---
+++ packages/iconv-codec/iconv-codec-0.0.1.tar.gz/iconv-codec-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="iconv-codec",
+    version="0.0.1",
+    packages=["iconv-codec"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
