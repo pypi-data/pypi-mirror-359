253493813685b0f2222e37a7ceaa6284139edae6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-sqlalchemy-common/0.2/dl-sqlalchemy-common-0.2.tar.gz

---
+++ packages/dl-sqlalchemy-common/dl-sqlalchemy-common-0.2.tar.gz/dl-sqlalchemy-common-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-sqlalchemy-common",
+    version="0.2",
+    packages=["dl-sqlalchemy-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
