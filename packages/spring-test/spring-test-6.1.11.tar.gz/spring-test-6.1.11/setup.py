cd6ad529c9a8cbc020345c8926a2cc0769500470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package spring-test/6.1.11/spring-test-6.1.11.tar.gz

---
+++ packages/spring-test/spring-test-6.1.11.tar.gz/spring-test-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-test",
+    version="6.1.11",
+    packages=["spring-test"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
