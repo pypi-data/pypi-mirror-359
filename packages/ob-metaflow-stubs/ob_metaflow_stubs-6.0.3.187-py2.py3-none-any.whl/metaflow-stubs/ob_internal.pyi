29102a88293f9e1ef45eb0fed955977747e9bfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:12 +0000

    Add package ob-metaflow-stubs/6.0.3.187/ob_metaflow_stubs-6.0.3.187-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.187-py2.py3-none-any.whl/metaflow-stubs/ob_internal.pyi
@@ -0,0 +1,11 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-01T15:21:03.472086                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .mf_extensions.outerbounds.plugins.kubernetes.pod_killer import PodKiller as PodKiller
+
---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.3.187-py2.py3-none-any.whl/metaflow-stubs/plugins/gcp/gs_exceptions.pyi
@@ -0,0 +1,18 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-01T15:21:03.560185                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ...exception import MetaflowException as MetaflowException
+
+class MetaflowGSPackageError(metaflow.exception.MetaflowException, metaclass=type):
+    ...
+
