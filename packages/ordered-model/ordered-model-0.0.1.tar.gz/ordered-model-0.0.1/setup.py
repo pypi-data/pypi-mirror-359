b1d846690595292fae34537a494d43c7b736d4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package ordered-model/0.0.1/ordered-model-0.0.1.tar.gz

---
+++ packages/ordered-model/ordered-model-0.0.1.tar.gz/ordered-model-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ordered-model",
+    version="0.0.1",
+    packages=["ordered-model"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
