4e3c55112dbdb8c8ab8a7f058bc217393c879874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:44 +0000

    Add package prefect/3.4.8.dev2/prefect-3.4.8.dev2.tar.gz

---
+++ packages/prefect/prefect-3.4.8.dev2.tar.gz/prefect-3.4.8.dev2/src/prefect/server/ui/assets/404-CNYOr8L7.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dx as n}from"./index-B895fP76.js";import{u as r}from"./usePageTitle-DWiD-upr.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-CNYOr8L7.js.map
---
+++ packages/prefect/prefect-3.4.8.dev2.tar.gz/prefect-3.4.8.dev2/src/prefect/server/ui/assets/usePrefectApi-BCmfiLwL.js
@@ -0,0 +1,2 @@
+import{cH as r}from"./index-B895fP76.js";import{p as e}from"./api-CXh0l6Je.js";function o(){return r(e)}export{o as u};
+//# sourceMappingURL=usePrefectApi-BCmfiLwL.js.map
