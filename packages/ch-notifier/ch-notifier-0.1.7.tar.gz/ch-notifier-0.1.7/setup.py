c8e4400948d4975c76f6cc8dba8b986e670ffd86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:49 +0000

    Add package ch-notifier/0.1.7/ch-notifier-0.1.7.tar.gz

---
+++ packages/ch-notifier/ch-notifier-0.1.7.tar.gz/ch-notifier-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ch-notifier",
+    version="0.1.7",
+    packages=["ch-notifier"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
