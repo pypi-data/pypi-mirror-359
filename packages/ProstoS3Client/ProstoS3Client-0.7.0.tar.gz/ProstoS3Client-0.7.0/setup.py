b125efc8d7459b41975e4b60ef3ea3527b1086dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package ProstoS3Client/0.7.0/ProstoS3Client-0.7.0.tar.gz

---
+++ packages/ProstoS3Client/ProstoS3Client-0.7.0.tar.gz/ProstoS3Client-0.7.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoS3Client",
+    version="0.7.0",
+    packages=["ProstoS3Client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
