ba6d95962e1c669baf6326c8b7cf576fa13b5335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package voice_crawler/0.7.0/voice_crawler-0.7.0.tar.gz

---
+++ packages/voice_crawler/voice_crawler-0.7.0.tar.gz/voice_crawler-0.7.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="voice_crawler",
+    version="0.7.0",
+    packages=["voice_crawler"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
