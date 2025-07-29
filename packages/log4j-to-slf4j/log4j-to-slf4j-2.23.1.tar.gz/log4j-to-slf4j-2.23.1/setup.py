97d143a3802eeaff4f313ad29695938f9e1fa405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:45 +0000

    Add package log4j-to-slf4j/2.23.1/log4j-to-slf4j-2.23.1.tar.gz

---
+++ packages/log4j-to-slf4j/log4j-to-slf4j-2.23.1.tar.gz/log4j-to-slf4j-2.23.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="log4j-to-slf4j",
+    version="2.23.1",
+    packages=["log4j-to-slf4j"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
