cc7caa9b1d14bb1fac3923d766ba2e58da5e41c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:51 +0000

    Add package resnet-utils/0.0.1/resnet-utils-0.0.1.tar.gz

---
+++ packages/resnet-utils/resnet-utils-0.0.1.tar.gz/resnet-utils-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="resnet-utils",
+    version="0.0.1",
+    packages=["resnet-utils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
