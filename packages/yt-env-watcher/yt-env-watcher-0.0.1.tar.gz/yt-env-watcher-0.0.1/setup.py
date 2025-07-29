7d2a3a06742d584ab677bd222f428c66f4d73927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package yt-env-watcher/0.0.1/yt-env-watcher-0.0.1.tar.gz

---
+++ packages/yt-env-watcher/yt-env-watcher-0.0.1.tar.gz/yt-env-watcher-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="yt-env-watcher",
+    version="0.0.1",
+    packages=["yt-env-watcher"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
