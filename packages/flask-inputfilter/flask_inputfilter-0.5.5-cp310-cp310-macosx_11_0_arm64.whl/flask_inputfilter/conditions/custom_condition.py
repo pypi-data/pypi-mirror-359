19558d81fe68505fc3170c6773055d78ec7fbe82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:48 +0000

    Add package flask-inputfilter/0.5.5/flask_inputfilter-0.5.5-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/flask-inputfilter/flask_inputfilter-0.5.5-cp310-cp310-macosx_11_0_arm64.whl/flask_inputfilter/conditions/custom_condition.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Any
+
+from flask_inputfilter.conditions import BaseCondition
+
+
+class CustomCondition(BaseCondition):
+    """
+    Allows defining a custom condition using a user-provided callable.
+
+    **Parameters:**
+
+    - **condition** (*Callable[[dict[str, Any]], bool]*): A function that
+        takes the input data and returns a boolean indicating whether the
+        condition is met.
+
+    **Expected Behavior:**
+
+    Executes the provided callable with the input data. The condition passes
+    if the callable returns ``True``, and fails otherwise.
+
+    **Example Usage:**
+
+    .. code-block:: python
+
+        def my_custom_condition(data):
+            return data.get('age', 0) >= 18
+
+        class CustomFilter(InputFilter):
+            def __init__(self):
+                super().__init__()
+
+                self.add(
+                    'age',
+                    validators=[IsIntegerValidator()]
+                )
+
+                self.add_condition(
+                    CustomCondition(
+                        condition=my_custom_condition
+                    )
+                )
+    """
+
+    __slots__ = ("condition",)
+
+    def __init__(self, condition: Callable[[dict[str, Any]], bool]) -> None:
+        self.condition = condition
+
+    def check(self, data: dict[str, Any]) -> bool:
+        return self.condition(data)
