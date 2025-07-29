26fff52555e429901cfbc69b85d4605e7ff9cf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:02 +0000

    Add package importlib-metadatapython_version/0.0.1/importlib-metadatapython_version-0.0.1.tar.gz

---
+++ packages/importlib-metadatapython_version/importlib-metadatapython_version-0.0.1.tar.gz/importlib-metadatapython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="importlib-metadatapython_version",
+    version="0.0.1",
+    packages=["importlib-metadatapython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
