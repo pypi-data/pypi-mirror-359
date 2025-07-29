f8da9e8cfc01e774995714c31857cf2325ed5d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:46 +0000

    Add package mongoql-executor/0.0.1/mongoql-executor-0.0.1.tar.gz

---
+++ packages/mongoql-executor/mongoql-executor-0.0.1.tar.gz/mongoql-executor-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mongoql-executor",
+    version="0.0.1",
+    packages=["mongoql-executor"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
