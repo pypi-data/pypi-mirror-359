b5b453a1baa63ab12f8c4572acd4f0b1353a538d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:52 +0000

    Add package hadoop-staff/0.0.1/hadoop-staff-0.0.1.tar.gz

---
+++ packages/hadoop-staff/hadoop-staff-0.0.1.tar.gz/hadoop-staff-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hadoop-staff",
+    version="0.0.1",
+    packages=["hadoop-staff"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
