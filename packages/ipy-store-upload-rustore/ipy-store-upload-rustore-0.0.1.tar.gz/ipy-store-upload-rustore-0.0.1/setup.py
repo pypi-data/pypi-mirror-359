de2963c87b52835ec590df92112cf79767c309b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:41 +0000

    Add package ipy-store-upload-rustore/0.0.1/ipy-store-upload-rustore-0.0.1.tar.gz

---
+++ packages/ipy-store-upload-rustore/ipy-store-upload-rustore-0.0.1.tar.gz/ipy-store-upload-rustore-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-store-upload-rustore",
+    version="0.0.1",
+    packages=["ipy-store-upload-rustore"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
