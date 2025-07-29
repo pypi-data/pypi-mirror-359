8d9338fb8b003f288db328459648a579aa2e1f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:48 +0000

    Add package jetty-websocket-core-common/12.0.11/jetty-websocket-core-common-12.0.11.tar.gz

---
+++ packages/jetty-websocket-core-common/jetty-websocket-core-common-12.0.11.tar.gz/jetty-websocket-core-common-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-websocket-core-common",
+    version="12.0.11",
+    packages=["jetty-websocket-core-common"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
