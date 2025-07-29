2514577964a67bb04890656f6051032d6b9d7627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:05:42 +0000

    Add package server-unite/0.0.1/server_unite-0.0.1-py2.py3-none-any.whl

---
+++ packages/server-unite/server_unite-0.0.1-py2.py3-none-any.whl/Server/__main__.py
@@ -0,0 +1,31 @@
+import argparse
+
+from . application import Application
+
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(prog="Server", description="Manage server deployment")
+	subparsers = parser.add_subparsers(dest="command")
+
+	deploy_parser = subparsers.add_parser("deploy", help="Deploy a new server")
+	deploy_parser.add_argument('repository', help="Repository URL")
+
+	args = parser.parse_args()
+	if args.command is None:
+		parser.print_help()
+		return
+
+	return args
+
+
+
+def main():
+	args = parse_args()
+	if args is None:
+		return
+
+	Application.Run(args)
+
+if __name__ == '__main__':
+	main()
