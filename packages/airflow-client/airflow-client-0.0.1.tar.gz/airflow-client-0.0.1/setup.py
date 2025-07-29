fdf7caa23464c03520e324130ca7bd759928d847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:47 +0000

    Add package airflow-client/0.0.1/airflow-client-0.0.1.tar.gz

---
+++ packages/airflow-client/airflow-client-0.0.1.tar.gz/airflow-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="airflow-client",
+    version="0.0.1",
+    packages=["airflow-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
