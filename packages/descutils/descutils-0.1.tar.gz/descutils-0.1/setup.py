08a2fb9bac2f119dad4f18dc3d1c001219042b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package descutils/0.1/descutils-0.1.tar.gz

---
+++ packages/descutils/descutils-0.1.tar.gz/descutils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="descutils",
+    version="0.1",
+    packages=["descutils"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
