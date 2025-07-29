fe924262b90245edb75b07bba198eb66615ccf58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-i18n/0.0.1/dl-i18n-0.0.1.tar.gz

---
+++ packages/dl-i18n/dl-i18n-0.0.1.tar.gz/dl-i18n-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-i18n",
+    version="0.0.1",
+    packages=["dl-i18n"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
