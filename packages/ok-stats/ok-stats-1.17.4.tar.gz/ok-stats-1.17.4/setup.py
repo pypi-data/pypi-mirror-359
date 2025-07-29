3c2d5a85694155b72869b30d37476c9aa2f9e3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package ok-stats/1.17.4/ok-stats-1.17.4.tar.gz

---
+++ packages/ok-stats/ok-stats-1.17.4.tar.gz/ok-stats-1.17.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ok-stats",
+    version="1.17.4",
+    packages=["ok-stats"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
