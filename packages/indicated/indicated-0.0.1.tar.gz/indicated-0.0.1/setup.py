bfe47a1b022b417df6cad12101a78c8b8341cc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:09 +0000

    Add package indicated/0.0.1/indicated-0.0.1.tar.gz

---
+++ packages/indicated/indicated-0.0.1.tar.gz/indicated-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="indicated",
+    version="0.0.1",
+    packages=["indicated"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
