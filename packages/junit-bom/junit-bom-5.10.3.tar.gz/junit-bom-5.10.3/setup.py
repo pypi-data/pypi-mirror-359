e97b35b214efe73e7012f6fec51757356a305f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package junit-bom/5.10.3/junit-bom-5.10.3.tar.gz

---
+++ packages/junit-bom/junit-bom-5.10.3.tar.gz/junit-bom-5.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-bom",
+    version="5.10.3",
+    packages=["junit-bom"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
