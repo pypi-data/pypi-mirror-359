b9eb7a6a93244a12cb535241756fc9acd7440685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:40 +0000

    Add package ipy-testflight-groups-changelog/0.0.1/ipy-testflight-groups-changelog-0.0.1.tar.gz

---
+++ packages/ipy-testflight-groups-changelog/ipy-testflight-groups-changelog-0.0.1.tar.gz/ipy-testflight-groups-changelog-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ipy-testflight-groups-changelog",
+    version="0.0.1",
+    packages=["ipy-testflight-groups-changelog"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
