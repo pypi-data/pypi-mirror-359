49003f7c519a2aa6eece68e539e2c62cbdef302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package zabbix-openstack/0.0.1/zabbix-openstack-0.0.1.tar.gz

---
+++ packages/zabbix-openstack/zabbix-openstack-0.0.1.tar.gz/zabbix-openstack-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="zabbix-openstack",
+    version="0.0.1",
+    packages=["zabbix-openstack"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
