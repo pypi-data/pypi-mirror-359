11e1819c5d8d809021851b2b83ec678c5ff4e010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:48 +0000

    Add package report-sender/0.0.1/report-sender-0.0.1.tar.gz

---
+++ packages/report-sender/report-sender-0.0.1.tar.gz/report-sender-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="report-sender",
+    version="0.0.1",
+    packages=["report-sender"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
