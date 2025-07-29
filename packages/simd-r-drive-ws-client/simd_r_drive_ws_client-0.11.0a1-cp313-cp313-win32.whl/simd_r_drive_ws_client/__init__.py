3d7d0dde2d150207240aad0d4acd976e14abd32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:33 +0000

    Add package simd-r-drive-ws-client/0.11.0a1/simd_r_drive_ws_client-0.11.0a1-cp313-cp313-win32.whl

---
+++ packages/simd-r-drive-ws-client/simd_r_drive_ws_client-0.11.0a1-cp313-cp313-win32.whl/simd_r_drive_ws_client/__init__.py
@@ -0,0 +1,14 @@
+from .simd_r_drive_ws_client import (
+    NamespaceHasher,
+    setup_logging,
+    test_rust_logging,
+)
+
+from .data_store_ws_client import DataStoreWsClient
+
+__all__ = [
+    "DataStoreWsClient",
+    "NamespaceHasher",
+    "setup_logging",
+    "test_rust_logging",
+]
