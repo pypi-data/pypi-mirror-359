bc108b231905b20d393b9961a29f6a47d0727566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:56 +0000

    Add package vkpay-sync/0.0.0/vkpay-sync-0.0.0.tar.gz

---
+++ packages/vkpay-sync/vkpay-sync-0.0.0.tar.gz/vkpay-sync-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vkpay-sync",
+    version="0.0.0",
+    packages=["vkpay-sync"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
