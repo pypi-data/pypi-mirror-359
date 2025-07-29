3f3d0b9f589ecea19e47b51c292620cf684ccba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:32 +0000

    Add package core-main-registry-app/2.16.0/core_main_registry_app-2.16.0.tar.gz

---
+++ packages/core-main-registry-app/core_main_registry_app-2.16.0.tar.gz/core_main_registry_app-2.16.0/core_main_registry_app/rest/template_version_manager/views.py
@@ -0,0 +1,32 @@
+""" REST views for the registry template version manager API
+"""
+
+from rest_framework.permissions import IsAuthenticated
+
+from core_main_app.rest.template_version_manager.abstract_views import (
+    AbstractStatusTemplateVersion,
+)
+from core_main_registry_app.components.version_manager import (
+    api as version_manager_api,
+)
+
+
+class CurrentTemplateVersion(AbstractStatusTemplateVersion):
+    """Update status to current"""
+
+    permission_classes = (IsAuthenticated,)
+
+    def status_update(self, template_object):
+        """Update status to current
+
+        Args:
+
+            template_object: template_version
+
+        Returns:
+
+            TemplateVersion
+        """
+        return version_manager_api.set_current(
+            template_object, request=self.request
+        )
