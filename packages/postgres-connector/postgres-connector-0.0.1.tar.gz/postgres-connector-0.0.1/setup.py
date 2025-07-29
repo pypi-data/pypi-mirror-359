058c493e84c88bc36e67d189c9b3c59dca3e6b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:32 +0000

    Add package postgres-connector/0.0.1/postgres-connector-0.0.1.tar.gz

---
+++ packages/postgres-connector/postgres-connector-0.0.1.tar.gz/postgres-connector-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="postgres-connector",
+    version="0.0.1",
+    packages=["postgres-connector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
