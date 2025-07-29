041e0ce0dd3cdb2698fed1d335870153ce59df1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package pycodestylepython_version/0.0.1/pycodestylepython_version-0.0.1.tar.gz

---
+++ packages/pycodestylepython_version/pycodestylepython_version-0.0.1.tar.gz/pycodestylepython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pycodestylepython_version",
+    version="0.0.1",
+    packages=["pycodestylepython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
