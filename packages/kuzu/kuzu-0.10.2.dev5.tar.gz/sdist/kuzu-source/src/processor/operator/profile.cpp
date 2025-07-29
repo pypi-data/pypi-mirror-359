5022c0ec94b9dcfc41891f952adf35eb0daa2a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:39 +0000

    Add package kuzu/0.10.2.dev5/kuzu-0.10.2.dev5.tar.gz

---
+++ packages/kuzu/kuzu-0.10.2.dev5.tar.gz/sdist/kuzu-source/src/processor/operator/profile.cpp
@@ -0,0 +1,32 @@
+#include "processor/operator/profile.h"
+
+#include "main/plan_printer.h"
+#include "processor/execution_context.h"
+
+using namespace kuzu::common;
+
+namespace kuzu {
+namespace processor {
+
+void Profile::initLocalStateInternal(ResultSet* resultSet, ExecutionContext* /*context*/) {
+    outputVector = resultSet->getValueVector(outputPos).get();
+}
+
+bool Profile::getNextTuplesInternal(ExecutionContext* context) {
+    if (localState.hasExecuted) {
+        return false;
+    }
+    localState.hasExecuted = true;
+    ku_string_t profileStr;
+    const auto planInString =
+        main::PlanPrinter::printPlanToOstream(info.physicalPlan, context->profiler).str();
+    StringVector::addString(outputVector, profileStr, planInString.c_str(), planInString.length());
+    auto& selVector = outputVector->state->getSelVectorUnsafe();
+    selVector.setSelSize(1);
+    outputVector->setValue<ku_string_t>(selVector[0], profileStr);
+    metrics->numOutputTuple.incrementByOne();
+    return true;
+}
+
+} // namespace processor
+} // namespace kuzu
