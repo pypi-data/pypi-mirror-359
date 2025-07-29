25f999bd0df8c9d0ac96ff9f4ed5b9b95b4ec36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:06 +0000

    Add package simplejira/1.0.5/simplejira-1.0.5.tar.gz

---
+++ packages/simplejira/simplejira-1.0.5.tar.gz/simplejira-1.0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="simplejira",
+    version="1.0.5",
+    packages=["simplejira"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
