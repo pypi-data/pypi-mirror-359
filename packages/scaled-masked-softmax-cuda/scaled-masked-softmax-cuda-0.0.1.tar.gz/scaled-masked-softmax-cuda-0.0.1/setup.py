aad78fabb52214a5dba45544318ce4f2946e5174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:54 +0000

    Add package scaled-masked-softmax-cuda/0.0.1/scaled-masked-softmax-cuda-0.0.1.tar.gz

---
+++ packages/scaled-masked-softmax-cuda/scaled-masked-softmax-cuda-0.0.1.tar.gz/scaled-masked-softmax-cuda-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scaled-masked-softmax-cuda",
+    version="0.0.1",
+    packages=["scaled-masked-softmax-cuda"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
