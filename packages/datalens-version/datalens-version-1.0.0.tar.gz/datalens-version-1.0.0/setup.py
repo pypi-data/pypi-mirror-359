1285fc6514f2105201ae436d75b0bf7e6705839e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:29 +0000

    Add package datalens-version/1.0.0/datalens-version-1.0.0.tar.gz

---
+++ packages/datalens-version/datalens-version-1.0.0.tar.gz/datalens-version-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-version",
+    version="1.0.0",
+    packages=["datalens-version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
