1768e72999268b301a3384156f36bb139054fa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:29 +0000

    Add package up2date-client/0.0.1/up2date-client-0.0.1.tar.gz

---
+++ packages/up2date-client/up2date-client-0.0.1.tar.gz/up2date-client-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="up2date-client",
+    version="0.0.1",
+    packages=["up2date-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
