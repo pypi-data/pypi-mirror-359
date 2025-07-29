9e2acd8713584c74d432a7709bd0e7fde19c758c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:30 +0000

    Add package devicehub-client/1.3.10/devicehub-client-1.3.10.tar.gz

---
+++ packages/devicehub-client/devicehub-client-1.3.10.tar.gz/devicehub-client-1.3.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="devicehub-client",
+    version="1.3.10",
+    packages=["devicehub-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
