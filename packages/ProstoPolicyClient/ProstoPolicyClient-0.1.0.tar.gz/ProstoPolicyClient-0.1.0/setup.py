3e386b96e3d81b270a28aa73f57f0667ff5698f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package ProstoPolicyClient/0.1.0/ProstoPolicyClient-0.1.0.tar.gz

---
+++ packages/ProstoPolicyClient/ProstoPolicyClient-0.1.0.tar.gz/ProstoPolicyClient-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ProstoPolicyClient",
+    version="0.1.0",
+    packages=["ProstoPolicyClient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
