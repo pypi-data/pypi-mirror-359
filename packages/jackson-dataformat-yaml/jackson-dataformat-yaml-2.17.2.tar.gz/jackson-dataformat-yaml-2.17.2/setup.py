52a43e7fb4a2b27cf9fb3dd673c83505cdf75e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:47 +0000

    Add package jackson-dataformat-yaml/2.17.2/jackson-dataformat-yaml-2.17.2.tar.gz

---
+++ packages/jackson-dataformat-yaml/jackson-dataformat-yaml-2.17.2.tar.gz/jackson-dataformat-yaml-2.17.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jackson-dataformat-yaml",
+    version="2.17.2",
+    packages=["jackson-dataformat-yaml"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
