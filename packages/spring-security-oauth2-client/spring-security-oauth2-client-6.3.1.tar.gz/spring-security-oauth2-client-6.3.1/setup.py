4731051cf6274d3a00075533dc07477e3ee2da1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:51 +0000

    Add package spring-security-oauth2-client/6.3.1/spring-security-oauth2-client-6.3.1.tar.gz

---
+++ packages/spring-security-oauth2-client/spring-security-oauth2-client-6.3.1.tar.gz/spring-security-oauth2-client-6.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-security-oauth2-client",
+    version="6.3.1",
+    packages=["spring-security-oauth2-client"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
