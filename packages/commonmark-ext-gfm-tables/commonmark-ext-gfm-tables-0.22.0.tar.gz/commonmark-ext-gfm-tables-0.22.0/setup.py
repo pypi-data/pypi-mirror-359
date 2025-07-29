2d06039377b57dad862c1de919703ab0a0213b05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:53 +0000

    Add package commonmark-ext-gfm-tables/0.22.0/commonmark-ext-gfm-tables-0.22.0.tar.gz

---
+++ packages/commonmark-ext-gfm-tables/commonmark-ext-gfm-tables-0.22.0.tar.gz/commonmark-ext-gfm-tables-0.22.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="commonmark-ext-gfm-tables",
+    version="0.22.0",
+    packages=["commonmark-ext-gfm-tables"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
