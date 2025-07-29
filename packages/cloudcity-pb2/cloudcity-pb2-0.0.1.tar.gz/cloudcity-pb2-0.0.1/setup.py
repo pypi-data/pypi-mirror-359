3aaee1ff5576ffa0db4c0a4be3bab5aa0c9c34b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package cloudcity-pb2/0.0.1/cloudcity-pb2-0.0.1.tar.gz

---
+++ packages/cloudcity-pb2/cloudcity-pb2-0.0.1.tar.gz/cloudcity-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cloudcity-pb2",
+    version="0.0.1",
+    packages=["cloudcity-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
