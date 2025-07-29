698257ec9a1f851e23a6dec351aa85f3f8091e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:46 +0000

    Add package dl-app-tools/0.0.1/dl-app-tools-0.0.1.tar.gz

---
+++ packages/dl-app-tools/dl-app-tools-0.0.1.tar.gz/dl-app-tools-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-app-tools",
+    version="0.0.1",
+    packages=["dl-app-tools"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
