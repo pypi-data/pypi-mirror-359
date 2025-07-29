7b8746fba71be3bf7b2e60359538222a57e00012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:43 +0000

    Add package prostogrpcclient/0.0.1/prostogrpcclient-0.0.1.tar.gz

---
+++ packages/prostogrpcclient/prostogrpcclient-0.0.1.tar.gz/prostogrpcclient-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="prostogrpcclient",
+    version="0.0.1",
+    packages=["prostogrpcclient"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
