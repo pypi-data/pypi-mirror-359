d7775976a96f6f6153f809f5b09f1e0f46764555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package junit-jupiter/5.10.3/junit-jupiter-5.10.3.tar.gz

---
+++ packages/junit-jupiter/junit-jupiter-5.10.3.tar.gz/junit-jupiter-5.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-jupiter",
+    version="5.10.3",
+    packages=["junit-jupiter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
