988f2c18a8262f15adf5233295ae6d94fdcf05c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:55 +0000

    Add package junit-platform-commons/1.10.3/junit-platform-commons-1.10.3.tar.gz

---
+++ packages/junit-platform-commons/junit-platform-commons-1.10.3.tar.gz/junit-platform-commons-1.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-platform-commons",
+    version="1.10.3",
+    packages=["junit-platform-commons"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
