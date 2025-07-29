2c5776c668352fdcfc0e4904fad2e5c7a57711b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:05 +0000

    Add package radiusauth/0.0.1/radiusauth-0.0.1.tar.gz

---
+++ packages/radiusauth/radiusauth-0.0.1.tar.gz/radiusauth-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="radiusauth",
+    version="0.0.1",
+    packages=["radiusauth"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
