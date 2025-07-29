ef207680df3e37eba5bf928a04895382c5465e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:13 +0000

    Add package jetty-io/12.0.11/jetty-io-12.0.11.tar.gz

---
+++ packages/jetty-io/jetty-io-12.0.11.tar.gz/jetty-io-12.0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="jetty-io",
+    version="12.0.11",
+    packages=["jetty-io"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
