800e240838cd440cf7c4c37efbc5de12e2c04f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-jdbc/6.1.11/spring-jdbc-6.1.11.tar.gz

---
+++ packages/spring-jdbc/spring-jdbc-6.1.11.tar.gz/spring-jdbc-6.1.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-jdbc",
+    version="6.1.11",
+    packages=["spring-jdbc"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
