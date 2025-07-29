f4e82538aa0f9b5016358f7cb371a686e399f891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:50 +0000

    Add package spring-data-jpa/3.3.2/spring-data-jpa-3.3.2.tar.gz

---
+++ packages/spring-data-jpa/spring-data-jpa-3.3.2.tar.gz/spring-data-jpa-3.3.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="spring-data-jpa",
+    version="3.3.2",
+    packages=["spring-data-jpa"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
