7b60eaa30f42a9f552a1e9fd7e4ef6832434f809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:02 +0000

    Add package pkg_action/0.0.1/pkg_action-0.0.1.tar.gz

---
+++ packages/pkg_action/pkg_action-0.0.1.tar.gz/pkg_action-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pkg_action",
+    version="0.0.1",
+    packages=["pkg_action"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
