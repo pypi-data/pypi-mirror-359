cffeec86987dd6e1fee5b951cc79b8593457a25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:12 +0000

    Add package rest-framework-filters/0.0.1/rest-framework-filters-0.0.1.tar.gz

---
+++ packages/rest-framework-filters/rest-framework-filters-0.0.1.tar.gz/rest-framework-filters-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="rest-framework-filters",
+    version="0.0.1",
+    packages=["rest-framework-filters"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
