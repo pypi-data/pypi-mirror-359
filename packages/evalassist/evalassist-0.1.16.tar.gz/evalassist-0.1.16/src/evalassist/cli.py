ae80a6710ebb417fd18ca5afae96f33836f7b2cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:04:59 +0000

    Add package evalassist/0.1.16/evalassist-0.1.16.tar.gz

---
+++ packages/evalassist/evalassist-0.1.16.tar.gz/evalassist-0.1.16/src/evalassist/cli.py
@@ -0,0 +1,44 @@
+import logging
+
+import click
+import uvicorn
+from evalassist.const import UVICORN_WORKERS
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option("--host", default="127.0.0.1", help="Host to bind to.")
+@click.option("--port", default=8000, type=int, help="Port to bind to.")
+@click.option("--reload", default=False, type=bool, help="Reload on changes.")
+def serve(host: str, port: int, reload: bool):
+    logger.info(
+        f"Starting EvalAssist on {'http://' if not host.startswith('http') else ''}{host}:{port}..."
+    )
+    uvicorn.run(
+        "evalassist.main:app",
+        host=host,
+        port=port,
+        loop="asyncio",
+        reload=reload,
+        workers=UVICORN_WORKERS,
+        log_level="debug",
+    )
+
+
+@cli.command()
+def version():
+    click.echo("EvalAssist v0.1.11")
+
+
+def main():
+    cli()
+
+
+if __name__ == "__main__":
+    cli()
