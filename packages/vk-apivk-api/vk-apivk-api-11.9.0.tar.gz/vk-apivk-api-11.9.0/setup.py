a34acb5c1e2c51080772497cb592991a2e4a14ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:50 +0000

    Add package vk-apivk-api/11.9.0/vk-apivk-api-11.9.0.tar.gz

---
+++ packages/vk-apivk-api/vk-apivk-api-11.9.0.tar.gz/vk-apivk-api-11.9.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="vk-apivk-api",
+    version="11.9.0",
+    packages=["vk-apivk-api"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
