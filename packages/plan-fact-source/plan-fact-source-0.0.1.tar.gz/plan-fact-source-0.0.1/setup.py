9c00a73e1299dbcf0ae9690a51d0bc10f745d416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:08:32 +0000

    Add package plan-fact-source/0.0.1/plan-fact-source-0.0.1.tar.gz

---
+++ packages/plan-fact-source/plan-fact-source-0.0.1.tar.gz/plan-fact-source-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="plan-fact-source",
+    version="0.0.1",
+    packages=["plan-fact-source"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
