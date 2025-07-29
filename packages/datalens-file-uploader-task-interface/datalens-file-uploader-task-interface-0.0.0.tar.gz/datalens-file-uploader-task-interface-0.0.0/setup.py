5925377c58ccde15ce58d06e86c3f06214adf166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:06:56 +0000

    Add package datalens-file-uploader-task-interface/0.0.0/datalens-file-uploader-task-interface-0.0.0.tar.gz

---
+++ packages/datalens-file-uploader-task-interface/datalens-file-uploader-task-interface-0.0.0.tar.gz/datalens-file-uploader-task-interface-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="datalens-file-uploader-task-interface",
+    version="0.0.0",
+    packages=["datalens-file-uploader-task-interface"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
