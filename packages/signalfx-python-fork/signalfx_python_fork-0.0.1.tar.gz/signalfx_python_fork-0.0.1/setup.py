2c61580145832fa98aadd1fda67d00542f1755cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:44 +0000

    Add package signalfx-python-fork/0.0.1/signalfx_python_fork-0.0.1.tar.gz

---
+++ packages/signalfx-python-fork/signalfx_python_fork-0.0.1.tar.gz/signalfx_python_fork-0.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2013-2014 SignalFuse, Inc. All rights reserved.
+# Copyright (C) 2015-2019 SignalFx, Inc. All rights reserved.
+
+from setuptools import setup, find_packages
+
+with open('signalfx/version.py') as f:
+    exec(f.read())
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+test_requirements = ['httmock']
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name=name,  # noqa
+    version=version,  # noqa
+    author='SignalFx, Inc',
+    author_email='info@signalfx.com',
+    description='SignalFx Python Library',
+    license='Apache Software License v2',
+    long_description=long_description,
+    zip_safe=True,
+    packages=find_packages(),
+    install_requires=requirements,
+    tests_require=test_requirements,
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    url='https://github.com/signalfx/signalfx-python',
+)
