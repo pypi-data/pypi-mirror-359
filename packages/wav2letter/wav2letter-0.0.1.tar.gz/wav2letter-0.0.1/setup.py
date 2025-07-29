f76eb14296d31a8eb26fe067efa024905ee0a33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package wav2letter/0.0.1/wav2letter-0.0.1.tar.gz

---
+++ packages/wav2letter/wav2letter-0.0.1.tar.gz/wav2letter-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="wav2letter",
+    version="0.0.1",
+    packages=["wav2letter"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
