dbbe6d372e711d22081aaa9dd818fe6750457828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-connector-oracle/0.0.1/dl-connector-oracle-0.0.1.tar.gz

---
+++ packages/dl-connector-oracle/dl-connector-oracle-0.0.1.tar.gz/dl-connector-oracle-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-connector-oracle",
+    version="0.0.1",
+    packages=["dl-connector-oracle"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
