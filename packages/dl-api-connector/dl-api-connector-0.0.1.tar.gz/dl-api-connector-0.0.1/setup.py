a58047f99a7e11dc7f90600811b3775e0e0bbd10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:47 +0000

    Add package dl-api-connector/0.0.1/dl-api-connector-0.0.1.tar.gz

---
+++ packages/dl-api-connector/dl-api-connector-0.0.1.tar.gz/dl-api-connector-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-api-connector",
+    version="0.0.1",
+    packages=["dl-api-connector"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
