35799b3b816e47ea196d71b8347b3468d4669a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package performance-tests/0.0.1/performance-tests-0.0.1.tar.gz

---
+++ packages/performance-tests/performance-tests-0.0.1.tar.gz/performance-tests-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="performance-tests",
+    version="0.0.1",
+    packages=["performance-tests"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
