191e16033b8457a037ad4ae100ba6f40faf536f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:03 +0000

    Add package corney-pb2/0.0.1/corney-pb2-0.0.1.tar.gz

---
+++ packages/corney-pb2/corney-pb2-0.0.1.tar.gz/corney-pb2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="corney-pb2",
+    version="0.0.1",
+    packages=["corney-pb2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
