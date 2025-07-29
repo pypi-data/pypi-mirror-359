93133e0bf2b5b242c806a5f430e7496cf726ece0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:57 +0000

    Add package lorrystream/0.0.9/lorrystream-0.0.9-py3-none-any.whl

---
+++ packages/lorrystream/lorrystream-0.0.9-py3-none-any.whl/lorrystream/model.py
@@ -0,0 +1,132 @@
+# Copyright (c) 2013-2023, The Kotori developers and contributors.
+# Distributed under the terms of the LGPLv3 license, see LICENSE.
+
+import dataclasses
+import operator
+import typing as t
+from urllib.parse import parse_qs, urlparse
+
+import funcy
+from streamz import Sink, Source
+
+from lorrystream.exceptions import InvalidSinkError
+from lorrystream.streamz.model import URL, BusMessage
+from lorrystream.util.data import asbool, split_list
+
+SinkInputType = t.Union[str, t.Callable, None]
+
+
+@dataclasses.dataclass
+class Channel:
+    source: Source
+    pipeline: t.Any
+    sink: t.Union[t.Callable, Sink]
+
+    def tap(self, callback: t.Callable):
+        self.pipeline.stream.sink(callback)
+
+
+@dataclasses.dataclass
+class Packet:
+    payload: t.Any
+    busmsg: t.Optional[BusMessage] = None
+
+    @staticmethod
+    def payloads(data):
+        return list(map(operator.attrgetter("payload"), data))
+
+
+class ConnectionString:
+    """
+    Helper class to support ``IoAccessor.export()``.
+    """
+
+    def __init__(self, url):
+        self.url_raw = url
+        self.url = urlparse(url)
+
+    def get_database(self):
+        # Try to get database name from query parameter.
+        database = self.get_query_param("database")
+
+        # Try to get database name from URL path.
+        if database is None:
+            if self.url.path.startswith("/"):
+                database = self.url.path[1:]
+
+        return database or "dwd"
+
+    def get_table(self):
+        return self.get_query_param("table") or "weather"
+
+    def get_path(self):
+        return self.url.path or self.url.netloc
+
+    def get_query_param(self, name):
+        query = parse_qs(self.url.query)
+        try:
+            return query[name][0]
+        except (KeyError, IndexError):
+            return None
+
+
+@dataclasses.dataclass
+class StreamAddress:
+    uri: URL
+    options: t.Dict[str, str] = dataclasses.field(default_factory=dict)
+
+    @classmethod
+    def from_url(cls, url: str):
+        uri = URL(url)
+        options = cls.extract_options(uri)
+        return cls(uri=uri, options=options)
+
+    @staticmethod
+    def extract_options(uri: URL) -> t.Dict[str, str]:
+        # Separate URI query parameters used by LorryStream.
+        control_option_names = [
+            # General options.
+            "content-type",
+            "reconnect",
+            # AMQP options.
+            "exchange",
+            "exchange-type",
+            "queue",
+            "routing-key",
+            "setup",
+        ]
+        list_options = [
+            "setup",
+        ]
+        boolean_options = [
+            "reconnect",
+        ]
+        options = funcy.project(uri.query_params, control_option_names)
+        query_params = funcy.omit(uri.query_params, control_option_names)
+        uri.query_params = query_params
+
+        for list_option in list_options:
+            if list_option in options:
+                options[list_option] = split_list(options[list_option])
+
+        for boolean_option in boolean_options:
+            if boolean_option in options:
+                options[boolean_option] = asbool(options[boolean_option])
+
+        return options
+
+    @classmethod
+    def resolve_sink(cls, sink: SinkInputType):
+        """
+        Select sink element of pipeline.
+        """
+
+        if callable(sink):
+            url = f"callable://{sink.__name__}"
+        elif isinstance(sink, str):
+            url = sink
+        else:
+            raise InvalidSinkError(f"Invalid sink: {sink}")
+
+        uri = URL(url)
+        return cls(uri=uri)
---
+++ packages/lorrystream/lorrystream-0.0.9-py3-none-any.whl/lorrystream/util/about.py
@@ -0,0 +1,120 @@
+# Copyright (c) 2013-2023, The Kotori developers and contributors.
+# Distributed under the terms of the LGPLv3 license, see LICENSE.
+
+import platform
+import sys
+import textwrap
+import typing as t
+from collections import abc
+
+from colorama import Fore, Style
+
+string_or_list = t.Union[str, t.List[str], t.Iterable[str]]
+
+
+def section(text: str):
+    return armor(text, title)
+
+
+def subsection(text: str):
+    return armor(text, subtitle)
+
+
+def armor(text: str, formatter: t.Callable):
+    guard = "=" * len(text)
+    print(guard)
+    print(formatter(text))
+    print(guard)
+
+
+def title(text: str):
+    return Fore.CYAN + Style.BRIGHT + text + Style.RESET_ALL
+
+
+def subtitle(text: str):
+    return Fore.YELLOW + Style.BRIGHT + text + Style.RESET_ALL
+
+
+def text_list(data: string_or_list):
+    if isinstance(data, str):
+        return data
+    elif isinstance(data, t.List):
+        return ", ".join(data)
+    else:
+        raise ValueError("Unable to flatten list")
+
+
+def bullet_list(data: t.List[str]):
+    data.sort()
+    lines = [f"- {text_list(line)}" for line in data]
+    return "\n".join(lines)
+
+
+def wrap_list(data: t.List[str], **kwargs):
+    data = [item for item in data if item is not None]
+    data.sort()
+    text = ", ".join(data)
+    return "\n".join(textwrap.wrap(text, width=80, **kwargs))
+
+
+def bullet_item(data: string_or_list, label: str = None):
+    if data is None:
+        return None
+    if label is None:
+        label = ""
+    else:
+        label = f"{label}: "
+    if isinstance(data, (list, tuple, abc.KeysView, abc.ValuesView)):
+        text = wrap_list(list(data), subsequent_indent="  ")
+    else:
+        text = data
+    return f"- {label}{text}"
+
+
+class AboutReport:
+    @staticmethod
+    def platform():
+        section("Platform")
+        print()
+
+        subsection("Python")
+        print(bullet_item(platform.platform()))
+        print()
+
+        # SQLAlchemy
+        from importlib.metadata import entry_points
+
+        import sqlalchemy.dialects
+
+        subsection("SQLAlchemy")
+        print(bullet_item(sqlalchemy.dialects.__all__, label="Dialects built-in"))  # type: ignore[attr-defined]
+        dialects: t.List[str]
+        if sys.version_info >= (3, 10):
+            eps = entry_points(group="sqlalchemy.dialects")
+            dialects = [dialect.name for dialect in eps]
+        else:
+            dialects = []
+        print(bullet_item(dialects, label="Dialects 3rd-party"))
+        print(bullet_item(sqlalchemy.dialects.plugins.impls.keys(), label="Plugins"))  # type: ignore[attr-defined]
+        print()
+
+        # fsspec
+        try:
+            import fsspec
+
+            subsection("fsspec protocols")
+            print(bullet_item(fsspec.available_protocols()))
+            print()
+
+            subsection("fsspec compressions")
+            print(bullet_item(fsspec.available_compressions()))
+            print()
+        except ImportError:
+            pass
+
+        # pandas
+        subsection("pandas module versions")
+        import pandas
+
+        pandas.show_versions(as_json=False)
+        print()
