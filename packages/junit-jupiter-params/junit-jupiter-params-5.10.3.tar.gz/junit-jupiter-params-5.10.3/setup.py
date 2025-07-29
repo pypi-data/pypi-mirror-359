7631be496408f10b5db0307bfc8e1827ba7e3eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:54 +0000

    Add package junit-jupiter-params/5.10.3/junit-jupiter-params-5.10.3.tar.gz

---
+++ packages/junit-jupiter-params/junit-jupiter-params-5.10.3.tar.gz/junit-jupiter-params-5.10.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="junit-jupiter-params",
+    version="5.10.3",
+    packages=["junit-jupiter-params"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
