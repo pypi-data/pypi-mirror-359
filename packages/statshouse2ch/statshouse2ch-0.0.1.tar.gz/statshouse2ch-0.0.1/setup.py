7ff105479adbf3b61b987fe532c2e5daa3c7bf11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:33 +0000

    Add package statshouse2ch/0.0.1/statshouse2ch-0.0.1.tar.gz

---
+++ packages/statshouse2ch/statshouse2ch-0.0.1.tar.gz/statshouse2ch-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="statshouse2ch",
+    version="0.0.1",
+    packages=["statshouse2ch"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
