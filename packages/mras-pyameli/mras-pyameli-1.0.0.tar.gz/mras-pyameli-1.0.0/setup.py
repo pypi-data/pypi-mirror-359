e6ae16ece97814dd3b8037d5a8752b9084163e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:01 +0000

    Add package mras-pyameli/1.0.0/mras-pyameli-1.0.0.tar.gz

---
+++ packages/mras-pyameli/mras-pyameli-1.0.0.tar.gz/mras-pyameli-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mras-pyameli",
+    version="1.0.0",
+    packages=["mras-pyameli"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
