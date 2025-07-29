81595a2aba479a53ffe3de3356d80b236560dec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:07:48 +0000

    Add package dl-file-secure-reader-lib/0.1.0/dl-file-secure-reader-lib-0.1.0.tar.gz

---
+++ packages/dl-file-secure-reader-lib/dl-file-secure-reader-lib-0.1.0.tar.gz/dl-file-secure-reader-lib-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dl-file-secure-reader-lib",
+    version="0.1.0",
+    packages=["dl-file-secure-reader-lib"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
