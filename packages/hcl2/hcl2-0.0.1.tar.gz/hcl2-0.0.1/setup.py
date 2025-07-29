7577ff906f896055b797b453824a6f2ea0663451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:00 +0000

    Add package hcl2/0.0.1/hcl2-0.0.1.tar.gz

---
+++ packages/hcl2/hcl2-0.0.1.tar.gz/hcl2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hcl2",
+    version="0.0.1",
+    packages=["hcl2"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
