014d2f99279fe70894786476e7370208ff369a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:37 +0000

    Add package logician/0.0.0.dev2/logician-0.0.0.dev2.tar.gz

---
+++ packages/logician/logician-0.0.0.dev2.tar.gz/logician-0.0.0.dev2/src/logician/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+"""
+Global constants for the vt-logger.
+"""
+
+VT_ALL_LOG_ENV_VAR = 'VT_ALL_LOG'
+"""
+Set logging level for all logging that applies to VT.
+"""
