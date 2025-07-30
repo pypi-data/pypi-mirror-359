345fbcd43671e845bccd38c13b0d63643cdd01a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:27 +0000

    Add package natn/0.0.8/natn-0.0.8-py3-none-any.whl

---
+++ packages/natn/natn-0.0.8-py3-none-any.whl/natn/__init__.py
@@ -0,0 +1,18 @@
+from .clipboard import copy_to_clipboard
+from .core import ask
+
+async def round(question: str, python_only: int = 1, show_answer: int = 0) -> None:
+    answer = await ask(question, bool(python_only))
+
+    if bool(show_answer):
+        print(answer)
+
+    if len(answer) == 0:
+       copy_to_clipboard("-1")
+    else:
+        copy_to_clipboard(answer)
+
+    if not answer:
+        print("-1")
+    else:
+        print("1")
