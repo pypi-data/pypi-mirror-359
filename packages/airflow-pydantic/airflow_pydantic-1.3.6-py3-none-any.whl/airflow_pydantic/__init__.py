c8f4c22117c815a1f0e27736992d7ab6cd382b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:30 +0000

    Add package airflow-pydantic/1.3.6/airflow_pydantic-1.3.6-py3-none-any.whl

---
+++ packages/airflow-pydantic/airflow_pydantic-1.3.6-py3-none-any.whl/airflow_pydantic/__init__.py
@@ -0,0 +1,9 @@
+from .base import *
+from .dag import *
+from .task import *
+from .utils import *
+
+# NOTE: last to avoid circular imports
+from .operators import *  # isort: skip
+
+__version__ = "1.3.6"
