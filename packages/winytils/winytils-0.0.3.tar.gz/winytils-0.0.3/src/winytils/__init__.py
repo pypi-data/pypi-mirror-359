93e3dd46f2a696f4b9dda9407ab8e1e2ebfb4f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:46 +0000

    Add package winytils/0.0.3/winytils-0.0.3.tar.gz

---
+++ packages/winytils/winytils-0.0.3.tar.gz/winytils-0.0.3/src/winytils/__init__.py
@@ -0,0 +1,11 @@
+"""
+To execute every module directly - remove relative imports "."
+"""
+
+from . import workstation_events
+from . import privileges
+from . import windows
+from . import workstation
+
+
+__version__ = "0.0.3"
