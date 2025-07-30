7f8ed011cbe671eec7476afe71144b50b44d2427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:27 +0000

    Add package archnemesis/1.0.4/archnemesis-1.0.4.tar.gz

---
+++ packages/archnemesis/archnemesis-1.0.4.tar.gz/archnemesis-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='archnemesis',
+      version='1.0.4',
+      description='Python implementation of the NEMESIS radiative transfer and retrieval code',
+      packages=['archnemesis'],
+      install_requires=[
+            'numpy',
+            'matplotlib',
+            'numba>=0.57.0',
+            'scipy',
+            'pymultinest',
+            'cdsapi',
+            'pygrib',
+            'joblib',
+            'h5py',
+            'basemap',
+            'pytest'],
+      )
