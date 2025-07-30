ec2b5be7cee863a4d6a00195c882ee7637a830c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:24 +0000

    Add package aa-memberaudit-dc/0.0.2/aa_memberaudit_dc-0.0.2-py3-none-any.whl

---
+++ packages/aa-memberaudit-dc/aa_memberaudit_dc-0.0.2-py3-none-any.whl/madc/api/schema.py
@@ -0,0 +1,21 @@
+# Third Party
+from ninja import Schema
+
+
+class Message(Schema):
+    message: str
+
+
+class Character(Schema):
+    character_name: str
+    character_id: int
+    corporation_id: int
+    corporation_name: str
+    alliance_id: int | None = None
+    alliance_name: str | None = None
+
+
+class CharacterDoctrines(Schema):
+    character: Character
+    doctrines: dict
+    # skills: dict
