90285c5ced6b2272231e77b8eefec81356ddd4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:40 +0000

    Add package py-xl-sindy/2.0.4/py_xl_sindy-2.0.4.tar.gz

---
+++ packages/py-xl-sindy/py_xl_sindy-2.0.4.tar.gz/py_xl_sindy-2.0.4/src/xlsindy/catalog_base/_lagrangian.py
@@ -0,0 +1,79 @@
+"""
+Contains the function responsible for the lagrangian part of the catalog.
+"""
+
+import sympy 
+import numpy as np
+from .. import euler_lagrange
+
+from ..catalog import CatalogCategory
+
+class Lagrange(CatalogCategory):
+    """
+    Lagrange based catalog. 
+
+    Args:
+        catalog (List[sympy.Expr]): The catalog of functions to be used in the Lagrangian equations.
+        symbol_matrix (np.ndarray): The matrix of symbolic variables (external forces, positions, velocities, and accelerations).
+        time_symbol (sympy.Symbol): The symbolic variable representing time.
+    """
+    def __init__(self, catalog,symbol_matrix: np.ndarray, time_symbol: sympy.Symbol):
+    
+
+        self.catalog= catalog
+        self.symbol_matrix =symbol_matrix
+        self.time_symbol = time_symbol
+
+        ## Required attributes
+        self.catalog_length = int(len(catalog))
+        self.num_coordinate = int(self.symbol_matrix.shape[1])
+
+
+    def create_solution_vector(self, expression: sympy.Expr):
+        expanded_expression_terms = sympy.expand(
+            sympy.expand_trig(expression)
+        ).args  # Expand the expression in order to get base function (ex: x, x^2, sin(s), ...)
+        solution_vector = np.zeros((len(self.catalog), 1))
+
+        for term in expanded_expression_terms:
+            for idx, catalog_term in enumerate(self.catalog):
+                test = term / catalog_term
+                if (
+                    len(test.args) == 0
+                ):  # if test is a constant it means that catalog_term is inside equation
+                    solution_vector[idx, 0] = test
+
+        return solution_vector.reshape(-1, 1)
+
+    def expand_catalog(self):
+
+        res = np.empty((len(self.catalog), self.num_coordinate), dtype=object)
+
+        for i in range(self.num_coordinate):
+
+            catalog_lagrange = list(
+                map(
+                    lambda x: euler_lagrange.compute_euler_lagrange_equation(
+                        x, self.symbol_matrix, self.time_symbol, i
+                    ),
+                    self.catalog,
+                )
+            )
+            res[:, i] = catalog_lagrange
+
+        return res
+
+    def label(self):
+        raise NotImplementedError
+    
+    def separate_by_mask(self, mask):
+
+        return Lagrange(
+            catalog=self.catalog[mask==1],
+            symbol_matrix=self.symbol_matrix,
+            time_symbol=self.time_symbol
+        ), Lagrange(
+            catalog=self.catalog[mask==0],
+            symbol_matrix=self.symbol_matrix,
+            time_symbol=self.time_symbol
+        )
