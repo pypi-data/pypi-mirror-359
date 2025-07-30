413dc56c77819ae269c29bd8a8faedc74d1929ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:15 +0000

    Add package piegy/2.3.1/piegy-2.3.1-cp311-cp311-musllinux_1_2_i686.whl

---
+++ packages/piegy/piegy-2.3.1-cp311-cp311-musllinux_1_2_i686.whl/piegy/build_info.py
@@ -0,0 +1,11 @@
+"""
+Contains build info, whether it's local built, or a pre-compiled wheel.
+Auto-generated at compile time.
+"""
+
+build_info = {
+    "version": "2.3.1",
+    "build date": "2025-07-02 08:01:01",
+    "python used": "3.11.12",
+    "platform": "linux"
+}
