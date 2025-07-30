3b7c2a5b6e3dc1e68e1395ac1489809d34c880e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:39 +0000

    Add package xkings/0.1.1/xkings-0.1.1.tar.gz

---
+++ packages/xkings/xkings-0.1.1.tar.gz/xkings-0.1.1/xkings/__init__.py
@@ -0,0 +1,9 @@
+"""
+xkings: eine kleine Library mit deinem Druck-Kürzel hs.
+"""
+
+__version__ = "0.1.0"
+
+from .core import hs
+
+__all__ = ["hs"]
---
+++ packages/xkings/xkings-0.1.1.tar.gz/xkings-0.1.1/xkings/core.py
@@ -0,0 +1,6 @@
+def hs(*args, sep=' ', end='\n'):
+    """
+    hs(*args, sep=' ', end='\\n') funktioniert wie print, aber unter deinem Kürzel.
+    """
+    # Einfach Python's eingebaute print benutzen
+    print(*args, sep=sep, end=end)
