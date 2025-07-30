9f94baeba5d4da8ad5d867c33630b30e698a5e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:46 +0000

    Add package samplehc/0.3.1/samplehc-0.3.1.tar.gz

---
+++ packages/samplehc/samplehc-0.3.1.tar.gz/samplehc-0.3.1/src/samplehc/types/v2/hie/__init__.py
@@ -0,0 +1,6 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .patient_retrieve_documents_params import PatientRetrieveDocumentsParams as PatientRetrieveDocumentsParams
+from .patient_retrieve_documents_response import PatientRetrieveDocumentsResponse as PatientRetrieveDocumentsResponse
