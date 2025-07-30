734ea787389fb6092f3b31daa1b8c9aac7c4d5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:01 +0000

    Add package barcode-validator/2.0.7/barcode_validator-2.0.7.tar.gz

---
+++ packages/barcode-validator/barcode_validator-2.0.7.tar.gz/barcode_validator-2.0.7/barcode_validator/idservices/factory.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from nbitk.config import Config
+from barcode_validator.idservices.idservice import IDService
+from barcode_validator.constants import RefDB
+
+
+class IDServiceFactory:
+
+    @staticmethod
+    def create_idservice(config: Config, refdb: RefDB) -> Optional[IDService]:
+        from barcode_validator.idservices.ncbi import NCBI
+        if refdb == RefDB.NCBI:
+            return NCBI(config)
+        return None
