204aab86c792754e841142eb388e94a9e4354fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:02 +0000

    Add package scikit-learnpython_version/0.0.1/scikit-learnpython_version-0.0.1.tar.gz

---
+++ packages/scikit-learnpython_version/scikit-learnpython_version-0.0.1.tar.gz/scikit-learnpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="scikit-learnpython_version",
+    version="0.0.1",
+    packages=["scikit-learnpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
