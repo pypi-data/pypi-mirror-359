1778c326830782bbc04058f9d4460eeb018db243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:34 +0000

    Add package ubc-solar-physics/1.8.3/ubc_solar_physics-1.8.3-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/ubc-solar-physics/ubc_solar_physics-1.8.3-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/physics/models/__init__.py
@@ -0,0 +1,13 @@
+from .arrays import BasicArray
+from .battery import BasicBattery
+from .lvs import BasicLVS
+from .motor import BasicMotor
+from .regen import BasicRegen
+
+__all__ = [
+    "BasicArray",
+    "BasicBattery",
+    "BasicLVS",
+    "BasicMotor",
+    "BasicRegen"
+]
