bc226bca1fba449268e23adeb61fa1d73d844a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:09:41 +0000

    Add package excelipy/0.1.3/excelipy-0.1.3.tar.gz

---
+++ packages/excelipy/excelipy-0.1.3.tar.gz/excelipy-0.1.3/excelipy/writers/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "write_table",
+    "write_fill",
+    "write_text",
+]
+
+from excelipy.writers.fill import write_fill
+from excelipy.writers.table import write_table
+from excelipy.writers.text import write_text
