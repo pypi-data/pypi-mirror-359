01bf573cbd41e8b345a837d55700dcacd77c72cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:06 +0000

    Add package spinlab/1.0.0/spinlab-1.0.0.tar.gz

---
+++ packages/spinlab/spinlab-1.0.0.tar.gz/spinlab-1.0.0/spinlab/math/__init__.py
@@ -0,0 +1,6 @@
+"""Modules to constructs numpy arrays from input arguments"""
+
+from . import lineshape
+from . import window
+from . import relaxation
+from . import pulses
---
+++ packages/spinlab/spinlab-1.0.0.tar.gz/spinlab-1.0.0/spinlab/widgets/__init__.py
@@ -0,0 +1,4 @@
+"""Matplotlib widgets for processing and to increase usability"""
+
+from .align_widget import align_widget
+from .phase_widget import phase_widget
