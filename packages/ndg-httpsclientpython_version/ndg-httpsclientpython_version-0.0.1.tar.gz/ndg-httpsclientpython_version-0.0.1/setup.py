78be6cea128fc745b784be12b7a38b4d2e9bc2ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:49 +0000

    Add package ndg-httpsclientpython_version/0.0.1/ndg-httpsclientpython_version-0.0.1.tar.gz

---
+++ packages/ndg-httpsclientpython_version/ndg-httpsclientpython_version-0.0.1.tar.gz/ndg-httpsclientpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="ndg-httpsclientpython_version",
+    version="0.0.1",
+    packages=["ndg-httpsclientpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
