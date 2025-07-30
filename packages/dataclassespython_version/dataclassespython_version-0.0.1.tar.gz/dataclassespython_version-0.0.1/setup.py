356cae4fd6346b35f3cefb2ef7082655cff0cbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:07 +0000

    Add package dataclassespython_version/0.0.1/dataclassespython_version-0.0.1.tar.gz

---
+++ packages/dataclassespython_version/dataclassespython_version-0.0.1.tar.gz/dataclassespython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dataclassespython_version",
+    version="0.0.1",
+    packages=["dataclassespython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
