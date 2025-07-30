1105b8f41685c069a2f2352445fa134732504f00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:15 +0000

    Add package dodopayments/1.37.0/dodopayments-1.37.0-py3-none-any.whl

---
+++ packages/dodopayments/dodopayments-1.37.0-py3-none-any.whl/dodopayments/types/payment_retrieve_line_items_response.py
@@ -0,0 +1,26 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+
+from .._models import BaseModel
+from .currency import Currency
+
+__all__ = ["PaymentRetrieveLineItemsResponse", "Item"]
+
+
+class Item(BaseModel):
+    amount: int
+
+    items_id: str
+
+    tax: int
+
+    description: Optional[str] = None
+
+    name: Optional[str] = None
+
+
+class PaymentRetrieveLineItemsResponse(BaseModel):
+    currency: Currency
+
+    items: List[Item]
