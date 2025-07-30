c4be6fd15d08178a49f86013022c5153adfbe894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:16 +0000

    Add package exohammer/0.1.0/exohammer-0.1.0.tar.gz

---
+++ packages/exohammer/exohammer-0.1.0.tar.gz/exohammer-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as infile:
+	long_description = infile.read()
+
+version = {}
+with open(path.join(here, 'exohammer', 'version.py')) as infile:
+	exec(infile.read(), version)
+
+with open(path.join(here, 'requirements.txt')) as f:
+	required = f.read().splitlines()
+
+setup(
+	name='exohammer',
+	version=version['__version__'],
+	description='TTV and RV MCMC generator',
+	url='https://github.com/nick-juliano/exohammer',
+	long_description = long_description,
+	long_description_content_type="text/markdown",
+	author='Nick Juliano',
+	author_email='nick_juliano@icloud.com',
+	license='GPL',
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=required,
+	extras_require={"extras": ["pickle", "matplotlib", "easygui"], }
+)
