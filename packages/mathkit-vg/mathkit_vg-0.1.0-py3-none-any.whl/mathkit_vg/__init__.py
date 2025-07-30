2a2249a879c587af1dd30f33bc20eb106da6d16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:27 +0000

    Add package mathkit-vg/0.1.0/mathkit_vg-0.1.0-py3-none-any.whl

---
+++ packages/mathkit-vg/mathkit_vg-0.1.0-py3-none-any.whl/mathkit_vg/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    is_even,
+    is_odd,
+    factorial,
+    fibonacci_upto,
+    is_fibonacci,
+    reverse_number,
+    digital_root,
+    gcd,
+    lcm,
+    is_strong_number,
+    is_automorphic,
+    prime_factors,
+    next_prime,
+    is_abundant_number,
+    count_zeros
+)
