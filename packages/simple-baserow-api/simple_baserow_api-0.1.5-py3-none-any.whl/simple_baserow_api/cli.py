d0d7beb3e2ed70b04df6d8a4d706d4d1276e921b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:15 +0000

    Add package simple-baserow-api/0.1.5/simple_baserow_api-0.1.5-py3-none-any.whl

---
+++ packages/simple-baserow-api/simple_baserow_api-0.1.5-py3-none-any.whl/simple_baserow_api/cli.py
@@ -0,0 +1,21 @@
+"""CLI interface for simple_baserow_api project.
+"""
+
+
+def main():  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m simple_baserow_api` and `$ simple_baserow_api `.
+
+    This is your program's entry point.
+
+    You can change this function to do whatever you want.
+    Examples:
+        * Run a test suite
+        * Run a server
+        * Do some other stuff
+        * Run a command line application (Click, Typer, ArgParse)
+        * List all available tasks
+        * Run an application (Flask, FastAPI, Django, etc.)
+    """
+    print("This will do something")
