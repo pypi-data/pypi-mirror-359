7ca308a8a4c5b2678df60853dd02b625e96f4b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:27 +0000

    Add package composio-praisonai/0.7.20/composio_praisonai-0.7.20-py3-none-any.whl

---
+++ packages/composio-praisonai/composio_praisonai-0.7.20-py3-none-any.whl/composio_praisonai/__init__.py
@@ -0,0 +1,14 @@
+from composio import Action, App, Tag, Trigger, WorkspaceType, action
+
+from composio_praisonai.toolset import ComposioToolSet
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "Trigger",
+    "ComposioToolSet",
+    "WorkspaceType",
+    "action",
+)
