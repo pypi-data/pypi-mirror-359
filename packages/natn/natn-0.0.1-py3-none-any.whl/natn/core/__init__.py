1ffebab978d201848647d7e5cc06f0c7058f5c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:09 +0000

    Add package natn/0.0.1/natn-0.0.1-py3-none-any.whl

---
+++ packages/natn/natn-0.0.1-py3-none-any.whl/natn/core/__init__.py
@@ -0,0 +1,21 @@
+import httpx
+
+from ..config import API_URLS, API_NAMES
+from ..network import post, is_valid_response
+
+async def ask(question: str, python_only: bool):
+    payload = {
+        "question": question,
+        "python_only": python_only,
+        "subject": "fisica"
+    }
+
+    for i, url in enumerate(API_URLS):
+        response = await post(url, payload)
+        if is_valid_response(response):
+            response_json = response.get("json", None)
+            raw_answer = response_json.get("answer", None)
+            if raw_answer is not None:
+                return raw_answer
+
+    return ""
