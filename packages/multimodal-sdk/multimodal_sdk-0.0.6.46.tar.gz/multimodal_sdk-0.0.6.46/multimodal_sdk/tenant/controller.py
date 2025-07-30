2949d5744d8edcda7ac3180ec12d7db9a7b90bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:41 +0000

    Add package multimodal-sdk/0.0.6.46/multimodal_sdk-0.0.6.46.tar.gz

---
+++ packages/multimodal-sdk/multimodal_sdk-0.0.6.46.tar.gz/multimodal_sdk-0.0.6.46/multimodal_sdk/tenant/controller.py
@@ -0,0 +1,92 @@
+import aiohttp
+import logging
+from multimodal_sdk.common.refresh import token_refresh_handler
+from multimodal_sdk.common.http_status_handler import HTTPStatusHandler
+from multimodal_sdk.common.decorators import log_and_authorize
+from multimodal_sdk.common.retry_decorator import retry_on_client_error
+from multimodal_sdk.common.rate_limiter import rate_limiter
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(201)
+async def create_tenant_func(tenant_name, **kwargs):
+    url = f"{kwargs['base_url']}/tenants"
+    payload = {"name": tenant_name}
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.post(url, json=payload, headers=kwargs['headers'])
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(204)
+async def delete_tenant_func(tenant_name, **kwargs):
+    url = f"{kwargs['base_url']}/tenants"
+    payload = {"name": tenant_name}
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.delete(url, json=payload, headers=kwargs['headers'])
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(200)
+async def get_tenant_func(**kwargs):
+    url = f"{kwargs['base_url']}/tenants"
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.get(url, headers=kwargs['headers'])
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(201)
+async def create_knowledge_base_func(
+    tenant_resource_id,
+    kb_name,
+    **kwargs
+):
+    url = f"{kwargs['base_url']}/tenants/{tenant_resource_id}/knowledge-bases"
+    payload = {"name": kb_name}
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.post(url, json=payload, headers=kwargs['headers'])
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(204)
+async def delete_knowledge_base_func(
+    tenant_resource_id,
+    kb_name,
+    **kwargs
+):
+    url = f"{kwargs['base_url']}/tenants/{tenant_resource_id}/knowledge-bases"
+    payload = {"name": kb_name}
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.delete(url, json=payload, headers=kwargs['headers'])
+
+@retry_on_client_error()
+@token_refresh_handler
+@log_and_authorize
+@HTTPStatusHandler(200)
+async def get_knowledge_base_func(
+    tenant_resource_id,
+    **kwargs
+):
+    url = f"{kwargs['base_url']}/tenants/{tenant_resource_id}/knowledge-bases"
+
+    async with aiohttp.ClientSession() as session:
+        async with rate_limiter:
+            return await session.get(url, headers=kwargs['headers'])
+
