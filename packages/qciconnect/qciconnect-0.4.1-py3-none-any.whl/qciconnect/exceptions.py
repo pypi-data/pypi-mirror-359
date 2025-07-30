602ad2cd2cf4c05235cbcfbc99a168a3a54be140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:13 +0000

    Add package qciconnect/0.4.1/qciconnect-0.4.1-py3-none-any.whl

---
+++ packages/qciconnect/qciconnect-0.4.1-py3-none-any.whl/qciconnect/exceptions.py
@@ -0,0 +1,11 @@
+class KeycloakTokenManagerException(Exception):
+    """Exception class for exceptions from KeycloakTokenManager."""
+
+class QciConnectClientException(Exception):
+    """Exception class for exceptions from QciConnectClient."""
+
+class InvalidIdentifierException(Exception):
+    """Thrown when identifiers contain forbidden characters."""
+
+class ResultException(Exception):
+    """Exception class for exceptions related to results handling."""
