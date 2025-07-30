6ddafc7a0da1e330450f8560a282e3ba36b105eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package gunicornpython_version/0.0.1/gunicornpython_version-0.0.1.tar.gz

---
+++ packages/gunicornpython_version/gunicornpython_version-0.0.1.tar.gz/gunicornpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="gunicornpython_version",
+    version="0.0.1",
+    packages=["gunicornpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
