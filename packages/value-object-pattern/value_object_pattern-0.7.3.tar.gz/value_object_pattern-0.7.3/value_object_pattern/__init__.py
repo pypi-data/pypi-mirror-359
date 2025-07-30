88b070d68fb1c86eecb65369a3942129fe29ed7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:12 +0000

    Add package value-object-pattern/0.7.3/value_object_pattern-0.7.3.tar.gz

---
+++ packages/value-object-pattern/value_object_pattern-0.7.3.tar.gz/value_object_pattern-0.7.3/value_object_pattern/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.7.3'
+
+from .decorators import process, validation
+from .models import BaseModel, EnumerationValueObject, ValueObject
+
+__all__ = (
+    'BaseModel',
+    'EnumerationValueObject',
+    'ValueObject',
+    'process',
+    'validation',
+)
