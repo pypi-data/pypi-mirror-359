e1c40955b08a4280de81fcac6ca2bc6455db4b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:08:08 +0000

    Add package DecoderBot/1.0.0/decoderbot-1.0.0.tar.gz

---
+++ packages/DecoderBot/decoderbot-1.0.0.tar.gz/decoderbot-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='decoderbot',
+    version='1.0.0',
+    py_modules=['ChatBotModule'],
+    author='Unknown Decoder',
+    description='A simple trainable chatbot using OOP in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
