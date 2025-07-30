13316539f21810400c0355f1ca3ec49446782c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:31 +0000

    Add package netmind/0.1.6/netmind-0.1.6.tar.gz

---
+++ packages/netmind/netmind-0.1.6.tar.gz/netmind-0.1.6/src/netmind/resources/__init__.py
@@ -0,0 +1,16 @@
+from netmind.resources.chat import Chat, AsyncChat
+from netmind.resources.embeddings import Embeddings, AsyncEmbeddings
+from netmind.resources.files import Files, AsyncFiles
+from netmind.resources.parse_pro import ParsePro, AsyncParsePro
+
+
+__all__ = [
+    "Chat",
+    "AsyncChat",
+    "Embeddings",
+    "AsyncEmbeddings",
+    "Files",
+    "AsyncFiles",
+    "ParsePro",
+    "AsyncParsePro",
+]
