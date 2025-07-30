d70c4516e5a723d010c980ad5e0cf62b00bb5fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:30 +0000

    Add package pywayne/1.0.1.4.0/pywayne-1.0.1.4.0.tar.gz

---
+++ packages/pywayne/pywayne-1.0.1.4.0.tar.gz/pywayne-1.0.1.4.0/pywayne/__init__.py
@@ -0,0 +1,24 @@
+# from . import tools, tt_api
+from . import statistics
+
+__all__ = [
+    'tools',
+    'dsp',
+    'gui',
+    'maths',
+    'statistics',
+    'data_structure',
+    'plot',
+    'ahrs',
+    'calibration',
+    'lark_custom_bot',
+    'lark_bot',
+    'lark_bot_listener',
+    'tts',
+    'aliyun_oss',
+    'helper',
+    # 'tt_api',
+    # 'ocs_api',
+]
+
+name = 'pywayne'
