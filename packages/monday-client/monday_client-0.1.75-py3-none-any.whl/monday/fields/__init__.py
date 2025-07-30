79c74850eaefd8f2530332dc1e918b9c54b78bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:28 +0000

    Add package monday-client/0.1.75/monday_client-0.1.75-py3-none-any.whl

---
+++ packages/monday-client/monday_client-0.1.75-py3-none-any.whl/monday/fields/__init__.py
@@ -0,0 +1,5 @@
+from monday.fields.board_fields import BoardFields
+from monday.fields.column_fields import ColumnFields
+from monday.fields.group_fields import GroupFields
+from monday.fields.item_fields import ItemFields
+from monday.fields.user_fields import UserFields
