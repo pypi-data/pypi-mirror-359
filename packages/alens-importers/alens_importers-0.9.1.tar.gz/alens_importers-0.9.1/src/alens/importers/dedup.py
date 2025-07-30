87322c2b12f1ee93321df3931e5e7285bdfcfbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:47 +0000

    Add package alens-importers/0.9.1/alens_importers-0.9.1.tar.gz

---
+++ packages/alens-importers/alens_importers-0.9.1.tar.gz/alens_importers-0.9.1/src/alens/importers/dedup.py
@@ -0,0 +1,50 @@
+'''
+Deduplication of transactions.
+'''
+
+from beancount.core import data
+
+
+# Metadata field that indicates the entry is a likely duplicate.
+DUPLICATE = "__duplicate__"
+
+
+def _get_transaction_key(t: data.Transaction):
+    if not isinstance(t, data.Transaction):
+        return None
+
+    def _get_posting_key(p: data.Posting):
+        if not p.units:
+            return (p.account, "", "")
+
+        number = str(p.units.number) if p.units.number is not None else ""
+        currency = p.units.currency if p.units.currency else ""
+        return (p.account, number, currency)
+
+    # Sort postings to ensure consistent key generation regardless of order
+    filtered_postings = [p for p in t.postings if not p.account.startswith('Equity:Currency')]
+    postings_sorted = tuple(sorted([_get_posting_key(p) for p in filtered_postings]))
+    return (t.date, postings_sorted)
+
+def deduplicate(entries: data.Entries, existing: data.Entries) -> None:
+    """
+    Deduplicate transactions
+
+    Entries that are determined to be duplicates of existing entries
+    are marked setting the "__duplicate__" metadata field.
+    """
+    existing_transactions_map = {}
+    for entry in existing:
+        if isinstance(entry, data.Transaction) and DUPLICATE not in entry.meta:
+            key = _get_transaction_key(entry)
+            if key:
+                existing_transactions_map[key] = entry
+
+    for entry in entries:
+        if not isinstance(entry, data.Transaction):
+            continue
+
+        key = _get_transaction_key(entry)
+        if key and key in existing_transactions_map:
+            entry.meta[DUPLICATE] = True
+            del existing_transactions_map[key] # Remove to prevent re-matching
