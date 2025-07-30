33f7f50582414f2166430aa9390237abc3dca79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:02 +0000

    Add package PyMIsys_platform/0.0.1/PyMIsys_platform-0.0.1.tar.gz

---
+++ packages/PyMIsys_platform/PyMIsys_platform-0.0.1.tar.gz/PyMIsys_platform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="PyMIsys_platform",
+    version="0.0.1",
+    packages=["PyMIsys_platform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
