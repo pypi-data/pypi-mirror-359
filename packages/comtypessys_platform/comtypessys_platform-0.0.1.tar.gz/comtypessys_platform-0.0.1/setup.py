9990467ec419ce586d0fd62c5e54095e4d669141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:45 +0000

    Add package comtypessys_platform/0.0.1/comtypessys_platform-0.0.1.tar.gz

---
+++ packages/comtypessys_platform/comtypessys_platform-0.0.1.tar.gz/comtypessys_platform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="comtypessys_platform",
+    version="0.0.1",
+    packages=["comtypessys_platform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
