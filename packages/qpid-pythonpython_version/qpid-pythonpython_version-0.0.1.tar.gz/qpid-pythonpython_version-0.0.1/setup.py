3224f69c89c125082b6ae9c74772e2f54db77ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package qpid-pythonpython_version/0.0.1/qpid-pythonpython_version-0.0.1.tar.gz

---
+++ packages/qpid-pythonpython_version/qpid-pythonpython_version-0.0.1.tar.gz/qpid-pythonpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="qpid-pythonpython_version",
+    version="0.0.1",
+    packages=["qpid-pythonpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
