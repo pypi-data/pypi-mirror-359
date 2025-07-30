6438b2eb2ff307b9f904e82099b84bf1973ab046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package dnspythonpython_version/0.0.1/dnspythonpython_version-0.0.1.tar.gz

---
+++ packages/dnspythonpython_version/dnspythonpython_version-0.0.1.tar.gz/dnspythonpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="dnspythonpython_version",
+    version="0.0.1",
+    packages=["dnspythonpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
