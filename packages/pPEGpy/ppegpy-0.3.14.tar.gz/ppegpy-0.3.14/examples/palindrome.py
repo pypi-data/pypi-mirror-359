b217776443d713ce158a8735906e5a9f3aa7bbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:08:16 +0000

    Add package pPEGpy/0.3.14/ppegpy-0.3.14.tar.gz

---
+++ packages/pPEGpy/ppegpy-0.3.14.tar.gz/ppegpy-0.3.14/examples/palindrome.py
@@ -0,0 +1,103 @@
+# import pPEGpy as peg  # local file
+from pPEGpy import peg  # pip install pPEGpy
+
+import extras
+
+extensions = extras.extensions()
+
+p = peg.compile(
+    """
+    p  = x1 m <same x1> / x?
+    m  = (x &x)*
+    x1 = x
+    x  : [a-z]
+    """,
+    same=extras.same,
+    # **extensions
+)
+
+t = p.parse("abba")
+
+t.dump()
+
+print(t)
+
+print("=====================")
+
+p = peg.compile(
+    """
+    p  = x1 m x2 / x1?
+    m  = (x &x)*
+    x1 = x
+    x2 = <same x1>
+    x  : [a-z]
+    """,
+    same=extras.same,
+)
+
+t = p.parse("abba")
+
+t.dump()
+
+print(t)
+
+print("=====================")
+
+code = peg.compile(
+    """
+    p  = x1 m x2 <eq x2 x1>
+    m  = (x &x)*  # <match m p>
+    x1 = x
+    x2 = x
+    x  : [a-z]
+""",
+    eq=extras.eq,
+)
+
+p = code.parse("racecar")  # "abba")  #
+p.dump(0)
+print(p)
+
+print("=====================")
+
+code = peg.compile(
+    """
+    Code = t1 code t1
+    code = ~(t2 <eq t1 t2>)*
+    t1   = '`'+
+    t2   = '`'+
+    """,
+    **extensions,
+)
+
+p = code.parse(R"```a``y``z```", debug=1)
+# p.dump(0)
+print(p)
+
+print("=====================")
+
+pal = peg.compile(
+    """
+    p  = x1 p1 x2 / x1?
+    p1 = <fork p m>
+    m  = (x &x)*
+    x1 = x
+    x2 = <same x1>
+    x  : [a-z]
+    """,
+    **extensions,
+)
+
+
+def palon(s):
+    t = pal.parse(s)
+    return t.transform(p1=p1)
+
+
+def p1(args):
+    return palon(args[1][1])
+
+
+x = palon("racecar")
+
+print(x)
