e074e9dcdbcfe825969374008ddbb943175a203b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:11 +0000

    Add package oslotestpython_version/0.0.1/oslotestpython_version-0.0.1.tar.gz

---
+++ packages/oslotestpython_version/oslotestpython_version-0.0.1.tar.gz/oslotestpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oslotestpython_version",
+    version="0.0.1",
+    packages=["oslotestpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
