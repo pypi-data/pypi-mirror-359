f999e58bb14fa0fb80de4c440816076660696f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:32 +0000

    Add package nemo-library/1.6.11/nemo_library-1.6.11.tar.gz

---
+++ packages/nemo-library/nemo_library-1.6.11.tar.gz/nemo_library-1.6.11/tests/test_fox.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from tests.testutils import getNL
+
+PROJECT_NAME = "gs-unit-test-fox"
+
+def test_load_fox_file():
+    nl = getNL()
+    nl.ReUploadFile(
+        projectname=PROJECT_NAME,
+        filename="./tests/GuetzowWetter2000.fox",
+        update_project_settings=False,
+    )
+
+def test_clean():
+    nl = getNL()
+    id = nl.getProjectID(PROJECT_NAME)
+    if id:
+        nl.deleteProjects([id])
+    assert True  # Ensure the test passes after cleanup
