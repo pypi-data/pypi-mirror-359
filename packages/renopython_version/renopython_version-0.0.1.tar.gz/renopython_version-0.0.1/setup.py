a0b472fbf0fb59645cfe370b1c2c2f1615701f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:10 +0000

    Add package renopython_version/0.0.1/renopython_version-0.0.1.tar.gz

---
+++ packages/renopython_version/renopython_version-0.0.1.tar.gz/renopython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="renopython_version",
+    version="0.0.1",
+    packages=["renopython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
