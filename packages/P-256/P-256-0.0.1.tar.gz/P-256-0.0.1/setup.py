d32ab960dc5ab113eaa9fbad1b2fff66ea0680f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:55 +0000

    Add package P-256/0.0.1/P-256-0.0.1.tar.gz

---
+++ packages/P-256/P-256-0.0.1.tar.gz/P-256-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="P-256",
+    version="0.0.1",
+    packages=["P-256"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
