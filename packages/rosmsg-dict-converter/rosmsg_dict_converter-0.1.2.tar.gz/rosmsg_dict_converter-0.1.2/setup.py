60de4434af776faba138af55609cfa898cc1c394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:59 +0000

    Add package rosmsg-dict-converter/0.1.2/rosmsg_dict_converter-0.1.2.tar.gz

---
+++ packages/rosmsg-dict-converter/rosmsg_dict_converter-0.1.2.tar.gz/rosmsg_dict_converter-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rosmsg_dict_converter",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+        "rich",
+        "pydantic",
+    ],
+    author="Wing mun Fung",
+    author_email="fengrongman@gmail.com",
+    description="Provide a  converter to convert ROS message to dict and vice versa",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
