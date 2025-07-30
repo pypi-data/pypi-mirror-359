fa5c8ec40b0610ee106c4c4baf12ed652cf30fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:32:45 +0000

    Add package edmt/1.0.1.dev0/edmt-1.0.1.dev0.tar.gz

---
+++ packages/edmt/edmt-1.0.1.dev0.tar.gz/edmt-1.0.1.dev0/edmt/conversion/__init__.py
@@ -0,0 +1,19 @@
+from .conversion import (
+    sdf_to_gdf,
+    generate_uuid,
+    get_utm_epsg,
+    to_gdf,
+    convert_distance,
+    convert_time,
+    convert_speed
+)
+
+__all__ = [
+    'sdf_to_gdf', 
+    'generate_uuid', 
+    'get_utm_epsg',
+    'to_gdf',
+    'convert_distance',
+    'convert_time',
+    'convert_speed'
+    ]
---
+++ packages/edmt/edmt-1.0.1.dev0.tar.gz/edmt-1.0.1.dev0/edmt/models/__init__.py
@@ -0,0 +1,15 @@
+from .drones import (
+    Airdata,
+    airPoint,
+    df_to_gdf,
+    airLine,
+    airSegment
+)
+
+__all__ = [
+    "Airdata",
+    "airPoint",
+    "df_to_gdf",
+    "airLine",
+    "airSegment"
+]
