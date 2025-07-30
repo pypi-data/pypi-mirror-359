82e7c6640e706fb4719e63ff1733a96d0a6c7116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:24 +0000

    Add package aviatrix-migration/0.2.148/aviatrix_migration-0.2.148-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/aviatrix-migration/aviatrix_migration-0.2.148-cp310-cp310-macosx_11_0_arm64.whl/dm/arm/add_avtx_cidr.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from dm.arm._add_avtx_cidr import *  # noqa: F403
+
+if __name__ == "__main__":
+    main()  # noqa: F405
---
+++ packages/aviatrix-migration/aviatrix_migration-0.2.148-cp310-cp310-macosx_11_0_arm64.whl/dm/arm/tfhelper.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from dm.arm._tfhelper import *  # noqa: F403
+
+if __name__ == "__main__":
+    main()  # noqa: F405
