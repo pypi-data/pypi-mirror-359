944be7aef6a3e87828a217fc8dd6c1c510f1c1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:48 +0000

    Add package UW-RestClients-GWS/2.3.9/uw_restclients_gws-2.3.9.tar.gz

---
+++ packages/UW-RestClients-GWS/uw_restclients_gws-2.3.9.tar.gz/uw_restclients_gws-2.3.9/uw_gws/exceptions.py
@@ -0,0 +1,12 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""
+Contains the custom exceptions used by the GWS client.
+"""
+
+
+class InvalidGroupID(Exception):
+    """Exception for invalid group id."""
+    pass
---
+++ packages/UW-RestClients-GWS/uw_restclients_gws-2.3.9.tar.gz/uw_restclients_gws-2.3.9/uw_gws/utilities.py
@@ -0,0 +1,8 @@
+# Copyright 2025 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+
+from commonconf import override_settings
+
+
+fdao_gws_override = override_settings(RESTCLIENTS_GWS_DAO_CLASS='Mock')
