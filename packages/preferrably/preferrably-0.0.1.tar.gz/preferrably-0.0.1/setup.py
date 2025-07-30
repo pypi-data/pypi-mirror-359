767e3a3dee5a52a1e29ab2ad98728fafccd54c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package preferrably/0.0.1/preferrably-0.0.1.tar.gz

---
+++ packages/preferrably/preferrably-0.0.1.tar.gz/preferrably-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="preferrably",
+    version="0.0.1",
+    packages=["preferrably"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
