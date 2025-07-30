1ef91106ac701aa456e8d8ac5da7de79b8f4b14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:10 +0000

    Add package seisvis/0.1.2/seisvis-0.1.2.tar.gz

---
+++ packages/seisvis/seisvis-0.1.2.tar.gz/seisvis-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='seisvis',
+    version='0.1.2',
+    description='Seismic data visualization and plotting utilities',
+    author='Qi Pang',
+    author_email='pangjiutian@gmail.com',
+    packages=find_packages(),  
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    package_data={
+        'seisvis.opendtect_colormaps': ['ColTabs'], 
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
