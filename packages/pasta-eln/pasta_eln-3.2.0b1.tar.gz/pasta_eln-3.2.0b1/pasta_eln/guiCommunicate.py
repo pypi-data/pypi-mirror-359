3244054e79c484d3507f33dfee626dfdb0555537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:19 +0000

    Add package pasta-eln/3.2.0b1/pasta_eln-3.2.0b1.tar.gz

---
+++ packages/pasta-eln/pasta_eln-3.2.0b1.tar.gz/pasta_eln-3.2.0b1/pasta_eln/guiCommunicate.py
@@ -0,0 +1,44 @@
+""" Communication class that sends signals between widgets, incl. backend"""
+from typing import Any, Callable
+from PySide6.QtCore import QObject, Signal                                 # pylint: disable=no-name-in-module
+from .backend import Backend
+from .GUI.waitDialog import WaitDialog, Worker
+
+
+class Communicate(QObject):
+  """ Communication class that sends signals between widgets, incl. backend"""
+
+
+  def __init__(self, backend:Backend, palette:Any):
+    super().__init__()
+    self.backend               = backend
+    self.palette               = palette
+    self.projectID             = ''
+    self.waitDialog            = WaitDialog()
+    self.worker:Worker|None    = None
+
+
+  def progressWindow(self, taskFunction:Callable[[Callable[[str,str],None]],Any]) -> None:
+    """ Show a progress window and execute function
+    Args:
+      taskFunction (func): function to execute
+    """
+    self.waitDialog.show()
+    self.worker = Worker(taskFunction)
+    self.worker.progress.connect(self.waitDialog.updateProgressBar)
+    self.worker.start()
+    return
+
+
+  # Signals: specify emitter and receiver
+  # BE SPECIFIC ABOUT WHAT THIS ACTION DOES
+  changeSidebar      = Signal(str)       # redraw sidebar after hide/show of project in table, focus on this projectID
+  changeTable        = Signal(str, str)  # send doctype,projectID from sidebar to main-table
+                                         #      can also be used for hiding the details on right side if nothing to show
+  changeDetails      = Signal(str)       # send docID from main-table to details
+                                         #      docID (str): document-id; ''=draw nothing; 'redraw' implies redraw
+  changeProject      = Signal(str, str)  # send docID,projectID from sidebar or main-table to projects
+  formDoc            = Signal(dict)      # send doc from details to new/edit dialog: dialogForm
+  testExtractor      = Signal()          # execute extractorTest in widgetDetails
+  stopSequentialEdit = Signal()          # in sequential edit, stop if there is a cancel
+  restart            = Signal()          # restart GUI  #TODO rename to softRestart to avoid confusion
