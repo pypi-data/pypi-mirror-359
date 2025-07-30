c4b4c602bbc7a1addb03aa57be665e528ec3970c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:33 +0000

    Add package sni-requests/0.1.2/sni_requests-0.1.2.tar.gz

---
+++ packages/sni-requests/sni_requests-0.1.2.tar.gz/sni_requests-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='sni_requests',
+    version='0.1.2',
+    description='A requests wrapper that allows setting a custom SNI hostname for HTTPS connections',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author='hacked_by_zen',
+    url='https://github.com/42zen/sni_requests',
+    packages=['sni_requests'],
+    package_dir={'sni_requests': '.'},
+    py_modules=['sni_requests'],
+    install_requires=[
+        'requests',
+        'urllib3',
+    ]
+)
