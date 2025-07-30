a6dec8e57a71818bf5f3ad1f351a4bbfe6144439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:23 +0000

    Add package venai/0.1.0/venai-0.1.0.tar.gz

---
+++ packages/venai/venai-0.1.0.tar.gz/venai-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='venai',
+    version='0.1.0',
+    packages=find_packages(),
+    author='fswair',
+    description='venai is a Python package.',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
