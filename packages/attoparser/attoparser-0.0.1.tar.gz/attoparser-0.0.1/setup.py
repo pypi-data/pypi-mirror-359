5816f25ee766b69ae6cf0d005c8951461d83e7d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:56 +0000

    Add package attoparser/0.0.1/attoparser-0.0.1.tar.gz

---
+++ packages/attoparser/attoparser-0.0.1.tar.gz/attoparser-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="attoparser",
+    version="0.0.1",
+    packages=["attoparser"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
