a296c2e9fe22803511c3ab23ded83bd470cfbb07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:57 +0000

    Add package cubicweb-folder/3.1.0/cubicweb_folder-3.1.0.tar.gz

---
+++ packages/cubicweb-folder/cubicweb_folder-3.1.0.tar.gz/cubicweb_folder-3.1.0/test/test_folder.py
@@ -0,0 +1,19 @@
+import unittest
+
+from cubicweb_web.devtools.testlib import AutomaticWebTest
+
+
+class FolderAutomaticWebTest(AutomaticWebTest):
+
+    def to_test_etypes(self):
+        return set(('Folder',))
+
+    def list_startup_views(self):
+        return ()
+
+
+del AutomaticWebTest
+
+
+if __name__ == '__main__':
+    unittest.main()
