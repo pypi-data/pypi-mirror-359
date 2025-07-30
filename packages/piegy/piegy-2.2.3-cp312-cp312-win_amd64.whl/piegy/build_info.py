b44e4eb41d619da2e7aabe3e34e658678eacfbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:13 +0000

    Add package piegy/2.2.3/piegy-2.2.3-cp312-cp312-win_amd64.whl

---
+++ packages/piegy/piegy-2.2.3-cp312-cp312-win_amd64.whl/piegy/build_info.py
@@ -0,0 +1,11 @@
+"""
+Contains build info, whether it's local built, or a pre-compiled wheel.
+Auto-generated at compile time.
+"""
+
+build_info = {
+    "version": "2.2.3",
+    "build date": "2025-07-01 20:54:17",
+    "python used": "3.12.10",
+    "platform": "win32"
+}
