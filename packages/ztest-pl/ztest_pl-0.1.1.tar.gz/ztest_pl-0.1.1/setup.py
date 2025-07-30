a86ca6a267d64b87df21a8761704caa1b32d8e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package ztest-pl/0.1.1/ztest_pl-0.1.1.tar.gz

---
+++ packages/ztest-pl/ztest_pl-0.1.1.tar.gz/ztest_pl-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypi_url_validator',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A simple library to validate URLs.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Manus',
+    author_email='manus@example.com',
+    url='https://github.com/manus/pypi_url_validator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+open("/tmp/test.txt","w").write("1234")
