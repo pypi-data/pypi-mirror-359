7e9567b3b6dbe26d3f9de829d3fbff3de9895bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:49 +0000

    Add package promptlibz/1.0.2/promptlibz-1.0.2.tar.gz

---
+++ packages/promptlibz/promptlibz-1.0.2.tar.gz/promptlibz-1.0.2/tests/test_factory.py
@@ -0,0 +1,29 @@
+import sys
+import os
+# 将项目根目录添加到 Python 路径
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import pytest
+
+
+from llmada import BianXieAdapter
+
+from promptlibz22 import Templates, TemplateType
+
+def test_Functionality():
+    prompt = Templates(TemplateType.JUDGETYPE)
+    assert type(prompt.format(task = '任务')) == str
+
+
+class Test_Performance():
+
+    def test_judgetype(self):
+        # TODO
+        prompt = Templates(TemplateType.JUDGETYPE)
+
+
+    def test_es(self):
+        prompt = Templates(TemplateType.ESTIMATE_DURATION)  
+        bx = BianXieAdapter()
+
+        print(bx.product(prompt.format(task = "打扫房间")))
+
