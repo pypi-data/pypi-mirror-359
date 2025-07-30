68e43e2de73feea0ec7d40acf00cf4ce6594aab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:38 +0000

    Add package psr-factory/4.1.0b12/psr_factory-4.1.0b12-py3-none-win_amd64.whl

---
+++ packages/psr-factory/psr_factory-4.1.0b12-py3-none-win_amd64.whl/psr/factory/__init__.py
@@ -0,0 +1,7 @@
+# PSR Factory. Copyright (C) PSR, Inc - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+
+__version__ = "4.1.0b12"
+
+from .api import *
