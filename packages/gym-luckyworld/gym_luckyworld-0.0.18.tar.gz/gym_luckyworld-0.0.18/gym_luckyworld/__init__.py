1d17279fac9f12a0e863769591bbb479088b3e82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:39 +0000

    Add package gym-luckyworld/0.0.18/gym_luckyworld-0.0.18.tar.gz

---
+++ packages/gym-luckyworld/gym_luckyworld-0.0.18.tar.gz/gym_luckyworld-0.0.18/gym_luckyworld/__init__.py
@@ -0,0 +1,31 @@
+from gymnasium.envs.registration import register
+
+register(
+    id="gym_luckyworld/LuckyWorld-PickandPlace-v0",
+    entry_point="gym_luckyworld.env:LuckyWorld",
+    max_episode_steps=300,
+    nondeterministic=True,
+    kwargs={
+        "scene": "ArmLevel",
+        "task": "pickandplace",
+        "robot": "so100",
+        "obs_type": "pixels_agent_pos",
+        "render_mode": "rgb_array",
+        # "game_path": None,
+    },
+)
+
+register(
+    id="gym_luckyworld/LuckyWorld-Navigation-v0",
+    entry_point="gym_luckyworld.env:LuckyWorld",
+    max_episode_steps=300,
+    nondeterministic=True,
+    kwargs={
+        "scene": "loft",
+        "task": "navigation",
+        "robot": "stretch_v1",
+        "obs_type": "pixels_agent_pos",
+        "render_mode": "rgb_array",
+        # "game_path": None,
+    },
+)
