e05ee0ab4e478d4b8dfcc1c10e3c45e4d2a6b3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:30 +0000

    Add package graph_widget/0.1.13/graph_widget-0.1.13-py2.py3-none-any.whl

---
+++ packages/graph_widget/graph_widget-0.1.13-py2.py3-none-any.whl/graph_widget/__init__.py
@@ -0,0 +1,24 @@
+import importlib.metadata
+from importlib.resources import files
+
+import anywidget
+import traitlets
+
+try:
+    __version__ = importlib.metadata.version("graph_widget")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
+class ForceGraphWidget(anywidget.AnyWidget):
+    # _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
+    # _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+    _esm = files("graph_widget").joinpath("static/widget.js")
+    _css = files("graph_widget").joinpath("static/widget.css")
+    data = traitlets.Dict().tag(sync=True)
+    repulsion = traitlets.Int().tag(sync=True)
+    node_scale = traitlets.Int().tag(sync=True)
+    colour_feature = traitlets.Unicode().tag(sync=True)
+    colour_scale_type = traitlets.Unicode().tag(sync=True)
+    selected_ids = traitlets.List([]).tag(sync=True)
+    width = traitlets.Int().tag(sync=True)
+    height = traitlets.Int().tag(sync=True)
