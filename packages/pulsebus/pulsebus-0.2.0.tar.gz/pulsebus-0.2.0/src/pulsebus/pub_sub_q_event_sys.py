6acd783ecb90ccd60e8d74913a1fd024dbe4f7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:25 +0000

    Add package pulsebus/0.2.0/pulsebus-0.2.0.tar.gz

---
+++ packages/pulsebus/pulsebus-0.2.0.tar.gz/pulsebus-0.2.0/src/pulsebus/pub_sub_q_event_sys.py
@@ -0,0 +1,11 @@
+from .backends.event_driven.system_manager import (
+    EventSystem,
+    BaseProducer,
+    BaseConsumer
+)
+
+__all__ = [
+    "EventSystem",
+    "BaseProducer",
+    "BaseConsumer",
+]
