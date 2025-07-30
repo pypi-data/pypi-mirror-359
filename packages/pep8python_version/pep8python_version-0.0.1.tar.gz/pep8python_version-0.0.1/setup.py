7ea00d6de5dbdc0c3263a5288ec3ee4a216eaea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:54 +0000

    Add package pep8python_version/0.0.1/pep8python_version-0.0.1.tar.gz

---
+++ packages/pep8python_version/pep8python_version-0.0.1.tar.gz/pep8python_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pep8python_version",
+    version="0.0.1",
+    packages=["pep8python_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
