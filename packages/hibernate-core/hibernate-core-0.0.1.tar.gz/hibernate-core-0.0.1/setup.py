6ebc40cbf27770a50fcecbe474f2ebea98d7a1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:57 +0000

    Add package hibernate-core/0.0.1/hibernate-core-0.0.1.tar.gz

---
+++ packages/hibernate-core/hibernate-core-0.0.1.tar.gz/hibernate-core-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hibernate-core",
+    version="0.0.1",
+    packages=["hibernate-core"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
