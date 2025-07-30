23c5b36b158e1ddf93c5e85d263c526082379f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:13 +0000

    Add package composio-autogen/0.7.20/composio_autogen-0.7.20-py3-none-any.whl

---
+++ packages/composio-autogen/composio_autogen-0.7.20-py3-none-any.whl/composio_autogen/__init__.py
@@ -0,0 +1,14 @@
+from composio import Action, App, Tag, Trigger, WorkspaceType, action
+
+from composio_autogen.toolset import ComposioToolSet
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "WorkspaceType",
+    "action",
+    "ComposioToolSet",
+    "Trigger",
+)
