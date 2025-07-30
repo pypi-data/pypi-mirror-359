fe9faef8c94b40ce4d831be4bcf99af3d3170926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:39 +0000

    Add package scriptmonkey/2.0.0/scriptmonkey-2.0.0.tar.gz

---
+++ packages/scriptmonkey/scriptmonkey-2.0.0.tar.gz/scriptmonkey-2.0.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scriptmonkey",
+    version="2.0.0",
+    description="A simple CLI utility for copying files and directory trees to clipboard for easy sharing with LLMs.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/lukerbs/ScriptMonkey",
+    author="Luke Kerbs",
+    author_email="LDK.kerbs@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["rich", "pyperclip"],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "scriptmonkey=scriptmonkey.core:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "clipboard",
+        "file sharing",
+        "directory tree",
+        "development tools",
+        "python tools",
+        "LLM helper",
+        "code sharing",
+        "project structure",
+        "file utility",
+        "developer tools",
+    ],
+    include_package_data=True,
+)
