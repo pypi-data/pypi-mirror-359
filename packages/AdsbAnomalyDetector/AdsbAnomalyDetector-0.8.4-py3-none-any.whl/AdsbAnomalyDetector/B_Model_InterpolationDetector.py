cd5cbe4ba612e9789d117be972a3d965e6c16327
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:14:14 +0000

    Add package AdsbAnomalyDetector/0.8.4/AdsbAnomalyDetector-0.8.4-py3-none-any.whl

---
+++ packages/AdsbAnomalyDetector/AdsbAnomalyDetector-0.8.4-py3-none-any.whl/AdsbAnomalyDetector/B_Model_InterpolationDetector.py
@@ -0,0 +1,95 @@
+
+import tensorflow as tf
+from keras.layers import *
+
+from .B_Model_AbstractModel import Model as AbstactModel
+from .B_Model_Utils_TF_Modules import *
+
+from numpy_typing import np, ax
+
+from ._Utils_os_wrapper import os
+
+from . import _Utils_Color as C
+from ._Utils_Color import prntC
+
+class Model(AbstactModel):
+
+    name = "TLeNet"
+
+    def __init__(self, CTX:dict):
+
+        # load context
+        self.CTX = CTX
+        self.dropout = CTX["DROPOUT"]
+
+        # save the number of training steps
+        self.nb_train = 0
+
+        x_input_shape = (self.CTX["INPUT_LEN"], self.CTX["FEATURES_IN"])
+        x = Input(shape=x_input_shape, name='input')
+        
+        conv_1 = Conv1D(filters=5,kernel_size=5,activation='relu', padding='same')(x)
+        conv_1 = MaxPool1D(pool_size=2)(conv_1)
+        
+        conv_2 = Conv1D(filters=20, kernel_size=5, activation='relu', padding='same')(conv_1)
+        conv_2 = MaxPool1D(pool_size=4)(conv_2)
+        
+        # they did not mention the number of hidden units in the fully-connected layer
+        # so we took the lenet they referenced 
+        
+        flatten_layer = Flatten()(conv_2)
+        fully_connected_layer = Dense(500,activation='relu')(flatten_layer)
+        
+        # output
+        z = Dense(1, activation="sigmoid")(fully_connected_layer)
+        y = z
+
+        self.model = tf.keras.Model(x, y)
+
+
+        # define loss function
+        self.loss = tf.keras.losses.MeanSquaredError()
+
+        # define optimizer
+        self.opt = tf.keras.optimizers.Adam(learning_rate=CTX["LEARNING_RATE"])
+        # self.opt = tf.keras.optimizers.SGD(learning_rate=CTX["LEARNING_RATE"])
+
+
+    def predict(self, x):
+        return self.model(x)
+
+    def compute_loss(self, x, y):
+        y_ = self.model(x)
+        return self.loss(y_, y), y_
+
+    def training_step(self, x, y):
+        with tf.GradientTape(watch_accessed_variables=True) as tape:
+            loss, output = self.compute_loss(x, y)
+
+            gradients = tape.gradient(loss, self.model.trainable_variables)
+            self.opt.apply_gradients(zip(gradients, self.model.trainable_variables))
+
+        self.nb_train += 1
+        return loss, output
+
+
+
+    def visualize(self, save_path="./_Artifacts/"):
+
+        params = 0
+        for i in range(len(self.model.trainable_variables)):
+            params += np.prod(self.model.trainable_variables[i].shape)
+
+        filename = os.path.join(save_path, self.name+".png")
+        tf.keras.utils.plot_model(self.model, to_file=filename, show_shapes=True)
+
+    def nb_parameters(self):
+        return np.sum([np.prod(v.get_shape().as_list()) for v in self.model.trainable_variables])
+
+
+    def get_variables(self):
+        return self.model.variables
+
+    def set_variables(self, variables):
+        for i in range(len(variables)):
+            self.model.variables[i].assign(variables[i])
