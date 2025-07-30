cf883a8615cc20a3354b0b8951fbeed80589e1b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:12 +0000

    Add package blipper/0.1.8/blipper-0.1.8-py3-none-any.whl

---
+++ packages/blipper/blipper-0.1.8-py3-none-any.whl/blipper/__init__.py
@@ -0,0 +1,1 @@
+from .blipper import Blipper
---
+++ packages/blipper/blipper-0.1.8-py3-none-any.whl/blipper/config.py
@@ -0,0 +1,4 @@
+blipper_url = "https://blipper.epystemic.com/"
+# blipper_url = "http://localhost:7000/"
+BLIPPER_AUTH_URL = "http://veracityai.imprevia.com/api/blipper/auth"
+ANTHROPIC_AUTH_URL = "https://api.anthropic.com/v1/messages"
