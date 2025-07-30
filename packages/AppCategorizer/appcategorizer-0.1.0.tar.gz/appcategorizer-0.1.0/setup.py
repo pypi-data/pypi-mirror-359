eab512c35153cf32b93780dd25b5e9ca0814015e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:26 +0000

    Add package AppCategorizer/0.1.0/appcategorizer-0.1.0.tar.gz

---
+++ packages/AppCategorizer/appcategorizer-0.1.0.tar.gz/appcategorizer-0.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="AppCategorizer",
+    version="0.1.0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "beautifulsoup4==4.13.4",
+        "pandas==2.3.0",
+        "Requests==2.32.4",
+        "selenium==4.33.0",
+        "transformers==4.52.4"
+    ],
+    entry_points={
+        "console_scripts": [
+            "AppCategorizer=AppCategorizer.main:fetch_category",
+        ],
+    },
+)
