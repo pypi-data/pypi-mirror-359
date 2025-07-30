0cd04624f7e06c29b7537e60425cdd1b04df0cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:06:48 +0000

    Add package gitbolt/0.0.0.dev1/gitbolt-0.0.0.dev1.tar.gz

---
+++ packages/gitbolt/gitbolt-0.0.0.dev1.tar.gz/gitbolt-0.0.0.dev1/src/gitbolt/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+"""
+Git command interfaces with default implementation using subprocess calls.
+"""
+
+# region imports
+# region base related imports
+from gitbolt.base import Git as Git
+from gitbolt.base import CanOverrideGitOpts as CanOverrideGitOpts
+from gitbolt.base import HasGitUnderneath as HasGitUnderneath
+from gitbolt.base import GitSubCommand as GitSubCommand
+from gitbolt.base import LsTree as LsTree
+from gitbolt.base import Version as Version
+from gitbolt.base import Add as Add
+# endregion
+# endregion
