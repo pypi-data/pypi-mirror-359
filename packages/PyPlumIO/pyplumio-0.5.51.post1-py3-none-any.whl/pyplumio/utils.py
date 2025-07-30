a735f11391386b90491cee0751556d1971fb7b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:25 +0000

    Add package PyPlumIO/0.5.51.post1/pyplumio-0.5.51.post1-py3-none-any.whl

---
+++ packages/PyPlumIO/pyplumio-0.5.51.post1-py3-none-any.whl/pyplumio/utils.py
@@ -0,0 +1,43 @@
+"""Contains various utility methods."""
+
+from __future__ import annotations
+
+from collections.abc import Mapping
+from typing import TypeVar
+
+KT = TypeVar("KT")  # Key type.
+VT = TypeVar("VT")  # Value type.
+
+
+def ensure_dict(initial: dict[KT, VT] | None, *args: dict[KT, VT]) -> dict[KT, VT]:
+    """Create or merge multiple dictionaries."""
+    data = initial if initial is not None else {}
+    for extra in args:
+        data |= extra
+
+    return data
+
+
+def to_camelcase(text: str, overrides: Mapping[str, str] | None = None) -> str:
+    """Convert snake_case to CamelCase."""
+    if not overrides:
+        return "".join((x.capitalize() or "_") for x in text.split("_"))
+
+    return "".join(
+        (x.capitalize() or "_") if x.lower() not in overrides else overrides[x.lower()]
+        for x in text.split("_")
+    )
+
+
+def is_divisible(a: float, b: float, precision: int = 6) -> bool:
+    """Check if a is divisible by b."""
+    scale: int = 10**precision
+    b_scaled = round(b * scale)
+    if b_scaled == 0:
+        raise ValueError("Division by zero is not allowed.")
+
+    a_scaled = round(a * scale)
+    return a_scaled % b_scaled == 0
+
+
+__all__ = ["ensure_dict", "is_divisible", "to_camelcase"]
