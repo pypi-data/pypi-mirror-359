1ea0e661154b6efcb35deaa8fa0cdd42f3492fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:08:19 +0000

    Add package mooch/1.0.0/mooch-1.0.0-py3-none-any.whl

---
+++ packages/mooch/mooch-1.0.0-py3-none-any.whl/mooch/decorators/__init__.py
@@ -0,0 +1,5 @@
+from .logging import log_entry_exit
+from .retry import retry
+from .silent import silent
+
+__all__ = ["log_entry_exit", "retry", "silent"]
---
+++ packages/mooch/mooch-1.0.0-py3-none-any.whl/mooch/require/exceptions.py
@@ -0,0 +1,3 @@
+class RequirementError(Exception):
+    def __init__(self, *args):  # noqa: ANN002
+        super().__init__(*args)
