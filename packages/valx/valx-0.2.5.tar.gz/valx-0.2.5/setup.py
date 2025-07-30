c71f7eb46fe44881ec531227fabb27c618b56bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:42 +0000

    Add package valx/0.2.5/valx-0.2.5.tar.gz

---
+++ packages/valx/valx-0.2.5.tar.gz/valx-0.2.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='valx',
+    version='0.2.5',
+    author='Infinitode Pty Ltd',
+    author_email='infinitode.ltd@gmail.com',
+    description='An open-source Python library for data cleaning tasks. Includes profanity detection, and removal. Now includes offensive language and hate speech detection using an AI model.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/infinitode/valx',
+    packages=find_packages(),
+    package_data={'valx': ['models/*']},
+    install_requires=[
+        'scikit-learn'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    python_requires='>=3.6',
+)
