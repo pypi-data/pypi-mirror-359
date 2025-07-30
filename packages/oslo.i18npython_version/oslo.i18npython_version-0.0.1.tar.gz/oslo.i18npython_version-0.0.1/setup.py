b8d0ccc2baf8acd23ce879d964d92e52af388d03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:07 +0000

    Add package oslo.i18npython_version/0.0.1/oslo.i18npython_version-0.0.1.tar.gz

---
+++ packages/oslo.i18npython_version/oslo.i18npython_version-0.0.1.tar.gz/oslo.i18npython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="oslo.i18npython_version",
+    version="0.0.1",
+    packages=["oslo.i18npython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
