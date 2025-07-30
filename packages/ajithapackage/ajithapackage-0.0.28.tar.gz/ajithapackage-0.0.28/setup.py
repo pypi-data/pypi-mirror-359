e06944e872c0bc42b2097d0b4849bde25969f94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:40 +0000

    Add package ajithapackage/0.0.28/ajithapackage-0.0.28.tar.gz

---
+++ packages/ajithapackage/ajithapackage-0.0.28.tar.gz/ajithapackage-0.0.28/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "ajithapackage",
+    "version": "0.0.28",
+    "description": "ajithapackage",
+    "license": "Apache-2.0",
+    "url": "https://github.com/ajestharl/genet-test-repo.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Ajitha<ajithamanit@gmail.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/ajestharl/genet-test-repo.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "ajithapackage",
+        "ajithapackage._jsii"
+    ],
+    "package_data": {
+        "ajithapackage._jsii": [
+            "ajithapackage@0.0.28.jsii.tgz"
+        ],
+        "ajithapackage": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "aws-cdk-lib>=2.1.0, <3.0.0",
+        "constructs>=10.0.5, <11.0.0",
+        "jsii>=1.108.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard>=2.13.3,<4.3.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
