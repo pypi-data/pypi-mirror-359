d54b236a04a4807370faa826b28d338c4bead46c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:38 +0000

    Add package InformationGain/0.1.0/informationgain-0.1.0.tar.gz

---
+++ packages/InformationGain/informationgain-0.1.0.tar.gz/informationgain-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="InformationGain",
+    version="0.1.0",
+    description="A Python package for calculating information gain .",
+    author="abhinav Masih",
+    author_email="abhnv.msh@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.12",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
