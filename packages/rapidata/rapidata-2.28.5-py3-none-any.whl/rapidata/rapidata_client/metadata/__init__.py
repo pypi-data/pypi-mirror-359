4f18ffb85ff192481868ad1f622af914beb9a108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:36 +0000

    Add package rapidata/2.28.5/rapidata-2.28.5-py3-none-any.whl

---
+++ packages/rapidata/rapidata-2.28.5-py3-none-any.whl/rapidata/rapidata_client/metadata/__init__.py
@@ -0,0 +1,6 @@
+from ._base_metadata import Metadata
+from ._private_text_metadata import PrivateTextMetadata
+from ._public_text_metadata import PublicTextMetadata
+from ._prompt_metadata import PromptMetadata
+from ._select_words_metadata import SelectWordsMetadata
+from ._media_asset_metadata import MediaAssetMetadata
