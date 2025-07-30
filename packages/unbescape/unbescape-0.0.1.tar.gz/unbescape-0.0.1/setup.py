ad31012ea21c3abff9b2cf32ef5140e92aa0d5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:46 +0000

    Add package unbescape/0.0.1/unbescape-0.0.1.tar.gz

---
+++ packages/unbescape/unbescape-0.0.1.tar.gz/unbescape-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="unbescape",
+    version="0.0.1",
+    packages=["unbescape"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
