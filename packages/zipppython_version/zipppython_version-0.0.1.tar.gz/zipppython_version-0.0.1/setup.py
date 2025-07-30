d857d22cb1ce80475232bc07fe36fde68536f8d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:12 +0000

    Add package zipppython_version/0.0.1/zipppython_version-0.0.1.tar.gz

---
+++ packages/zipppython_version/zipppython_version-0.0.1.tar.gz/zipppython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="zipppython_version",
+    version="0.0.1",
+    packages=["zipppython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
