40e43c34b15203ca322b3a734baec93d2870b770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:57 +0000

    Add package thymeleaf-extras-springsecurity5/0.0.1/thymeleaf-extras-springsecurity5-0.0.1.tar.gz

---
+++ packages/thymeleaf-extras-springsecurity5/thymeleaf-extras-springsecurity5-0.0.1.tar.gz/thymeleaf-extras-springsecurity5-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="thymeleaf-extras-springsecurity5",
+    version="0.0.1",
+    packages=["thymeleaf-extras-springsecurity5"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
