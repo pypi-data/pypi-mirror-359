3cfc7d77bf43b9f1c71eb3bacfe655110e5c5873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:52 +0000

    Add package raganything/1.1.0/raganything-1.1.0.tar.gz

---
+++ packages/raganything/raganything-1.1.0.tar.gz/raganything-1.1.0/raganything/__init__.py
@@ -0,0 +1,8 @@
+from .raganything import RAGAnything as RAGAnything
+from .raganything import RAGAnythingConfig as RAGAnythingConfig
+
+__version__ = "1.1.0"
+__author__ = "Zirui Guo"
+__url__ = "https://github.com/HKUDS/RAG-Anything"
+
+__all__ = ["RAGAnything", "RAGAnythingConfig"]
