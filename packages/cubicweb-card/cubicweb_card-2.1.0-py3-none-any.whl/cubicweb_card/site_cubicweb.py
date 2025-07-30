e918c13fd60099ffa48f4c4976092a2e197d4396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:32 +0000

    Add package cubicweb-card/2.1.0/cubicweb_card-2.1.0-py3-none-any.whl

---
+++ packages/cubicweb-card/cubicweb_card-2.1.0-py3-none-any.whl/cubicweb_card/site_cubicweb.py
@@ -0,0 +1,22 @@
+from docutils import nodes, utils
+from docutils.parsers.rst.roles import register_canonical_role, set_classes
+
+
+def card_reference_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
+    text = text.strip()
+    try:
+        wikiid, rest = text.split(":", 1)
+    except Exception:
+        wikiid, rest = text, text
+    context = inliner.document.settings.context
+    ref = context._cw.build_url("card/" + wikiid)
+    rset = context._cw.execute("Card C WHERE C wikiid %(w)s", {"w": wikiid})
+    set_classes(options)
+    if not rset:
+        options["classes"] = ["doesnotexist"]
+    else:
+        options.pop("classes", None)
+    return [nodes.reference(rawtext, utils.unescape(rest), refuri=ref, **options)], []
+
+
+register_canonical_role("card", card_reference_role)
