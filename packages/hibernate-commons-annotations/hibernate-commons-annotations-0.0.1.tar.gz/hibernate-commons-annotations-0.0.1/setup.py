bceb885f3c89904bb69d693a8a91dbd608b5b262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:46 +0000

    Add package hibernate-commons-annotations/0.0.1/hibernate-commons-annotations-0.0.1.tar.gz

---
+++ packages/hibernate-commons-annotations/hibernate-commons-annotations-0.0.1.tar.gz/hibernate-commons-annotations-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="hibernate-commons-annotations",
+    version="0.0.1",
+    packages=["hibernate-commons-annotations"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
