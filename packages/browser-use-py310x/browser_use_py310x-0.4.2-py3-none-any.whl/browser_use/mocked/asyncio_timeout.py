097cb531dd829f80c646370da14692c634b38f1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:47 +0000

    Add package browser-use-py310x/0.4.2/browser_use_py310x-0.4.2-py3-none-any.whl

---
+++ packages/browser-use-py310x/browser_use_py310x-0.4.2-py3-none-any.whl/browser_use/mocked/asyncio_timeout.py
@@ -0,0 +1,16 @@
+from contextlib import asynccontextmanager
+import asyncio
+
+@asynccontextmanager
+async def async_timeout(timeout):
+    task = asyncio.current_task()
+    loop = asyncio.get_event_loop()
+    if task is None:
+        raise RuntimeError("async_timeout must be used within a running asyncio Task")
+    handle = loop.call_later(timeout, task.cancel)
+    try:
+        yield
+    except asyncio.CancelledError:
+        raise asyncio.TimeoutError(f"Operation exceeded {timeout} seconds")
+    finally:
+        handle.cancel()
