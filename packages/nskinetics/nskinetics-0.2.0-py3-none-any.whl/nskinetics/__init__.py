0df4baaca5cdaeac92d0ed00a22aec9546ee7fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:07:42 +0000

    Add package nskinetics/0.2.0/nskinetics-0.2.0-py3-none-any.whl

---
+++ packages/nskinetics/nskinetics-0.2.0-py3-none-any.whl/nskinetics/__init__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# NSKinetics: simulation of Non-Steady state enzyme Kinetics and inhibitory phenomena
+# Copyright (C) 2025-, Sarang S. Bhagwat <sarangbhagwat.developer@gmail.com>
+# 
+# This module is under the MIT open-source license. See 
+# https://github.com/sarangbhagwat/nskinetics/blob/main/LICENSE
+# for license details.
+
+__version__ = '0.2.0'
+
+# %% Initialize NSKinetics 
+
+from . import species
+from .species import *
+
+from . import reactions
+from .reactions import *
+
+from . import reaction_systems 
+from .reaction_systems import *
+
+from . import doe
+# from .doe import *
+
+from . import utils
+
+__all__ = (
+     *species.__all__,
+     *reactions.__all__,
+     *reaction_systems.__all__,
+     'utils',
+     'doe',
+     )
