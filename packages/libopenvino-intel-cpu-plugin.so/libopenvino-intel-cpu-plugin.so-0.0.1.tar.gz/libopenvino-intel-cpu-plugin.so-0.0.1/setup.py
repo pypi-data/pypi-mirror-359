27e343a19c30dc755366f335de45d63c24a0ad98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:56 +0000

    Add package libopenvino-intel-cpu-plugin.so/0.0.1/libopenvino-intel-cpu-plugin.so-0.0.1.tar.gz

---
+++ packages/libopenvino-intel-cpu-plugin.so/libopenvino-intel-cpu-plugin.so-0.0.1.tar.gz/libopenvino-intel-cpu-plugin.so-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="libopenvino-intel-cpu-plugin.so",
+    version="0.0.1",
+    packages=["libopenvino-intel-cpu-plugin.so"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
