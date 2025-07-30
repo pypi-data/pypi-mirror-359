2d174a439e1aa0af5094762619eebdf68c572105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:29 +0000

    Add package ss-gcj02towgs84/0.1.15/ss_gcj02towgs84-0.1.15.tar.gz

---
+++ packages/ss-gcj02towgs84/ss_gcj02towgs84-0.1.15.tar.gz/ss_gcj02towgs84-0.1.15/mcpServer/__init__.py
@@ -0,0 +1,9 @@
+# '''
+# Author: WQ
+# Date: 2025-07-03 10:19:20
+# LastEditTime: 2025-07-03 10:19:31
+# LastEditors: WQ
+# Description: 
+# FilePath: \mcp_server\mcpServer\__init__.py
+# '''
+from .server import app
