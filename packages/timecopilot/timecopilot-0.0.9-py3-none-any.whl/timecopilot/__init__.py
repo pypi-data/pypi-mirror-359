965980d5573b7596eee0618ece0cd3117ed036ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:27 +0000

    Add package timecopilot/0.0.9/timecopilot-0.0.9-py3-none-any.whl

---
+++ packages/timecopilot/timecopilot-0.0.9-py3-none-any.whl/timecopilot/__init__.py
@@ -0,0 +1,3 @@
+from .agent import TimeCopilot
+
+__all__ = ["TimeCopilot"]
---
+++ packages/timecopilot/timecopilot-0.0.9-py3-none-any.whl/timecopilot/models/benchmarks/__init__.py
@@ -0,0 +1,27 @@
+from .stats import (
+    ADIDA,
+    IMAPA,
+    AutoARIMA,
+    AutoCES,
+    AutoETS,
+    CrostonClassic,
+    DOTheta,
+    HistoricAverage,
+    SeasonalNaive,
+    Theta,
+    ZeroModel,
+)
+
+__all__ = [
+    "ADIDA",
+    "AutoARIMA",
+    "AutoCES",
+    "AutoETS",
+    "CrostonClassic",
+    "DOTheta",
+    "HistoricAverage",
+    "IMAPA",
+    "SeasonalNaive",
+    "Theta",
+    "ZeroModel",
+]
