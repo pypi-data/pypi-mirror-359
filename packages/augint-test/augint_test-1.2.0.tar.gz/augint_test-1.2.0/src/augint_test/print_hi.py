f03cca55bdb32cd462087329bb2a43b8d76553a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:10 +0000

    Add package augint-test/1.2.0/augint_test-1.2.0.tar.gz

---
+++ packages/augint-test/augint_test-1.2.0.tar.gz/augint_test-1.2.0/src/augint_test/print_hi.py
@@ -0,0 +1,13 @@
+def print_hi(name):
+    print(f"Hi, {name}")
+
+
+# Press the green button in the gutter to run the script.
+def main():
+    print_hi("Test Project")
+
+
+if __name__ == "__main__":
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
