04be0c58a5666d0c66cc5bd47aabf685bcd45501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:14 +0000

    Add package pycoro/0.1.12/pycoro-0.1.12.tar.gz

---
+++ packages/pycoro/pycoro-0.1.12.tar.gz/pycoro-0.1.12/src/pycoro/system.py
@@ -0,0 +1,51 @@
+from __future__ import annotations
+
+import time
+from threading import Event, Thread
+from typing import TYPE_CHECKING
+
+from pycoro.scheduler import Computation, Handle, Scheduler
+
+if TYPE_CHECKING:
+    from pycoro.io import IO
+
+
+class Pycoro[I, O]:
+    def __init__(self, io: IO[I, O], size: int, dequeue_size: int) -> None:
+        self._io = io
+        self._scheduler = Scheduler(self._io, size)
+        self._dequeue_size = dequeue_size
+        self._thread = Thread(target=self._loop, daemon=True, name="pycoro-main-thread")
+        self._stop = Event()
+        self._stopped = Event()
+
+    def start(self) -> None:
+        self._io.start()
+        self._thread.start()
+
+    def _loop(self) -> None:
+        while True:
+            self.tick(int(time.time() * 1_000))
+
+            if self.done():
+                self._scheduler.shutdown()
+                self._stopped.set()
+                return
+
+    def shutdown(self) -> None:
+        self._stop.set()
+        self._stopped.wait()
+        self._thread.join()
+
+    def tick(self, time: int) -> None:
+        for cqe in self._io.dequeue(self._dequeue_size):
+            cqe.callback(cqe.value)
+
+        self._scheduler.run_until_blocked(time)
+        self._io.flush(time)
+
+    def done(self) -> bool:
+        return self._stop.is_set() and self._scheduler.size() == 0
+
+    def add(self, c: Computation[I, O] | I) -> Handle[O]:
+        return self._scheduler.add(c)
