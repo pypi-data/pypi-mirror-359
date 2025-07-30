66732c7582cf741d3d7530725c283896bfaa6be1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:00 +0000

    Add package ha-conversation-api-forwarder/0.0.3/ha_conversation_api_forwarder-0.0.3-py3-none-any.whl

---
+++ packages/ha-conversation-api-forwarder/ha_conversation_api_forwarder-0.0.3-py3-none-any.whl/ha_conversation_api_forwarder/api_forwarder.py
@@ -0,0 +1,31 @@
+import requests
+import aiohttp
+import pytest
+
+
+class ApiForwarder:
+    def __init__(self, server_hub_url: str,):
+        """
+        Initialize an ApiForwarder object.
+        :param server_hub_url: http(s)://some-domen:xxxx/route
+        """
+        self.server_hub_url = server_hub_url
+        # self.server_hub_port = server_hub_port
+        # self.fetch_route = fetch_route
+
+    @pytest.mark.asyncio
+    async def forward(self, message: str) -> str:
+        """
+
+        :return:
+        """
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                    url=self.server_hub_url,
+                    json={"message": message}
+            ) as resp:
+                response_json = await resp.json()
+                print(response_json)
+        # resp = requests.post(self.server_hub_url, json={"message": message})
+        # print(resp.json())
+        return "It was successful"
