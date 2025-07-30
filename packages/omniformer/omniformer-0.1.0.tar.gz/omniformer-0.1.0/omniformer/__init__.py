4497744a788fdcf23c8abcdaac4aaca46bf86c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:29 +0000

    Add package omniformer/0.1.0/omniformer-0.1.0.tar.gz

---
+++ packages/omniformer/omniformer-0.1.0.tar.gz/omniformer-0.1.0/omniformer/__init__.py
@@ -0,0 +1,4 @@
+from .model import Omniformer
+from .utils import filter_events, OmniformerCSVDataset
+
+__all__ = ["Omniformer", "filter_events", "OmniformerCSVDataset"]
---
+++ packages/omniformer/omniformer-0.1.0.tar.gz/omniformer-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="omniformer",
+    version="0.1.0",
+    description="Omniformer: Context-aware Transformer for GW Signal Classification",
+    author="Shantanu Parmar",
+    packages=find_packages(),
+    package_data={"omniformer": ["config.py"]},
+    install_requires=[
+        "torch>=1.13",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "tqdm",
+        "matplotlib",
+        "streamlit",
+        "fastapi",
+        "uvicorn"
+    ],
+    entry_points={
+        "console_scripts": [
+            "omniformer-train=omniformer.train:main",
+            "omniformer-infer=omniformer.inference:main",
+        ]
+    },
+    include_package_data=True,
+    python_requires=">=3.8",
+)
