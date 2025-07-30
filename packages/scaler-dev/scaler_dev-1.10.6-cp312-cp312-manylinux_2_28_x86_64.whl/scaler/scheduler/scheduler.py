b77e23c5b9b02d87cb53305c6303c597a4c5639b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:20 +0000

    Add package scaler-dev/1.10.6/scaler_dev-1.10.6-cp312-cp312-manylinux_2_28_x86_64.whl

---
+++ packages/scaler-dev/scaler_dev-1.10.6-cp312-cp312-manylinux_2_28_x86_64.whl/scaler/scheduler/scheduler.py
@@ -0,0 +1,218 @@
+import asyncio
+import functools
+import logging
+
+import zmq.asyncio
+
+from scaler.io.async_binder import AsyncBinder
+from scaler.io.async_connector import AsyncConnector
+from scaler.io.async_object_storage_connector import AsyncObjectStorageConnector
+from scaler.io.config import CLEANUP_INTERVAL_SECONDS, STATUS_REPORT_INTERVAL_SECONDS
+from scaler.protocol.python.common import ObjectStorageAddress
+from scaler.protocol.python.message import (
+    ClientDisconnect,
+    ClientHeartbeat,
+    DisconnectRequest,
+    GraphTask,
+    GraphTaskCancel,
+    ObjectInstruction,
+    Task,
+    TaskCancel,
+    TaskResult,
+    WorkerHeartbeat,
+)
+from scaler.protocol.python.mixins import Message
+from scaler.scheduler.client_manager import VanillaClientManager
+from scaler.scheduler.config import SchedulerConfig
+from scaler.scheduler.graph_manager import VanillaGraphTaskManager
+from scaler.scheduler.object_manager import VanillaObjectManager
+from scaler.scheduler.status_reporter import StatusReporter
+from scaler.scheduler.task_manager import VanillaTaskManager
+from scaler.scheduler.worker_manager import VanillaWorkerManager
+from scaler.utility.event_loop import create_async_loop_routine
+from scaler.utility.exceptions import ClientShutdownException
+from scaler.utility.identifiers import ClientID, WorkerID
+from scaler.utility.zmq_config import ZMQConfig, ZMQType
+
+
+class Scheduler:
+    def __init__(self, config: SchedulerConfig):
+        if config.address.type != ZMQType.tcp:
+            raise TypeError(
+                f"{self.__class__.__name__}: scheduler address must be tcp type: {config.address.to_address()}"
+            )
+
+        if config.storage_address is None:
+            self._storage_address = ObjectStorageAddress.new_msg(
+                host=config.address.host,
+                port=config.address.port + 1
+            )
+        else:
+            self._storage_address = ObjectStorageAddress.new_msg(
+                host=config.storage_address.host,
+                port=config.storage_address.port
+            )
+
+        if config.monitor_address is None:
+            self._address_monitor = ZMQConfig(type=ZMQType.tcp, host=config.address.host, port=config.address.port + 2)
+        else:
+            self._address_monitor = config.monitor_address
+
+        self._context = zmq.asyncio.Context(io_threads=config.io_threads)
+        self._binder = AsyncBinder(context=self._context, name="scheduler", address=config.address)
+        self._binder_monitor = AsyncConnector(
+            context=self._context,
+            name="scheduler_monitor",
+            socket_type=zmq.PUB,
+            address=self._address_monitor,
+            bind_or_connect="bind",
+            callback=None,
+            identity=None,
+        )
+        self._connector_storage = AsyncObjectStorageConnector()
+
+        logging.info(f"{self.__class__.__name__}: listen to scheduler address {config.address.to_address()}")
+        logging.info(
+            f"{self.__class__.__name__}: connect to object storage server {self._storage_address!r}"
+        )
+        logging.info(
+            f"{self.__class__.__name__}: listen to scheduler monitor address {self._address_monitor.to_address()}"
+        )
+
+        self._client_manager = VanillaClientManager(
+            client_timeout_seconds=config.client_timeout_seconds,
+            protected=config.protected,
+            storage_address=self._storage_address,
+        )
+        self._object_manager = VanillaObjectManager()
+        self._graph_manager = VanillaGraphTaskManager()
+        self._task_manager = VanillaTaskManager(max_number_of_tasks_waiting=config.max_number_of_tasks_waiting)
+        self._worker_manager = VanillaWorkerManager(
+            per_worker_queue_size=config.per_worker_queue_size,
+            timeout_seconds=config.worker_timeout_seconds,
+            load_balance_seconds=config.load_balance_seconds,
+            load_balance_trigger_times=config.load_balance_trigger_times,
+            storage_address=self._storage_address,
+        )
+        self._status_reporter = StatusReporter(self._binder_monitor)
+
+        # register
+        self._binder.register(self.on_receive_message)
+        self._client_manager.register(
+            self._binder, self._binder_monitor, self._object_manager, self._task_manager, self._worker_manager
+        )
+        self._object_manager.register(
+            self._binder, self._binder_monitor, self._connector_storage, self._client_manager, self._worker_manager
+        )
+        self._graph_manager.register(
+            self._binder,
+            self._binder_monitor,
+            self._connector_storage,
+            self._client_manager,
+            self._task_manager,
+            self._object_manager,
+        )
+        self._task_manager.register(
+            self._binder,
+            self._binder_monitor,
+            self._client_manager,
+            self._object_manager,
+            self._worker_manager,
+            self._graph_manager,
+        )
+        self._worker_manager.register(self._binder, self._binder_monitor, self._task_manager)
+
+        self._status_reporter.register_managers(
+            self._binder, self._client_manager, self._object_manager, self._task_manager, self._worker_manager
+        )
+
+    async def connect_to_storage(self):
+        await self._connector_storage.connect(self._storage_address.host, self._storage_address.port)
+
+    async def on_receive_message(self, source: bytes, message: Message):
+        # =====================================================================================
+        # receive from upstream
+        if isinstance(message, ClientHeartbeat):
+            await self._client_manager.on_heartbeat(ClientID(source), message)
+            return
+
+        if isinstance(message, GraphTask):
+            await self._graph_manager.on_graph_task(ClientID(source), message)
+            return
+
+        if isinstance(message, GraphTaskCancel):
+            await self._graph_manager.on_graph_task_cancel(ClientID(source), message)
+            return
+
+        if isinstance(message, Task):
+            await self._task_manager.on_task_new(ClientID(source), message)
+            return
+
+        if isinstance(message, TaskCancel):
+            await self._task_manager.on_task_cancel(ClientID(source), message)
+            return
+
+        # scheduler receives client shutdown request from upstream
+        if isinstance(message, ClientDisconnect):
+            await self._client_manager.on_client_disconnect(ClientID(source), message)
+            return
+
+        # =====================================================================================
+        # receive from downstream
+        # receive worker heartbeat from downstream
+        if isinstance(message, WorkerHeartbeat):
+            await self._worker_manager.on_heartbeat(WorkerID(source), message)
+            return
+
+        # receive task result from downstream
+        if isinstance(message, TaskResult):
+            await self._worker_manager.on_task_result(message)
+            return
+
+        # scheduler receives worker disconnect request from downstream
+        if isinstance(message, DisconnectRequest):
+            await self._worker_manager.on_disconnect(WorkerID(source), message)
+            return
+
+        # =====================================================================================
+        # object related
+        # scheduler receives object request from upstream
+        if isinstance(message, ObjectInstruction):
+            await self._object_manager.on_object_instruction(source, message)
+            return
+
+        logging.error(f"{self.__class__.__name__}: unknown message from {source=}: {message}")
+
+    async def get_loops(self):
+        await self.connect_to_storage()
+
+        loops = [
+            create_async_loop_routine(self._binder.routine, 0),
+            create_async_loop_routine(self._connector_storage.routine, 0),
+            create_async_loop_routine(self._graph_manager.routine, 0),
+            create_async_loop_routine(self._task_manager.routine, 0),
+            create_async_loop_routine(self._client_manager.routine, CLEANUP_INTERVAL_SECONDS),
+            create_async_loop_routine(self._object_manager.routine, CLEANUP_INTERVAL_SECONDS),
+            create_async_loop_routine(self._worker_manager.routine, CLEANUP_INTERVAL_SECONDS),
+            create_async_loop_routine(self._status_reporter.routine, STATUS_REPORT_INTERVAL_SECONDS),
+        ]
+
+        # if self._log_forwarder is not None:
+        #     loops.append(create_async_loop_routine(self._log_forwarder.routine, 0))
+
+        try:
+            await asyncio.gather(*loops)
+        except asyncio.CancelledError:
+            pass
+        except ClientShutdownException as e:
+            logging.info(f"{self.__class__.__name__}: {e}")
+            pass
+
+        self._binder.destroy()
+        self._binder_monitor.destroy()
+
+
+@functools.wraps(Scheduler)
+async def scheduler_main(*args, **kwargs):
+    scheduler = Scheduler(*args, **kwargs)
+    await scheduler.get_loops()
---
+++ packages/scaler-dev/scaler_dev-1.10.6-cp312-cp312-manylinux_2_28_x86_64.whl/scaler/worker/agent/processor/processor.py
@@ -0,0 +1,294 @@
+import contextlib
+import logging
+import multiprocessing
+import os
+import signal
+from contextvars import ContextVar, Token
+from multiprocessing.synchronize import Event as EventType
+from typing import Callable, List, Optional, Tuple, cast
+
+import tblib.pickling_support
+import zmq
+
+from scaler.io.config import DUMMY_CLIENT
+from scaler.io.sync_connector import SyncConnector
+#from scaler.io.sync_object_storage_connector import SyncObjectStorageConnector
+from scaler.io.batch_sync_object_storage_connector import BatchSyncObjectStorageConnector
+from scaler.protocol.python.common import ObjectMetadata, TaskStatus
+from scaler.protocol.python.message import (
+    ObjectInstruction,
+    ProcessorInitialized,
+    Task,
+    TaskResult,
+)
+from scaler.protocol.python.mixins import Message
+from scaler.utility.logging.utility import setup_logger
+from scaler.utility.identifiers import ClientID, ObjectID, TaskID
+from scaler.utility.object_storage_config import ObjectStorageConfig
+from scaler.utility.serialization import serialize_failure
+from scaler.utility.zmq_config import ZMQConfig
+from scaler.worker.agent.processor.object_cache import ObjectCache
+
+SUSPEND_SIGNAL = "SIGUSR1"  # use str instead of a signal.Signal to not trigger an import error on unsupported systems.
+
+_current_processor: ContextVar[Optional["Processor"]] = ContextVar("_current_processor", default=None)
+
+
+class Processor(multiprocessing.get_context("spawn").Process):  # type: ignore
+    def __init__(
+        self,
+        event_loop: str,
+        agent_address: ZMQConfig,
+        storage_address: ObjectStorageConfig,
+        resume_event: Optional[EventType],
+        resumed_event: Optional[EventType],
+        garbage_collect_interval_seconds: int,
+        trim_memory_threshold_bytes: int,
+        logging_paths: Tuple[str, ...],
+        logging_level: str,
+    ):
+        multiprocessing.Process.__init__(self, name="Processor")
+
+        self._event_loop = event_loop
+        self._agent_address = agent_address
+        self._storage_address = storage_address
+
+        self._resume_event = resume_event
+        self._resumed_event = resumed_event
+
+        self._garbage_collect_interval_seconds = garbage_collect_interval_seconds
+        self._trim_memory_threshold_bytes = trim_memory_threshold_bytes
+        self._logging_paths = logging_paths
+        self._logging_level = logging_level
+
+        self._object_cache: Optional[ObjectCache] = None
+
+        self._current_task: Optional[Task] = None
+
+    def run(self) -> None:
+        self.__initialize()
+        self.__run_forever()
+
+    @staticmethod
+    def get_current_processor() -> Optional["Processor"]:
+        """Returns the current Processor instance controlling the current process, if any."""
+        return _current_processor.get()
+
+    def current_task(self) -> Optional[Task]:
+        return self._current_task
+
+    def __initialize(self):
+        # modify the logging path and add process id to the path
+        logging_paths = [f"{path}-{os.getpid()}" for path in self._logging_paths if path != "/dev/stdout"]
+        if "/dev/stdout" in self._logging_paths:
+            logging_paths.append("/dev/stdout")
+
+        setup_logger(log_paths=tuple(logging_paths), logging_level=self._logging_level)
+        tblib.pickling_support.install()
+
+        self._connector_agent = SyncConnector(
+            context=zmq.Context(), socket_type=zmq.DEALER, address=self._agent_address, identity=None
+        )
+        self._connector_storage = BatchSyncObjectStorageConnector(self._storage_address.host, self._storage_address.port)
+
+        self._object_cache = ObjectCache(
+            garbage_collect_interval_seconds=self._garbage_collect_interval_seconds,
+            trim_memory_threshold_bytes=self._trim_memory_threshold_bytes,
+        )
+        self._object_cache.start()
+
+        self.__register_signals()
+
+    def __register_signals(self):
+        self.__register_signal("SIGTERM", self.__interrupt)
+
+        if self._resume_event is not None:
+            self.__register_signal(SUSPEND_SIGNAL, self.__suspend)
+
+    def __interrupt(self, *args):
+        self._connector_agent.close()  # interrupts any blocking socket.
+
+    def __suspend(self, *args):
+        assert self._resume_event is not None
+        assert self._resumed_event is not None
+
+        self._resume_event.wait()  # stops any computation in the main thread until the event is triggered
+
+        # Ensures the processor agent knows we stopped waiting on `_resume_event`, as to avoid re-entrant wait on the
+        # event.
+        self._resumed_event.set()
+
+    def __run_forever(self):
+        try:
+            self._connector_agent.send(ProcessorInitialized.new_msg())
+            while True:
+                message = self._connector_agent.receive()
+                if message is None:
+                    continue
+
+                self.__on_connector_receive(message)
+
+        except zmq.error.ZMQError as e:
+            if e.errno != zmq.ENOTSOCK:  # ignore if socket got closed
+                raise
+
+        except (KeyboardInterrupt, InterruptedError):
+            pass
+
+        except Exception as e:
+            logging.exception(f"Processor[{self.pid}]: failed with unhandled exception:\n{(e)}")
+
+        finally:
+            self._object_cache.destroy()
+            self._connector_agent.close()
+
+            self._object_cache.join()
+
+    def __on_connector_receive(self, message: Message):
+        if isinstance(message, ObjectInstruction):
+            self.__on_receive_object_instruction(message)
+            return
+
+        if isinstance(message, Task):
+            self.__on_received_task(message)
+            return
+
+        logging.error(f"unknown {message=}")
+
+    def __on_receive_object_instruction(self, instruction: ObjectInstruction):
+        if instruction.instruction_type == ObjectInstruction.ObjectInstructionType.Delete:
+            for object_id in instruction.object_metadata.object_ids:
+                self._object_cache.del_object(object_id)
+            return
+
+        logging.error(f"worker received unknown object instruction type {instruction=}")
+
+    def __on_received_task(self, task: Task):
+        self._current_task = task
+
+        self.__cache_required_object_ids(task)
+
+        self.__process_task(task)
+
+    def __cache_required_object_ids(self, task: Task) -> None:
+        required_object_ids = self.__get_required_object_ids_for_task(task)
+
+        # for object_id in required_object_ids:
+        #     if self._object_cache.has_object(object_id):
+        #         continue
+
+        #     object_content = self._connector_storage.get_object(object_id)
+        #     self._object_cache.add_object(task.source, object_id, object_content)
+
+        missing_object_ids = [
+            object_id
+            for object_id in required_object_ids
+            if not self._object_cache.has_object(object_id)
+        ]
+
+        missings_objects = self._connector_storage.get_objects(missing_object_ids)
+
+        for object_id, object_content in zip(missing_object_ids, missings_objects):
+            self._object_cache.add_object(task.source, object_id, object_content)
+
+    @staticmethod
+    def __get_required_object_ids_for_task(task: Task) -> List[ObjectID]:
+        serializer_id = ObjectID.generate_serializer_object_id(task.source)
+        object_ids = [
+            serializer_id,
+            task.func_object_id,
+            *(cast(ObjectID, argument) for argument in task.function_args),
+        ]
+        return object_ids
+
+    def __process_task(self, task: Task):
+        try:
+            function = self._object_cache.get_object(task.func_object_id)
+            function_with_logger = self.__get_object_with_client_logger(DUMMY_CLIENT, function)
+
+            args = [
+                self._object_cache.get_object(cast(ObjectID, arg))
+                for arg in task.function_args
+            ]
+
+            with self.__processor_context():
+                result = function_with_logger(*args)
+            result_bytes = self._object_cache.serialize(task.source, result)
+            status = TaskStatus.Success
+
+        except Exception as e:
+            logging.exception(f"exception when processing task_id={task.task_id.hex()}:")
+            status = TaskStatus.Failed
+            result_bytes = serialize_failure(e)
+
+        self.__send_result(task.source, task.task_id, status, result_bytes)
+
+    def __get_object_with_client_logger(self, client: ClientID, fn: Callable) -> Callable:
+        assert self is not None
+        return fn
+
+        # if client in self._client_to_decorator:
+        #     wrap = self._client_to_decorator[client]
+        #     return wrap(fn)
+        #
+        # def _generate_wrapper(handler: WorkerLogPublisher):
+        #     def decorator(func: Callable):
+        #         @functools.wraps(func)
+        #         def wrapper(*args, **kwargs):
+        #             logger = logging.getLogger()
+        #             logger.addHandler(handler)
+        #             result = func(*args, **kwargs)
+        #             logger.removeHandler(handler)
+        #             return result
+        #
+        #         return wrapper
+        #
+        #     return decorator
+        #
+        # wrap = _generate_wrapper(WorkerLogPublisher(client, self._log_hub_address, log_level=logging.DEBUG))
+        # self._client_to_decorator[client] = wrap
+        # return wrap(fn)
+
+    def __send_result(self, source: ClientID, task_id: TaskID, status: TaskStatus, result_bytes: bytes):
+        self._current_task = None
+
+        result_object_id = ObjectID.generate_object_id(source)
+
+        self._connector_storage.set_objects([(result_object_id, result_bytes)])
+        self._connector_agent.send(
+            ObjectInstruction.new_msg(
+                ObjectInstruction.ObjectInstructionType.Create,
+                source,
+                ObjectMetadata.new_msg(
+                    (result_object_id,),
+                    (ObjectMetadata.ObjectContentType.Object,),
+                    (f"<res {repr(result_object_id)}>".encode(),),
+                ),
+            )
+        )
+        self._connector_agent.send(
+            TaskResult.new_msg(task_id, status, metadata=b"", results=[bytes(result_object_id)])
+        )
+
+    @staticmethod
+    def __set_current_processor(context: Optional["Processor"]) -> Token:
+        if context is not None and _current_processor.get() is not None:
+            raise ValueError("cannot override a previously set processor context.")
+
+        return _current_processor.set(context)
+
+    @contextlib.contextmanager
+    def __processor_context(self):
+        self.__set_current_processor(self)
+        try:
+            yield
+        finally:
+            self.__set_current_processor(None)
+
+    @staticmethod
+    def __register_signal(signal_name: str, handler: Callable) -> None:
+        signal_instance = getattr(signal, signal_name, None)
+        if signal_instance is None:
+            raise RuntimeError(f"unsupported platform, signal not available: {signal_name}.")
+
+        signal.signal(signal_instance, handler)
