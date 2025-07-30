2c5d9b9d5e33bff3e9b58ae7b46b578478828bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:57 +0000

    Add package st-annotator/0.0.2/st_annotator-0.0.2-py3-none-any.whl

---
+++ packages/st-annotator/st_annotator-0.0.2-py3-none-any.whl/st_annotator/frontend/src/types/annotatorTypes.ts
@@ -0,0 +1,28 @@
+export enum ActionTypes {
+    SET_TEXT_LABELS = 'SET_TEXT_LABELS',
+    RENDER_TEXT = 'RENDER_TEXT',
+    ADD_LABEL = 'ADD_LABEL',
+    SELECT_LABEL = 'SELECT_LABEL',
+    REMOVE_LABEL = 'REMOVE_LABEL',
+}
+
+export type ILabel = {
+    start: number;
+    end: number;
+    label: string;
+}
+
+export interface IState {
+    text: string;
+    actual_text: JSX.Element[];
+    selectedLabel: string;
+    labels: { [key: string]: ILabel[]};
+    in_snake_case: boolean;
+    show_label_input: boolean;
+    colors: { [key: string]: string };
+}
+
+export interface IAction {
+    type: ActionTypes;
+    payload?: any;
+  }
