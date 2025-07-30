667036c844188a9e6480b384ec9adccfc7182433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:05:09 +0000

    Add package textdraw/0.2.0/textdraw-0.2.0-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/textdraw/textdraw-0.2.0-cp311-cp311-manylinux_2_17_s390x.manylinux2014_s390x.whl/textdraw/__init__.py
@@ -0,0 +1,29 @@
+from .textdraw import (
+    BoundingBox,
+    Box,
+    Pixel,
+    PixelGroup,
+    Point,
+    Style,
+    TextPath,
+    arrow,
+    duplicate_shifted,
+    multipath,
+    render,
+    text,
+)
+
+__all__ = [
+    'BoundingBox',
+    'Box',
+    'Pixel',
+    'PixelGroup',
+    'Point',
+    'Style',
+    'TextPath',
+    'arrow',
+    'duplicate_shifted',
+    'multipath',
+    'render',
+    'text',
+]
