412b508fa2cc73cc2cc3062db299fc4974f7f30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:18 +0000

    Add package ubc-solar-physics/1.8.3/ubc_solar_physics-1.8.3-cp313-cp313-win_amd64.whl

---
+++ packages/ubc-solar-physics/ubc_solar_physics-1.8.3-cp313-cp313-win_amd64.whl/physics/models/motor/__init__.py
@@ -0,0 +1,9 @@
+from .base_motor import BaseMotor
+from .basic_motor import BasicMotor
+from .advanced_motor import AdvancedMotor
+
+__all__ = [
+    "BaseMotor",
+    "BasicMotor",
+    "AdvancedMotor",
+]
