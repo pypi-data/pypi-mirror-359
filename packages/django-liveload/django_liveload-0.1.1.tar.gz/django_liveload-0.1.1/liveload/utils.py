fc1c23c3b4c1c94ef860b71a86add7fc9bc27dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:21 +0000

    Add package django-liveload/0.1.1/django_liveload-0.1.1.tar.gz

---
+++ packages/django-liveload/django_liveload-0.1.1.tar.gz/django_liveload-0.1.1/liveload/utils.py
@@ -0,0 +1,2 @@
+# utils.py
+# Helper functions for the liveload package.
---
+++ packages/django-liveload/django_liveload-0.1.1.tar.gz/django_liveload-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-liveload",
+    version="0.1.1",
+    description="Easily add live progress updates in Django using Django Channels.",
+    long_description=open("README.md",encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Sanat Jha",
+    author_email="sanatjha4@gmail.com",
+    url="https://github.com/Sanat-Jha/django-liveload",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "channels>=4.0.0",
+        "asgiref>=3.5.0",
+        "daphne>=4.0.0"
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
