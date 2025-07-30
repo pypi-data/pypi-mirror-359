a33ea6345216079ce095faaddc4024928945b3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package funcsigspython_version/0.0.1/funcsigspython_version-0.0.1.tar.gz

---
+++ packages/funcsigspython_version/funcsigspython_version-0.0.1.tar.gz/funcsigspython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="funcsigspython_version",
+    version="0.0.1",
+    packages=["funcsigspython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
