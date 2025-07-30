ade72749b92f18b43b9f2d24f58307106729e56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:27 +0000

    Add package tomwer/1.5.0rc0/tomwer-1.5.0rc0.tar.gz

---
+++ packages/tomwer/tomwer-1.5.0rc0.tar.gz/tomwer-1.5.0rc0/src/tomwer/core/process/reconstruction/axis/side.py
@@ -0,0 +1,8 @@
+from silx.utils.enum import Enum as _Enum
+
+
+class Side(_Enum):
+    LEFT = "left"
+    RIGHT = "right"
+    CENTER = "center"
+    ALL = "all"
---
+++ packages/tomwer/tomwer-1.5.0rc0.tar.gz/tomwer-1.5.0rc0/src/tomwer/core/scan/scantype.py
@@ -0,0 +1,7 @@
+from silx.utils.enum import Enum as _Enum
+
+
+class ScanType(_Enum):
+    BLISS = "bliss-hdf5"
+    SPEC = "spec-edf"
+    NX_TOMO = "NXtomo"
