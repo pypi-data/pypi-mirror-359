1160cdfe7e343dc851098357fe1cca97eeced673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:05:59 +0000

    Add package pymultigit/0.0.95/pymultigit-0.0.95.tar.gz

---
+++ packages/pymultigit/pymultigit-0.0.95.tar.gz/pymultigit-0.0.95/src/pymultigit/configs.py
@@ -0,0 +1,115 @@
+"""
+All configurations for pymultigit
+"""
+
+
+from pytconf import Config, ParamCreator
+
+
+class ConfigOutput(Config):
+    """
+    Parameters to control output
+    """
+    terse = ParamCreator.create_bool(
+        help_string="be terse?",
+        default=False,
+    )
+    stats = ParamCreator.create_bool(
+        help_string="show statistics are the end?",
+        default=False,
+    )
+    output = ParamCreator.create_bool(
+        help_string="show projects?",
+        default=True,
+    )
+    print_not = ParamCreator.create_bool(
+        help_string="print what the project is not",
+        default=False,
+    )
+
+
+class ConfigDebug(Config):
+    """
+    Parameters for debug
+    """
+    debug_verbose = ParamCreator.create_bool(
+        help_string="be verbose?",
+        default=False,
+    )
+    git_verbose = ParamCreator.create_bool(
+        help_string="add --verbose when running git?",
+        default=False,
+    )
+    git_quiet = ParamCreator.create_bool(
+        help_string="add --quiet when running git?",
+        default=False,
+    )
+
+
+class ConfigMain(Config):
+    """
+    Main parameters
+    """
+    sort = ParamCreator.create_bool(
+        help_string="sort results by project name?",
+        default=True,
+    )
+    glob = ParamCreator.create_str(
+        help_string="what glob to use?",
+        default="*/*.git"
+    )
+    use_glob = ParamCreator.create_bool(
+        help_string="Should I use glob or list of folders?",
+        default=True,
+    )
+    folders = ParamCreator.create_list_str(
+        help_string="what list of folders to use?",
+        default=[],
+    )
+    stop = ParamCreator.create_bool(
+        help_string="Stop when failing?",
+        default=True,
+    )
+    print_no_projects = ParamCreator.create_bool(
+        help_string="Print a message when no projects are found?",
+        default=True,
+    )
+
+
+class ConfigGrep(Config):
+    """
+    Parameters for the grep command
+    """
+
+    regexp = ParamCreator.create_str(
+        help_string="what regexp to look for?",
+    )
+    files = ParamCreator.create_str_or_none(
+        help_string="what regexp to look for?",
+        default=None,
+    )
+
+
+class ConfigPull(Config):
+    """
+    Parameters for the pull command
+    """
+
+    pull_quiet = ParamCreator.create_bool(
+        help_string="add --quiet when running git pull?",
+        default=False,
+    )
+
+
+class ConfigSubprocess(Config):
+    """
+    Parameters to configure how we run subprocess
+    """
+    print_command = ParamCreator.create_bool(
+        help_string="print out commands",
+        default=False,
+    )
+    quiet = ParamCreator.create_bool(
+        help_string="Suppress output?",
+        default=False,
+    )
---
+++ packages/pymultigit/pymultigit-0.0.95.tar.gz/pymultigit-0.0.95/src/pymultigit/utils/subprocess.py
@@ -0,0 +1,30 @@
+"""
+subprocess.py
+"""
+
+import os
+import os.path
+import subprocess
+
+from pymultigit.configs import ConfigSubprocess, ConfigOutput
+
+
+def check_call_ve(orig_args: list[str]) -> None:
+    if not os.path.isdir(".venv/default"):
+        if ConfigOutput.print_not:
+            print("not a make venv folder (.venv/default is not there)")
+        return
+    # we call 'venv-run' with absolute path since it may change folder
+    args = [
+        "venv-run",
+        "--venv",
+        os.path.abspath(".venv/default"),
+        "--",
+    ]
+    args.extend(orig_args)
+    if ConfigSubprocess.print_command:
+        print(f"running {args}")
+    if ConfigSubprocess.quiet:
+        subprocess.check_call(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    else:
+        subprocess.check_call(args)
