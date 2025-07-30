c72062f140d3f9104cc26b2d5f03bcbb70b99648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:59 +0000

    Add package model-compose/0.1.6/model_compose-0.1.6.tar.gz

---
+++ packages/model-compose/model_compose-0.1.6.tar.gz/model_compose-0.1.6/src/mindor/core/controller/runner/mcp_client.py
@@ -0,0 +1,10 @@
+from typing import Type, Union, Literal, Optional, Dict, List, Tuple, Set, Annotated, Any
+from mindor.dsl.schema.controller import ControllerConfig
+from .runner import ControllerClient
+
+class McpControllerClient(ControllerClient):
+    def __init__(self, config: ControllerConfig):
+        super().__init__(config)
+
+    async def run_workflow(self, workflow_id: Optional[str], input: Any) -> Any:
+        pass
