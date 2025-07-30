203392eb213c5d2c217936615c2a11e781145849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:07:51 +0000

    Add package polars-uuid/0.1.1/polars_uuid-0.1.1-cp311-abi3-macosx_10_12_x86_64.whl

---
+++ packages/polars-uuid/polars_uuid-0.1.1-cp311-abi3-macosx_10_12_x86_64.whl/polars_uuid/__init__.py
@@ -0,0 +1,6 @@
+from polars_uuid._register_plugin import uuid_v4 as uuid_v4
+from polars_uuid._register_plugin import uuid_v4_single as uuid_v4_single
+from polars_uuid._register_plugin import uuid_v7 as uuid_v7
+from polars_uuid._register_plugin import uuid_v7_now as uuid_v7_now
+from polars_uuid._register_plugin import uuid_v7_now_single as uuid_v7_now_single
+from polars_uuid._register_plugin import uuid_v7_single as uuid_v7_single
