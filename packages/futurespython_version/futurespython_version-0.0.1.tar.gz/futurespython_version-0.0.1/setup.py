b91e74334320a566fea7cd461c316b9d20472f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:04 +0000

    Add package futurespython_version/0.0.1/futurespython_version-0.0.1.tar.gz

---
+++ packages/futurespython_version/futurespython_version-0.0.1.tar.gz/futurespython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="futurespython_version",
+    version="0.0.1",
+    packages=["futurespython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
