415f0c4e889805b9a5eeb8909277dd5acd33d8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:27 +0000

    Add package SocialED/1.1.6/socialed-1.1.6.tar.gz

---
+++ packages/SocialED/socialed-1.1.6.tar.gz/socialed-1.1.6/SocialED/detector/dame.py
@@ -0,0 +1,12 @@
+class DAMe:
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def evaluate(self, X, y):
+        pass
