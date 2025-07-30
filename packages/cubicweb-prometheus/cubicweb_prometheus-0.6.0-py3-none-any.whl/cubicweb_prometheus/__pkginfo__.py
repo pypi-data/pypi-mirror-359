4f78219fb56a5d19162420c4aead241a6daeb3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:12:16 +0000

    Add package cubicweb-prometheus/0.6.0/cubicweb_prometheus-0.6.0-py3-none-any.whl

---
+++ packages/cubicweb-prometheus/cubicweb_prometheus-0.6.0-py3-none-any.whl/cubicweb_prometheus/__pkginfo__.py
@@ -0,0 +1,25 @@
+# pylint: disable=W0622
+"""cubicweb-prometheus application packaging information"""
+
+
+modname = "cubicweb_prometheus"
+distname = "cubicweb-prometheus"
+
+numversion = (0, 6, 0)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+author = "LOGILAB S.A. (Paris, FRANCE)"
+author_email = "contact@logilab.fr"
+description = "Exporting prometheus metrics"
+web = "https://forge.extranet.logilab.fr/cubicweb/cubes/prometheus"
+
+__depends__ = {"cubicweb": ">=4.5.2,<6.0.0", "prometheus-client": ">=0.16.0,<0.17.0"}
+__recommends__ = {}
+
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: JavaScript",
+]
