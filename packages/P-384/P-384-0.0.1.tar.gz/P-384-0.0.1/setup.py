84bf6d9b41aca1850ae1377d88b3b7e627014d1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:56 +0000

    Add package P-384/0.0.1/P-384-0.0.1.tar.gz

---
+++ packages/P-384/P-384-0.0.1.tar.gz/P-384-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="P-384",
+    version="0.0.1",
+    packages=["P-384"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
