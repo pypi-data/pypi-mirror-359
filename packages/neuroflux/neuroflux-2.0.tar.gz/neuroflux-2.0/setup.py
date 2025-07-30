1ab038d1a0a2d7ccd34d291d9ab53be0f0afef9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:36 +0000

    Add package neuroflux/2.0/neuroflux-2.0.tar.gz

---
+++ packages/neuroflux/neuroflux-2.0.tar.gz/neuroflux-2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="neuroflux",
+    version="2.0",
+    description="MRI Brain Tumor Diagnosis and Grad-CAM Visualization",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Neuroflux-AI/neuroflux",
+    install_requires=[
+        "numpy",
+        "nibabel",
+        "opencv-python",
+        "tensorflow>=2.0",
+        "matplotlib"
+    ],
+)
