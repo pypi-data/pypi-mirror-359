f57d5cddc1e1a46d0eddada2dd7397cc4a661c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:55 +0000

    Add package secp256r1/0.0.1/secp256r1-0.0.1.tar.gz

---
+++ packages/secp256r1/secp256r1-0.0.1.tar.gz/secp256r1-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="secp256r1",
+    version="0.0.1",
+    packages=["secp256r1"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
