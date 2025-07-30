3bfce850b34964af07c33942b7279b22ec4fe59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:08:23 +0000

    Add package django-auditlog/3.2.1/django_auditlog-3.2.1.tar.gz

---
+++ packages/django-auditlog/django_auditlog-3.2.1.tar.gz/django_auditlog-3.2.1/auditlog_tests/test_settings.py
@@ -0,0 +1,65 @@
+"""
+Settings file for the Auditlog test suite.
+"""
+
+import os
+
+DEBUG = True
+
+SECRET_KEY = "test"
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.admin",
+    "django.contrib.staticfiles",
+    "django.contrib.postgres",
+    "auditlog",
+    "test_app",
+]
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "auditlog.middleware.AuditlogMiddleware",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv(
+            "TEST_DB_NAME", "auditlog" + os.environ.get("TOX_PARALLEL_ENV", "")
+        ),
+        "USER": os.getenv("TEST_DB_USER", "postgres"),
+        "PASSWORD": os.getenv("TEST_DB_PASS", ""),
+        "HOST": os.getenv("TEST_DB_HOST", "127.0.0.1"),
+        "PORT": os.getenv("TEST_DB_PORT", "5432"),
+    }
+}
+
+TEMPLATES = [
+    {
+        "APP_DIRS": True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    },
+]
+
+STATIC_URL = "/static/"
+
+ROOT_URLCONF = "test_app.urls"
+
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
