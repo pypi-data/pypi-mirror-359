22d0211d035634afbcd1592c2eae4d29e2495b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:18 +0000

    Add package weiser-ai/0.1.14/weiser_ai-0.1.14.tar.gz

---
+++ packages/weiser-ai/weiser_ai-0.1.14.tar.gz/weiser_ai-0.1.14/weiser/drivers/__init__.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+from weiser.loader.models import Datasource, DBType
+from weiser.drivers.base import BaseDriver
+from weiser.drivers.postgres import PostgresDriver
+from weiser.drivers.snowflake import SnowflakeDriver
+
+
+DB_DRIVER_MAP = {
+    DBType.postgresql: PostgresDriver,
+    DBType.snowflake: SnowflakeDriver,
+}
+
+DBDriverType = Union[BaseDriver, PostgresDriver, SnowflakeDriver]
+
+
+class DriverFactory:
+    @staticmethod
+    def create_driver(data_source: Datasource) -> DBDriverType:
+        return DB_DRIVER_MAP.get(data_source.type, BaseDriver)(data_source)
