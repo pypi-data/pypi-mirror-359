82b8421f0ac82544a10f20cf3053f81350078500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:17 +0000

    Add package cuda-core/0.3.1/cuda_core-0.3.1-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/cuda-core/cuda_core-0.3.1-cp310-cp310-manylinux_2_24_x86_64.manylinux_2_28_x86_64.whl/cuda/core/experimental/utils.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024-2025, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from cuda.core.experimental._memoryview import (
+    StridedMemoryView,  # noqa: F401
+    args_viewable_as_strided_memory,  # noqa: F401
+)
