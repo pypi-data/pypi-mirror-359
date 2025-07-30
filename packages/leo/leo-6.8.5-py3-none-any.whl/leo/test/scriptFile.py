f8bc9d2823b43fcc214abb713214e1e8cd52fc38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:40 +0000

    Add package leo/6.8.5/leo-6.8.5-py3-none-any.whl

---
+++ packages/leo/leo-6.8.5-py3-none-any.whl/leo/test/scriptFile.py
@@ -0,0 +1,10 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20150502050609.1: * @button backup
+"""
+Back up this .leo file.
+
+os.environ['LEO_BACKUP'] must be the path to an existing (writable) directory.
+"""
+c.backup_helper(sub_dir='leoPy')
+#@-leo
+
