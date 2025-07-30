0649eca6bf69b91e130143435c4d9159d977fab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:07:53 +0000

    Add package dm-aioaiagent/0.5.0/dm_aioaiagent-0.5.0.tar.gz

---
+++ packages/dm-aioaiagent/dm_aioaiagent-0.5.0.tar.gz/dm_aioaiagent-0.5.0/dm_aioaiagent/types.py
@@ -0,0 +1,34 @@
+from typing import Literal, Union, Type
+from typing_extensions import TypedDict
+from pydantic import BaseModel
+from langchain_core.messages import BaseMessage
+
+OutputSchemaType = Union[Type[TypedDict], Type[BaseModel], None]
+
+
+class ImageMessageTextItem(TypedDict):
+    type: Literal['text']
+    text: str
+
+
+class ImageMessageImageItem(TypedDict):
+    type: Literal['image_url']
+    image_url: dict
+
+
+class ImageMessage(TypedDict):
+    role: Literal["user"]
+    content: list[Union[ImageMessageTextItem, ImageMessageImageItem]]
+
+
+class TextMessage(TypedDict):
+    role: Literal["user", "ai"]
+    content: str
+
+
+InputMessage = Union[TextMessage, ImageMessage, BaseMessage]
+
+
+class State(TypedDict):
+    messages: list[InputMessage]
+    new_messages: list[BaseMessage]
