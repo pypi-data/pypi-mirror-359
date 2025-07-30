75c9e2787a13daf15407854863a849eed73a7a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:43 +0000

    Add package terraback-community/0.1.0/terraback_community-0.1.0-py3-none-any.whl

---
+++ packages/terraback-community/terraback_community-0.1.0-py3-none-any.whl/terraback/_build_info.py
@@ -0,0 +1,9 @@
+# Build information - tier is determined at runtime based on license
+BUILD_EDITION = 'universal'  # Single build for all tiers
+VERSION = 'v0.0.2'
+BUILD_TIME = '2025-07-01T18:09:34Z'
+BUILD_COMMIT = 'f609d5c932143ed6bc87b264a688f9ca1854ed2b'
+
+# License tiers are determined at runtime by checking the license
+# This allows the same binary to work with different license tiers
+AVAILABLE_TIERS = ['free', 'pro', 'enterprise']
