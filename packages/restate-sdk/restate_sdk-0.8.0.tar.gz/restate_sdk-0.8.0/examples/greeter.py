dfd4c2f5da4389e870533dbd4d36ee566945f5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:05 +0000

    Add package restate-sdk/0.8.0/restate_sdk-0.8.0.tar.gz

---
+++ packages/restate-sdk/restate_sdk-0.8.0.tar.gz/restate_sdk-0.8.0/examples/greeter.py
@@ -0,0 +1,21 @@
+#
+#  Copyright (c) 2023-2024 - Restate Software, Inc., Restate GmbH
+#
+#  This file is part of the Restate SDK for Python,
+#  which is released under the MIT license.
+#
+#  You can find a copy of the license in file LICENSE in the root
+#  directory of this repository or package, or at
+#  https://github.com/restatedev/sdk-typescript/blob/main/LICENSE
+#
+"""example.py"""
+# pylint: disable=C0116
+# pylint: disable=W0613
+
+from restate import Service, Context
+
+greeter = Service("greeter")
+
+@greeter.handler()
+async def greet(ctx: Context, name: str) -> str:
+    return f"Hello {name}!"
