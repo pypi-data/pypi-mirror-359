67e0e798522b0fcbbd82f8aa5db5a6cafd8b8afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:22 +0000

    Add package scpert/0.1.0/scpert-0.1.0.tar.gz

---
+++ packages/scpert/scpert-0.1.0.tar.gz/scpert-0.1.0/scpert/__init__.py
@@ -0,0 +1,6 @@
+from .scpert import scPert
+from .train_model import train
+from .predict import predict
+from .ProcePertdata import PertData
+
+__all__ = ['scPert', 'train', 'predict', 'scPert', 'PertData']   
