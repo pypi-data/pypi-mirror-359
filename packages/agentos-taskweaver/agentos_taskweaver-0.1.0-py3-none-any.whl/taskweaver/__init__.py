6bbc0dfebccf5d949c582a71534cfd418c45e8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:22 +0000

    Add package agentos-taskweaver/0.1.0/agentos_taskweaver-0.1.0-py3-none-any.whl

---
+++ packages/agentos-taskweaver/agentos_taskweaver-0.1.0-py3-none-any.whl/taskweaver/__init__.py
@@ -0,0 +1,3 @@
+__author__ = "Microsoft TaskWeaver"
+__email__ = "taskweaver@microsoft.com"
+__version__ = "0.1.0a0+local.20250702213241"  # Refer to `/version.json` file when updating version string, the line is auto-updated
