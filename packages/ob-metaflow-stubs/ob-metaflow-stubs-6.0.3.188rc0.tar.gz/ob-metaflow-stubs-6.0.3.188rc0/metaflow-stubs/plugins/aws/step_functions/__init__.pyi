490526adb9ebc20c3e9e5dcf8439e1157db203a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:58 +0000

    Add package ob-metaflow-stubs/6.0.3.188rc0/ob-metaflow-stubs-6.0.3.188rc0.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.3.188rc0.tar.gz/ob-metaflow-stubs-6.0.3.188rc0/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.18.1+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-03T01:34:48.349376                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
