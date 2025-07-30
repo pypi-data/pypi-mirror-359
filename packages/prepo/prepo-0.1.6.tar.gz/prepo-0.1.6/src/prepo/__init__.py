36255895ef1fbaaed2d38e616e877fff9e148fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:34 +0000

    Add package prepo/0.1.6/prepo-0.1.6.tar.gz

---
+++ packages/prepo/prepo-0.1.6.tar.gz/prepo-0.1.6/src/prepo/__init__.py
@@ -0,0 +1,9 @@
+"""
+prepo: A package for preprocessing pandas DataFrames
+
+This package provides tools for cleaning, scaling, and processing pandas DataFrames.
+"""
+
+from .preprocessor import FeaturePreProcessor
+
+__all__ = ['FeaturePreProcessor']
