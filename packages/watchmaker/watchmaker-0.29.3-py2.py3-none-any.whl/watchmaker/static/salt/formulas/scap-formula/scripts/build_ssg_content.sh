70a501d02bbb2f717cd186b284a422fad3543a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:25 +0000

    Add package watchmaker/0.29.3/watchmaker-0.29.3-py2.py3-none-any.whl

---
+++ packages/watchmaker/watchmaker-0.29.3-py2.py3-none-any.whl/watchmaker/static/salt/formulas/scap-formula/scripts/build_ssg_content.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+set -e
+
+# Set vars
+REPO=https://github.com/ComplianceAsCode/content.git
+PROFILES=( C2S stig )
+MAKE_TARGETS_RHEL=(
+  rhel8-content
+  rhel9-content
+)
+MAKE_TARGETS_DERIVATIVES=(
+  centos8-content
+  cs9-content
+)
+MAKE_TARGETS_OTHERS=(
+  almalinux9-content
+  ol8-content
+  ol9-content
+)
+TMPDIR="${TMPDIR:-/tmp}"
+BUILD_DIR="${TMPDIR}/ComplianceAsCode/content"
+DIST_DIR="${TMPDIR}/dist"
+
+export SOURCE_DATE_EPOCH=1614699939
+
+# Remove old SSG build directory
+echo "Removing directory ${BUILD_DIR}..."
+rm -rf "$BUILD_DIR" "$DIST_DIR"
+
+mkdir -p "$DIST_DIR"
+exec > >(tee "${DIST_DIR}/build-content.log") 2>&1
+
+# Clone the repo and checkout the latest tag
+git clone "$REPO" "$BUILD_DIR" && pushd "$BUILD_DIR"
+TAG="${SSG_VER:-$(git describe --tags "$(git rev-list --tags --max-count=1)")}"
+echo
+echo "Tag to build: ${TAG}"
+echo
+git checkout "$TAG"
+echo
+
+# Update standard_profiles
+echo "Ensuring ssg content includes required profiles: ${PROFILES[*]}"
+ssg_constants="${BUILD_DIR}/ssg/constants.py"
+for profile in "${PROFILES[@]}"
+do
+  if grep -e 'standard_profiles' "$ssg_constants" | grep -e \'"$profile"\'; then
+    echo "-- Profile '$profile' already exists.  Will not be added."
+  else
+    echo "-- Profile '$profile' was not found. Profile '$profile' will be added to standard_profiles..."
+    sed -i '/standard_profiles = \[/ s/]/,\ '\'"$profile"\''&/' "$ssg_constants"
+  fi
+
+  for target in "${MAKE_TARGETS_OTHERS[@]//-content/}"
+  do
+    elver="${target: -1}"
+    if [[ -e "${BUILD_DIR}/products/${target}/profiles/${profile}.profile" ]]
+    then
+      echo "-- Profile '$profile' already exists for target '$target'. Will not be added..."
+    elif [[ -e "${BUILD_DIR}/products/rhel${elver}/profiles/${profile}.profile" ]]
+    then
+      echo "-- Profile '$profile' was found for 'rhel${elver}'. Adding profile '$profile' to target '${target}'..."
+      cp -n "${BUILD_DIR}/products/rhel${elver}/profiles/${profile}.profile" "${BUILD_DIR}/products/${target}/profiles/${profile}.profile"
+    else
+      echo "-- Profile '$profile' does not exist for 'rhel${elver}'. Skipping target '${target}'..."
+    fi
+  done
+done
+echo "Done adding profiles..."
+echo
+
+# Build content
+echo "Building ssg xml content..."
+echo
+cd build
+
+# Build SCAP 1.3 content
+# more info here: https://complianceascode.readthedocs.io/en/latest/manual/developer/02_building_complianceascode.html#building
+
+cmake -G Ninja -DSSG_TARGET_OVAL_MINOR_VERSION:STRING=11 ../
+
+# Build RHEL first to generate dependencies for CentOS and Other targets
+ninja -j 4 "${MAKE_TARGETS_RHEL[@]}"
+ninja -j 4 "${MAKE_TARGETS_DERIVATIVES[@]}"
+ninja -j 4 "${MAKE_TARGETS_OTHERS[@]}"
+
+cp ./*-ds.xml ./*-xccdf.xml ./*-oval.xml ./*-cpe-dictionary.xml ./*-ocil.xml "$DIST_DIR"
+
+echo
+echo 'Done building content!'
+
+popd
