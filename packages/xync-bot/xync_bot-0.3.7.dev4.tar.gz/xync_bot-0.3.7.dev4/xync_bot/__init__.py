ca5cbc7e8877afbbf9b2575a7b255b04e76fd75e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:13 +0000

    Add package xync-bot/0.3.7.dev4/xync_bot-0.3.7.dev4.tar.gz

---
+++ packages/xync-bot/xync_bot-0.3.7.dev4.tar.gz/xync_bot-0.3.7.dev4/xync_bot/__init__.py
@@ -0,0 +1,23 @@
+import logging
+from asyncio import run
+
+from PGram import Bot
+from aiogram.client.default import DefaultBotProperties
+from x_model import init_db
+
+from xync_bot.routers.cond import cr
+
+# from xync_bot.routers.main import mr
+from xync_bot.routers.pay.main import pay as pay
+
+if __name__ == "__main__":
+    from xync_bot.loader import TOKEN, TORM
+
+    logging.basicConfig(level=logging.INFO)
+
+    async def main() -> None:
+        cn = await init_db(TORM)
+        bot = Bot(TOKEN, [cr, pay], cn, default=DefaultBotProperties(parse_mode="HTML"))
+        await bot.start()
+
+    run(main())
