15568c9c9a0a8b2d57584170d74826cc7324275d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:12:03 +0000

    Add package idds-monitor/2.2.42/idds_monitor-2.2.42.tar.gz

---
+++ packages/idds-monitor/idds_monitor-2.2.42.tar.gz/idds_monitor-2.2.42/data/conf.js
@@ -0,0 +1,9 @@
+
+var appConfig = {
+    'iddsAPI_request': "https://pkrvmbietmlfzoi:443/idds/monitor_request/null/null",
+    'iddsAPI_transform': "https://pkrvmbietmlfzoi:443/idds/monitor_transform/null/null",
+    'iddsAPI_processing': "https://pkrvmbietmlfzoi:443/idds/monitor_processing/null/null",
+    'iddsAPI_request_detail': "https://pkrvmbietmlfzoi:443/idds/monitor/null/null/true/false/false",
+    'iddsAPI_transform_detail': "https://pkrvmbietmlfzoi:443/idds/monitor/null/null/false/true/false",
+    'iddsAPI_processing_detail': "https://pkrvmbietmlfzoi:443/idds/monitor/null/null/false/false/true"
+}
