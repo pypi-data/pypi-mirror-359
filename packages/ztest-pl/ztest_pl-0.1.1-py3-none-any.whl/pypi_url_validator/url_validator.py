ee28d761ee4dccaf28fa2be17f35c7f39831ae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:36 +0000

    Add package ztest-pl/0.1.1/ztest_pl-0.1.1-py3-none-any.whl

---
+++ packages/ztest-pl/ztest_pl-0.1.1-py3-none-any.whl/pypi_url_validator/url_validator.py
@@ -0,0 +1,16 @@
+
+import re
+
+def is_url(text):
+    """
+    Checks if the given text is a valid URL.
+    """
+    url_pattern = re.compile(
+        r'^(?:http|ftp)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(url_pattern, text) is not None
+
