4843f599494c6d61fda56c79362ad556a6aca4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:12 +0000

    Add package mixed-precision-for-JAX/0.1.6/mixed_precision_for_jax-0.1.6.tar.gz

---
+++ packages/mixed-precision-for-JAX/mixed_precision_for_jax-0.1.6.tar.gz/mixed_precision_for_jax-0.1.6/mpx/_dtypes.py
@@ -0,0 +1,28 @@
+import jax.numpy as jnp
+
+HALF_PRECISION_DATATYPE = jnp.float16
+FLOAT16_MAX = jnp.ones([], dtype=jnp.float32) * (2 - 2**(-10)) * 2**15
+BFLOAT16_MAX = jnp.array([((2**8 - 1) * 2**(120))], dtype=jnp.float32)[0]
+
+def set_half_precision_datatype(datatype):
+    """
+    Set the half precision datatype for the module.
+    
+    Args:
+        datatype: The datatype to set as half precision (e.g., jnp.float16).
+    """
+    global HALF_PRECISION_DATATYPE
+    if isinstance(datatype, str):
+        if datatype == 'float16':
+            datatype = jnp.float16
+        elif datatype == 'bfloat16':
+            datatype = jnp.bfloat16
+        else:
+            raise ValueError(f"Unsupported datatype: {datatype}. Use 'float16' or 'bfloat16'.")
+    elif datatype in (jnp.float16, jnp.bfloat16):
+        HALF_PRECISION_DATATYPE = datatype
+    else:
+        raise TypeError("Datatype must be a string or in (jnp.float16, jnp.bfloat16).")
+
+def half_precision_datatype():
+    return HALF_PRECISION_DATATYPE
