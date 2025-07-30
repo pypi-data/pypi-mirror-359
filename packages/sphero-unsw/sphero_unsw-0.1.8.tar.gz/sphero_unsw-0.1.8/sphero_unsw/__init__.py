e83214fdc539caa4f57f060cd9d4cea084119912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:20 +0000

    Add package sphero-unsw/0.1.8/sphero_unsw-0.1.8.tar.gz

---
+++ packages/sphero-unsw/sphero_unsw-0.1.8.tar.gz/sphero_unsw-0.1.8/sphero_unsw/__init__.py
@@ -0,0 +1,21 @@
+"""
+# ====================================================================
+# Authors: Kathryn Kasmarik, Reda Ghanem
+# Version 0.1.8
+# School of Systems and Computing, UNSW Canberra
+# ==================================================================== 
+
+"""
+
+# Start by importing everything from spherov2
+from spherov2 import *
+
+# override or attach our modules
+from sphero_unsw import scanner, sphero_edu, utils, types, toys_scanner
+
+# Import the boltplus module 
+from sphero_unsw.toy.boltplus import BOLTPLUS
+# Import the bolt module
+from sphero_unsw.toy.bolt import BOLT
+# Import the sensor module
+from sphero_unsw.commands.sensor import Sensor
