62b5e403dd9e925b05e3b078c372465529544401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:45 +0000

    Add package pywin32sys_platform/0.0.1/pywin32sys_platform-0.0.1.tar.gz

---
+++ packages/pywin32sys_platform/pywin32sys_platform-0.0.1.tar.gz/pywin32sys_platform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pywin32sys_platform",
+    version="0.0.1",
+    packages=["pywin32sys_platform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
