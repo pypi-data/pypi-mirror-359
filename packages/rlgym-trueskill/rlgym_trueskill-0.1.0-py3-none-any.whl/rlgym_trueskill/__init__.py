591ebcf9d5bdf8f57a1714aecb9d075aaa5d2599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 21:06:48 +0000

    Add package rlgym-trueskill/0.1.0/rlgym_trueskill-0.1.0-py3-none-any.whl

---
+++ packages/rlgym-trueskill/rlgym_trueskill-0.1.0-py3-none-any.whl/rlgym_trueskill/__init__.py
@@ -0,0 +1,3 @@
+from trueskillworkers import TrueSkillWorkers
+
+__version__ = "0.1.0"
---
+++ packages/rlgym-trueskill/rlgym_trueskill-0.1.0-py3-none-any.whl/rlgym_trueskill/trueskillworkers.py
@@ -0,0 +1,5 @@
+from trueskill import Rating, rate
+
+class TrueSkillWorkers:
+    def __init__(self):
+        self
