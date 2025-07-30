2f26fdb6fe7faa5162d67c20d97ccfa165e603a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:08:29 +0000

    Add package exitcodelib/1.1.0/exitcodelib-1.1.0.tar.gz

---
+++ packages/exitcodelib/exitcodelib-1.1.0.tar.gz/exitcodelib-1.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='exitcodelib',
+    version='1.1.0',
+    description='Standardized exit code system with messages for Python programs',
+    author='johny eldry',
+    author_email='umyashinderu@gmail.com',
+    py_modules=['exitcodelib'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
