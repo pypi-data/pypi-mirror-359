46f0ba612994e9287abfa315aad07d34823ac55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:05 +0000

    Add package taskvine-report-tool/2025.6.7/taskvine_report_tool-2025.6.7-py3-none-any.whl

---
+++ packages/taskvine-report-tool/taskvine_report_tool-2025.6.7-py3-none-any.whl/taskvine_report/routes/file_sizes.py
@@ -0,0 +1,30 @@
+from taskvine_report.utils import *
+from flask import Blueprint, jsonify, current_app
+
+file_sizes_bp = Blueprint('file_sizes', __name__, url_prefix='/api')
+
+@file_sizes_bp.route('/file-sizes')
+@check_and_reload_data()
+def get_file_sizes():
+    try:
+        df = read_csv_to_fd(current_app.config["RUNTIME_STATE"].csv_file_sizes)
+        # find the size column by looking for a column that starts with 'Size ('
+        size_col = next(col for col in df.columns if col.startswith('Size ('))
+        points, unit = extract_size_points_from_df(df, 'File Index', size_col)
+        x_domain = extract_x_range_from_points(points)
+        y_domain = extract_y_range_from_points(points)
+
+        return jsonify({
+            'points': downsample_points(points),
+            'x_domain': x_domain,
+            'y_domain': y_domain,
+            'x_tick_values': compute_linear_tick_values(x_domain),
+            'y_tick_values': compute_linear_tick_values(y_domain),
+            'x_tick_formatter': d3_int_formatter(),
+            'y_tick_formatter': d3_size_formatter(unit),
+            'file_idx_to_names': dict(zip(df['File Index'], df['File Name'])),
+        })
+
+    except Exception as e:
+        current_app.config["RUNTIME_STATE"].log_error(f"Error in get_file_sizes: {e}")
+        return jsonify({'error': str(e)}), 500
