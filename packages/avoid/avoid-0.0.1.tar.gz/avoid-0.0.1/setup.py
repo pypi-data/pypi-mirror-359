d1f6c16b82433f35328cb5f791ba317e12c5acbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package avoid/0.0.1/avoid-0.0.1.tar.gz

---
+++ packages/avoid/avoid-0.0.1.tar.gz/avoid-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="avoid",
+    version="0.0.1",
+    packages=["avoid"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
