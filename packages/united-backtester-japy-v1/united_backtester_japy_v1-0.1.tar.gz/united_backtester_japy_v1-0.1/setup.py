34fd7380654d84a9cf4ebf78ef46f2f3d59abe30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:35 +0000

    Add package united-backtester-japy-v1/0.1/united_backtester_japy_v1-0.1.tar.gz

---
+++ packages/united-backtester-japy-v1/united_backtester_japy_v1-0.1.tar.gz/united_backtester_japy_v1-0.1/setup.py
@@ -0,0 +1,21 @@
+# hamoni/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="united_backtester_japy_v1",
+    version="0.1",
+    packages=find_packages(where="libraries"),
+    package_dir={"": "libraries"},
+    python_requires=">=3.7",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "matplotlib",
+        "requests",
+        "python-binance",
+        "openpyxl",
+        "pandas_ta==0.3.14b0",
+        "pytz",
+        
+    ],
+)
