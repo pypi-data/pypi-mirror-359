3237cc95456d41e9d6154f5408c2825eca8a04f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:07 +0000

    Add package reflex/0.8.0a7/reflex-0.8.0a7.tar.gz

---
+++ packages/reflex/reflex-0.8.0a7.tar.gz/reflex-0.8.0a7/reflex/.templates/web/vite.config.js
@@ -0,0 +1,38 @@
+import { fileURLToPath, URL } from "url";
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+
+export default defineConfig((config) => ({
+  plugins: [reactRouter()],
+  build: {
+    rollupOptions: {
+      jsx: {},
+    },
+  },
+  server: {
+    port: process.env.PORT,
+    watch: {
+      ignored: [
+        "**/.web/backend/**",
+        "**/.web/reflex.install_frontend_packages.cached",
+      ],
+    },
+  },
+  resolve: {
+    mainFields: ["browser", "module", "jsnext"],
+    alias: [
+      {
+        find: "$",
+        replacement: fileURLToPath(new URL("./", import.meta.url)),
+      },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./public", import.meta.url)),
+      },
+    ].concat(
+      config.command === "build"
+        ? [{ find: "react-dom/server", replacement: "react-dom/server.node" }]
+        : [],
+    ),
+  },
+}));
