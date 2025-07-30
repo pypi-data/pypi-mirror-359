50fb6910f52cf7778a047d68eacc121a1a56e4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package enum34python_version/0.0.1/enum34python_version-0.0.1.tar.gz

---
+++ packages/enum34python_version/enum34python_version-0.0.1.tar.gz/enum34python_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="enum34python_version",
+    version="0.0.1",
+    packages=["enum34python_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
