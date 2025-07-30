ccbddc1201f194c310c5cc51323988083b236091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:04 +0000

    Add package MainShortcuts2/2.5.6/mainshortcuts2-2.5.6-py3-none-any.whl

---
+++ packages/MainShortcuts2/mainshortcuts2-2.5.6-py3-none-any.whl/MainShortcuts2/__init__.py
@@ -0,0 +1,8 @@
+"""Упрощение и сокращение вашего кода благодаря этой библиотеке
+Разработчик: MainPlay TG
+https://t.me/MainPlayCh"""
+__all__ = ["ms", "NoLogger"]
+__scripts__ = []
+from ._module_info import version as __version__
+# 2.0.0
+from .core import ms, NoLogger
