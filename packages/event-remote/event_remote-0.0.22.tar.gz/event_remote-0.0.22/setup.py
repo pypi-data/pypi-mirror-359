89a666d6dc5b1461a291ff8dfda517ce301b08ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:15 +0000

    Add package event-remote/0.0.22/event_remote-0.0.22.tar.gz

---
+++ packages/event-remote/event_remote-0.0.22.tar.gz/event_remote-0.0.22/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+PACKAGE_NAME = "event-remote"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/event-remote/
+    version="0.0.22",
+    author="Circles",
+    author_email="info@circlez.ai",
+    description="PyPI Package for Circles event-remote Python",
+    long_description="PyPI Package for Circles event-remote Python",
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-restapi-python-package",
+    # packages=setuptools.find_packages(),
+    packages=[package_dir],
+    package_dir={package_dir: f"{package_dir}/src"},
+    package_data={package_dir: ["*.py"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "event-external-local>=0.0.21",
+        "logger-local>=0.0.170",
+        "python-sdk-remote>=0.0.93",
+        "url-remote",
+        "user-context-remote>=0.0.116",
+    ],
+)
