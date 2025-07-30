4e1f9a021f7e05239cec0612cd7561eb6eef845e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:56 +0000

    Add package secp384r1/0.0.1/secp384r1-0.0.1.tar.gz

---
+++ packages/secp384r1/secp384r1-0.0.1.tar.gz/secp384r1-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="secp384r1",
+    version="0.0.1",
+    packages=["secp384r1"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
