0aa562d36f8cafe533916a780a1d8b3694e01edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:01 +0000

    Add package networkxpython_version/0.0.1/networkxpython_version-0.0.1.tar.gz

---
+++ packages/networkxpython_version/networkxpython_version-0.0.1.tar.gz/networkxpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="networkxpython_version",
+    version="0.0.1",
+    packages=["networkxpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
