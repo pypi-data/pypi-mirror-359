6fac58c77a781b4ef6b3c34162af354f860b77b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:38 +0000

    Add package air/0.9.0/air-0.9.0-py3-none-any.whl

---
+++ packages/air/air-0.9.0-py3-none-any.whl/air/templates.py
@@ -0,0 +1,38 @@
+from fastapi import Request
+from fastapi.templating import Jinja2Templates
+from typing import Any
+
+
+class Jinja2Renderer:
+    """Template renderer to make Jinja2 easier in Air.
+
+    Args:
+        directory: Template directory
+
+    Example:
+        >>> # Instantiate the render callable
+        >>> render = TemplateRenderer('templates')
+        >>>
+        >>> # Use for returning Jinja2 from views
+        >>> @app.get('/')
+        >>> async def home(request: Request):
+        >>> return render(
+        ...     request,
+        ...     'home.html',
+        ...     context={'id': 5}
+        ... )
+    """
+
+    def __init__(self, directory: str):
+        """Initialize with template directory path"""
+        self.templates = Jinja2Templates(directory=directory)
+
+    def __call__(
+        self, request: Request, name: str, context: dict[Any, Any] | None = None
+    ):
+        """Render template with request and context"""
+        if context is None:
+            context = {}
+        return self.templates.TemplateResponse(
+            request=request, name=name, context=context
+        )
