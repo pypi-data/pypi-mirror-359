34c5b8627670fb5c2bb2b2a692db4584d6cd1952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:39 +0000

    Add package minimamba/1.0.1/minimamba-1.0.1.tar.gz

---
+++ packages/minimamba/minimamba-1.0.1.tar.gz/minimamba-1.0.1/minimamba/_version.py
@@ -0,0 +1,14 @@
+"""Version information for minimamba package."""
+
+__version__ = "1.0.1"
+__version_info__ = (1, 0, 1)
+
+# Release information
+MAJOR = 1
+MINOR = 0
+PATCH = 1
+
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+
+# Release status
+RELEASE_STATUS = "Production/Stable"
