b7c86e196f912d087a7d6bdf4541c03d5c0c2dcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:54 +0000

    Add package thymeleaf-spring6/0.0.1/thymeleaf-spring6-0.0.1.tar.gz

---
+++ packages/thymeleaf-spring6/thymeleaf-spring6-0.0.1.tar.gz/thymeleaf-spring6-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="thymeleaf-spring6",
+    version="0.0.1",
+    packages=["thymeleaf-spring6"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
