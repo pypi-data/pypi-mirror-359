c99d73aaa4804ad036d1d82c8471fb501068d3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:09 +0000

    Add package langvision/0.0.1/langvision-0.0.1.tar.gz

---
+++ packages/langvision/langvision-0.0.1.tar.gz/langvision-0.0.1/src/langvision/utils/config.py
@@ -0,0 +1,15 @@
+default_config = {
+    'img_size': 224,
+    'patch_size': 16,
+    'in_chans': 3,
+    'num_classes': 1000,
+    'embed_dim': 768,
+    'depth': 12,
+    'num_heads': 12,
+    'mlp_ratio': 4.0,
+    'lora': {
+        'r': 4,
+        'alpha': 1.0,
+        'dropout': 0.1,
+    },
+} 
