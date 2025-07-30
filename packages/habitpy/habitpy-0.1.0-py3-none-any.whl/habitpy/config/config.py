5398befae418a40b3e82218b52568c1006e1ce25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:22 +0000

    Add package habitpy/0.1.0/habitpy-0.1.0-py3-none-any.whl

---
+++ packages/habitpy/habitpy-0.1.0-py3-none-any.whl/habitpy/config/config.py
@@ -0,0 +1,13 @@
+"""Global variables"""
+
+# Database
+DATABASE_PATH = "user/user_data.db"
+
+# Config
+CONFIG_PATH = "habitpy/config/config.json"
+
+# user folder
+USER_FOLDER_PATH = "user/"
+
+# metadata
+METADATA_PATH = "metadata.json"
