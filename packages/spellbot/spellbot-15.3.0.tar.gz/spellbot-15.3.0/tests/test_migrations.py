44d19078dbf4ce35352efb6e1d1e7a30464a25d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:05:45 +0000

    Add package spellbot/15.3.0/spellbot-15.3.0.tar.gz

---
+++ packages/spellbot/spellbot-15.3.0.tar.gz/spellbot-15.3.0/tests/test_migrations.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+if TYPE_CHECKING:
+    from spellbot.settings import Settings
+
+
+class TestMigrations:
+    @pytest.mark.nosession
+    def test_alembic(self, settings: Settings) -> None:
+        from spellbot.models import create_all, reverse_all
+
+        create_all(settings.DATABASE_URL)
+        reverse_all(settings.DATABASE_URL)
