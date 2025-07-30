adaba15037de61bf7679ce51bdf4196a7949c318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:13 +0000

    Add package model-compose/0.1.5/model_compose-0.1.5-py3-none-any.whl

---
+++ packages/model-compose/model_compose-0.1.5-py3-none-any.whl/mindor/dsl/schema/controller/impl/common.py
@@ -0,0 +1,11 @@
+from typing import Type, Union, Literal, Optional, Dict, List, Tuple, Set, Annotated, Any
+from pydantic import BaseModel, Field
+from .types import ControllerType
+from .webui import ControllerWebUIConfig
+
+class CommonControllerConfig(BaseModel):
+    type: ControllerType
+    runtime: Literal[ "docker", "native" ] = "native"
+    max_concurrent_count: int = 1
+    threaded: bool = False
+    webui: Optional[ControllerWebUIConfig] = None
