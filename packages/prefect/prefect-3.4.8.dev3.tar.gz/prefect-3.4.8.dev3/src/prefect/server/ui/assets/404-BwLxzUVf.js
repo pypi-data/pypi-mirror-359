c3c0048fdef53323968f89380a6c5aff494b1876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:48 +0000

    Add package prefect/3.4.8.dev3/prefect-3.4.8.dev3.tar.gz

---
+++ packages/prefect/prefect-3.4.8.dev3.tar.gz/prefect-3.4.8.dev3/src/prefect/server/ui/assets/404-BwLxzUVf.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dx as n}from"./index-B-tufU-W.js";import{u as r}from"./usePageTitle-KBR9NuAY.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-BwLxzUVf.js.map
