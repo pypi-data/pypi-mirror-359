d8a7ef57c4668efda65b24196214f288003f7310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:05:53 +0000

    Add package atomicshop/3.2.6/atomicshop-3.2.6-py3-none-any.whl

---
+++ packages/atomicshop/atomicshop-3.2.6-py3-none-any.whl/atomicshop/basics/tracebacks.py
@@ -0,0 +1,16 @@
+import traceback
+
+
+def get_as_string(one_line: bool = False, replace_end: str = str()) -> str:
+    """
+    Returns traceback as string.
+
+    :param one_line: If 'True', traceback will be returned as one line.
+    :param replace_end: If 'one_line' is 'True', this string will be used to replace '\n' in traceback.
+    :return: Traceback as string.
+    """
+
+    if not one_line:
+        return traceback.format_exc()
+    else:
+        return traceback.format_exc().replace('\n', replace_end)
