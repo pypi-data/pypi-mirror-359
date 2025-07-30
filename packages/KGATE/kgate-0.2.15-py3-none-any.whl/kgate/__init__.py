747218634ac6529bf7d22e54c8ac7f0f68932385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:27 +0000

    Add package KGATE/0.2.15/kgate-0.2.15-py3-none-any.whl

---
+++ packages/KGATE/kgate-0.2.15-py3-none-any.whl/kgate/__init__.py
@@ -0,0 +1,5 @@
+from .knowledgegraph import KnowledgeGraph
+from .kgate import Architect
+from .utils import init_embedding, read_training_metrics
+from .grid_search import run_grid_search
+from .evaluators import LinkPredictionEvaluator, TripletClassificationEvaluator
