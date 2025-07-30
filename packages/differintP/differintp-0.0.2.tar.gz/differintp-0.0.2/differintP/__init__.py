09deef2fe87953f73832183ac8a8ca874a98a989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:14 +0000

    Add package differintP/0.0.2/differintp-0.0.2.tar.gz

---
+++ packages/differintP/differintp-0.0.2.tar.gz/differintp-0.0.2/differintP/__init__.py
@@ -0,0 +1,15 @@
+from .core import *
+__all__ = [
+    "GL",
+    "GLcoeffs",
+    "functionCheck",
+    "checkValues",
+    "isInteger",
+    "GLI",
+    "GLpoint",
+    "RLcoeffs",
+    "RLmatrix",
+    "RL",
+    "poch",
+    "RLpoint",
+]
