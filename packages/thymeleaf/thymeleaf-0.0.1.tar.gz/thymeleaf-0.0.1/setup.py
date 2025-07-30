8597e1b0de0aa0ff6c22f7001e9ce9dd35b9bcd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:55 +0000

    Add package thymeleaf/0.0.1/thymeleaf-0.0.1.tar.gz

---
+++ packages/thymeleaf/thymeleaf-0.0.1.tar.gz/thymeleaf-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="thymeleaf",
+    version="0.0.1",
+    packages=["thymeleaf"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
