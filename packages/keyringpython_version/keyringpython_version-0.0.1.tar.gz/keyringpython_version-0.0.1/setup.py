88ca7636312c9d74f294d8fbc72b714696fa7910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:04 +0000

    Add package keyringpython_version/0.0.1/keyringpython_version-0.0.1.tar.gz

---
+++ packages/keyringpython_version/keyringpython_version-0.0.1.tar.gz/keyringpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="keyringpython_version",
+    version="0.0.1",
+    packages=["keyringpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
