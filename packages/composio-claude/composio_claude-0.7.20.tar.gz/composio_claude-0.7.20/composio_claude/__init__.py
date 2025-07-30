63a050beaea993ab8a910d448080c06963db55bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:18:27 +0000

    Add package composio-claude/0.7.20/composio_claude-0.7.20.tar.gz

---
+++ packages/composio-claude/composio_claude-0.7.20.tar.gz/composio_claude-0.7.20/composio_claude/__init__.py
@@ -0,0 +1,18 @@
+from composio import Action, App, Tag, Trigger, WorkspaceType, action
+
+from composio_claude.toolset import ComposioToolSet
+
+
+ComposioToolset = ComposioToolSet
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "Trigger",
+    "WorkspaceType",
+    "action",
+    "ComposioToolset",
+    "ComposioToolSet",
+)
