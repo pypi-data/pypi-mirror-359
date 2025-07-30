1e76eca08949ba45a49ba8bb228f2c4638cc0afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package GitPythonpython_version/0.0.1/GitPythonpython_version-0.0.1.tar.gz

---
+++ packages/GitPythonpython_version/GitPythonpython_version-0.0.1.tar.gz/GitPythonpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="GitPythonpython_version",
+    version="0.0.1",
+    packages=["GitPythonpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
