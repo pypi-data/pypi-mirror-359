972a33060f697fa70d6ccfcd216b674dd931372a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:12:25 +0000

    Add package ntree/0.1.1/ntree-0.1.1.tar.gz

---
+++ packages/ntree/ntree-0.1.1.tar.gz/ntree-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ntree",
+    version="0.1.1",
+    description="A better version of the Linux tree command in Python",
+    author="Vijay Satheesh",
+    packages=find_packages(),
+    install_requires=[
+        "humanize"
+    ],
+    entry_points={
+        "console_scripts": [
+            "ntree=ntree.libntree:main"
+        ]
+    },
+    python_requires=">=3.6",
+)
