b9429e376fe182c9e07cc163bbd7658cddce099a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:57 +0000

    Add package pycritic/0.2.1/pycritic-0.2.1.tar.gz

---
+++ packages/pycritic/pycritic-0.2.1.tar.gz/pycritic-0.2.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pycritic',
+    version='0.2.1',
+    author='Alexander Smirnov',
+    author_email='aleqsonder@icloud.com',
+    description='A package for estimating a set of parameters',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Alexcoratt/pycritic',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.11',
+    install_requires=[
+		"jsonschema",
+		"requests"
+    ]
+)
