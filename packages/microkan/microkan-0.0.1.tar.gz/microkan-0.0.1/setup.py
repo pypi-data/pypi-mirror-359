d4725159c5563f0447d413c135e0e96f7c7f1dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:43 +0000

    Add package microkan/0.0.1/microkan-0.0.1.tar.gz

---
+++ packages/microkan/microkan-0.0.1.tar.gz/microkan-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="microkan",
+    version="0.0.1",
+    author="Alireza Afzal Aghaei",
+    author_email="alirezaafzalaghaei@gmail.com",
+    description="FOR FUTURE USE",
+    license="MIT",
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
