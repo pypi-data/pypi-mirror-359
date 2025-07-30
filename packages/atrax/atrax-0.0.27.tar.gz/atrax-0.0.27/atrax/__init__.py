6d9951f5ee60811d17846735e9943f8498b9a336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:03 +0000

    Add package atrax/0.0.27/atrax-0.0.27.tar.gz

---
+++ packages/atrax/atrax-0.0.27.tar.gz/atrax-0.0.27/atrax/__init__.py
@@ -0,0 +1,6 @@
+from .version import __version__
+from .Dataset.dataset import Dataset
+from .Series.series import Series
+from .utils import to_datetime, date_range, parse_date
+from .Atrax import Atrax 
+
