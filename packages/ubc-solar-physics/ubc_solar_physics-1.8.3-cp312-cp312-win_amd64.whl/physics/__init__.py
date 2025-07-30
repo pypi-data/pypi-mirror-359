68469ebeb4d9273b35aab2ccebf23c625ed580b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:18 +0000

    Add package ubc-solar-physics/1.8.3/ubc_solar_physics-1.8.3-cp312-cp312-win_amd64.whl

---
+++ packages/ubc-solar-physics/ubc_solar_physics-1.8.3-cp312-cp312-win_amd64.whl/physics/__init__.py
@@ -0,0 +1,14 @@
+from .environment import (
+    meteorology,
+    gis
+)
+
+from .models import (
+    arrays,
+    battery,
+    lvs,
+    motor,
+    regen
+)
+
+from ._version import __version__
