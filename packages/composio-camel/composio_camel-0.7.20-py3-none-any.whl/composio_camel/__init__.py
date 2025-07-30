99a83412e2402a9aadacf4de69b6078b5e1cebd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:13 +0000

    Add package composio-camel/0.7.20/composio_camel-0.7.20-py3-none-any.whl

---
+++ packages/composio-camel/composio_camel-0.7.20-py3-none-any.whl/composio_camel/__init__.py
@@ -0,0 +1,14 @@
+from composio import Action, App, Tag, Trigger, WorkspaceType, action
+
+from composio_camel.toolset import ComposioToolSet
+
+
+__all__ = (
+    "Action",
+    "App",
+    "Tag",
+    "ComposioToolSet",
+    "WorkspaceType",
+    "action",
+    "Trigger",
+)
