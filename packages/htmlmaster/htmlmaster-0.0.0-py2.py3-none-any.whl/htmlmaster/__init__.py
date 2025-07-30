fbe3fbe14f6632d486dedd621865fb248e0ca52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:27 +0000

    Add package htmlmaster/0.0.0/htmlmaster-0.0.0-py2.py3-none-any.whl

---
+++ packages/htmlmaster/htmlmaster-0.0.0-py2.py3-none-any.whl/htmlmaster/__init__.py
@@ -0,0 +1,25 @@
+"""
+# htmlmaster
+A template repository for building python packages. Please replace `$package` with the
+package's name in `metadata.yml`.
+
+## See Also
+### Github repository
+* https://github.com/Chitaoji/htmlmaster/
+
+### PyPI project
+* https://pypi.org/project/htmlmaster/
+
+## License
+This project falls under the BSD 3-Clause License.
+
+"""
+
+from typing import List
+
+from . import core
+from .__version__ import __version__
+from .core import *
+
+__all__: List[str] = []
+__all__.extend(core.__all__)
