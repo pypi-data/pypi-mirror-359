3d9e5d8cf92b9a2c2d0348c3be6f87f6b109f034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:49 +0000

    Add package composio/1.0.0rc8/composio-1.0.0rc8.tar.gz

---
+++ packages/composio/composio-1.0.0rc8.tar.gz/composio-1.0.0rc8/composio/core/provider/base.py
@@ -0,0 +1,24 @@
+"""
+BaseProvider module
+
+Defines the barebones provider metaclass that needs to be subclassed for every provider.
+"""
+
+from __future__ import annotations
+
+import typing as t
+
+TTool = t.TypeVar("TTool", covariant=True)
+TToolCollection = t.TypeVar("TToolCollection", covariant=True)
+
+
+class BaseProvider(t.Generic[TTool, TToolCollection]):
+    """
+    BaseProvider class
+
+    All providers should inherit from this class and implement `wrap_tools` so that
+    they can be used with the core Composio class.
+    """
+
+    name: str
+    """Name of the provider"""
