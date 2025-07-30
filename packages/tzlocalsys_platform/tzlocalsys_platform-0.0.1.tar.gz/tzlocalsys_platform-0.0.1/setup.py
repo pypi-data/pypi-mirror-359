ba3b1695b9a9a3c1ef3b57245396cec2cd723b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:45 +0000

    Add package tzlocalsys_platform/0.0.1/tzlocalsys_platform-0.0.1.tar.gz

---
+++ packages/tzlocalsys_platform/tzlocalsys_platform-0.0.1.tar.gz/tzlocalsys_platform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="tzlocalsys_platform",
+    version="0.0.1",
+    packages=["tzlocalsys_platform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
