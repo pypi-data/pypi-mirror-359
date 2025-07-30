0b77f8b26d411361fa8f17e06a72c62d129124f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:23 +0000

    Add package fitsm/0.0.5/fitsm-0.0.5-py3-none-any.whl

---
+++ packages/fitsm/fitsm-0.0.5-py3-none-any.whl/fitsm/create_fm_db.sql
@@ -0,0 +1,41 @@
+CREATE TABLE files (
+    date text, 
+    path text PRIMARY KEY,
+    instrument text, 
+    filter text, 
+    type text, 
+    object text, 
+    width int, 
+    height int, 
+    jd real,
+    ra real,
+    dec real,
+    id int, 
+    exposure real,
+    hash text UNIQUE,
+    FOREIGN KEY(id) REFERENCES observations(id)
+);
+
+CREATE TABLE observations (
+  id INTEGER PRIMARY KEY,
+  date text,
+  instrument text,
+  filter text,
+  type text,
+  object text,
+  width int,
+  height int,
+  exposure real,
+  files int,
+  UNIQUE(date, instrument, filter, object, type, width, height, exposure)
+);
+
+CREATE TABLE products (
+  id int,
+  datetime text,
+  version text,
+  files int,
+  path text,
+  status text,
+  FOREIGN KEY(id) REFERENCES observations(id)
+);
