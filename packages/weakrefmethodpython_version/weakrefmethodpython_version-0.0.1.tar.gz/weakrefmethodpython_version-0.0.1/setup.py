130a91d378d6ca7c73ce1083b2bf06c90f9f4f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:01 +0000

    Add package weakrefmethodpython_version/0.0.1/weakrefmethodpython_version-0.0.1.tar.gz

---
+++ packages/weakrefmethodpython_version/weakrefmethodpython_version-0.0.1.tar.gz/weakrefmethodpython_version-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="weakrefmethodpython_version",
+    version="0.0.1",
+    packages=["weakrefmethodpython_version"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
