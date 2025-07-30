b58f09cc7b2b6a43f83f5f402071bca2e59cd9de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:06 +0000

    Add package fastjet/3.5.1.0/fastjet-3.5.1.0-cp311-cp311-macosx_11_0_x86_64.whl

---
+++ packages/fastjet/fastjet-3.5.1.0-cp311-cp311-macosx_11_0_x86_64.whl/fastjet/include/fastjet/contrib/softkiller_defines.h
@@ -0,0 +1,20 @@
+// Required definitions for exporting static variables in windows builds.
+// This is only needed for static data variables since we use
+// the CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON automation when building with cmake.
+// That automation handles all member functions.
+// So, when making a static variable please add in the beginning of a variable
+// definition, like a keyword. It is very important to use the visibility relevant
+// to the library you are working in, see below for possibilities!
+// e.g.: Nsubjettiness_VISIBILITY static bool verbosity; // inside a SomeClass.h, for instance
+// Similarly for declarations you must prefix the appropriate VISIBILITY
+// e.g. Nsubjettiness_VISIBILITY static bool SomeClass::verbosity = true; // in SomeClass.cc
+#ifdef _WIN32
+    #if defined(softkiller_EXPORTS) || defined(fastjetcontrib_EXPORTS)
+        #define softkiller_VISIBILITY __declspec(dllexport) // Export when building the DLL
+    #else
+        #define softkiller_VISIBILITY __declspec(dllimport) // Import when using the DLL
+    #endif
+#else
+    // For Linux/macOS
+    #define softkiller_VISIBILITY
+#endif
