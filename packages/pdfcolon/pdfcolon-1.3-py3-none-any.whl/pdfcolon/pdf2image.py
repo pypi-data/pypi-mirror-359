3f24de7beff13668bb6030651d15041a75b97d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:52 +0000

    Add package pdfcolon/1.3/pdfcolon-1.3-py3-none-any.whl

---
+++ packages/pdfcolon/pdfcolon-1.3-py3-none-any.whl/pdfcolon/pdf2image.py
@@ -0,0 +1,8 @@
+"""i mean its obvious what it does
+
+are u dumb it turns pdf to and image file duhh."""
+
+
+def convert():
+    """thats the only function for this module dumbass."""
+    print("pdf 2 image")
