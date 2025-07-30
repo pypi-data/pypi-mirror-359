bac89e549eb8b4fe9201f237bf6b56f4f08fe649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:05 +0000

    Add package excelipy/0.1.1/excelipy-0.1.1-py3-none-any.whl

---
+++ packages/excelipy/excelipy-0.1.1-py3-none-any.whl/excelipy/service.py
@@ -0,0 +1,65 @@
+import logging
+from typing import Tuple
+
+import xlsxwriter
+from xlsxwriter.workbook import Workbook, Worksheet
+
+from excelipy.models import Component, Excel, Fill, Style, Table, Text
+from excelipy.writers import (
+    write_fill,
+    write_table,
+    write_text,
+)
+
+log = logging.getLogger("excelipy")
+
+
+def write_component(
+    workbook: Workbook,
+    worksheet: Worksheet,
+    component: Component,
+    default_style: Style,
+    origin: Tuple[int, int] = (0, 0),
+) -> Tuple[int, int]:
+    writing_map = {
+        Table: write_table,
+        Text: write_text,
+        Fill: write_fill,
+    }
+
+    render_func = writing_map.get(type(component))
+
+    return render_func(
+        workbook,
+        worksheet,
+        component,
+        default_style,
+        origin,
+    )
+
+
+def save(excel: Excel):
+    workbook = xlsxwriter.Workbook(excel.path)
+    log.debug("Workbook opened")
+    for sheet in excel.sheets:
+        origin = (
+            sheet.style.pl(),
+            sheet.style.pt(),
+        )
+        worksheet = workbook.add_worksheet(sheet.name)
+        for component in sheet.components:
+            cur_origin = (
+                origin[0] + component.style.pl(),
+                origin[1] + component.style.pt(),
+            )
+            x, y = write_component(
+                workbook,
+                worksheet,
+                component,
+                sheet.style,
+                cur_origin,
+            )
+            origin = origin[0], origin[1] + y
+
+    workbook.close()
+    log.debug("Workbook closed")
