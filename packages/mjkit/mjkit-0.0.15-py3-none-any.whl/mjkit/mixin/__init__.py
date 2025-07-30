bb7b731a7b6e88b222867a993ba9c51da4b8d152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:45 +0000

    Add package mjkit/0.0.15/mjkit-0.0.15-py3-none-any.whl

---
+++ packages/mjkit/mjkit-0.0.15-py3-none-any.whl/mjkit/mixin/__init__.py
@@ -0,0 +1,14 @@
+"""
+자동 생성 파일입니다. 직접 수정하지 마세요.
+생성일자: 2025-07-02
+생성 위치: mixin/__init__.py
+"""
+from .prevent_override_mixin import PreventOverrideMixin
+from .logging_mixin import LoggingMixin
+from .attribute_printer_mixin import AttributePrinterMixin
+
+__all__ = [
+    "PreventOverrideMixin",
+    "LoggingMixin",
+    "AttributePrinterMixin"
+]
