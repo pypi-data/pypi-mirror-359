07d58bfe236c47e1d8847d5212c6c062ff7e992e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:21:04 +0000

    Add package argparser-ng/1.0.1/argparser_ng-1.0.1.tar.gz

---
+++ packages/argparser-ng/argparser_ng-1.0.1.tar.gz/argparser_ng-1.0.1/project.mk
@@ -0,0 +1,23 @@
+
+PROJECT_OWNER := AccidentallyTheCable
+PROJECT_EMAIL := cableninja@cableninja.net
+PROJECT_FIRST_YEAR := 2025
+PROJECT_LICENSE := GPLv3
+PROJECT_NAME := argparser-ng
+PROJECT_DESCRIPTION := NextGen Argument Parser
+PROJECT_VERSION := 1.0.1
+
+## Enable Feature 'Python'
+BUILD_PYTHON := 1
+## Enable Feature 'Shell'
+BUILD_SHELL := 0
+## Enable Feature 'Docker'
+BUILD_DOCKER := 0
+## Enable python `dist` Phase for Projects destined for PYPI
+PYTHON_PYPI_PROJECT := 1
+## Additional Flags for pylint. EX --ignore-paths=mypath
+PYLINT_EXTRA_FLAGS := 
+
+### Any Further Project-specific make targets can go here
+
+#### CHECKSUM c56fe9af019dadbe6154e3cd318a355732e671f5a8743ecdbf106db65e26a06e
