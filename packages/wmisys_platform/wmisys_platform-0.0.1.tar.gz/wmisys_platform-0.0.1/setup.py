410a03fcb1f4421cde052fa9d883509234facf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:20:00 +0000

    Add package wmisys_platform/0.0.1/wmisys_platform-0.0.1.tar.gz

---
+++ packages/wmisys_platform/wmisys_platform-0.0.1.tar.gz/wmisys_platform-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="wmisys_platform",
+    version="0.0.1",
+    packages=["wmisys_platform"],
+    author="",
+    author_email="",
+    description="Minimal package created automatically",
+    license="MIT",
+)
