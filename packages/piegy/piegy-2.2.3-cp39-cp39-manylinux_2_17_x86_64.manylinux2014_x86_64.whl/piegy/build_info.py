122161606c73119f54cfb7c27091ac305e863570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:17 +0000

    Add package piegy/2.2.3/piegy-2.2.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/piegy/piegy-2.2.3-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/piegy/build_info.py
@@ -0,0 +1,11 @@
+"""
+Contains build info, whether it's local built, or a pre-compiled wheel.
+Auto-generated at compile time.
+"""
+
+build_info = {
+    "version": "2.2.3",
+    "build date": "2025-07-01 20:49:22",
+    "python used": "3.9.22",
+    "platform": "linux"
+}
